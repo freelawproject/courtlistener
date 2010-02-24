359c257d4c67ccd74e59174333bccc5f98886116
Michael Lissner <mlissner@michaeljaylissner.com>
2010-02-24 11:18:47 -0800

    more hook testing. ugh.

--- alert/settings.py
+++ alert/settings.py
@@ -29,9 +29,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django_alerts',                      # Or path to database file if using sqlite3.
-        'USER': 'django-alerts',                      # Not used with sqlite3.
-        'PASSWORD': '@1er3alkncKJuas76wec[]',                  # Not used with sqlite3.
+        'NAME': 'YOUR DB NAME HERE',                      # Or path to database file if using sqlite3.
+        'USER': 'YOUR DB USER HERE',                      # Not used with sqlite3.
+        'PASSWORD': 'YOUR PWD HERE',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
