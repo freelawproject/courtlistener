b1e525c2ec532f9e41367fa77991ac98ed28320f
Michael Lissner <mlissner@michaeljaylissner.com>
2010-03-22 15:55:19 -0700

    Hg missed the changes to urls.py. Not sure why. Checking them in now though.

--- alert/urls.py
+++ alert/urls.py
@@ -1,16 +1,16 @@
-# This software and any associated files are copyright 2010 Brian Carver and 
+# This software and any associated files are copyright 2010 Brian Carver and
 # Michael Lissner.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -38,31 +38,31 @@ urlpatterns = patterns('',
     # Example:
     # (r'^alert/', include('alert.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-     
-    # The scraper URL 
+
+    # The scraper URL
     (r'^scrape/(\d{1,2})/$', scrape),
-    
+
     # The parser URL
     (r'^parse/(\d{1,2})/$', parse),
-    
+
     # Sitemap generator
     (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
         {'sitemaps': sitemaps}),
-    
+
     # Court listing pages
-    (r'^opinions/(ca1|ca2|ca3|ca4|ca5|ca6|ca7|ca8|ca9|ca10|ca11|cadc|cafc|all)/$', 
+    (r'^opinions/(ca1|ca2|ca3|ca4|ca5|ca6|ca7|ca8|ca9|ca10|ca11|cadc|cafc|all)/$',
         viewDocumentListByCourt),
-    
+
     # Display a case
-    url(r'^(ca1|ca2|ca3|ca4|ca5|ca6|ca7|ca8|ca9|ca10|ca11|cadc|cafc)/(.*)/$', 
+    url(r'^(ca1|ca2|ca3|ca4|ca5|ca6|ca7|ca8|ca9|ca10|ca11|cadc|cafc)/(.*)/$',
         viewCases, name="viewCases"),
-        
+
     # Contact us pages
     (r'^contact/$', contact),
     (r'^contact/thanks/$', thanks),
@@ -73,10 +73,10 @@ urlpatterns = patterns('',
 if settings.DEVELOPMENT:
     urlpatterns += patterns('',
     (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': '/home/mlissner/Documents/Cal/Final Project/alert/assets/media', 
+        {'document_root': '/home/mlissner/Documents/Cal/Final Project/alert/assets/media',
         'show_indexes': True}),
-    (r'^500/$', 'django.views.generic.simple.direct_to_template', 
+    (r'^500/$', 'django.views.generic.simple.direct_to_template',
         {'template': '500.html'}),
-    (r'^404/$', 'django.views.generic.simple.direct_to_template', 
+    (r'^404/$', 'django.views.generic.simple.direct_to_template',
         {'template': '404.html'}),
 )
