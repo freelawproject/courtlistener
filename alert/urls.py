b5972bdb5bf7ab2149bd022ead57757244c0678d
Michael Lissner <mlissner@michaeljaylissner.com>
2010-04-18 23:02:50 -0700

    more emailstuff

--- alert/urls.py
+++ alert/urls.py
@@ -60,6 +60,7 @@ urlpatterns = patterns('',
     (r'^parse/(\d{1,2})/$', parse),
     (r'^email/(daily|weekly|monthly)/$', emailer),
     (r'^email/emailfoo/$', emailFoo),
+    
 
     # Court listing pages
     (r'^opinions/(' + "|".join(pacer_codes) + '|all)/$', viewDocumentListByCourt),
