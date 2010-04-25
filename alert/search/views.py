71a0894c10019477d460cd6cc5ab68845acacff4
Michael Lissner <mlissner@michaeljaylissner.com>
2010-04-25 16:23:34 -0700

    Merged version of the scraper.

---
+++ alert/search/views.py
@@ -0,0 +1,285 @@
+# This software and any associated files are copyright 2010 Brian Carver and
+# Michael Lissner.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.shortcuts import render_to_response, HttpResponseRedirect
+from django.template import RequestContext
+from alert.search.forms import SearchForm, CreateAlertForm
+from alert.alertSystem.models import Document
+from alert.userHandling.models import Alert, UserProfile
+import re
+
+def oxford_comma(seq):
+    seq = tuple(seq)
+    if not seq:
+        return ''
+    elif len(seq) == 1:
+        return seq[0]
+    elif len(seq) == 2:
+        return '%s and %s' % seq
+    else:
+        return '%s, and %s' % (', '.join(seq[:-1]), seq[-1])
+
+
+def home(request):
+    """Show the homepage"""
+    if "q" in request.GET:
+        # using get because that way users can email queries (a good thing)
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            cd = form.cleaned_data
+            query = cd['q']
+            return HttpResponseRedirect('/search/results/?q=' + query)
+    else:
+        # the form is loading for the first time
+        form = SearchForm()
+    return render_to_response('home_page.html', {'form': form},
+        RequestContext(request))
+
+
+def showResults(request):
+    """Show the results for a query as either an alert or a search"""
+
+    try:
+        query = request.GET['q']
+    except:
+        # if somebody is URL hacking at /search/results/
+        query = ""
+
+    # this handles the alert creation form.
+    if request.method == 'POST':
+        from alert.userHandling.models import Alert
+        # an alert has been created
+        alertForm = CreateAlertForm(request.POST)
+        if alertForm.is_valid():
+            cd = alertForm.cleaned_data
+
+            # save the alert
+            a = CreateAlertForm(cd)
+            alert = a.save() # this method saves it and returns it
+
+            # associate the user with the alert
+            try:
+                # This works...but only if they already have an account.
+                up = request.user.get_profile()
+            except:
+                # if the user doesn't have a profile yet, we make them one, and
+                # associate it with their username.
+                u = request.user
+                up = UserProfile()
+                up.user = u
+                up.save()
+            up.alert.add(alert)
+            messages.add_message(request, messages.SUCCESS,
+                'Your alert was created successfully.')
+
+            # and redirect to the alerts page
+            return HttpResponseRedirect('/profile/alerts/')
+    else:
+        # the form is loading for the first time, load it, then load the rest
+        # of the page!
+        alertForm = CreateAlertForm(initial = {'alertText': query, 'alertFrequency': "dly"})
+
+    # before searching, check that all fieldnames are valid. Create message if not.
+    # this thread has a solution using pyparse which is probably a better approach to investigate:
+    # http://stackoverflow.com/questions/2677713/regex-for-finding-valid-sphinx-fields
+    # for testing: @court @casename foo,bar @(doctext, courthouse, docstatus) @(docstatus, casename) @(casename) @courtname (court | doctext)
+    # this catches simple fields such as @court, @field and puts them in a list
+    attributes = re.findall('(?:@)([^\( ]*)', query)
+    
+    # this catches more complicated ones, like @(court), and @(court, test)
+    regex0 = re.compile('''
+        @               # at sign
+        (?:             # start non-capturing group
+            \w+             # non-whitespace, one or more
+            \b              # a boundary character (i.e. no more \w)
+            |               # OR
+            (               # capturing group
+                \(              # left paren
+                [^@(),]+        # not an @(),
+                (?:                 # another non-caputing group
+                    , *             # a comma, then some spaces
+                    [^@(),]+        # not @(),
+                )*              # some quantity of this non-capturing group
+                \)              # a right paren
+            )               # end of non-capuring group
+        )           # end of non-capturing group
+        ''', re.VERBOSE)
+    
+    # and this puts them into the attributes list.
+    groupedAttributes = re.findall(regex0, query)
+    for item in groupedAttributes:
+        attributes.extend(item.strip("(").strip(")").split(", "))
+
+    # check if the values are valid.
+    validRegex = re.compile(r'^court$|^casename$|^docstatus$|^doctext$|^casenumber$')
+    
+    # if they aren't add them to a new list.
+    badAttrs = []
+    for attribute in attributes:
+        if len(attribute) == 0:
+            # if it's a zero length attribute, we punt
+            continue
+        if validRegex.search(attribute.lower()) == None:
+            # if the attribute from the search isn't in the valid list
+            if attribute not in badAttrs:
+                # and the attribute isn't already in the list
+                badAttrs.append(attribute)
+
+    # pluralization is a pain, but we must do it...
+    if len(badAttrs) == 1:
+        messageText = 'We completed your search, but <strong>' + \
+        oxford_comma(badAttrs) + '</strong> is not a valid attribute.<br>\
+        Valid attributes are @court, @caseName, @caseNumber, @docStatus and @docText.'
+        messages.add_message(request, messages.INFO, messageText)
+    elif len(badAttrs) > 1:
+        messageText = 'We completed your search, but <strong>' + \
+        oxford_comma(badAttrs) + '</strong> are not valid attributes.<br>\
+        Valid attributes are @court, @caseName, @caseNumber, @docStatus and @docText.'
+        messages.add_message(request, messages.INFO, messageText)
+
+    
+    # OLD SEARCH METHOD
+    # results = Document.objects.filter(documentPlainText__icontains=query).order_by("-dateFiled")
+
+    # NEW SEARCH METHOD
+    try:
+        queryset = Document.search.query(query)
+        results = queryset.set_options(mode="SPH_MATCH_EXTENDED2")\
+            .order_by('-dateFiled')
+    except:
+        results = []
+
+    # next, we paginate we will show ten results/page
+    paginator = Paginator(results, 10)
+    
+    # this will fail when the search fails, so try/except is needed.
+    try:
+        numResults = paginator.count
+    except:
+        numResults = 0
+    
+    # Make sure page request is an int. If not, deliver first page.
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    # If page request is out of range, deliver last page of results.
+    try:
+        results = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        results = paginator.page(paginator.num_pages)
+    except:
+        results = []
+
+    return render_to_response('search/results.html', {'results': results, 
+        'numResults': numResults, 'query': query, 'alertForm': alertForm}, 
+        RequestContext(request))
+
+
+@login_required
+def editAlert(request, alertID):
+    user = request.user.get_profile()
+
+    try:
+        alertID = int(alertID)
+    except:
+        return HttpResponseRedirect('/')
+
+    # check if the user can edit this, or if they are url hacking...
+    for alert in user.alert.all():
+        if alertID == alert.alertUUID:
+            # they can edit it
+            canEdit = True
+            # pull it from the DB
+            alert = Alert.objects.get(alertUUID = alertID)
+            break
+        else:
+            canEdit = False
+
+    if canEdit == False:
+        # we just send them home, they can continue playing
+        return HttpResponseRedirect('/')
+
+    elif canEdit:
+        # they can edit the item, therefore, we load the form.
+        if request.method == 'POST':
+            form = CreateAlertForm(request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+
+                # save the changes
+                a = CreateAlertForm(cd, instance=alert)
+                a.save() # this method saves it and returns it
+                messages.add_message(request, messages.SUCCESS,
+                    'Your alert was saved successfully.')
+
+                # redirect to the alerts page
+                return HttpResponseRedirect('/profile/alerts/')
+
+        else:
+            # the form is loading for the first time
+            form = CreateAlertForm(instance = alert)
+
+        return render_to_response('profile/edit_alert.html', {'form': form, 'alertID': alertID}, RequestContext(request))
+
+
+@login_required
+def deleteAlert(request, alertID):
+    user = request.user.get_profile()
+
+    try:
+        alertID = int(alertID)
+    except:
+        return HttpResponseRedirect('/')
+
+    # check if the user can edit this, or if they are url hacking...
+    for alert in user.alert.all():
+        if alertID == alert.alertUUID:
+            # they can edit it
+            canEdit = True
+            # pull it from the DB
+            alert = Alert.objects.get(alertUUID = alertID)
+            break
+        else:
+            canEdit = False
+
+    if canEdit == False:
+        # we send them home
+        return HttpResponseRedirect('/')
+
+    elif canEdit:
+        # Then we delete it, and redirect them.
+        alert.delete()
+        messages.add_message(request, messages.SUCCESS,
+            'Your alert was deleted successfully.')
+        return HttpResponseRedirect('/profile/alerts/')
+
+
+@login_required
+def deleteAlertConfirm(request, alertID):
+    try:
+        alertID = int(alertID)
+    except:
+        return HttpResponseRedirect('/')
+    return render_to_response('profile/delete_confirm.html', {'alertID': alertID}, RequestContext(request))
+
+
+def toolsPage(request):
+    return render_to_response('search/tools.html', {}, RequestContext(request))
---
+++ alert/send_emails.py
@@ -0,0 +1,201 @@
+# This software and any associated files are copyright 2010 Brian Carver and
+# Michael Lissner.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import settings
+from django.core.management import setup_environ
+setup_environ(settings)
+
+
+from userHandling.models import Alert, UserProfile
+from userHandling.models import FREQUENCY
+from alertSystem.models import Document
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext, loader, Context
+from django.core.mail import send_mail, EmailMultiAlternatives
+
+import datetime, time, calendar
+from optparse import OptionParser
+
+def emailer(rate, verbose, simulate):
+    """This will load all the users each day/week/month, and send them emails."""
+    # remap the FREQUENCY variable from the model so the human keys relate back
+    # to the indices
+    rates = {}
+    for r in FREQUENCY:
+        rates[r[1].lower()] = r[0]
+    RATE = rates[rate]
+
+    # sphinx likes time in UnixTime format, which the below accomplishes. Sadly,
+    # there doesn't appear to be an easier way to do this. The output can be
+    # tested in the shell with: date --date="2010-04-16" +%s
+    today = datetime.date.today()
+    unixTimeToday = int(time.mktime(today.timetuple()))
+    unixTimesPastWeek = []
+    unixTimesPastMonth = []
+    i = 0
+    while i < 7:
+        unixTimesPastWeek.append(unixTimeToday - (86400 * i))
+        i += 1
+    i = 0
+    while i < calendar.mdays[datetime.date.today().month]:
+        unixTimesPastMonth.append(unixTimeToday - (86400 * i))
+        i += 1
+
+    EMAIL_SUBJECT = 'New hits for your alert at CourtListener.com'
+    EMAIL_SENDER = 'no-reply@courtlistener.com'
+
+    # query all users with alerts of the desired frequency
+    # use the distinct method to only return one instance of each person.
+    userProfiles = UserProfile.objects.filter(alert__alertFrequency = RATE).distinct()
+    
+    if verbose:
+        print "today: " + str(today)
+        print "unixTimeToday: " + str(unixTimeToday)
+        print "unixTimesPastWeek: " + str(unixTimesPastWeek)
+        print "unixTimesPastMonth: " + str(unixTimesPastMonth)
+        print "userProfiles (with " + rate + " alerts): " + str(userProfiles)
+
+    # for each user with a daily, weekly or monthly alert...
+    for userProfile in userProfiles:
+        #...get their alerts...
+        alerts = userProfile.alert.filter(alertFrequency = RATE)
+        if verbose: 
+            print rate + " alerts for user " + userProfile.user.email + ": " + str(alerts)
+
+        hits = []
+        # ...and iterate over their alerts.
+        for alert in alerts:
+            if RATE == 'dly':
+                # query the alert
+                queryset = Document.search.query(alert.alertText)
+                results = queryset.set_options(mode="SPH_MATCH_EXTENDED2")\
+                    .filter(datefiled=unixTimeToday)
+            elif RATE == 'wly' and today.weekday() == 6:
+                # if it's a weekly alert and today is Sunday
+                queryset = Document.search.query(alert.alertText)
+                results = queryset.set_options(mode="SPH_MATCH_EXTENDED2")\
+                    .filter(datefiled=unixTimesPastWeek)
+            elif RATE == 'mly' and today.day == 19:
+                # if it's a monthly alert and today is the first of the month
+                queryset = Document.search.query(alert.alertText)
+                results = queryset.set_options(mode="SPH_MATCH_EXTENDED2")\
+                    .filter(datefiled=unixTimesPastMonth)
+            elif RATE == "off":
+                pass
+
+            if verbose: print "There were " + str(results.count()) + \
+                " hits for the alert \"" + alert.alertText + \
+                "\". Here are the first 0-20: " + str(results)
+            
+            # hits is a multidimensional array. Ugh.
+            # it consists of alerts, paired with a list of documents, of the form:
+            # [[alert1, [hit1, hit2, hit3, hit4]], [alert2, [hit1, hit2]]]
+            if results.count() > 0:
+                # very important! if you don't do the slicing here, you'll only
+                # get the first 20 hits. also very frustrating!
+                alertWithResults = [alert, results[0:results.count()]]
+                hits.append(alertWithResults)
+                # set the hit date to today
+                alert.lastHitDate = datetime.date.today()
+                alert.save()
+                if verbose:
+                    print "alertWithResults: " + str(alertWithResults)
+                    print "hits: " + str(hits)
+                
+            elif alert.sendNegativeAlert:
+                # if they want an alert even when no hits.
+                alertWithResults = [alert, "None"]
+                hits.append(alertWithResults)
+                
+                if verbose:
+                    print "Sending results for negative alert, " + alert.alertText + "."
+                    print "alertWithResults: " + str(alertWithResults)
+                    print "hits: " + str(hits)
+        
+        if len(hits) > 0:
+            # either the hits var has the value "None", or it has hits.
+            if userProfile.plaintextPreferred:
+                # send a plaintext email.
+                txtTemplate = loader.get_template('emails/email.txt')
+                c = Context({
+                    'hits': hits,
+                })
+                email_text = txtTemplate.render(c)
+                
+                if verbose and simulate: print "email_text: " + str(email_text)
+                
+                if not simulate:
+                    send_mail(
+                        EMAIL_SUBJECT,
+                        email_text,
+                        EMAIL_SENDER,
+                        [userProfile.user.email],
+                        fail_silently=False)
+            else:
+                # send a multi-part email
+                txtTemplate = loader.get_template('emails/email.txt')
+                htmlTemplate = loader.get_template('emails/email.html')
+                c = Context({
+                    'hits': hits,
+                })
+                email_text = txtTemplate.render(c)
+                html_text = htmlTemplate.render(c)
+
+                if verbose and simulate: 
+                    print "email_text: " + str(email_text)
+                    print "html_text: " + str(html_text) 
+                
+                if not simulate:
+                    msg = EmailMultiAlternatives(EMAIL_SUBJECT, email_text, EMAIL_SENDER, [userProfile.user.email])
+                    msg.attach_alternative(html_text, "text/html")
+                    msg.send(fail_silently=False)
+        elif verbose:
+            print "Not sending mail for this alert."
+            
+
+    return "Done"
+
+
+def main():
+    usage = "usage: %prog -r RATE | --rate=RATE"
+    parser = OptionParser(usage)
+    parser.add_option('-r', '--rate', dest='rate', metavar='RATE',
+        help="The rate to send emails")
+    parser.add_option('-v', '--verbose', action="store_true", dest='verbose', 
+        default=False, help="Display variable values during execution")
+    parser.add_option('-s', '--simulate', action="store_true", dest='simulate', 
+        default=False, help="Simulate the emails that would be sent, using the console backend")
+    (options, args) = parser.parse_args()
+    if not options.rate:
+        parser.error("You must specify a rate")
+    
+    rate = options.rate
+    verbose = options.verbose
+    simulate = options.simulate
+    
+    if simulate:
+        print "******************"
+        print "* NO EMAILS SENT *"
+        print "******************"
+    
+    return emailer(rate, verbose, simulate)
+
+
+if __name__ == '__main__':
+    main()
