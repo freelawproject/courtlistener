799129a23c9efde4a57336b51ba32d6763b61f37
Michael Lissner <mlissner@michaeljaylissner.com>
2010-04-28 00:10:49 -0700

    monkeying with the model.

--- alert/alertSystem/models.py
+++ alert/alertSystem/models.py
@@ -237,7 +237,7 @@ class Document(models.Model):
         if self.citation:
             return self.citation.caseNameShort
         else:
-            return self.documentSHA1
+            return self.documentUUID
 
     @models.permalink
     def get_absolute_url(self):
