4f2d7ea2a3b5ddba6a130c46129f26f123391942
Michael Lissner <mlissner@michaeljaylissner.com>
2010-07-15 16:31:44 -0700

    Some reorganization of the scraper to pull out the text cleaning stuff, and some fixes to fragment IDs in urls.py

--- alert/urls.py
+++ alert/urls.py
@@ -109,8 +109,8 @@ urlpatterns = patterns('',
 
 # redirects
 urlpatterns += patterns('django.views.generic.simple',
-    ('^privacy/$', 'redirect_to', {'url': '/terms/#privacy/'}),
-    ('^removal/$', 'redirect_to', {'url': '/terms/#removal/'}),
+    ('^privacy/$', 'redirect_to', {'url': '/terms/#privacy'}),
+    ('^removal/$', 'redirect_to', {'url': '/terms/#removal'}),
     ('^opinions/$', 'redirect_to', {'url': '/opinions/all/'}),
     ('^report/$', 'redirect_to', {'url': 'http://www.ischool.berkeley.edu/files/student_projects/Final_Report_Michael_Lissner_2010-05-07_2.pdf'}),
 )
