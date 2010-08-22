73b2ebe5bd9cd73f1e747238f955b59702998d33
Michael Lissner <mlissner@michaeljaylissner.com>
2010-08-22 11:35:52 -0700

    Added a callable for the User admin screen so that filters by confirmed email address can be made.

--- alert/userHandling/admin.py
+++ alert/userHandling/admin.py
@@ -1,16 +1,16 @@
 # This software and any associated files are copyright 2010 Brian Carver and
 # Michael Lissner.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -19,12 +19,18 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from alert.userHandling.models import *
 
+
+def getEmailConfirmed(obj):
+    return obj.get_profile().emailConfirmed
+
+
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
 
 
 class UserAdmin(admin.ModelAdmin):
     inlines = [UserProfileInline, ]
+    list_display = ('username', getEmailConfirmed)
 
 
 admin.site.register(Alert)
