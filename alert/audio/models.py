6aa9a77a8be0b5401c17658918dca9c62c130c37
Michael Lissner <mlissner@michaeljaylissner.com>
2010-10-26 00:11:29 -0700

    Many changes to various pieces. Lots of broken code. Do not deploy.

--- alert/audio/models.py
+++ alert/audio/models.py
@@ -1,16 +1,16 @@
 # This software and any associated files are copyright 2010 Brian Carver and
 # Michael Lissner.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -25,7 +25,7 @@
 #  ways how any modified versions differ from the original version.
 from django.db import models
 from django.utils.text import get_valid_filename
-from alertSystem.models import Document
+# from alertSystem.models import Document
 
 
 # Flash is a jerk about only accepting these three rates.
