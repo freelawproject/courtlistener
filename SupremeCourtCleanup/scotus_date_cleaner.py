c7516b18dfe54584411f33595768bb16d9efb865
Michael Lissner <mlissner@michaeljaylissner.com>
2011-01-22 23:54:12 -0800

    Cleaned up and created new version of scotus_date_cleaner.py

--- SupremeCourtCleanup/scotus_date_cleaner.py
+++ SupremeCourtCleanup/scotus_date_cleaner.py
@@ -1,16 +1,16 @@
 # This software and any associated files are copyright 2010 Brian Carver and
 # Michael Lissner.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -24,17 +24,29 @@
 #  within this covered work and you are required to mark in reasonable
 #  ways how any modified versions differ from the original version.
 '''
-Process is roughly as follows:
-For each row in the CSV:
-    extract the case number from the row
-    look up the case num in the DB
-    for each hit in the DB:
-        compute the difference ratio
-        if any difference ratio > threshold:
-            update the date for that case.
-            write the row to updated_cases.csv
-        else:
-            write the row to punted_cases.csv
+Process is as follows:
+    For each row in the CSV:
+        extract the case number from the row (caseNum = line.split("|")[0]
+        Search for the case number using a query such as @casenumber 107 676 @court scotus
+        for each hit from this search:
+            compute the difference ratio between the case name in the DB and that in the CSV
+            if the max(difference_ratio) > THRESHOLD:
+                update the date for that case.
+                write the row to updated_cases.csv
+            else:
+                write the row to punted_cases.csv
+
+Rationale for new method:
+    This method uses search to find the case, which should be more efficient
+    and accurate than using the DB. After using search, it uses the difference
+    ratio to ensure that a correct ammendment is being made.
+
+Spec/features:
+    - verbose mode which prints to stdout
+    - logs should be generated of the punted and completed files, including enough
+      detail to revert a change.
+    - simulate mode allows the user to generate the messages and logs without
+      editing the DB
 '''
 
 import sys
@@ -44,18 +56,156 @@ import settings
 from django.core.management import setup_environ
 setup_environ(settings)
 
-import difflib, re
 from alertSystem.models import Document, Citation
+import datetime
+import difflib
+from optparse import OptionParser
+import re
 
 
-THRESHOLD = 0.75
+def gen_diff_ratio(case_name_left, case_name_right):
+    '''
+    Genrates a difference between two strings, in this case case_names.
+    Returns a value between 0 and 1. 0 means the strings are totally diffferent.
+    1 means they are identical.
+    '''
+    # Remove common strings from all case names /before/ comparison.
+    # Doing so lowers the opportunity for false positives.
+    usa = re.compile(r'United States')
+    case_name_left  = usa.sub('', case_name_left)
+    case_name_right = usa.sub('', case_name_right)
 
-f            = open("date_of_decisions.csv", 'r')
-updated_file = open('updated_file.csv', 'w')
-punt_file    = open('punted_cases.csv', 'w')
+    vs = re.compile(r'v.')
+    case_name_left  = vs.sub('', case_name_left)
+    case_name_right = vs.sub('', case_name_right)
 
-for line in f:
-    caseNum = line.split("|")[0]
+    # compute the difference value
+    diff = difflib.SequenceMatcher(None, case_name_left, case_name_right).ratio()
+
+    return diff
+
+
+def cleaner(simulate=False, verbose=False):
+    THRESHOLD = 0.75
+
+    f            = open("date_of_decisions.csv", 'r')
+    updated_file = open('updated_file.csv', 'w')
+    punt_file    = open('punted_cases.csv', 'w')
+
+    line_num = 1
+    for line in f:
+        # extract the case number from the line in the CSV
+        csv_case_name      = line.split("|")[1]
+        csv_volume_num     = line.split("|")[2]
+        csv_page_num       = line.split("|")[3]
+        csv_date_published = line.split("|")[4]
+
+        query = "@casenumber " + csv_volume_num_csv + " " + csv_page_num + " @court scotus"
+
+        # search for the case number using Sphinx
+        queryset = Document.search.query(query)
+        results = queryset.set_options(mode="SPH_MATCH_EXTENDED2")
+
+        if results.count() == 0:
+            # No hits for the doc. Log and punt it.
+            if verbose: print "Punted line " + line_num + ": " + line
+            punt_file.write("Punted line " + line_num + ": " + line)
+
+        elif results.count() == 1:
+            # One hit returned make sure it's above THRESHOLD. If so, fix it.
+            db_case_name = str(results[0])
+            diff = gen_diff_ratio(db_case_name, csv_case_name)
+            if diff > THRESHOLD:
+                # Update the date in the DB
+                if not simulate:
+                    splitDate = csv_date_published.split('-')
+                    results[0].dateFiled = datetime.date(int(splitDate[0]),
+                        int(splitDate[1]), int(splitDate[2]))
+                    results[0].save()
+
+                # Log as appropriate
+                if verbose: print "Used line %d to update doc %d. Threshold: %d; \
+                    Line contents: %s" % (line_num, results[0].documentUUID, diff, line)
+                updated_file.write("Used line %d to update doc %d. Threshold: %d; \
+                    Line contents: %s" % (line_num, results[0].documentUUID, diff, line)
+
+            else:
+                # Below the threshold. Punt!
+                print "Line number %d below threshold. Threshold: %d. \
+                    Line contents: %s" % (line_num, diff, line)
+                punt_file.write("Line number %d below threshold. Threshold: %d. \
+                    Line contents: %s" % (line_num, diff, line))
+
+        else results.count() > 1:
+            # More than one hit. Find the best one using diff_lib
+            diff_ratios = []
+            for result in results:
+                # Calculate its diff_ratio, and add it to an array
+                db_case_name = str(result)
+                diff = gen_diff_ratio(db_case_name, csv_case_name)
+                diff_ratios.append(diff)
+
+            # Find the max ratio, and grab the corresponding result
+            max_ratio = max(diff_ratios)
+            i = diff_ratios.index(max_ratio)
+            if max_ratio >= THRESHOLD:
+                # Update the date in the DB
+                if not simulate:
+                    splitDate = csv_date_published.split('-')
+                    results[i].dateFiled = datetime.date(int(splitDate[0]),
+                        int(splitDate[1]), int(splitDate[2]))
+                    results[i].save()
+
+                # Log as appropriate
+                if verbose: print "Used line %d to update doc %d. Threshold: %d; \
+                    Line contents: %s" % (line_num, results[0].documentUUID, diff, line)
+                updated_file.write("Used line %d to update doc %d. Threshold: %d; \
+                    Line contents: %s" % (line_num, results[0].documentUUID, diff, line)
+
+            else:
+                # Below the threshold. Punt!
+                print "Line number %d below threshold. Threshold: %d. \
+                    Line contents: %s" % (line_num, diff, line)
+                punt_file.write("Line number %d below threshold. Threshold: %d. \
+                    Line contents: %s" % (line_num, diff, line))
+
+        else:
+            # Not close enough. Punt.
+            print "Ratio below threshold: " + str(max_ratio)
+            punt_file.write("Punted line: " + line)
+
+        # increment the line number counter
+        line_num++
+
+
+
+def main():
+    usage = "usage: %prog [--verbose] [---simulate]"
+    parser = OptionParser(usage)
+    parser.add_option('-v', '--verbose', action="store_true", dest='verbose',
+        default=False, help="Display log during execution")
+    parser.add_option('-s', '--simulate', action="store_true",
+        dest='simulate', default=False, help="Simulate the corrections without\
+        actually making them.")
+    (options, args) = parser.parse_args()
+
+    verbose = options.verbose
+    simulate = options.simulate
+
+    if simulate:
+        print "*******************************************"
+        print "* SIMULATE MODE - NO CHANGES WILL BE MADE *"
+        print "*******************************************"
+
+    return cleaner(simulate, verbose)
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
+
+
+'''
     docs = Document.objects.filter(citation__caseNumber = caseNum)
 
     diff_ratios = []
@@ -64,7 +214,7 @@ for line in f:
         caseNameCSV = line.split("|")[1]
 
         # Remove 'United States' from all case names /before/ comparison.
-        # Not doing so raises the opportunity for false positives.
+        # Not doing so lowers the opportunity for false positives.
         usa = re.compile(r'United States')
         caseNameDB  = usa.sub('', caseNameDB)
         caseNameCSV = usa.sub('', caseNameCSV)
@@ -90,11 +240,10 @@ for line in f:
         else:
             # Not close enough. Punt.
             print "Ratio below threshold: " + str(max_ratio)
-            punt_file.write("Punted: " + line)
+            punt_file.write("Punted line: " + line)
 
     else:
         # No diff ratios generated.
         print "Punted line: " + line
-        punt_file.write("Punted: " + line)
-
-exit(0)
+        punt_file.write("Punted line: " + line)
+'''
