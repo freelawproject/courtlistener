eeea5b351619c6ff519e9b585c6797cb5e9ddc85
mlissner <mlissner@michaeljaylissner.com>
2011-05-31 21:10:15 -0700

    Adding the data dumper.

---
+++ alert/lib/db_tools.py
@@ -0,0 +1,42 @@
+# This software and any associated files are copyright 2010 Brian Carver and
+# Michael Lissner.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gc
+from alert.settings import *
+
+def queryset_iterator(queryset, chunksize=1000):
+    '''
+    from: http://djangosnippets.org/snippets/1949/
+    Iterate over a Django Queryset ordered by the primary key
+
+    This method loads a maximum of chunksize (default: 1000) rows in its
+    memory at the same time while django normally would load all rows in its
+    memory. Using the iterator() method only causes it to not preload all the
+    classes.
+
+    Note that the implementation of the iterator does not support ordered query sets.
+    '''
+    if DEVELOPMENT:
+        chunksize = 5
+
+    documentUUID = 0
+    last_pk = queryset.order_by('-documentUUID')[0].documentUUID
+    queryset = queryset.order_by('documentUUID')
+    while documentUUID < last_pk:
+        for row in queryset.filter(documentUUID__gt=documentUUID)[:chunksize]:
+            documentUUID = row.documentUUID
+            yield row
+        gc.collect()
