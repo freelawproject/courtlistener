11cc64f01c5075994951569dcad81932c611f956
mlissner <mlissner@michaeljaylissner.com>
2011-07-04 01:21:12 -0700

    Mondo merge complete between 39 revisions.

--- Piwik/misc/cron/logrotate.sql
+++ Piwik/misc/cron/logrotate.sql
@@ -1,2 +1,9 @@
-use courtlistenerpiwik;
-DELETE piwik_log_visit, piwik_log_link_visit_action FROM piwik_log_visit INNER JOIN piwik_log_link_visit_action WHERE piwik_log_visit.idvisit = piwik_log_link_visit_action.idvisit AND visit_server_date <= CURRENT_DATE() - 84;
+use piwik;
+DELETE 
+    piwik_log_visit, piwik_log_link_visit_action
+FROM 
+    piwik_log_visit INNER JOIN piwik_log_link_visit_action
+WHERE 
+    piwik_log_visit.idvisit = piwik_log_link_visit_action.idvisit
+    AND visit_first_action_time <= DATE_SUB(CURDATE(), INTERVAL 84 DAY);
+OPTIMIZE TABLE piwik_log_visit, piwik_log_link_visit_action;
