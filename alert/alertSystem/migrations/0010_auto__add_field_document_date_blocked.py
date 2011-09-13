6c26928088bf67e7df4c91bfb0698ace8210fcac
mlissner <mlissner@michaeljaylissner.com>
2011-09-12 22:30:09 -0700

    Adding a necessary migration. Oops.

---
+++ alert/alertSystem/migrations/0010_auto__add_field_document_date_blocked.py
@@ -0,0 +1,65 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Adding field 'Document.date_blocked'
+        db.add_column('Document', 'date_blocked', self.gf('django.db.models.fields.DateField')(null = True, blank = True), keep_default = False)
+
+
+    def backwards(self, orm):
+
+        # Deleting field 'Document.date_blocked'
+        db.delete_column('Document', 'date_blocked')
+
+
+    models = {
+        'alertSystem.citation': {
+            'Meta': {'object_name': 'Citation', 'db_table': "'Citation'"},
+            'caseNameFull': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'caseNameShort': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '100', 'blank': 'True'}),
+            'citationUUID': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'docketNumber': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'lexisCite': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'null': 'True', 'db_index': 'True'}),
+            'westCite': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'})
+        },
+        'alertSystem.court': {
+            'Meta': {'ordering': "['courtUUID']", 'object_name': 'Court', 'db_table': "'Court'"},
+            'URL': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'courtUUID': ('django.db.models.fields.CharField', [], {'max_length': '100', 'primary_key': 'True'}),
+            'endDate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'shortName': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'startDate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'alertSystem.document': {
+            'Meta': {'ordering': "['-time_retrieved']", 'object_name': 'Document', 'db_table': "'Document'"},
+            'blocked': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'db_index': 'True', 'blank': 'True'}),
+            'citation': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['alertSystem.Citation']", 'null': 'True', 'blank': 'True'}),
+            'court': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['alertSystem.Court']"}),
+            'dateFiled': ('django.db.models.fields.DateField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
+            'date_blocked': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'documentHTML': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'documentPlainText': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'documentSHA1': ('django.db.models.fields.CharField', [], {'max_length': '40', 'db_index': 'True'}),
+            'documentType': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'documentUUID': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'download_URL': ('django.db.models.fields.URLField', [], {'max_length': '200', 'db_index': 'True'}),
+            'local_path': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'blank': 'True'}),
+            'source': ('django.db.models.fields.CharField', [], {'max_length': '3', 'blank': 'True'}),
+            'time_retrieved': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        },
+        'alertSystem.urltohash': {
+            'Meta': {'object_name': 'urlToHash', 'db_table': "'urlToHash'"},
+            'SHA1': ('django.db.models.fields.CharField', [], {'max_length': '40', 'blank': 'True'}),
+            'hashUUID': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '300', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['alertSystem']
