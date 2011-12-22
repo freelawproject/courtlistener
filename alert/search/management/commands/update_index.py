b05003a46a742fcb458c704534191c79c825e7dc
mlissner <mlissner@michaeljaylissner.com>
2011-12-21 17:29:00 -0800

    More work on the front end, updates to the Solr schema, addition of the index updating script, and code for realtime search.

---
+++ alert/search/management/commands/update_index.py
@@ -0,0 +1,157 @@
+
+
+import sys
+
+from alert.lib import sunburnt
+from alert.lib.db_tools import queryset_iterator
+from alert.search.models import Document
+from alert.search.search_indexes import InvalidDocumentError
+from alert.search.search_indexes import SearchDocument
+# Celery requires imports like this. Disregard syntax error.
+from search.tasks import delete_docs
+from search.tasks import add_or_update_docs
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from optparse import make_option
+
+class Command(BaseCommand):
+    '''
+    Builds the index from scratch. First deletes all values. Use with caution.
+    '''
+    option_list = BaseCommand.option_list + (
+        make_option('--update',
+            action='store_true',
+            dest='update_mode',
+            default=False,
+            help='Run the command in update mode. Use this to add or update documents.'),
+        make_option('--delete',
+            action='store_true',
+            dest='delete_mode',
+            default=False,
+            help='Run the command in delete mode. Use this to remove documents from the index.'),
+        make_option('--everything',
+            action='store_true',
+            dest='everything',
+            default=False,
+            help='Take action on everything in the database'),
+        make_option('--document',
+            action='store_true',
+            dest='document',
+            default=False,
+            help='Take action on a list of documents using a single Celery task'),
+        make_option('--debug',
+            action='store_true',
+            dest='debug',
+            default=False,
+            help='Run the command, but take no action. Be verbose.'),
+        )
+    args = '(--update | --delete) (--everything | --document <doc_id> <doc_id>) [--debug]'
+    help = 'Adds, updates or removes documents in the index.'
+
+    def delete(self, *documents):
+        '''
+        Given a document, creates a Celery task to delete it.
+        '''
+        self.stdout.write("Deleting document(s): %s\n" % list(documents))
+        # Use Celery to delete the document asynrhronously
+        delete_docs.delay(documents)
+
+    def delete_all(self):
+        '''
+        Deletes all documents from the database.
+        '''
+        self.stdout.write("\n")
+        yes_or_no = raw_input("WARNING: Are you **sure** you want to delete all documents? [y/N] ")
+        self.stdout.write('\n')
+        if not yes_or_no.lower().startswith('y'):
+            self.stdout.write("No action taken.\n")
+            sys.exit(0)
+
+        if self.verbosity >= 1:
+            self.stdout.write('Removing all documents from your index because you said so.\n')
+
+        if self.verbosity >= 1:
+            self.stdout.write('Marking all documents as deleted...\n')
+        self.si.delete_all()
+        if self.verbosity >= 1:
+            self.stdout.write('Committing the deletion...\n')
+        self.si.commit()
+        self.stdout.write('\nDone. Your index has been emptied. Hope this is what you intended.\n')
+        sys.exit(0)
+
+    def add_or_update(self, *documents):
+        '''
+        Given a document, adds it to the index, or updates it if it's already
+        in the index.
+        '''
+        self.stdout.write("Adding or updating document(s): %s\n" % list(documents))
+        # Use Celery to add or update the document asynrhronously
+        add_or_update_docs.delay(documents)
+
+    def add_or_update_all(self):
+        '''
+        Iterates over the entire corpus, adding it to the index. Can be run on 
+        an empty index or an existing one. If run on an existing index,
+        existing documents will be updated.
+        '''
+        self.stdout.write("Adding or updating all documents.\n")
+        everything = queryset_iterator(Document.objects.all())
+        for doc in everything:
+            # Make a search doc, and add it to the index
+            if self.verbosity >= 2:
+                self.stdout.write('Indexing document %s' % doc.pk)
+            try:
+                search_doc = SearchDocument(doc)
+                self.si.add(search_doc)
+            except InvalidDocumentError:
+                if self.verbosity >= 1:
+                    self.stderr.write('InvalidDocumentError: Unable to index document %s\n' % doc.pk)
+                pass
+        self.si.commit()
+
+
+    def handle(self, *args, **options):
+        self.verbosity = int(options.get('verbosity', 1))
+        self.si = sunburnt.SolrInterface(settings.SOLR_URL, mode='w')
+
+        if options.get('update_mode'):
+            if self.verbosity >= 1:
+                self.stdout.write('Running in update mode...\n')
+            if options.get('everything'):
+                self.add_or_update_all()
+            elif options.get('document'):
+                for doc in args:
+                    try:
+                        int(doc)
+                    except ValueError:
+                        self.stderr.write('Error: Document "%s" could not be converted to an ID.\n' % doc)
+                        sys.exit(1)
+                self.add_or_update(*args)
+            else:
+                self.stderr.write('Error: You must specify whether you wish to update everything or a single document.\n')
+                sys.exit(1)
+
+        elif options.get('delete_mode'):
+            if self.verbosity >= 1:
+                self.stdout.write('Running in deletion mode...\n')
+            if options.get('everything'):
+                self.delete_all()
+            elif options.get('document'):
+                for doc in args:
+                    try:
+                        int(doc)
+                    except ValueError:
+                        self.stderr.write('Error: Document "%s" could not be converted to an ID.\n' % doc)
+                        sys.exit(1)
+                self.delete(*args)
+            else:
+                self.stderr.write('Error: You must specify whether you wish to delete everything or a single document.\n')
+                sys.exit(1)
+
+        else:
+            self.stderr.write('Error: You must specify whether you wish to update or delete documents.\n')
+            sys.exit(1)
+
+
+
--- alert/search/search_indexes.py
+++ alert/search/search_indexes.py
@@ -1,22 +1,58 @@
-from haystack.indexes import CharField
-from haystack.indexes import DateField
-from haystack.indexes import RealTimeSearchIndex
-from haystack import site
-from alert.search.models import Document
+# This software and any associated files are copyright 2010 Brian Carver and
+# Michael Lissner.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-class DocumentIndex(RealTimeSearchIndex):
-    text = CharField(document=True, use_template=True)
-    dateFiled = DateField(model_attr='dateFiled', null=True)
-    court = CharField(model_attr='court__shortName', faceted=True)
-    caseName = CharField(model_attr='citation__caseNameFull', boost=1.25)
-    docketNumber = CharField(model_attr='citation__docketNumber', null=True)
-    westCite = CharField(model_attr='citation__westCite', null=True)
-    lexisCite = CharField(model_attr='citation__lexisCite', null=True)
-    status = CharField(model_attr='documentType', faceted=True, boost=1.25)
-    caseNumber = CharField(use_template=True, null=True, boost=1.25)
+from datetime import datetime
+from datetime import time
+from django.template import Context
+from django.template import loader
 
-    def index_queryset(self):
-        """Used when the entire index for model is updated."""
+class InvalidDocumentError(Exception):
+    "The document could not be formed"
+    pass
 
-        # TODO: Does this work on LARGE sets of documents? 
-        return Document.objects.all()
+class SearchDocument(object):
+    def __init__(self, doc):
+        # Used to nuke null and control characters.
+        null_map = dict.fromkeys(range(0, 10) + range(11, 13) + range(14, 32))
+
+        # Standard fields
+        self.id = doc.pk
+        if doc.dateFiled is not None:
+            self.dateFiled = datetime.combine(doc.dateFiled, time())
+        self.court = doc.court.short_name
+        self.court_id = doc.court.courtUUID
+        try:
+            self.caseName = doc.citation.caseNameFull
+        except AttributeError:
+            raise InvalidDocumentError
+        self.docketNumber = doc.citation.docketNumber
+        self.westCite = doc.citation.westCite
+        self.lexisCite = doc.citation.lexisCite
+        self.status = doc.documentType
+
+        # Load the caseName field using a template to make it a concatenation
+        case_name_template = loader.get_template('search/indexes/caseNumber.txt')
+        c = Context({ 'object': doc })
+        self.caseNumber = case_name_template.render(c)
+
+        # Load the document text using a template for cleanup and concatenation
+        text_template = loader.get_template('search/indexes/text.txt')
+        c = Context({ 'object': doc })
+        self.text = text_template.render(c).translate(null_map)
+
+        # Faceting fields
+        self.status_exact = doc.documentType
+        self.court_exact = doc.court.courtUUID
