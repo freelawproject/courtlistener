69449f4e515307f932fa6d6a58b41f9a56dbeb52
mlissner <mlissner@michaeljaylissner.com>
2012-02-03 10:38:29 -0800

    Fixes some validation stuff for search engines.

--- alert/pinger/views.py
+++ alert/pinger/views.py
@@ -22,19 +22,19 @@ from django.template import RequestContext
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
-def validateForYahoo(request):
-    return HttpResponse('36e23db9b9505bbb')
-
-def validateForBing(request):
+def validate_for_bing(request):
     return HttpResponse('<?xml version="1.0"?><users><user>3251009A11EF3EB9D6A7B40EAD9264AD</user></users>')
 
-def validateForGoogle(request):
+def validate_for_bing2(request):
+    return HttpResponse('<?xml version="1.0"?><users><user>8BA95D8EAA744379D80D9F70847EA156</user></users>')
+
+def validate_for_google(request):
     return HttpResponse('google-site-verification: googleef3d845637ccb353.html')
 
-def validateForGoogle2(request):
+def validate_for_google2(request):
     return HttpResponse('google-site-verification: google646349975c2495b6.html')
 
-def validateForGoogle3(request):
+def validate_for_google3(request):
     return HttpResponse('google-site-verification: google646349975c2495b6.html')
 
 def robots(request):
