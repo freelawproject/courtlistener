51fc5e1646d43082d2538b4b37ae340eeedcf096
rowyn <rowynm@gmail.com>
2012-04-12 14:43:25 -0700

    Added a note on how to update the script to use a virtual environment.

--- alert/settings/celeryd.sh
+++ alert/settings/celeryd.sh
@@ -1,8 +1,13 @@
 # Name of nodes to start, here we have a single node
 CELERYD_NODES="w1"
 
+ENV_PYTHON="python"
+# If you are using a virtual environment, comment out the line above and uncomment 
+# the folling line, substituting in the path to your virtual environment.
+#ENV_PYTHON="/path/to/my/virtualenv/bin/python"
+
 # How to call "manage.py celeryd_multi"
-CELERYD_MULTI="python /var/www/court-listener/alert/manage.py celeryd_multi"
+CELERYD_MULTI="$ENV_PYTHON /var/www/court-listener/alert/manage.py celeryd_multi"
 
 # Name of the celery config module.
 CELERY_CONFIG_MODULE="celeryconfig"
@@ -10,7 +15,7 @@ CELERY_CONFIG_MODULE="celeryconfig"
 # %n will be replaced with the nodename.
 CELERYD_LOG_FILE="/var/log/celery/%n.log"
 CELERYD_LOG_LEVEL="INFO"
-CELERYD_PID_FILE="/var/run/celery/celery@%n.pid"
+CELERYD_PID_FILE="$/var/run/celery/celery@%n.pid"
 
 # Workers should run as an unprivileged user.
 CELERYD_USER="celery"
