7fac32c2bf997fbe6fe614197c3cb119450f5296
mlr <mlissner@michaeljaylissner.com>
2012-04-21 23:07:43 -0700

    Tweaks to the citator.

---
+++ alert/lib/filesize.py
@@ -0,0 +1,49 @@
+# This software and any associated files are copyright 2010 Brian Carver and
+# Michael Lissner.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+alternative = [
+    (1024 ** 5, 'PB'),
+    (1024 ** 4, 'TB'),
+    (1024 ** 3, 'GB'),
+    (1024 ** 2, 'MB'),
+    (1024 ** 1, 'KB'),
+    (1024 ** 0, (' byte', ' bytes')),
+    ]
+
+def size(bytes, system=alternative):
+    """Human-readable file size. 
+    
+    """
+    for factor, suffix in system:
+        if bytes >= factor:
+            break
+    amount = float(bytes) / factor
+    if isinstance(suffix, tuple):
+        singular, multiple = suffix
+        if amount == 1:
+            suffix = singular
+        else:
+            suffix = multiple
+
+    if suffix == 'PB':
+        return '%.3f%s' % (amount, suffix)
+    elif suffix == 'TB':
+        return '%.2f%s' % (amount, suffix)
+    elif suffix == 'GB':
+        return '%.1f%s' % (amount, suffix)
+    else:
+        return '%d%s' % (amount, suffix)
