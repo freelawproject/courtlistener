539f913da6f8a296b46c1222df0ed82c8aa5de32
mlissner <mlissner@michaeljaylissner.com>
2012-04-30 23:32:37 -0700

    Purged m2m singla.

--- alert/search/__init__.py
+++
@@ -1,5 +0,0 @@
-from django.db.models.signals import m2m_changed
-from alert.citations.tasks import update_citation_counts
-from alert.search.models import Document
-
-m2m_changed.connect(update_citation_counts, sender=Document.cases_cited.through)
