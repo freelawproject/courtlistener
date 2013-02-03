0f1f8147d11fb9d6789c12ad0d535ed1d36a90f9
mlissner <mlissner@michaeljaylissner.com>
2013-02-03 12:09:38 -0800

    1. Updates the .hgignore file finally. 2. Removes some cruft. 3. Fixes an error in the retry logic from the last commit. 4. Adds some forgotten module __init__.py files.

--- Piwik/misc/cron/logrotate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mysql -u'piwik' -p'ascdalskdcju86782o37d67oaldskhI&LO^TDCLSDYT^^$%RFH:LY' < /home/mlissner/FinalProject/Piwik/misc/cron/logrotate.sql 
-
-exit 0
--- Piwik/misc/cron/logrotate.sql
+++
@@ -1,9 +0,0 @@
-use piwik;
-DELETE 
-    piwik_log_visit, piwik_log_link_visit_action
-FROM 
-    piwik_log_visit INNER JOIN piwik_log_link_visit_action
-WHERE 
-    piwik_log_visit.idvisit = piwik_log_link_visit_action.idvisit
-    AND visit_first_action_time <= DATE_SUB(CURDATE(), INTERVAL 84 DAY);
-OPTIMIZE TABLE piwik_log_visit, piwik_log_link_visit_action;
