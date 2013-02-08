d0fe80b7569804aa93df33f2d390e12b4a0af748
mlissner <mlissner@michaeljaylissner.com>
2013-02-08 15:44:33 -0800

    Fixes a really annoying dependency between crt.li and the database.

--- alert/tinyurl/views.py
+++ alert/tinyurl/views.py
@@ -1,30 +1,4 @@
 # -*- coding: utf-8 -*-
-# This software and any associated files are copyright 2010 Brian Carver and
-# Michael Lissner.
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#  Under Sections 7(a) and 7(b) of version 3 of the GNU Affero General Public
-#  License, that license is supplemented by the following terms:
-#
-#  a) You are required to preserve this legal notice and all author
-#  attributions in this program and its accompanying documentation.
-#
-#  b) You are prohibited from misrepresenting the origin of any material
-#  within this covered work and you are required to mark in reasonable
-#  ways how any modified versions differ from the original version.
-# Create your views here.
 
 from alert.search.models import Document
 from alert.tinyurl.encode_decode import ascii_to_num
@@ -54,6 +28,6 @@ def redirect_short_url(request, encoded_string):
     slug = doc.citation.slug
     court = str(doc.court.courtUUID)
     current_site = Site.objects.get_current()
-    URL = "http://" + current_site.domain + "/" + court + "/" + \
+    URL = "http://www.courtlistener.com/" + court + "/" + \
         encoded_string + "/" + slug + "/"
     return HttpResponsePermanentRedirect(URL)
