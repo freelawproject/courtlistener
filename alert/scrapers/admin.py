99552439a8ce38b58e914beb6064faf88c8f3435
mlissner <mlissner@michaeljaylissner.com>
2013-02-22 08:50:52 -0800

    Tweaks to admin.

--- alert/scrapers/admin.py
+++ alert/scrapers/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from alert.scrapers.models import urlToHash, ErrorLog
 
+
+class ErrorLogAdmin(admin.ModelAdmin):
+    readonly_fields = ('log_time', 'log_level', 'court', 'message',)
+
 admin.site.register(urlToHash)
-admin.site.register(ErrorLog)
+admin.site.register(ErrorLog, ErrorLogAdmin)
