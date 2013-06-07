06d741d65eb8551d9fbfa6771d092fb3be79bcf0
mlissner <mlissner@michaeljaylissner.com>
2013-06-07 09:52:00 -0700

    Adds a list_filter to the Scraper Errorlog admin page.

--- alert/scrapers/admin.py
+++ alert/scrapers/admin.py
@@ -5,6 +5,7 @@ from alert.scrapers.models import urlToHash, ErrorLog
 class ErrorLogAdmin(admin.ModelAdmin):
     readonly_fields = ('log_time', 'log_level', 'court', 'message',)
     list_display = ('log_level', 'log_time', 'court')
+    list_filter = ('court',)
 
 
 admin.site.register(urlToHash)
