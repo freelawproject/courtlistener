6932ba03d6ac5b79c5aad1394293047e3862ab19
mlissner <mlissner@michaeljaylissner.com>
2013-06-13 10:56:20 -0700

    Minor cleanup to purge some old code and move things to better locations.

--- alert/audio/models.py
+++ alert/audio/models.py
@@ -1,28 +1,3 @@
-# This software and any associated files are copyright 2010 Brian Carver and
-# Michael Lissner.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#  Under Sections 7(a) and 7(b) of version 3 of the GNU Affero General Public
-#  License, that license is supplemented by the following terms:
-#
-#  a) You are required to preserve this legal notice and all author
-#  attributions in this program and its accompanying documentation.
-#
-#  b) You are prohibited from misrepresenting the origin of any material
-#  within this covered work and you are required to mark in reasonable
-#  ways how any modified versions differ from the original version.
 from django.db import models
 from django.utils.text import get_valid_filename
 # from search.models import Document
@@ -35,19 +10,6 @@ MP3_SAMPLE_RATES = (
     ('medium', '22050'),
     ('high',   '44100'),
 )
-
-
-def make_upload_path(instance, filename):
-    """Return a string like audio/2010/08/13/foo_v._var.mp3, with the date set
-    as the date_filed for the case."""
-
-    # get the doc that has this audio file as a foreign key.
-    doc = Document.object.get(audio = instance.id)
-
-    # this code NOT cross platform. Use os.path.join or similar to fix.
-    return 'audio/' + doc.date_filed.strftime("%Y/%m/%d/") + \
-        get_valid_filename(filename)
-
 
 class Audio(models.Model):
     SHA1 = models.CharField("SHA1 hash of the audio file",
