6d008a1f51267fbc9046b6dd2b775ecc8a18c643
mlissner <mlissner@michaeljaylissner.com>
2013-06-13 14:16:35 -0700

    Updates a bunch of fields to better names. Adds a migration for current server.

--- alert/search/migrations/0004_manual__rename_fields.py
+++ alert/search/migrations/0004_manual__rename_fields.py
@@ -9,20 +9,42 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         db.rename_column('Citation', 'westCite', 'west_cite')
+        db.rename_column('Citation', 'docketNumber', 'docket_number')
+        db.rename_column('Citation', 'lexisCite', 'lexis_cite')
+        db.rename_column('Document', 'documentPlainText', 'plain_text')
+        db.rename_column('Document', 'documentHTML', 'html')
+        db.rename_column('Document', 'dateFiled', 'date_filed')
+        db.rename_column('Document', 'documentSHA1', 'sha1')
+        db.rename_column('Document', 'documentType', 'precedential_status')
+        # Adding field 'Citation.west_state_cite'
+        db.add_column('Citation', 'west_state_cite',
+                      self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True),
+                      keep_default=False)
 
     def backwards(self, orm):
         db.rename_column('Citation', 'west_cite', 'westCite')
+        db.rename_column('Citation', 'docket_number', 'docketNumber')
+        db.rename_column('Citation', 'lexis_cite', 'lexisCite')
+        db.rename_column('Document', 'plain_text', 'documentPlainText')
+        db.rename_column('Document', 'html', 'documentHTML')
+        db.rename_column('Document', 'date_filed', 'dateFiled')
+        db.rename_column('Document', 'sha1', 'documentSHA1')
+        db.rename_column('Document', 'precedential_status', 'documentType')
+        # Deleting field 'Citation.west_state_cite'
+        db.delete_column('Citation', 'west_state_cite')
+
 
     models = {
         'search.citation': {
             'Meta': {'object_name': 'Citation', 'db_table': "'Citation'"},
             'case_name': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'citationUUID': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'docketNumber': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
-            'lexisCite': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'docket_number': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'lexis_cite': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
             'neutral_cite': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
             'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'null': 'True'}),
-            'west_cite': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'})
+            'west_cite': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'west_state_cite': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'})
         },
         'search.court': {
             'Meta': {'ordering': "['position']", 'object_name': 'Court', 'db_table': "'Court'"},
@@ -44,17 +66,17 @@ class Migration(SchemaMigration):
             'citation': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['search.Citation']", 'null': 'True', 'blank': 'True'}),
             'citation_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
             'court': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['search.Court']"}),
-            'dateFiled': ('django.db.models.fields.DateField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
             'date_blocked': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
-            'documentHTML': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'documentPlainText': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'documentSHA1': ('django.db.models.fields.CharField', [], {'max_length': '40', 'db_index': 'True'}),
-            'documentType': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'date_filed': ('django.db.models.fields.DateField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
             'documentUUID': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'download_URL': ('django.db.models.fields.URLField', [], {'max_length': '200', 'db_index': 'True'}),
             'extracted_by_ocr': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'html': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'html_with_citations': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'local_path': ('django.db.models.fields.files.FileField', [], {'db_index': 'True', 'max_length': '100', 'blank': 'True'}),
+            'plain_text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'precedential_status': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'sha1': ('django.db.models.fields.CharField', [], {'max_length': '40', 'db_index': 'True'}),
             'source': ('django.db.models.fields.CharField', [], {'max_length': '3', 'blank': 'True'}),
             'time_retrieved': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'db_index': 'True', 'blank': 'True'})
         }
