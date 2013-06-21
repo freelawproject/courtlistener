4b4c45ba937b33a34179aa5a991bd496e0373796
mlissner <mlissner@michaeljaylissner.com>
2013-06-21 15:47:21 -0700

    Refactors the scraper commands. Converts the backscraper into a command.

--- alert/scrapers/management/commands/cl_scrape_and_extract.py
+++ alert/scrapers/management/commands/cl_scrape_and_extract.py
@@ -29,6 +29,174 @@ from optparse import make_option
 die_now = False
 
 
+def signal_handler(signal, frame):
+    # Trigger this with CTRL+4
+    logger.info('**************')
+    logger.info('Signal caught. Finishing the current court, then exiting...')
+    logger.info('**************')
+    global die_now
+    die_now = True
+
+
+def test_for_meta_redirections(r):
+    mime = magic.from_buffer(r.content, mime=True)
+    extension = mimetypes.guess_extension(mime)
+    if extension == '.html':
+        html_tree = html.fromstring(r.text)
+        try:
+            attr = html_tree.xpath("//meta[translate(@http-equiv, 'REFSH', 'refsh') = 'refresh']/@content")[0]
+            wait, text = attr.split(";")
+            if text.lower().startswith("url="):
+                url = text[4:]
+                return True, url
+        except IndexError:
+            return False, None
+    else:
+        return False, None
+
+
+def follow_redirections(r, s):
+    """
+    Recursive function that follows meta refresh redirections if they exist.
+    """
+    redirected, url = test_for_meta_redirections(r)
+    if redirected:
+        logger.info('Following a meta redirection to: %s' % url)
+        r = follow_redirections(s.get(url), s)
+    return r
+
+
+def get_extension(content):
+    """A handful of workarounds for gettings extensions we can trust."""
+    file_str = magic.from_buffer(content)
+    if file_str.startswith('Composite Document File V2 Document'):
+        # Workaround for issue with libmagic1==5.09-2 in Ubuntu 12.04. Fixed in libmagic 5.11-2.
+        mime = 'application/msword'
+    elif file_str == '(Corel/WP)':
+        mime = 'application/vnd.wordperfect'
+    elif file_str == 'C source, ASCII text':
+        mime = 'text/plain'
+    else:
+        # No workaround necessary
+        mime = magic.from_buffer(content, mime=True)
+    extension = mimetypes.guess_extension(mime)
+    if extension == '.obj':
+        # It's actually a wpd
+        extension = '.wpd'
+    if extension == '.wsdl':
+        # It's probably an HTML file, like those from Resource.org
+        extension = '.html'
+    if extension == '.ksh':
+        extension = '.txt'
+    return extension
+
+
+def scrape_court(site, full_crawl=False):
+    download_error = False
+    # Get the court object early for logging
+    # opinions.united_states.federal.ca9_u --> ca9
+    court_str = site.court_id.split('.')[-1].split('_')[0]
+    court = Court.objects.get(courtUUID=court_str)
+
+    dup_checker = DupChecker(court, full_crawl=full_crawl)
+    abort = dup_checker.abort_by_url_hash(site.url, site.hash)
+    if not abort:
+        for i in range(0, len(site.case_names)):
+            try:
+                url = site.download_urls[i]
+                if not url:
+                    # Occurs when a DeferredList fetcher fails.
+                    continue
+                s = requests.session()
+                r = s.get(url)
+
+                # test for empty files (thank you CA1)
+                if len(r.content) == 0:
+                    msg = 'EmptyFileError: %s\n%s' % (url,
+                                                      traceback.format_exc())
+                    logger.warn(msg)
+                    ErrorLog(log_level='WARNING', court=court, message=msg).save()
+                    continue
+
+                # test for and follow meta redirects
+                r = follow_redirections(r, s)
+            except:
+                msg = 'DownloadingError: %s\n%s' % (url,
+                                                    traceback.format_exc())
+                logger.warn(msg)
+                ErrorLog(log_level='WARNING', court=court, message=msg).save()
+                continue
+
+            current_date = site.case_dates[i]
+            try:
+                next_date = site.case_dates[i + 1]
+            except IndexError:
+                next_date = None
+
+            # Make a hash of the data
+            sha1_hash = hashlib.sha1(r.content).hexdigest()
+            onwards = dup_checker.should_we_continue(sha1_hash, current_date, next_date)
+
+            if onwards:
+                logger.info('Adding new document found at: %s' % site.download_urls[i])
+                dup_checker.reset()
+
+                # Make a citation
+                cite = Citation(case_name=site.case_names[i])
+                if site.docket_numbers:
+                    cite.docket_number = site.docket_numbers[i]
+                if site.neutral_citations:
+                    cite.neutral_cite = site.neutral_citations[i]
+                if site.west_citations:
+                    cite.west_cite = site.west_citations[i]
+                if site.west_state_citations:
+                    cite.west_state_cite = site.west_state_citations[i]
+
+                # Make the document object
+                doc = Document(source='C',
+                               sha1=sha1_hash,
+                               date_filed=site.case_dates[i],
+                               court=court,
+                               download_URL=site.download_urls[i],
+                               precedential_status=site.precedential_statuses[i])
+
+                # Make and associate the file object
+                try:
+                    cf = ContentFile(r.content)
+                    extension = get_extension(r.content)
+                    # See issue #215 for why this must be lower-cased.
+                    file_name = trunc(site.case_names[i].lower(), 75) + extension
+                    doc.local_path.save(file_name, cf, save=False)
+                except:
+                    msg = 'Unable to save binary to disk. Deleted document: % s.\n % s' % \
+                          (cite.case_name, traceback.format_exc())
+                    logger.critical(msg)
+                    ErrorLog(log_level='CRITICAL', court=court, message=msg).save()
+                    download_error = True
+                    continue
+
+                # Save everything, but don't update Solr index yet
+                cite.save(index=False)
+                doc.citation = cite
+                doc.save(index=False)
+
+                # Extract the contents asynchronously.
+                extract_doc_content(doc.pk, callback=subtask(extract_by_ocr))
+
+                logger.info("Successfully added: %s" % site.case_names[i])
+
+            else:
+                # No need to continue this Site.
+                dup_checker.update_site_hash(sha1_hash)
+                break
+
+        # Update the hash if everything finishes properly.
+        logger.info("%s: Successfully crawled." % site.court_id)
+        if not download_error and not full_crawl:
+            # Only update the hash if no errors occurred.
+            dup_checker.update_site_hash(sha1_hash)
+
+
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('-d',
@@ -59,174 +227,11 @@ class Command(BaseCommand):
     args = "-c COURTID [-d] [-r RATE]"
     help = 'Runs the Juriscraper toolkit against one or many courts.'
 
-    def signal_handler(self, signal, frame):
-        # Trigger this with CTRL+4
-        logger.info('**************')
-        logger.info('Signal caught. Finishing the current court, then exiting...')
-        logger.info('**************')
-        global die_now
-        die_now = True
-
-    def _test_for_meta_redirections(self, r):
-        mime = magic.from_buffer(r.content, mime=True)
-        extension = mimetypes.guess_extension(mime)
-        if extension == '.html':
-            html_tree = html.fromstring(r.text)
-            try:
-                attr = html_tree.xpath("//meta[translate(@http-equiv, 'REFSH', 'refsh') = 'refresh']/@content")[0]
-                wait, text = attr.split(";")
-                if text.lower().startswith("url="):
-                    url = text[4:]
-                    return True, url
-            except IndexError:
-                return False, None
-        else:
-            return False, None
-
-    def follow_redirections(self, r, s):
-        """
-        Recursive function that follows meta refresh redirections if they exist.
-        """
-        redirected, url = self._test_for_meta_redirections(r)
-        if redirected:
-            logger.info('Following a meta redirection to: %s' % url)
-            r = self.follow_redirections(s.get(url), s)
-        return r
-
-    def get_extension(self, content):
-        """A handful of workarounds for gettings extensions we can trust."""
-        file_str = magic.from_buffer(content)
-        if file_str.startswith('Composite Document File V2 Document'):
-            # Workaround for issue with libmagic1==5.09-2 in Ubuntu 12.04. Fixed in libmagic 5.11-2.
-            mime = 'application/msword'
-        elif file_str == '(Corel/WP)':
-            mime = 'application/vnd.wordperfect'
-        elif file_str == 'C source, ASCII text':
-            mime = 'text/plain'
-        else:
-            # No workaround necessary
-            mime = magic.from_buffer(content, mime=True)
-        extension = mimetypes.guess_extension(mime)
-        if extension == '.obj':
-            # It's actually a wpd
-            extension = '.wpd'
-        if extension == '.wsdl':
-            # It's probably an HTML file, like those from Resource.org
-            extension = '.html'
-        if extension == '.ksh':
-            extension = '.txt'
-        return extension
-
-    def scrape_court(self, site, full_crawl=False):
-        download_error = False
-        # Get the court object early for logging
-        # opinions.united_states.federal.ca9_u --> ca9
-        court_str = site.court_id.split('.')[-1].split('_')[0]
-        court = Court.objects.get(courtUUID=court_str)
-
-        dup_checker = DupChecker(court, full_crawl=full_crawl)
-        abort = dup_checker.abort_by_url_hash(site.url, site.hash)
-        if not abort:
-            for i in range(0, len(site.case_names)):
-                try:
-                    url = site.download_urls[i]
-                    if not url:
-                        # Occurs when a DeferredList fetcher fails.
-                        continue
-                    s = requests.session()
-                    r = s.get(url)
-
-                    # test for empty files (thank you CA1)
-                    if len(r.content) == 0:
-                        msg = 'EmptyFileError: %s\n%s' % (url,
-                                                          traceback.format_exc())
-                        logger.warn(msg)
-                        ErrorLog(log_level='WARNING', court=court, message=msg).save()
-                        continue
-
-                    # test for and follow meta redirects
-                    r = self.follow_redirections(r, s)
-                except:
-                    msg = 'DownloadingError: %s\n%s' % (url,
-                                                        traceback.format_exc())
-                    logger.warn(msg)
-                    ErrorLog(log_level='WARNING', court=court, message=msg).save()
-                    continue
-
-                current_date = site.case_dates[i]
-                try:
-                    next_date = site.case_dates[i + 1]
-                except IndexError:
-                    next_date = None
-
-                # Make a hash of the data
-                sha1_hash = hashlib.sha1(r.content).hexdigest()
-                onwards = dup_checker.should_we_continue(sha1_hash, current_date, next_date)
-
-                if onwards:
-                    logger.info('Adding new document found at: %s' % site.download_urls[i])
-                    dup_checker.reset()
-
-                    # Make a citation
-                    cite = Citation(case_name=site.case_names[i])
-                    if site.docket_numbers:
-                        cite.docket_number = site.docket_numbers[i]
-                    if site.neutral_citations:
-                        cite.neutral_cite = site.neutral_citations[i]
-                    if site.west_citations:
-                        cite.west_cite = site.west_citations[i]
-                    if site.west_state_citations:
-                        cite.west_state_cite = site.west_state_citations[i]
-
-                    # Make the document object
-                    doc = Document(source='C',
-                                   sha1=sha1_hash,
-                                   date_filed=site.case_dates[i],
-                                   court=court,
-                                   download_URL=site.download_urls[i],
-                                   precedential_status=site.precedential_statuses[i])
-
-                    # Make and associate the file object
-                    try:
-                        cf = ContentFile(r.content)
-                        extension = self.get_extension(r.content)
-                        # See issue #215 for why this must be lower-cased.
-                        file_name = trunc(site.case_names[i].lower(), 75) + extension
-                        doc.local_path.save(file_name, cf, save=False)
-                    except:
-                        msg = 'Unable to save binary to disk. Deleted document: % s.\n % s' % \
-                              (cite.case_name, traceback.format_exc())
-                        logger.critical(msg)
-                        ErrorLog(log_level='CRITICAL', court=court, message=msg).save()
-                        download_error = True
-                        continue
-
-                    # Save everything, but don't update Solr index yet
-                    cite.save(index=False)
-                    doc.citation = cite
-                    doc.save(index=False)
-
-                    # Extract the contents asynchronously.
-                    extract_doc_content(doc.pk, callback=subtask(extract_by_ocr))
-
-                    logger.info("Successfully added: %s" % site.case_names[i])
-
-                else:
-                    # No need to continue this Site.
-                    dup_checker.update_site_hash(sha1_hash)
-                    break
-
-            # Update the hash if everything finishes properly.
-            logger.info("%s: Successfully crawled." % site.court_id)
-            if not download_error and not full_crawl:
-                # Only update the hash if no errors occurred.
-                dup_checker.update_site_hash(sha1_hash)
-
     def handle(self, *args, **options):
         global die_now
 
         # this line is used for handling SIGTERM (CTRL+4), so things can die safely
-        signal.signal(signal.SIGTERM, self.signal_handler)
+        signal.signal(signal.SIGTERM, signal_handler)
 
         self.verbosity = int(options.get('verbosity', 1))
         daemon_mode = options.get('daemonmode', False)
@@ -238,10 +243,10 @@ class Command(BaseCommand):
         except (ValueError, AttributeError, TypeError):
             rate = 30
 
-        if not options.get('court_id'):
+        court_id = options.get('court_id')
+        if not court_id:
             raise CommandError('You must specify a court as a package or module.')
         else:
-            court_id = options.get('court_id')
             module_strings = build_module_list(court_id)
             if not len(module_strings):
                 raise CommandError('Unable to import module or package. Aborting.')
@@ -262,30 +267,28 @@ class Command(BaseCommand):
                                  globals(),
                                  locals(),
                                  [module])
-
+                # noinspection PyBroadException
                 try:
                     site = mod.Site().parse()
-                    self.scrape_court(site, full_crawl)
-                except:
+                    scrape_court(site, full_crawl)
+                except Exception, e:
                     msg = ('********!! CRAWLER DOWN !!***********\n'
                            '*****scrape_court method failed!*****\n'
-                           '********!! ACTION NEEDED !!**********\n%s') % \
-                          traceback.format_exc()
+                           '********!! ACTION NEEDED !!**********\n%s') % traceback.format_exc()
                     logger.critical(msg)
 
                     # opinions.united_states.federal.ca9_u --> ca9
                     court_str = mod.Site.__module__.split('.')[-1].split('_')[0]
                     court = Court.objects.get(courtUUID=court_str)
                     ErrorLog(log_level='CRITICAL', court=court, message=msg).save()
-                    i += 1
-                    continue
-
-                time.sleep(wait)
-                last_court_in_list = (i == (num_courts - 1))
-                if last_court_in_list and daemon_mode:
-                    i = 0
-                else:
-                    i += 1
+                finally:
+                    time.sleep(wait)
+                    last_court_in_list = (i == (num_courts - 1))
+                    if last_court_in_list and daemon_mode:
+                        # Start over...
+                        i = 0
+                    else:
+                        i += 1
 
         logger.info("The scraper has stopped.")
         sys.exit(0)
