059b5ab597a5ee32eb005593ae1a1d3213340f3c
Krist Jin <krist.jin.apply@gmail.com>
2013-09-24 06:53:29 -0700

    1. judge whether pagerank changes before update database 2. improve the progress status 3. add command options --verbosity

--- alert/search/management/commands/cl_calculate_pagerank.py
+++ alert/search/management/commands/cl_calculate_pagerank.py
@@ -10,47 +10,74 @@ class Command(BaseCommand):
     args = '<args>'
     help = 'Calculate pagerank value for every case'
 
-    def handle(self, *args, **options):
-        do_pagerank()
+    def do_pagerank(self):
+        DAMPING_FACTOR = 0.85
+        MAX_ITERATIONS = 100
+        MIN_DELTA = 0.00001
 
+        print('Initializing...')
+        graph_size = Document.objects.all().count()
+        min_value = (1.0 - DAMPING_FACTOR)
+        doc_dict = dict()
+        case_list = queryset_generator(Document.objects.only("documentUUID", "cases_cited", "citation", "pagerank"))
+        if self.verbosity >= 1:
+            self.stdout.write('graph_size is {0:d}.\n'.format(graph_size))
+            log_file = open('pagerank.log', 'w')
 
+        case_count = 0
+        for case in case_list:
+            sys.stdout.write("\rSaving data from database locally...{:.0%}".format(case_count * 1.0 / graph_size))
+            sys.stdout.flush()
+            attr_dict = dict()
+            id = case.documentUUID
+            attr_dict['cases_cited_count'] = case.cases_cited.all().count()
+            attr_dict['citing_cases_id'] = case.citation.citing_cases.values_list("documentUUID")
+            attr_dict['pagerank'] = case.pagerank
+            attr_dict['original_pagerank'] = case.pagerank
+            doc_dict[id] = attr_dict
+            case_count += 1
 
-def do_pagerank():
-    DAMPING_FACTOR = 0.85
-    MAX_ITERATIONS = 100
-    MIN_DELTA = 0.00001
-
-    #graph_size = Document.objects.all().count()
-    min_value = (1.0 - DAMPING_FACTOR)
-    doc_dict = dict()
-    case_list = queryset_generator(Document.objects.only("documentUUID", "cases_cited", "citation", "pagerank"))
-    print("Saving data from database locally...")
-    for case in case_list:
-        attr_dict = dict()
-        id = case.documentUUID
-        attr_dict['cases_cited_count'] = case.cases_cited.all().count()
-        attr_dict['citing_cases_id'] = case.citation.citing_cases.values_list("documentUUID")
-        attr_dict['pagerank'] = 1
-        doc_dict[id] = attr_dict
-
-    for i in range(MAX_ITERATIONS):
-        diff = 0
-        #print("No.{:d} iteration...({:d} times at most)".format(i, MAX_ITERATIONS))
-        sys.stdout.write("\rPagerank Calculating...{:.0%}".format(i*1.0/MAX_ITERATIONS))
+        print('')
+        i_times = 0
+        for i in range(MAX_ITERATIONS):
+            diff = 0
+            sys.stdout.write("\rPagerank Calculating...{:.0%}".format(i * 1.0 / MAX_ITERATIONS))
+            sys.stdout.flush()
+            for key, attr_dict in doc_dict.iteritems():
+                tmp_pagerank = min_value
+                for id in attr_dict['citing_cases_id']:
+                    citing_case_dict = doc_dict[id[0]]
+                    tmp_pagerank += DAMPING_FACTOR * citing_case_dict['pagerank'] / citing_case_dict['cases_cited_count']
+                diff += abs(attr_dict['pagerank'] - tmp_pagerank)
+                attr_dict['pagerank'] = tmp_pagerank
+            i_times += 1
+            if diff < MIN_DELTA:
+                break
+        sys.stdout.write("\rPagerank Calculating...100%\n")
         sys.stdout.flush()
+        if self.verbosity >= 1:
+            self.stdout.write('Iteration amount is {0:d}.\n'.format(i_times))
+
+        case_count = 0
+        update_count = 0
         for key, attr_dict in doc_dict.iteritems():
-            tmp_pagerank = min_value
-            for id in attr_dict['citing_cases_id']:
-                citing_case_dict = doc_dict[id[0]]
-                tmp_pagerank += DAMPING_FACTOR * citing_case_dict['pagerank'] / citing_case_dict['cases_cited_count']
-            diff += abs(attr_dict['pagerank'] - tmp_pagerank)
-            attr_dict['pagerank'] = tmp_pagerank
-        if diff < MIN_DELTA:
-            break
-    sys.stdout.write("\rPagerank Calculating...100%\n")
-    sys.stdout.flush()
-    print('Updating database...')
-    for key, attr_dict in doc_dict.iteritems():
-        Document.objects.filter(pk=key).update(pagerank=attr_dict['pagerank'])
-        #print(str(key)+":\t"+str(attr_dict['pagerank']))
-    print('PageRank calculation finish!')
+            sys.stdout.write("\rUpdating database...{:.0%}".format(case_count * 1.0 / graph_size))
+            sys.stdout.flush()
+            if self.verbosity >= 1:
+                log_file.write("ID: {0}\told pagerank is {1}\tnew pagerank is {2}\n".format(int(key), attr_dict['original_pagerank'], attr_dict['pagerank']))
+            if attr_dict['original_pagerank'] != attr_dict['pagerank']:
+                Document.objects.filter(pk=key).update(pagerank=attr_dict['pagerank'])
+                update_count += 1
+            case_count += 1
+        print('\nPageRank calculation finish! Updated {0:d} cases'.format(update_count))
+        if self.verbosity >= 1:
+            self.stdout.write('See the log in pagerank.log\n')
+            log_file.close()
+
+
+    def handle(self, *args, **options):
+        self.verbosity = int(options.get('verbosity', 1))
+        self.do_pagerank()
+
+
+
