ca1a5b075ea6d1aecd26e03b87cf9894597f6870
mlissner <mlissner@michaeljaylissner.com>
2013-09-28 17:04:01 -0700

    Fixes up the pagearank algo so hopefully it stays slim on memory usage. Also adds memory usage reporting.

--- alert/lib/filesize.py
+++ alert/lib/filesize.py
@@ -1,20 +1,3 @@
-# This software and any associated files are copyright 2010 Brian Carver and
-# Michael Lissner.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
 alternative = [
     (1024 ** 5, 'PB'),
     (1024 ** 4, 'TB'),
@@ -22,11 +5,12 @@ alternative = [
     (1024 ** 2, 'MB'),
     (1024 ** 1, 'KB'),
     (1024 ** 0, (' byte', ' bytes')),
-    ]
+]
+
 
 def size(bytes, system=alternative):
-    """Human-readable file size. 
-    
+    """Human-readable file size.
+
     """
     for factor, suffix in system:
         if bytes >= factor:
