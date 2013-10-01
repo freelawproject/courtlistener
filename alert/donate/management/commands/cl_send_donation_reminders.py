a84a07ba37de5ee118417e89e5c89b998bec7145
mlissner <mlissner@michaeljaylissner.com>
2013-10-01 10:49:05 -0700

    Wraps up the donation work, including adding a basic test for it backed by test fixtures.

--- alert/donate/management/commands/cl_send_donation_reminders.py
+++ alert/donate/management/commands/cl_send_donation_reminders.py
@@ -12,16 +12,30 @@ from datetime import timedelta
 
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option(
-            '--simulate',
-            action='store_true',
-            default=False,
-            help='Run the command in simulation mode, not sending any emails'
-        ),
-
-    )
     help = 'Sends the annual reminders to people that donated and wanted a reminder.'
+
+    def gather_stats_and_ups(self):
+        about_a_year_ago = now() - timedelta(days=355)
+
+        # Gather some stats to email
+        self.new_doc_count = Document.objects.filter(time_retrieved__gte=about_a_year_ago).count()
+        self.court_count = Court.objects.all().count()
+        self.bulk_data_count = Stat.objects.filter(
+            name__startswith='bulk_data',
+            date_logged__gte=about_a_year_ago
+        ).aggregate(Sum('count'))['count__sum']
+        self.alerts_sent_count = Stat.objects.filter(
+            name='alerts.sent',
+            date_logged__gte=about_a_year_ago,
+        ).aggregate(Sum('count'))['count__sum']
+
+        self.ups = UserProfile.objects.filter(
+            donation__date_created__year=about_a_year_ago.year,
+            donation__date_created__month=about_a_year_ago.month,
+            donation__date_created__day=about_a_year_ago.day,
+            donation__send_annual_reminder=True,
+            donation__status=4
+        ).annotate(Sum('donation__amount'))
 
     def send_reminder_email(self, up, amount):
         """Send an email imploring the person for another donation."""
@@ -49,35 +63,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.verbosity = int(options.get('verbosity', 1))
-
-        if options['simulate']:
-            self.stdout.write("**********************************\n")
-            self.stdout.write("* SIMULATE MODE - NO EMAILS SENT *\n")
-            self.stdout.write("**********************************\n")
-
-        about_a_year_ago = now() - timedelta(days=355)
-
-        # Gather some stats to email
-        self.new_doc_count = Document.objects.filter(time_retrieved__gte=about_a_year_ago).count()
-        self.court_count = Court.objects.all().count()
-        self.bulk_data_count = Stat.objects.filter(
-            name__startswith='bulk_data',
-            date_logged__gte=about_a_year_ago
-        ).aggregate(Sum('count'))['count__sum']
-        self.alerts_sent_count = Stat.objects.filter(
-            name='alerts.sent',
-            date_logged__gte=about_a_year_ago,
-        ).aggregate(Sum('count'))['count__sum']
-
-        ups = UserProfile.objects.filter(
-            donation__date_created__year=about_a_year_ago.year,
-            donation__date_created__month=about_a_year_ago.month,
-            donation__date_created__day=about_a_year_ago.day,
-            donation__send_annual_reminder=True,
-            donation__status=4
-        ).annotate(Sum('donation__amount'))
-
-        for up in ups:
+        self.gather_stats_and_ups()
+        for up in self.ups:
             # Iterate over the people and send them emails
-            if not options['simulate']:
-                self.send_reminder_email(up, up.donation__amount__sum)
+            self.send_reminder_email(up, up.donation__amount__sum)
