5a9cd050b22c0969db2f465f89204ace2bed3d6b
Krist Jin <krist.jin.apply@gmail.com>
2013-10-11 21:58:57 -0700

    update NetworkX way. It will generate a pr result file.

--- alert/search/management/commands/cl_nx_pr.py
+++ alert/search/management/commands/cl_nx_pr.py
@@ -20,99 +20,55 @@ class Command(BaseCommand):
         sys.stdout.write('Initializing...\n')
         graph_size = Document.objects.all().count()
         result_file = open('pr_result.txt', 'w')
-
-        ##########################
-        #        Stage I         #
-        # Build a Data Structure #
-        ##########################
+        ngraph = nx.DiGraph()
         d = Document.objects.all().order_by('date_filed')[0].date_filed
         qs = Document.objects.only(
             'documentUUID',
             'date_filed',
             'cases_cited',
-            'pagerank',
-        )
+            )
         case_list = queryset_generator(qs, chunksize=10000)
-
-        if verbosity >= 1:
-            sys.stdout.write('graph_size is {0:d} nodes.\n'.format(graph_size))
-
         case_count = 0
-        doc_dict = {}
         timings = []
         average_per_s = 0
-        for case in case_list:
+        for source_case in case_list:
             case_count += 1
-            if verbosity >= 1:
-                if case_count % 100 == 1:
-                    t1 = time.time()
-                if case_count % 100 == 0:
-                    t2 = time.time()
-                    timings.append(t2 - t1)
-                    average_per_s = 100 / (sum(timings) / float(len(timings)))
-                sys.stdout.write("\rGenerating data in memory...{:.0%} ({}/{}, {:.1f}/s) at {:<9}".format(
-                    case_count * 1.0 / graph_size,
-                    case_count,
-                    graph_size,
-                    average_per_s,
-                    size(sys.getsizeof(doc_dict)),
-                ))
-                sys.stdout.flush()
-            doc_dict[case.pk] = {
-                'pk': case.pk,
-                'pagerank': case.pagerank,
-                'cached_pagerank': case.pagerank,
-                'cases_cited': case.cases_cited.values_list('document__pk'),
-            }
-
-        ######################
-        #      Stage II      #
-        # Calculate PageRank #
-        ######################
-        graph = nx.DiGraph()
-        case_count = 0
-        if verbosity >= 1:
-            sys.stdout.write('\n')
-        for key, case in doc_dict.iteritems():
-            case_count += 1
-            sys.stdout.write('\rImport citing relation into NetworkX graph...{:.0%}'.format(
-                case_count * 1.0 / graph_size))
+            if case_count % 100 == 1:
+                t1 = time.time()
+            if case_count % 100 == 0:
+                t2 = time.time()
+                timings.append(t2 - t1)
+                average_per_s = 100 / (sum(timings) / float(len(timings)))
+            sys.stdout.write("\rGenerating relation file...{:.0%} ({}/{}, {:.1f}/s) at {:<9}".format(
+                case_count * 1.0 / graph_size,
+                case_count,
+                graph_size,
+                average_per_s,
+                size(sys.getsizeof(ngraph)),
+            ))
             sys.stdout.flush()
-            source_id = key
-            for item in case['cases_cited']:
-                target_id = item[0]
-                graph.add_edge(source_id, target_id)
+            for target_case in source_case.cases_cited.values_list('document__pk'):
+                ngraph.add_edge(source_case.documentUUID, target_case[0])
+
         if verbosity >= 1:
             sys.stdout.write('\n')
             sys.stdout.write('NetworkX PageRank calculating...\n')
-        pr_result = nx.pagerank(graph)
+        pr_result = nx.pagerank(ngraph)
 
         #####################
         #     Stage III     #
         # Update everything #
         #####################
-        case_count = 0
-        update_count = 0
-        for key, case in doc_dict.iteritems():
-            try:
-                case['pagerank'] = pr_result[key]
-            except KeyError:
-                case['pagerank'] = 0
-            case_count += 1
-            if verbosity >= 1:
-                sys.stdout.write("\rUpdating database...{:.0%}".format(case_count * 1.0 / graph_size))
-                sys.stdout.flush()
-            logger.info("ID: {0}\told pagerank is {1}\tnew pagerank is {2}\n".format(
-                case['pk'], case['cached_pagerank'], case['pagerank']))
-            result_file.write("{0}\t{1}\n".format(case['pk'], case['pagerank']))
-            if case['cached_pagerank'] != case['pagerank']:
-                # Only save if we have changed the value
-                Document.objects.filter(pk=key).update(pagerank=case['pagerank'])
-                update_count += 1
-
-        if verbosity >= 1:
-            sys.stdout.write('\nPageRank calculation finished! Updated {0:d} cases\n'.format(update_count))
-            sys.stdout.write('See the django log for more details.\n')
+        progress = 0
+        for id, pr in pr_result.iteritems():
+            progress += 1
+            result_file.write(str(id) + '\t' + str(pr) + '\n')
+            sys.stdout.write('\rRecording results...{:.0%}'.format(progress * 1.0 / len(pr_result)))
+            sys.stdout.flush()
+        sys.stdout.write('\n')
+        # if verbosity >= 1:
+        #     sys.stdout.write('\nPageRank calculation finished! Updated {0:d} cases\n'.format(update_count))
+        #     sys.stdout.write('See the django log for more details.\n')
         result_file.close()
 
     def handle(self, *args, **options):
