23957da044cb6c5192c3867b0a30343e25ad0a05
mlissner <mlissner@michaeljaylissner.com>
2014-06-18 15:08:38 -0700

    Makes the old urls, /alert/edit/$id/ work again via redirects.

--- alert/alerts/views.py
+++ alert/alerts/views.py
@@ -8,6 +8,25 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 @login_required
+def edit_alert_redirect(request, alert_id):
+    """Note that this method is still very useful because it gives people an opportunity to login if they
+    come to the site via one of our email alerts.
+    """
+    try:
+        alert_id = int(alert_id)
+    except ValueError:
+        return HttpResponseRedirect('/')
+
+    # check if the user can edit this, or if they are url hacking
+    alert = get_object_or_404(
+        Alert,
+        pk=alert_id,
+        userprofile=request.user.profile
+    )
+    return HttpResponseRedirect('/?%s&edit_alert=%s' % (alert.alertText, alert.pk))
+
+
+@login_required
 def delete_alert(request, alert_id):
     try:
         alert_id = int(alert_id)
