5f4043b783b17d161d89ab0c665f67890f9af236
mlissner <mlissner@michaeljaylissner.com>
2014-07-16 12:11:14 -0400

    Fixes an issue in the contact form around mutable data as function parameters, and runs the Intellij inspection to fix any other erroneous occurrences.

--- alert/simple_pages/tests.py
+++ alert/simple_pages/tests.py
@@ -1,16 +1,21 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class ContactTest(TestCase):
+    fixtures = ['authtest_data.json']
+
+    def test_multiple_requests_request(self):
+        """ Is state persisted in the contact form?
+
+        The contact form is abstracted in a way that it can have peculiar behavior when called multiple times. This test
+        makes sure that that behavior does not regress.
         """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+        self.client.login(username='pandora', password='password')
+        self.client.get('/contact/')
+        self.client.logout()
+
+        # Now, as an anonymous user, we get the page again. If the bug is resolved, we should not see anything about
+        # the previously logged-in user, pandora.
+        r = self.client.get('/contact/')
+        self.assertNotIn('pandora', r.content)
+
