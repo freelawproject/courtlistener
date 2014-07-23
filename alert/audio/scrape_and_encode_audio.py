b8b680029d2ff34b4c97a6a2964e0c7834cfcb1e
mlissner <mlissner@michaeljaylissner.com>
2014-07-23 12:22:58 -0400

    Merges changes accidentally made in the default branch.

--- alert/audio/scrape_and_encode_audio.py
+++ alert/audio/scrape_and_encode_audio.py
@@ -21,81 +21,9 @@
         - Need RSS feeds for audio
         - Coverage page needs to be updated
         - UI hooks on each page, with rewrite of pages so they look right when we only have audio for a case.
-
-    Court notes:
-     - First Circuit:
-        - RSS: http://www.ca1.uscourts.gov/files/audio/audiorss.php
-        - mp3
-        - meta: case name, docket num, arg date
-     - Second Circuit:
-        - None
-     - Third Circuit:
-        - Last seven days: http://www.ca3.uscourts.gov/oralargument/ListArguments7.aspx
-        - All: http://www.ca3.uscourts.gov/oralargument/ListArgumentsAll.aspx (can be chron sorted)
-        - wma
-        - meta: case name, docket num, arg date
-     - Fourth Circuit:
-        - None
-     - Fifth Circuit:
-        - http://www.ca5.uscourts.gov/OralArgumentRecordings.aspx (use date fields to return all oral args)
-        - http://www.ca5.uscourts.gov/Rss.aspx?Feed=OralArgRecs (Feed of args)
-        - wma
-        - meta: case name, docket num, arg date, appearing attorneys
-     - Sixth Circuit
-        - None
-     - Seventh Circuit
-        - RSS: http://www.ca7.uscourts.gov/fdocs/docs.fwx?submit=rss_args
-        - Also available via POST at http://www.ca7.uscourts.gov/fdocs/docs.fwx (submit 10 for all 2010 cases, etc.)
-        - Audio is at: http://www.ca7.uscourts.gov/fdocs/docs.fwx?submit=showbr&shofile=10-2056_001.mp3
-        - mp3
-        - meta (RSS): case name, docket num, case type
-        - meta (HTML): case name, docket num, more on linked page.
-     - Eighth Circuit:
-        - RSS: http://8cc-www.ca8.uscourts.gov/circ8rss.xml (reverse order?)
-        - mp3
-        - meta: case name, docket num, arg date
-     - Ninth Circuit:
-        - Cases are here: http://www.ca9.uscourts.gov/media/ (can be sorted)
-        - Links take the form: http://www.ca9.uscourts.gov/datastore/media/2010/10/13/08-16436.wma
-        - mp3
-        - meta: case name, docket num, arg date
-     - Tenth Circuit:
-        -None
-     - Eleventh Circuit:
-        - None
-     - DC Circuit:
-        - Denied.
-     - Fed. Circuit:
-        - RSS with ALL cases back to 2009-01: http://www.cafc.uscourts.gov/rss-audio-recordings.php
-        - Also available via search queries
-        - mp3
-        - meta: case name, docket num, date (?)
-     - SCOTUS:
-        - Latest from here: http://www.supremecourt.gov/oral_arguments/argument_audio.aspx
-        - Old from Oyez
-        - mp3, wma, ra
-        - meta: case name, docket num, arg date
-
-    Counts:
-     - 1st: Latest only
-     - 2nd: None
-     - 3rd: 574
-     - 4th: None
-     - 5th: 236
-     - 6th: None
-     - 7th: (2010: 136, 2009: 905, 2008: 1165, 2007: 1122, 2006: 1260,
-             2005: 1317, 2004: 1005, 2003: 933, 2002: 864, 2001: 806,
-             2000: 661, 1999: 576, 1998: 134, 1997: 20, 1996: 3, 1995: 1,
-             1994: 1, 1993: 1, 1992: 0)
-             TOTAL: ~10,000 cases, most lack audio...maybe 3,000 with audio.
-     - 8th: Latest only.
-     - 9th: ~6000
-     - D.C.: Denied
-     - Fed: 2152
-     - Scotus: Latest plus Oyez.
-     - GRAND TOTAL: 11962
-     - SIZE, AT 20MB/ARG: 233GB.
 '''
+import urllib2
+import eyed3
 
 
 def makeAudioFromUrl(url, audioLink):
@@ -139,7 +67,7 @@ def makeAudioFromUrl(url, audioLink):
     return myFile, audio, created, error
 
 
-def encode_audio(mime - type, data):
+def encode_audio(mime_type, data):
     '''
     Fires off a new thread that does the audio encoding for the file. If
     possible, uses a low nice value when doing encoding.
@@ -156,34 +84,22 @@ def encode_audio(mime - type, data):
      -ar 22050 -ab 64k -ac 1 -acodec libvorbis out.ogg
 
     Commands that work:
-     - Convert wma to ogg: ffmpeg -i 03-4353RayvWarrenetal.wma -metadata Artist="Artist name" -metadata Title="Case name" -metadata Album="Supreme court 2009 term" -metadata Year="2009" -metadata Comment="Docket number: 09-2661" -ar 22050 -ab 64k -ac 1 -acodec libvorbis 09-2661.new-ffmpeg.ogg
-     - Convert mp3 to ogg: ffmpeg -i 09-2661.mp3 -metadata Artist="Artist name" -metadata Title="Case name" -metadata Album="Supreme court 2009 term" -metadata Year="2009" -metadata Comment="Docket number: 09-2661" -ar 22050 -ab 64k -ac 1 -acodec libvorbis 09-2661.new-ffmpeg.ogg
-     - Convert mp3 to mp3: ffmpeg -i 09-2661.mp3 -metadata Artist="Artist name" -metadata Title="Case name" -metadata Album="Supreme court 2009 term" -metadata Year="2009" -metadata Comment="Docket number: 09-2661" -ar 22050 -ab 64k -ac 1 new-ffmpeg.mp3
+     - Convert wma to ogg: ffmpeg -i 03-4353RayvWarrenetal.wma -ar 22050 -ab 64k -ac 1 -acodec libvorbis 09-2661.new-ffmpeg.ogg
+     - Convert mp3 to ogg: ffmpeg -i 09-2661.mp3 -ar 22050 -ab 64k -ac 1 -acodec libvorbis 09-2661.new-ffmpeg.ogg
+     - Convert mp3 to mp3: ffmpeg -i 09-2661.mp3 -ar 22050 -ab 64k -ac 1 new-ffmpeg.mp3
 
     Sets meta data:
     ffmpeg -i in.avi -metadata title="my title" out.flv
 
     Returns an mp3 and an ogg of the file that are encoded properly.
     '''
-    # This might indicate how to set the nice value of this process:
-    # http://stackoverflow.com/questions/2463533/is-it-possible-to-renice-a-subprocess
-    # A better method is probably using a preexec_fn of os.nice() to the
-    # subprocess call.
-    if mime - type == "mp3":
-        # Convert the sample rate to something good, make it mono.
-    elif mime - type == "wma":
-        # Convert make an acceptable mp3 from the wma.
+    'avconv -i test_files/3.wma -ac 1 -ar 22050 3-mono-44100.mp3'
 
-    # make the ogg from the mp3.
+    return None
 
-    # add id3 data to the mp3
 
-    # add meta data to the ogg
-
-    return
-
-def set_meta_data(mp3, ogg, casename, court, year, argdate, docketNum):
-    '''
+def set_meta_data(mp3_path, casename, court, year, argdate, docketNum, download_url):
+    """
     Sets the meta data on the mp3 and ogg files as follows:
      - Title: Case name
      - Artist: Court name
@@ -193,7 +109,25 @@ def set_meta_data(mp3, ogg, casename, court, year, argdate, docketNum):
      - Album Cover: CL Logo?
 
     Returns the two new and improved files.
-    '''
+    """
+    audio_file = eyed3.load(mp3_path)
+    audio_file.tag.clear()
+
+    audio_file.tag.album = u'{court}, {year}'.format(court=court.full_name, year=year)
+    audio_file.tag.artist = court.full_name
+    audio_file.tag.artist_url = court.url
+    audio_file.tag.audio_source_url = download_url
+    audio_file.tag.comments = u'Argued: %s. Docket number: %s'.format(argdate, docketNum)
+    audio_file.tag.genre = u'Speech'
+    audio_file.tag.publisher = u'Free Law Project'
+    audio_file.tag.publisher_url = u'http://www.freelawproject.org'
+    audio_file.tag.recording_date = argdate
+    audio_file.tag.title = casename
+
+    # TODO Set the album cover to the court seal.
+
+    audio_file.tag.save()
+
 
 
 def scrapeCourt(courtID):
