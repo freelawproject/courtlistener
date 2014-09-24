d9963513bd7f550395bd78d4722ea136deb02aca
Michael Lissner <mlissner@michaeljaylissner.com>
2014-09-24 14:30:35 -0700

    Adds a new test for Stripe to prevent regressions when invalid cards are used.

--- alert/donate/stripe_helpers.py
+++ alert/donate/stripe_helpers.py
@@ -72,7 +72,7 @@ def process_stripe_payment(cd_donation_form, cd_user_form, stripe_token):
             'payment_id': charge.id,
             'redirect': '/donate/stripe/complete',
         }
-    except stripe.CardError, e:
+    except stripe.error.CardError, e:
         logger.warn("Stripe was unable to process the payment: %s" % e)
         response = {
             'message': 'Oops, we had a problem processing your card: '
