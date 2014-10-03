b2b4db4e2e5eb47d657517103a2b6e36e6cdf8cd
Michael Lissner <mlissner@michaeljaylissner.com>
2014-10-03 12:29:46 -0700

    Updates the dockets to display properly.
    Page hasn't gotten nearly enough love.

--- alert/audio/admin.py
+++ alert/audio/admin.py
@@ -3,20 +3,7 @@ from django.contrib import admin
 
 
 class AudioAdmin(admin.ModelAdmin):
-    # ordering is brutal. Don't put it here. Sorry.
-    #list_display = ('citation',)
-    #list_filter = ('court',)
-    # fields = ('citation', 'docket', 'source', 'sha1', 'date_filed', 'download_url', 'local_path', 'precedential_status',
-    #           'blocked', 'date_blocked', 'extracted_by_ocr', 'time_retrieved', 'date_modified',
-    #           'cases_cited', 'citation_count', 'plain_text', 'nature_of_suit', 'judges', 'html', 'html_lawbox',
-    #           'html_with_citations', )
-    # raw_id_fields = ('citation', 'cases_cited', 'docket')
-    # search_fields = ['plain_text', 'html', 'html_lawbox']
-    # readonly_fields = ('time_retrieved', 'date_modified', 'citation_count')
-    # list_filter = (
-    #     'source',
-    # )
-    pass
+    raw_id_fields = ('docket',)
 
 
 class CourtAdmin(admin.ModelAdmin):
