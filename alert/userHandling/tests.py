3df3ea95a584a7c09df934a3b2c5dd583f4be03c
Michael Lissner <mlissner@michaeljaylissner.com>
2014-11-13 12:38:41 -0800

    Adds more test cases for the pwd reset process.
    These didn't have tests before and, sure enough, we had an issue today. All should be fixed and future-proofed for now.

--- alert/userHandling/tests.py
+++ alert/userHandling/tests.py
@@ -1,7 +1,12 @@
 # coding=utf-8
 from datetime import timedelta
-from django.test import TestCase
+import os
+from django.contrib.auth.tokens import default_token_generator
+from django.core import mail
+from django.test import TestCase, LiveServerTestCase
 from django.utils.timezone import now
+from selenium import webdriver
+
 from alert.userHandling.models import UserProfile
 
 
@@ -28,31 +33,102 @@ class UserTest(TestCase):
             'username': 'pandora',
             'password': 'password',
         }
-        response = self.client.post('/sign-in/', params, follow=True)
-        self.assertRedirects(response, 'http://testserver/')
+        r = self.client.post('/sign-in/', params, follow=True)
+        self.assertRedirects(r, 'http://testserver/')
 
     def test_confirming_an_email_address(self):
-        """Tests whether we can confirm the case where an email is associated with a single account."""
+        """Tests whether we can confirm the case where an email is associated
+        with a single account.
+        """
         # Update the expiration since the fixture has one some time ago.
         u = UserProfile.objects.get(pk=2)
         u.key_expires = now() + timedelta(days=2)
         u.save()
 
-        response = self.client.get('/email/confirm/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/')
-        self.assertEqual(200, response.status_code,
-                         msg="Did not get 200 code when activating account. Instead got %s" % response.status_code)
-        self.assertIn('has been confirmed', response.content,
+        r = self.client.get('/email/confirm/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/')
+        self.assertEqual(200, r.status_code,
+                         msg="Did not get 200 code when activating account. "
+                             "Instead got %s" % r.status_code)
+        self.assertIn('has been confirmed', r.content,
                       msg="Test string not found in response.content")
 
     def test_confirming_an_email_when_it_is_associated_with_multiple_accounts(self):
-        """Tests the trickier case when an email is associated with many accounts."""
-        UserProfile.objects.filter(pk__in=(3, 4,)).update(key_expires=now() + timedelta(days=2))
-        response = self.client.get('/email/confirm/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab/')
-        self.assertIn('has been confirmed', response.content,
+        # Test the trickier case when an email is associated with many accounts
+        UserProfile.objects.filter(pk__in=(3, 4,))\
+            .update(key_expires=now() + timedelta(days=2))
+        r = self.client.get('/email/confirm/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab/')
+        self.assertIn('has been confirmed', r.content,
                       msg="Test string not found in response.content")
-        self.assertEqual(200, response.status_code,
-                         msg="Did not get 200 code when activating account. Instead got %s" % response.status_code)
+        self.assertEqual(200, r.status_code,
+                         msg="Did not get 200 code when activating account. "
+                             "Instead got %s" % r.status_code)
         ups = UserProfile.objects.filter(pk__in=(3, 4,))
         for up in ups:
             self.assertTrue(up.email_confirmed)
 
+
+class LiveUserTest(LiveServerTestCase):
+    fixtures = ['authtest_data.json']
+
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = webdriver.PhantomJS(
+            executable_path='/usr/local/phantomjs/phantomjs',
+            service_log_path='/var/log/courtlistener/django.log',
+        )
+        super(LiveUserTest, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(LiveUserTest, cls).tearDownClass()
+
+    def test_reset_password_using_the_HTML(self):
+        """Can we use the HTML form to send a reset email?
+
+        This test checks that the email goes out and that the status code
+        returned is valid.
+        """
+        self.selenium.get('%s%s' % (self.live_server_url, '/reset-password/'))
+        email_input = self.selenium.find_element_by_name("email")
+        email_input.send_keys('pandora@courtlistener.com')
+        email_input.submit()
+
+        #self.selenium.save_screenshot('/home/mlissner/phantom.png')
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(
+            self.selenium.current_url,
+            '%s%s' % (self.live_server_url,
+                      '/reset-password/instructions-sent/')
+        )
+
+    def test_set_password_using_the_HTML(self):
+        """Can we reset our password after generating a confirmation link?"""
+        # Generate a token and use it to visit a generated reset URL
+        up = UserProfile.objects.get(pk=1)
+        token = default_token_generator.make_token(up.user)
+        url = '%s/confirm-password/%s/%s/' % (
+            self.live_server_url,
+            up.user.pk,
+            token,
+        )
+        self.selenium.get(url)
+        #self.selenium.save_screenshot('/home/mlissner/phantom.png')
+
+        self.assertIn(
+            "Enter New Password",
+            self.selenium.page_source
+        )
+
+        # Next, change the user's password and submit the form.
+        pwd1 = self.selenium.find_element_by_name('new_password1')
+        pwd1.send_keys('password')
+        pwd2 = self.selenium.find_element_by_name('new_password2')
+        pwd2.send_keys('password')
+        pwd2.submit()
+
+        self.assertEqual(
+            self.selenium.current_url,
+            '%s%s' % (self.live_server_url, '/reset-password/complete/')
+        )
