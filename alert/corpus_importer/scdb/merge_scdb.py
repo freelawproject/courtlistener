bb46049029e7813a00cecbff726d2d52c2b36c96
Michael Lissner <mlissner@michaeljaylissner.com>
2014-12-18 14:47:30 -0800

    More tests for SCDB code.

--- alert/corpus_importer/scdb/merge_scdb.py
+++ alert/corpus_importer/scdb/merge_scdb.py
@@ -31,34 +31,47 @@ DATA_DIR = os.path.dirname(__name__)
 SCDB_FILENAME = os.path.join(DATA_DIR, 'SCDB_2014_01_caseCentered_Citation.csv')
 SCDB_BEGINS = date(1946, 11, 18)
 SCDB_ENDS = date(2014, 6, 19)
+START_ROW = 0
 
 with open(SCDB_FILENAME) as f:
     dialect = csv.Sniffer().sniff(f.read(1024))
     f.seek(0)
     reader = csv.DictReader(f, dialect=dialect)
-    for d in reader:
+    for i, d in enumerate(reader):
         # Iterate over every item, looking for matches in various ways.
+        if i < START_ROW:
+            continue
+        print "Row is: %s" % i
+
         ds = EmptyQuerySet()
         if len(ds) == 0:
+            print "  Checking by caseID...",
             ds = Document.objects.filter(
                 supreme_court_db_id=d['caseId'])
+            print "%s matches found." % len(ds)
         if d['usCite'].strip():
             # Only do these lookups if there is in fact a usCite value.
             if len(ds) == 0:
                 # None found by supreme_court_db_id. Try by citation number.
+                print "  Checking by federal_cite_one..",
                 ds = Document.objects.filter(
                     citation__federal_cite_one=d['usCite'])
+                print "%s matches found." % len(ds)
             if len(ds) == 0:
+                print "  Checking by federal_cite_one...",
                 ds = Document.objects.filter(
                     citation__federal_cite_two=d['usCite'])
+                print "%s matches found." % len(ds)
             if len(ds) == 0:
+                print "  Checking by federal_cite_three...",
                 ds = Document.objects.filter(
                     citation__federal_cite_three=d['usCite'])
+                print "%s matches found." % len(ds)
 
         if len(ds) == 0:
-            print 'No items found for %s' % d['caseId']
+            print '  No items found for %s' % d['caseId']
         elif len(ds) == 1:
-            print 'Exactly one match found for %s' % d['caseId']
+            print '  Exactly one match found for %s' % d['caseId']
         elif len(ds) > 1:
-            print 'Multiple items found for %s (we found %s): (%s)' % \
+            print '  Multiple items found for %s (we found %s): (%s)' % \
                   (d['caseId'], len(ds), ', '.join([str(d.citation.pk) for d in ds]))
