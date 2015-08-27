269a0c0c9a80aac07af56422d9f79050624270d3
Michael Lissner <mlissner@michaeljaylissner.com>
2015-08-27 15:01:37 -0400

    First pass at the models.
    Will need some refinement, but the basics are in place. Biggest area of contention is that I have cluster_start, cluster_end, and clusters, which all point to the clusters that form the vis. Seems redundant, but it's already proving useful in the make_title function. More TBD here.

--- cl/visualizations/models.py
+++ cl/visualizations/models.py
@@ -1,3 +1,125 @@
+# coding=utf-8
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+from cl.lib.string_utils import trunc
+from cl.search.models import OpinionCluster
+
+
+class SCOTUSMaps(models.Model):
+    user = models.ForeignKey(
+        User,
+        help_text="The user that owns the visualization",
+        related_name="scotus_maps",
+    )
+    cluster_start = models.ForeignKey(
+        OpinionCluster,
+        help_text="The starting cluster for the visualization",
+    )
+    cluster_end = models.ForeignKey(
+        OpinionCluster,
+        help_text="The ending cluster for the visualization",
+    )
+    clusters = models.ManyToManyField(
+        OpinionCluster,
+        help_text="The clusters involved in this visualization",
+        related_name="visualizations",
+        blank=True,
+    )
+    date_created = models.DateTimeField(
+        help_text="The time when this item was created",
+        auto_now_add=True,
+        db_index=True,
+    )
+    date_modified = models.DateTimeField(
+        help_text="The last moment when the item was modified.",
+        auto_now=True,
+        db_index=True,
+    )
+    title = models.CharField(
+        help_text="The title of the visualization that you're creating.",
+        max_length=200,
+    )
+    subtitle = models.CharField(
+        help_text="The subtitle of the visualization that you're creating.",
+        max_length=300,
+        blank=True,
+    )
+    slug = models.SlugField(
+        help_text="The URL path that the visualization will map to (the slug)",
+        max_length=75,
+    )
+    notes = models.TextField(
+        help_text="Any notes that help explain the diagram, in Markdown format",
+        blank=True,
+    )
+    degree_count = models.IntegerField(
+        help_text="The number of degrees to display between cases",
+    )
+    view_count = models.IntegerField(
+        help_text="The number of times the visualization has been seen.",
+        default=0,
+    )
+    published = models.BooleanField(
+        help_text="Whether the visualization can be seen publicly.",
+        default=False,
+    )
+    deleted = models.BooleanField(
+        help_text="Has a user chosen to delete this visualization?",
+        default=False,
+    )
+
+    def make_title(self):
+        """Make a title for the visualization
+
+        Title tries to use the shortest possible case name from the starting
+        and ending clusters plus the number of degrees.
+        """
+        def get_best_case_name(obj):
+            case_name_preference = [
+                obj.case_name_short,
+                obj.case_name,
+                obj.case_name_full
+            ]
+            return next((_ for _ in case_name_preference if _), "Unknown")
+
+        return "{start} to {end} ({degrees} degrees)".format(
+            start=get_best_case_name(self.cluster_start),
+            end=get_best_case_name(self.cluster_end),
+            degrees=self.degree_count,
+        )
+
+    def __unicode__(self):
+        return '{pk}: {title}'.format(self.pk, self.title)
+
+    def get_absolute_url(self):
+        pass
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.title = trunc(self.make_title(), 75, ellipsis='…')
+            self.slug = trunc(slugify(self.title))
+        super(SCOTUSMaps, self).save(*args, **kwargs)
+
+
+class JSONVersions(models.Model):
+    """Used for holding a variety of versions of the data."""
+    map = models.ForeignKey(
+        SCOTUSMaps,
+        help_text='The visualization that the json is affiliated with.',
+        related_name="json_versions",
+    )
+    date_created = models.DateTimeField(
+        help_text="The time when this item was created",
+        auto_now_add=True,
+        db_index=True,
+    )
+    date_modified = models.DateTimeField(
+        help_text="The last moment when the item was modified.",
+        auto_now=True,
+        db_index=True,
+    )
+    json_data = models.TextField(
+        help_text="The JSON data for a particular version of the visualization.",
+    )
