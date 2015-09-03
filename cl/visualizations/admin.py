0ed5b9e6a6ffac074feece611503252c58d9718f
Michael Lissner <mlissner@michaeljaylissner.com>
2015-09-02 22:04:02 -0400

    Configures the admin site for viz.

--- cl/visualizations/admin.py
+++ cl/visualizations/admin.py
@@ -1,3 +1,48 @@
+from cl.visualizations.models import SCOTUSMap, JSONVersion
 from django.contrib import admin
 
-# Register your models here.
+
+class JSONVersionAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'date_created',
+        'date_modified',
+    )
+    raw_id_fields = (
+        'map',
+    )
+
+class JSONVersionInline(admin.StackedInline):
+    model = JSONVersion
+    extra = 1
+
+class SCOTUSMapAdmin(admin.ModelAdmin):
+    inlines = (
+        JSONVersionInline,
+    )
+    raw_id_fields = (
+        'clusters',
+        'cluster_start',
+        'cluster_end',
+    )
+    readonly_fields = (
+        'date_created',
+        'date_modified',
+        'generation_time',
+    )
+    list_display = (
+        '__unicode__',
+        'user_id',
+        'date_created',
+        'date_modified',
+        'view_count',
+        'published',
+        'deleted',
+    )
+    list_filter = (
+        'published',
+        'deleted',
+    )
+
+
+admin.site.register(SCOTUSMap, SCOTUSMapAdmin)
+admin.site.register(JSONVersion, JSONVersionAdmin)
