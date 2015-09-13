d7609025c8ba16bc50fe9485511894582fd0ded2
Michael Lissner <mlissner@michaeljaylissner.com>
2015-09-13 11:45:16 -0700

    Fixes the SCRAPER_ADMIN variable.
    Commas and tuples are a frustrating Python thing.

--- alert/settings/10-public.py
+++ alert/settings/10-public.py
@@ -136,7 +136,7 @@ if DEVELOPMENT:
 SERVER_EMAIL = 'CourtListener <noreply@courtlistener.com>'
 DEFAULT_FROM_EMAIL = 'CourtListener <noreply@courtlistener.com>'
 SCRAPER_ADMINS = (
-    ('Juriscraper List', 'juriscraper@lists.freelawproject.org')
+    ('Juriscraper List', 'juriscraper@lists.freelawproject.org'),
 )
 
 
