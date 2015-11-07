584ed2eb00b580bba5f680cad8a82d4b4a1cf99e
Michael Lissner <mlissner@michaeljaylissner.com>
2015-11-07 15:15:34 -0800

    Formatting cleanup.

--- cl/corpus_importer/scdb/merge_scdb.py
+++ cl/corpus_importer/scdb/merge_scdb.py
@@ -14,66 +14,82 @@ Once located, we update items:
  - Citations (Lexis, US, L.Ed., etc.)
  - Docket number?
  - scdb_id
+ - votes_majority & votes_minority
+ - decision_direction
 """
-import os
 
-from cl.search.models import Document
 import csv
+import os
+from cl.search.models import OpinionCluster
 from datetime import date, datetime
-from django.db.models.query import EmptyQuerySet
-
 
 DATA_DIR = os.path.dirname(__name__)
-SCDB_FILENAME = os.path.join(DATA_DIR, 'SCDB_2014_01_caseCentered_Citation.csv')
+SCDB_FILENAME = os.path.join(DATA_DIR, 'data',
+                             'SCDB_2015_01_caseCentered_Citation.csv')
 SCDB_BEGINS = date(1946, 11, 18)
 SCDB_ENDS = date(2014, 6, 19)
 
-DEBUG = False
+DEBUG = True
+
 
 # Relevant numbers:
 #  - 7907: After this point we don't seem to have any citations for items.
 
 
-def merge_docs(first_pk, second_pk):
-    first = Document.objects.get(pk=first_pk)
-    second = Document.objects.get(pk=second_pk)
-    first.source = 'LR'
-    first.judges = second.judges
-    first.html_lawbox = second.html_lawbox
-    first.save()
-    first.case_name = second.case_name
-    first.slug = second.slug
-    first.save()
-    second.docket.delete()
-    second.delete()
-    from cl.citations.tasks import update_document_by_id
-    update_document_by_id(first_pk)
+def set_if_falsy(obj, attribute, new_value):
+    """Check if the value passed in is Falsy. If so, set it to the value of
+    new_value.
+    """
+    current_value = getattr(obj, attribute).strip()
+    if not current_value:
+        setattr(obj, attribute, new_value)
+    else:
+        # Report if there's a difference -- that might spell trouble.
+        if current_value != new_value:
+            print ("      WARNING: Didn't set '{attr}' attribute on obj "
+                   "{obj_id} because it already had a value, but the new "
+                   "value ('{new}') differs from current value "
+                   "('{current}').".format(
+                attr=attribute,
+                obj_id=obj.pk,
+                new=new_value,
+                old=current_value,
+            ))
 
 
-def enhance_item_with_scdb(doc, scdb_info):
-    """Good news: A single Document object was found for the SCDB record.
+def enhance_item_with_scdb(cluster, scdb_info):
+    """Good news: A single Cluster object was found for the SCDB record.
 
     Take that item and enhance it with the SCDB content.
     """
-    print '    --> Enhancing document %s with data from SCDB.' % doc.pk
-    doc.federal_cite_one = scdb_info['usCite']
-    doc.federal_cite_two = scdb_info['sctCite']
-    doc.federal_cite_three = scdb_info['ledCite']
-    doc.lexis_cite = scdb_info['lexisCite']
-    doc.docket_number = scdb_info['docket']
-    doc.scdb_id = scdb_info['caseId']
+    print '    --> Enhancing cluster %s with data from SCDB.' % cluster.pk
+    attribute_pairs = [
+        ('federal_cite_one', 'usCite'),
+        ('federal_cite_two', 'sctCite'),
+        ('federal_cite_three', 'ledCite'),
+        ('lexis_cite', 'lexisCite'),
+        ('scdb_id', 'caseId'),
+        ('scdb_votes_majority', 'majVotes'),
+        ('scdb_votes_minority', 'minVotes'),
+        ('scdb_decision_direction', 'decisionDirection'),
+    ]
+    for attr, lookup_key in attribute_pairs:
+        set_if_falsy(cluster, attr, scdb_info[lookup_key])
+
+    set_if_falsy(cluster.docket, 'docket_number', scdb_info['docket'])
 
     if not DEBUG:
-        doc.save()
+        cluster.docket.save()
+        cluster.save()
 
 
-def winnow_by_docket_number(docs, d):
-    """Go through each of the docs and see if they have a matching docket
+def winnow_by_docket_number(clusters, d):
+    """Go through each of the clusters and see if they have a matching docket
     number. Return only those ones that do.
     """
-    good_doc_ids = []
-    for doc in docs:
-        dn = doc.docket.docket_number
+    good_cluster_ids = []
+    for cluster in clusters:
+        dn = cluster.docket.docket_number
         if dn is not None:
             dn = dn.replace(', Original', ' ORIG')
             dn = dn.replace('___, ORIGINAL', 'ORIG')
@@ -82,30 +98,30 @@ def winnow_by_docket_number(docs, d):
             dn = dn.replace(' Misc', ' M')
             dn = dn.replace('NO. ', '')
             if dn == d['docket']:
-                good_doc_ids.append(doc.pk)
+                good_cluster_ids.append(cluster.pk)
 
     # Convert our list of IDs back into a QuerySet for consistency.
-    return Document.objects.filter(pk__in=good_doc_ids)
+    return OpinionCluster.objects.filter(pk__in=good_cluster_ids)
 
 
-def get_human_review(docs, d):
-    for i, doc in enumerate(docs):
-        print '    %s: Document %s:' % (i, doc.pk)
-        print '      https://www.courtlistener.com/opinion/%s/slug/' % doc.pk
-        print '      %s' % doc.case_name
-        print '      %s' % doc.docket.docket_number
+def get_human_review(clusters, d):
+    for i, cluster in enumerate(clusters):
+        print '    %s: Cluster %s:' % (i, cluster.pk)
+        print '      https://www.courtlistener.com/opinion/%s/slug/' % cluster.pk
+        print '      %s' % cluster.case_name
+        print '      %s' % cluster.docket.docket_number
     print '  SCDB info:'
     print '    %s' % d['caseName']
     print '    %s' % d['docket']
     choice = raw_input('  Which item should we update? [0-%s] ' %
-                       (len(docs) - 1))
+                       (len(clusters) - 1))
 
     try:
         choice = int(choice)
-        doc = docs[choice]
+        cluster = clusters[choice]
     except ValueError:
-        doc = None
-    return doc
+        cluster = None
+    return cluster
 
 
 def iterate_scdb_and_take_actions(
@@ -115,16 +131,16 @@ def iterate_scdb_and_take_actions(
         start_row=0):
     """Iterates over the SCDB, looking for a single match for every item. If
     a single match is identified it takes the action in the action_one
-    function using the Document identified and the dict of the SCDB
+    function using the Cluster identified and the dict of the SCDB
     information.
 
     If zero or many results are found it runs the action_zero or action_many
-    functions. The action_many function takes the QuerySet of documents and
+    functions. The action_many function takes the QuerySet of Clusters and
     the dict of SCDB info as parameters and returns the single item in
     the QuerySet that should have action_one performed on it.
 
     The action_zero function takes only the dict of SCDB information, and uses
-    that to construct or identify a Document object that should have action_one
+    that to construct or identify a Cluster object that should have action_one
     performed on it.
 
     If action_zero or action_many return None, no action is taken.
@@ -139,62 +155,60 @@ def iterate_scdb_and_take_actions(
                 continue
             print "Row is: %s. ID is: %s" % (i, d['caseId'])
 
-            # TODO: EmptyQuerySet cannot be instantiated since Django 1.6.
-            docs = EmptyQuerySet()
-            if len(docs) == 0:
-                print "  Checking by caseID...",
-                docs = Document.objects.filter(
-                    scdb_id=d['caseId'])
-                print "%s matches found." % len(docs)
+            clusters = OpinionCluster.objects.none()
+            if len(clusters) == 0:
+                print "  Checking scdb_id for SCDB field 'caseID'...",
+                clusters = OpinionCluster.objects.filter(scdb_id=d['caseId'])
+                print "%s matches found." % clusters.count()
             if d['usCite'].strip():
                 # Only do these lookups if there is in fact a usCite value.
-                if len(docs) == 0:
+                if clusters.count() == 0:
                     # None found by scdb_id. Try by citation number
                     print "  Checking by federal_cite_one..",
-                    docs = Document.objects.filter(
-                        citation__federal_cite_one=d['usCite'])
-                    print "%s matches found." % len(docs)
-                if len(docs) == 0:
+                    clusters = OpinionCluster.objects.filter(
+                        federal_cite_one=d['usCite'])
+                    print "%s matches found." % clusters.count()
+                if clusters.count() == 0:
                     print "  Checking by federal_cite_two...",
-                    docs = Document.objects.filter(
-                        citation__federal_cite_two=d['usCite'])
-                    print "%s matches found." % len(docs)
-                if len(docs) == 0:
+                    clusters = OpinionCluster.objects.filter(
+                        federal_cite_two=d['usCite'])
+                    print "%s matches found." % clusters.count()
+                if clusters.count() == 0:
                     print "  Checking by federal_cite_three...",
-                    docs = Document.objects.filter(
-                        citation__federal_cite_three=d['usCite'])
-                    print "%s matches found." % len(docs)
+                    clusters = OpinionCluster.objects.filter(
+                        federal_cite_three=d['usCite'])
+                    print "%s matches found." % clusters.count()
 
             # At this point, we need to start getting more experimental b/c
             # the easy ways to find items did not work. Items matched here are
             # ones that lack citations.
-            if len(docs) == 0:
+            if clusters.count() == 0:
                 # try by date and then winnow by docket number
                 print "  Checking by date...",
-                docs = Document.objects.filter(
+                clusters = OpinionCluster.objects.filter(
                     date_filed=datetime.strptime(
                         d['dateDecision'], '%m/%d/%Y'
                     ),
                     docket__court_id='scotus',
                 )
-                print "%s matches found." % len(docs)
+                print "%s matches found." % clusters.count()
                 print "    Winnowing by docket number...",
-                docs = winnow_by_docket_number(docs, d)
-                print "%s matches found." % len(docs)
+                clusters = winnow_by_docket_number(clusters, d)
+                print "%s matches found." % clusters.count()
 
             # Searching complete, run actions.
-            if len(docs) == 0:
+            if clusters.count() == 0:
                 print '  No items found.'
-                doc = action_zero(d)
-            elif len(docs) == 1:
+                cluster = action_zero(d)
+            elif clusters.count() == 1:
                 print '  Exactly one match found.'
-                doc = docs[0]
+                cluster = clusters[0]
             else:
-                print '  %s items found:' % len(docs)
-                doc = action_many(docs, d)
+                print '  %s items found:' % clusters.count()
+                cluster = action_many(clusters, d)
 
-            if doc is not None:
-                action_one(doc, d)
+            if cluster is not None:
+                action_one(cluster, d)
             else:
                 print '  OK. No changes will be made.'
 
