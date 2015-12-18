efba8de53658ae4d3bab09ecb4d1906adab3197a
Dave Voutila <voutilad@gmail.com>
2015-12-18 13:31:57 -0500

    test_basic_homepage_search_and_signin passing atm

--- cl/tests/test_functional.py
+++ cl/tests/test_functional.py
@@ -8,20 +8,18 @@ import sys
 
 class BasicUserTest(LiveServerTestCase):
 
+    fixtures = ['test_court.json', 'authtest_data.json',
+        'judge_judy.json', 'test_objects_search.json']
+
     @classmethod
     def setUpClass(cls):
+        cls.screenshot = True
         for arg in sys.argv:
             if 'liveserver' in arg:
                 cls.server_url = 'http://' + arg.split('=')[1]
-            if 'screenshot' in arg:
-                cls.screenshot = True
-            else:
-                cls.screenshot = False
-        try:
-            cls.server_url
-        except AttributeError:
-            super(BasicUserTest, cls).setUpClass()
-            cls.server_url = cls.live_server_url
+                return
+        super(BasicUserTest, cls).setUpClass()
+        cls.server_url = cls.live_server_url
 
     @classmethod
     def tearDownClass(cls):
@@ -33,10 +31,12 @@ class BasicUserTest(LiveServerTestCase):
             executable_path='/usr/local/phantomjs/phantomjs',
             service_log_path='/var/log/courtlistener/django.log',
         )
+        self.browser.implicitly_wait(1)
+        self.browser.set_window_size(1024, 768)
 
     def tearDown(self):
         if self.screenshot:
-            print 'saving screenshot...'
+            print '\nSaving screenshot...'
             self.browser.save_screenshot(type(self).__name__ + '.png')
         self.browser.quit()
 
@@ -86,15 +86,20 @@ class BasicUserTest(LiveServerTestCase):
         self.assertEqual('Sign In', btn.text)
 
         self.browser.find_element_by_id('username').send_keys('pandora')
-        self.browser.find_element_by_id('password').send_keys('password\n')
+        self.browser.find_element_by_id('password').send_keys('password')
+        btn.click()
 
         # upon redirect, she's brought back to her original search results
         # for 'lissner'
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn(
+            'Please enter a correct username and password.',
+            page_text
+        )
         search_box = self.browser.find_element_by_id('id_q')
         self.assertEqual('lissner', search_box.get_attribute('value'))
 
         # She now sees the form for creating an alert
-        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertIn('Create an Alert', page_text)
         self.assertIn('Give the alert a name', page_text)
         self.assertIn('How often should we notify you?', page_text)
@@ -108,14 +113,13 @@ class BasicUserTest(LiveServerTestCase):
         # page, clicks it, and selects Sign out
         profile_dropdown = self.browser.\
             find_element_by_css_selector('a.dropdown-toggle')
-        self.assertEqual(profile_dropdown.text, 'Profile')
+        self.assertEqual(profile_dropdown.text.strip(), u'Profile')
 
         dropdown_menu = self.browser.\
             find_element_by_css_selector('ul.dropdown-menu')
-        self.assertEqual('none', dropdown_menu.get_attribute('display'))
+        self.assertIsNone(dropdown_menu.get_attribute('display'))
 
         profile_dropdown.click()
-        self.assertNotEqual('none', dropdown_menu.get_attribute('display'))
 
         sign_out = self.browser.find_element_by_link_text('Sign out')
         sign_out.click()
@@ -123,8 +127,8 @@ class BasicUserTest(LiveServerTestCase):
         # She receives a sign out confirmation with links back to the homepage,
         # the block, and an option to sign back in.
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertIn('You Have Succesfully Signed Out', page_text)
-        links = self.browser.find_element_by_tag_name('a')
+        self.assertIn('You Have Successfully Signed Out', page_text)
+        links = self.browser.find_elements_by_tag_name('a')
         self.assertIn('Go to the homepage', [link.text for link in links])
         self.assertIn('Read our blog', [link.text for link in links])
 
