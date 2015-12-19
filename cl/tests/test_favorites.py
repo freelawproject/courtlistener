300ec8f717e7886b69bad9e76e7ebefd4a637ce6
Dave Voutila <voutilad@gmail.com>
2015-12-18 23:41:28 -0500

    initial logic for test_anonymous_user_is_prompted_when_favoriting_an_opinion

--- cl/tests/test_favorites.py
+++ cl/tests/test_favorites.py
@@ -12,12 +12,32 @@ class UserFavoritesTest(BaseSeleniumTest):
     def test_anonymous_user_is_prompted_when_favoriting_an_opinion(self):
         # Dora needs to do some research, so she fires up CL and performs
         # an initial query on her subject: Lissner
+        self.browser.get(self.server_url)
+        search_box = self.browser.find_element_by_id('id_q')
+        search_box.send_keys('lissner\n')
 
         # She looks over the results and sees one in particular possibly of
         # interest so she clicks on the title
+        articles = self.browser.find_elements_by_tag_name('article')
+        self.assertTrue(len(articles) > 1, 'Should have more than 1 result')
+
+        title_anchor = articles[1].find_elements_by_tag_name('a')[0]
+        self.assertNotEqual(title_anchor.text.strip(), '')
+        title_anchor.click()
 
         # On the detail page she now sees it might be useful later, so she
         # clicks on the little star next to the result result title
+        self.assertIn(
+            'Back to Search Results',
+            self.browser.find_element_by_tag_name('body').text
+        )
+
+        star = self.browser.find_element_by_id('favorites-star')
+        self.assertEqual(
+            star.get_attribute('title').strip(),
+            'Save this record as a favorite in your profile'
+        )
+        star.click()
 
         # Oops! She's not signed in and she sees a prompt telling her as such
 
