f394151ae0ffe9496eac5ba3d4fb0257eeb807f1
Dave Voutila <voutilad@gmail.com>
2015-12-29 16:24:19 -0500

    added StaticFilesTest to help test and isolate current issues with cl.simple_pages.views.serve_static_file causing functional test failures

--- cl/simple_pages/tests.py
+++ cl/simple_pages/tests.py
@@ -1,5 +1,6 @@
+from django.http import HttpRequest
 from django.test import TestCase
-
+from cl.simple_pages.views import serve_static_file
 
 class ContactTest(TestCase):
     fixtures = ['authtest_data.json']
@@ -23,3 +24,33 @@ class ContactTest(TestCase):
         r = self.client.get('/robots.txt')
         self.assertTrue(r.status_code, 200)
 
+
+class StaticFilesTest(TestCase):
+
+    def test_serve_static_file_serves_mp3(self):
+        request = HttpRequest()
+        file_path = 'test/audio/2.mp3'
+        response = serve_static_file(request, file_path=file_path)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'audio/mpeg')
+        self.assertIn('attachment;', response['Content-Disposition'])
+
+    def test_serve_static_file_serves_txt(self):
+        request = HttpRequest()
+        file_path = 'test/search/opinion_text.txt'
+        response = server_static_file(request, file_path=file_path)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'text')
+        self.assertIn('attachment;', response['Content-Disposition'])
+        self.assertIn(
+            'FOR THE DISTRICT OF COLUMBIA CIRCUIT',
+            response.content
+        )
+
+    def test_serve_static_file_serves_pdf(self):
+        request = HttpRequest()
+        file_path = 'test/search/opinion_pdf_text_based.pdf'
+        response = server_static_file(request, file_path=file_path)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/pdf')
+        self.assertIn('attachment;', response['Content-Disposition'])
