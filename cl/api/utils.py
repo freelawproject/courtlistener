3efcb74e02f890111d83d109124295733ef847b8
Michael Lissner <mlissner@michaeljaylissner.com>
2015-12-30 15:51:37 -0800

    API: Adds special access to the API to people that are configured in the settings file.

--- cl/api/utils.py
+++ cl/api/utils.py
@@ -7,6 +7,7 @@ from django.utils.encoding import force_text
 from rest_framework import serializers
 from rest_framework.metadata import SimpleMetadata
 from rest_framework_filters import RelatedFilter
+from rest_framework.throttling import UserRateThrottle
 
 DATETIME_LOOKUPS = ['exact', 'gte', 'gt', 'lte', 'lt', 'range', 'year',
                     'month', 'day', 'hour', 'minute', 'second']
@@ -137,3 +138,37 @@ class LoggingMixin(object):
         pipe.zincrby('api:v3.endpoint.d:%s.counts' % d, endpoint)
 
         pipe.execute()
+
+
+class ExceptionalUserRateThrottle(UserRateThrottle):
+    def allow_request(self, request, view):
+        """
+        Give special access to a few special accounts.
+
+        Mirrors code in super class with minor tweaks.
+        """
+        if self.rate is None:
+            return True
+
+        self.key = self.get_cache_key(request, view)
+        if self.key is None:
+            return True
+
+        self.history = self.cache.get(self.key, [])
+        self.now = self.timer()
+
+        # Adjust if user has special privileges.
+        override_rate = settings.REST_FRAMEWORK['OVERRIDE_THROTTLE_RATES'].get(
+            request.user.username,
+            None,
+        )
+        if override_rate is not None:
+            self.num_requests, self.duration = self.parse_rate(override_rate)
+
+        # Drop any requests from the history which have now passed the
+        # throttle duration
+        while self.history and self.history[-1] <= self.now - self.duration:
+            self.history.pop()
+        if len(self.history) >= self.num_requests:
+            return self.throttle_failure()
+        return self.throttle_success()
