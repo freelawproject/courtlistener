96454c6bfe7ed1eecdf5a6573da30c398b9e6f22
Dave Voutila <voutilad@gmail.com>
2016-01-09 18:48:44 -0500

    added in oral argument support for showing blocked from search widget

--- cl/tests/test_issue_0412.py
+++ cl/tests/test_issue_0412.py
@@ -14,7 +14,7 @@ class OpinionBlockedFromSearchEnginesTest(BaseSeleniumTest):
     indications of whether Opinions are blocked from Search Engines
     """
     fixtures = ['test_court.json', 'authtest_data.json', 'judge_judy.json',
-                'opinions-issue-412.json']
+                'opinions-issue-412.json', 'audio-issue-412.json']
 
     def test_admin_viewing_blocked_opinion(self):
         """ For a blocked Opinion, an Admin should see indication. """
@@ -72,6 +72,8 @@ class DocketBlockedFromSearchEnginesTest(BaseSeleniumTest):
     Tests for validating UX elements of showing or not showing visual
     indications of whether Dockets are blocked from Search Engines
     """
+    fixtures = ['test_court.json', 'authtest_data.json', 'judge_judy.json',
+                'opinions-issue-412.json', 'audio-issue-412.json']
 
     def test_admin_viewing_blocked_docket(self):
         """ For a blocked Dockets, an Admin should see indication. """
@@ -91,15 +93,68 @@ class AudioBlockedFromSearchEnginesTest(BaseSeleniumTest):
     Tests for validating UX elements of showing or not showing visual
     indications of whether Audio pages are blocked from Search Engines
     """
+    fixtures = ['test_court.json', 'authtest_data.json', 'judge_judy.json',
+                'opinions-issue-412.json', 'audio-issue-412.json']
 
     def test_admin_viewing_blocked_audio_page(self):
         """ For a blocked Audio pages, an Admin should see indication. """
-        self.fail('Finish test_admin_viewing_blocked_opinion')
+        # Admin logs into CL using her admin account
+        self.browser.get(self.server_url)
+        self.attempt_sign_in('admin', 'password')
+
+        # She selects Oral Arguments to toggle the results to audio
+        self.browser \
+            .find_element_by_css_selector('label[for="id_type_1"]') \
+            .click()
+
+        # The SERP updates and she selects the one she knows is blocked
+        blocked_argument = self.browser.find_element_by_link_text(
+            'Blocked Oral Argument (Test 2015)'
+        )
+        blocked_argument.click()
+
+        # She notices a widget letting her know it's blocked by search engines
+        sidebar = self.browser.find_element_by_id('sidebar')
+        self.assertIn('Blocked from Search', sidebar.text)
 
     def test_non_admin_viewing_blocked_audio_page(self):
         """ For a blocked Audio pages, a Non-admin should see NO indication. """
-        self.fail('Finish test_non_admin_viewing_blocked_opinion')
+        # Pandora logs into CL using her admin account
+        self.browser.get(self.server_url)
+        self.attempt_sign_in('pandora', 'password')
+
+        # She selects Oral Arguments to toggle the results to audio
+        self.browser \
+            .find_element_by_css_selector('label[for="id_type_1"]') \
+            .click()
+
+        # The SERP updates and she selects the one she knows is blocked
+        blocked_argument = self.browser.find_element_by_link_text(
+            'Blocked Oral Argument (Test 2015)'
+        )
+        blocked_argument.click()
+
+        # She notices a widget letting her know it's blocked by search engines
+        sidebar = self.browser.find_element_by_id('sidebar')
+        self.assertNotIn('Blocked from Search', sidebar.text)
 
     def test_admin_viewing_not_blocked_audio_page(self):
         """ For a non-blocked Audio pages, there should be no indication """
-        self.fail('test_admin_viewing_not_blocked_opinion')
+        # Admin logs into CL using her admin account
+        self.browser.get(self.server_url)
+        self.attempt_sign_in('admin', 'password')
+
+        # She selects Oral Arguments to toggle the results to audio
+        self.browser \
+            .find_element_by_css_selector('label[for="id_type_1"]') \
+            .click()
+            
+        # The SERP updates and she selects the one she knows is blocked
+        blocked_argument = self.browser.find_element_by_link_text(
+            'Not Blocked Oral Argument (Test 2015)'
+        )
+        blocked_argument.click()
+
+        # She notices a widget letting her know it's blocked by search engines
+        sidebar = self.browser.find_element_by_id('sidebar')
+        self.assertNotIn('Blocked from Search', sidebar.text)
