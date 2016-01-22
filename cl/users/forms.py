00d3b99bb016bdd4d33233c6e80674c88797def6
Michael Lissner <mlissner@michaeljaylissner.com>
2016-01-22 15:03:57 -0800

    Fixes issue #368 by adding lots and lots of autocomplete fields.
    These fields make the browser better able to understand what the field is and what kind of data should be auto-filled into it.

--- cl/users/forms.py
+++ cl/users/forms.py
@@ -7,6 +7,10 @@ from django.forms import ModelForm
 from localflavor.us.forms import USStateField, USZipCodeField
 from localflavor.us.us_states import STATE_CHOICES
 
+# Many forms in here use unusual autocomplete attributes. These conform with
+# https://html.spec.whatwg.org/multipage/forms.html#autofill, and enables them
+# to be autofilled in various ways.
+
 
 class ProfileForm(ModelForm):
     STATE_CHOICES = list(STATE_CHOICES)
@@ -14,12 +18,18 @@ class ProfileForm(ModelForm):
     state = USStateField(
         widget=forms.Select(
             choices=STATE_CHOICES,
-            attrs={'class': 'form-control'}
+            attrs={
+                'class': 'form-control',
+                'autocomplete': 'address-level1',
+            },
         ),
         required=False,
     )
     zip_code = USZipCodeField(
-        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'autocomplete': 'postal-code',
+        }),
         required=False,
     )
 
@@ -37,13 +47,25 @@ class ProfileForm(ModelForm):
             'plaintext_preferred',
         )
         widgets = {
-            'employer': forms.TextInput(attrs={'class': 'form-control'}),
+            'employer': forms.TextInput(attrs={
+                'class': 'form-control',
+                'autocomplete': 'organization',
+            }),
             'barmembership': forms.SelectMultiple(
                 attrs={'size': '8', 'class': 'form-control'}
             ),
-            'address1': forms.TextInput(attrs={'class': 'form-control'}),
-            'address2': forms.TextInput(attrs={'class': 'form-control'}),
-            'city': forms.TextInput(attrs={'class': 'form-control'}),
+            'address1': forms.TextInput(attrs={
+                'class': 'form-control',
+                'autocomplete': 'address-line1',
+            }),
+            'address2': forms.TextInput(attrs={
+                'class': 'form-control',
+                'autocomplete': 'address-line2',
+            }),
+            'city': forms.TextInput(attrs={
+                'class': 'form-control',
+                'autocomplete': 'address-level2',
+            }),
     #        'wants_newsletter': forms.TextInput(attrs={'class': 'form-control'}),
     #        'plaintext_preferred': forms.TextInput(attrs={'class': 'form-control'}),
         }
@@ -52,7 +74,10 @@ class ProfileForm(ModelForm):
 class UserForm(ModelForm):
     email = forms.EmailField(
         required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'autocomplete': 'email',
+        })
     )
 
     class Meta:
@@ -63,8 +88,14 @@ class UserForm(ModelForm):
             'email',
         )
         widgets = {
-            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'autocomplete': 'given-name',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'autocomplete': 'family-name',
+            }),
         }
 
 
@@ -88,9 +119,26 @@ class UserCreationFormExtended(UserCreationForm):
         for field in self.fields.values():
             field.widget.attrs.update({'class': 'form-control'})
 
+        self.fields['username'].widget.attrs.update({
+            'class': 'auto-focus form-control',
+            'autocomplete': 'username',
+        })
         self.fields['email'].required = True
-        self.fields['username'].widget.attrs.update(
-            {'class': 'auto-focus form-control'})
+        self.fields['email'].widget.attrs.update({
+            'autocomplete': 'email',
+        })
+        self.fields['password1'].widget.attrs.update({
+            'autocomplete': 'new-password',
+        })
+        self.fields['password2'].widget.attrs.update({
+            'autocomplete': 'new-password',
+        })
+        self.fields['first_name'].widget.attrs.update({
+            'autocomplete': 'given-name',
+        })
+        self.fields['last_name'].widget.attrs.update({
+            'autocomplete': 'family-name',
+        })
 
     class Meta:
         model = User
@@ -103,26 +151,42 @@ class UserCreationFormExtended(UserCreationForm):
 
 
 class EmailConfirmationForm(forms.Form):
-    email = forms.EmailField()
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control auto-focus input-lg',
+            'placeholder': "Your Email Address",
+            'autocomplete': 'email',
+        }),
+        required=True,
+    )
 
 
 class CustomPasswordChangeForm(PasswordChangeForm):
     """
     A form that lets a user change his/her password by entering
     their old password. Overrides Django default form to allow
-    the customization of class attributes.
+    the customization of attributes.
     """
     old_password = forms.CharField(
         label="Old password",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'autocomplete': 'current-password',
+        }),
     )
     new_password1 = forms.CharField(
         label="New password",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'autocomplete': 'new-password',
+        }),
     )
     new_password2 = forms.CharField(
         label="New password confirmation",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'autocomplete': 'new-password',
+        }),
     )
 
 
@@ -137,7 +201,7 @@ class CustomPasswordResetForm(PasswordResetForm):
             {
                 'class': 'auto-focus form-control input-lg',
                 'placeholder': 'Your Email Address',
-                'autocomplete': 'off',
+                'autocomplete': 'email',
             }
         )
 
@@ -149,13 +213,11 @@ class CustomSetPasswordForm(SetPasswordForm):
     def __init__(self, user, *args, **kwargs):
         super(CustomSetPasswordForm, self).__init__(user, *args, **kwargs)
 
-        self.fields['new_password1'].widget.attrs.update(
-            {
-                'class': 'auto-focus form-control',
-            }
-        )
-        self.fields['new_password2'].widget.attrs.update(
-            {
-                'class': 'form-control',
-            }
-        )
+        self.fields['new_password1'].widget.attrs.update({
+            'class': 'auto-focus form-control',
+            'autocomplete': 'new-password',
+        })
+        self.fields['new_password2'].widget.attrs.update({
+            'class': 'form-control',
+            'autocomplete': 'new-password',
+        })
