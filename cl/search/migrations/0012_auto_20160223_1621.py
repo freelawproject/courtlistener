ed8c45cd2c75461555f9203d9a1db5c3df0fd376
Elliott Ash <elliott.ash@columbia.edu>
2016-02-23 19:22:03 -0500

    Changes judges.Judge to people_db.Person in search.models. Migrates successfully. 	new file:   cl/search/migrations/0012_auto_20160223_1621.py 	modified:   cl/search/models.py

---
+++ cl/search/migrations/0012_auto_20160223_1621.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('search', '0011_auto_20151222_1240'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='opinion',
+            name='author',
+            field=models.ForeignKey(related_name='opinions_written', blank=True, to='people_db.Person', help_text=b'The primary author of this opinion', null=True),
+        ),
+        migrations.AlterField(
+            model_name='opinion',
+            name='joined_by',
+            field=models.ManyToManyField(help_text=b'Other judges that joined the primary author in this opinion', related_name='opinions_joined', to='people_db.Person', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='opinioncluster',
+            name='non_participating_judges',
+            field=models.ManyToManyField(help_text=b'The judges that heard the case, but did not participate in the opinion', related_name='opinion_clusters_non_participating_judges', to='people_db.Person', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='opinioncluster',
+            name='panel',
+            field=models.ManyToManyField(help_text=b'The judges that heard the oral arguments', related_name='opinion_clusters_participating_judges', to='people_db.Person', blank=True),
+        ),
+    ]
