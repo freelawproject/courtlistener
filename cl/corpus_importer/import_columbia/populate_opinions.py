d474e73631234293131d9c950d191960725e6d02
Jeff Gortmaker <j.gortmaker@columbia.edu>
2016-02-25 17:55:21 -0500

    opinion population so far
    haven't tested on a working server yet (getting connection problems)

--- cl/corpus_importer/import_columbia/populate_opinions.py
+++ cl/corpus_importer/import_columbia/populate_opinions.py
@@ -2,7 +2,9 @@
 
 from django.utils.timezone import now
 
-from cl.search.models import Docket, Opinion, OpinionCluster
+from cl.search.models import Docket, Opinion, OpinionCluster, Court
+from cl.judges.models import Judge
+from cl.citations.find_citations import  get_citations
 from cl.lib.import_lib import map_citations_to_models
 
 
@@ -19,12 +21,16 @@ OPINION_TYPE_MAPPING = {
 }
 
 
-def make_objects(item):
-    """Takes the case data <item> and associates it with objects.
+def find_judge(name, court_id, case_date):
+    """Identifies a judge by name and metadata."""
+    pass
+    # TODO: actually find a judge once the database has been populated with them
+    # judges = Judge.objects.filter(name_last=name)
+    # court = Court.objects.get(pk=court_id)
 
-    Returns a (Docket, OpinionCluster, Opinion, Opinion, ...) list.
-    The objects' save method should be called in the same order as they are returned.
-    """
+
+def make_and_save(item):
+    """Takes the case data <item> and associates it with objects. Saves these objects."""
     if not item:
         return []
     # find relevent dates
@@ -48,7 +54,7 @@ def make_objects(item):
             else:                
                 opinion_date = date_info[1]
 
-    # instantiate docket object
+    # saves docket object
     docket = Docket(
         date_created=now(),
         date_modified=now(),
@@ -61,46 +67,44 @@ def make_objects(item):
         case_name_full=item.get('case_name_full'),
         docket_number=item.get('docket')
     )
+    docket.save()
 
     # grab citations in, e.g. the {'federal_cite_one': '1 U.S. 1', ...} form
     # will be passed as kwargs
-    all_citations = map_citations_to_models(item['citation'])
+    all_citations = map_citations_to_models([get_citations(c)[0] for c in item['citations']])
 
-    # instantiate opinion cluster object
+    # saves opinion cluster object
     cluster = OpinionCluster(
-        precedential_status=('Unpublished' if item['unpublished'] else 'Published'),
         docket=docket,
+        precedential_status=('Unpublished' if item['unpublished'] else 'Published'),
         date_created=now(),
         date_modified=now(),
         date_filed=opinion_date, 
-        panel=item.get('panel', []),
+        # panel=item.get('panel', []),
         case_name_short=item.get('case_name_short'),
         case_name=item.get('case_name'),
         case_name_full=item.get('case_name_full'),
-        source='Z (columbia archive)',
+        source='Z',
         attorneys=item.get('attorneys'),
         posture=item.get('posture'),
         **all_citations
     )
+    cluster.save()
 
-    # create opinion, concurrence, and dissent objects
-    opinions = []
+    # save opinion objects
     for opinion_info in item['opinions']:
-        opinions.append(Opinion(
+        # author = find_judge(
+        #     opinion_info['byline'],
+        #     item.get('court_id'),
+        #     opinion_date or reargued_date
+        # )
+        opinion = Opinion(
             cluster=cluster,
-            author=opinion_info['byline'],
+            # author=author,
+            # joined_by=
             date_created=now(),
             date_modified=now(),
             type=OPINION_TYPE_MAPPING[opinion_info['type']],
             html_columbia=opinion_info['opinion']
-        ))
-        
-    return [docket, cluster] + opinions
-
-
-if __name__ == '__main__':
-    import parse_opinions
-    for parsed in parse_opinions.parse_many("../data/", limit=100, court_fallback_regex=r"data/([a-z_]+?/[a-z_]+?)/"):
-        case_objects = make_objects(parsed)
-        # for object in case_objects:
-        #     object.save()
+        )
+        opinion.save()
