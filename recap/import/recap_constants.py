955032f3b67276e48217d9f677089283dcec603a
ishammi <isham@mangaloreinfotech.in>
2016-02-27 14:23:58 +0530

    Setting a limit of 100000 lines for each CSV file.

--- recap/import/recap_constants.py
+++ recap/import/recap_constants.py
@@ -17,7 +17,7 @@ STATUS_TASK_ADDED  = 'TASK ADDED'
 CSV_FILEPATH = "./recap_db_references/"
 REFERENCE_FILEPATH = CSV_FILEPATH + "recap_csv_file_reference.csv"
 REFERENCE_RESULT_FILEPATH = CSV_FILEPATH + "recap_csv_file_reference_results.csv"
-MAX_NUMBER_OF_XML_PER_TASK = 3
+MAX_NUMBER_OF_XML_PER_TASK = 100000
 
 IA_XML_DOCKET_PATH_FORMAT_STRING = "https://archive.org/download/%s/%s.docket.xml"
 XML_DOWNLOAD_FOLDER_PATH = "./recap_downloads/"
