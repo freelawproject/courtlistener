b72ffd71285a93691d7932fb85aec9abebda6f20
Michael Lissner <mlissner@michaeljaylissner.com>
2016-03-02 14:57:57 -0800

    Updates the downloading code to make it a management command.
    Also:
     - It now uses a CSV as its seed.
     - The downloader is a celery task placed in the conventional location.
     - Errors from the celery task should be caught and logged.
     - A new setting, BASE_DIR, is added. Apparently this is part of default Django projects now.
     - Pandas is added as a dependency so that folks can use it if desired.

--- cl/settings.py
+++ cl/settings.py
@@ -13,3 +13,5 @@ conf_files = glob.glob(os.path.join(os.path.dirname(__file__), 'settings', '*.py
 conf_files.sort()
 for f in conf_files:
     execfile(os.path.abspath(f))
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
