66ec7e871a465d81d51c83c295d6ea63b7e3768a
Michael Lissner <mlissner@michaeljaylissner.com>
2016-03-14 16:14:21 -0700

    Whitespace tweaks

--- cl/people_db/api_serializers.py
+++ cl/people_db/api_serializers.py
@@ -18,6 +18,7 @@ class EducationSerializer(DynamicFieldsModelSerializer,
     class Meta:
         model = Education
 
+
 class PoliticalAffiliationSerializer(DynamicFieldsModelSerializer,
                                      serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -37,14 +38,14 @@ class ABARatingSerializer(DynamicFieldsModelSerializer,
 
 
 class PersonSerializer(DynamicFieldsModelSerializer,
-                      serializers.HyperlinkedModelSerializer):
+                       serializers.HyperlinkedModelSerializer):
     race = serializers.StringRelatedField(many=True)
     positions = serializers.HyperlinkedRelatedField(
         many=True,
         view_name='position-detail',
         read_only=True,
     )
-    educations = EducationSerializer(many=True, read_only=True)    
+    educations = EducationSerializer(many=True, read_only=True)
     political_affiliations = PoliticalAffiliationSerializer(many=True,
                                                             read_only=True)
     sources = SourceSerializer(many=True, read_only=True)
@@ -52,6 +53,7 @@ class PersonSerializer(DynamicFieldsModelSerializer,
 
     class Meta:
         model = Person
+
 
 class RetentionEventSerializer(DynamicFieldsModelSerializer,
                                serializers.HyperlinkedModelSerializer):
@@ -63,7 +65,7 @@ class PositionSerializer(DynamicFieldsModelSerializer,
                          serializers.HyperlinkedModelSerializer):
     appointer = PersonSerializer(many=False, read_only=True)
     retention_events = RetentionEventSerializer(many=True, read_only=True)
-    # TODO: add clerks 
+    # TODO: add clerks
 
     class Meta:
         model = Position
