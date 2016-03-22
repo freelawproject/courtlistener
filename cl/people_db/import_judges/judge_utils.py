2958bc0608ce6a735279f29560a1c7084e6443ab
Michael Lissner <mlissner@michaeljaylissner.com>
2016-03-21 17:53:04 -0700

    Converts judge import into management command and does some work to make it all compile neatly.
    An example usage would be:
    
        manage.py cl_import_judges --action import-fjc-judges --input_file cl/people_db/fjc-data.xlsx --debug

--- cl/people_db/import_judges/judge_utils.py
+++ cl/people_db/import_judges/judge_utils.py
@@ -26,23 +26,23 @@ def process_date(year,month,day):
     return pdate, granularity
 
 def process_date_string(date_input):
-    
+
     if pd.isnull(date_input):
         return None
-    
+
     date_object = datetime.strptime(date_input, '%m/%d/%Y')
-    
+
     return date_object
 
 from collections import Counter
 C = Counter() # for fixing school names.
-    
+
 def get_school(schoolname, testing=False):
-    "Takes the name of a school from judges data and tries to match to a unique School object."        
-    
+    "Takes the name of a school from judges data and tries to match to a unique School object."
+
     if schoolname.isspace():
         return None
-        
+
     schools = School.objects.filter(name__iexact=schoolname)
     if len(schools) == 1:
         school = schools[0]
@@ -50,10 +50,10 @@ def get_school(schoolname, testing=False):
             return school.is_alias_of
         else:
             return school
-            
+
     #print('No exact matches: ' + schoolname + '. Running "contains".')
-    
-    schools = School.objects.filter(name__icontains=schoolname)  
+
+    schools = School.objects.filter(name__icontains=schoolname)
     if len(schools) > 1:
         schools = [x for x in schools if not x.is_alias_of]
     if len(schools) == 1:
@@ -79,12 +79,12 @@ def get_school(schoolname, testing=False):
         if f not in filterwords:
             normname = normname + ' ' + f
     normname = normname.strip()
-    
+
     if normname.isspace():
         print('Fully normed:',schoolname)
         return None
-    
-    schools = School.objects.filter(name__icontains=normname)  
+
+    schools = School.objects.filter(name__icontains=normname)
     if len(schools) == 1:
         school = schools[0]
         if school.is_alias_of is not None:
@@ -97,7 +97,7 @@ def get_school(schoolname, testing=False):
         #print('Multiple normalized matches:',schoolname,[x.name for x in schools])
         C[schoolname+ ',' + ','.join([x.name for x in schools])] += 1
         return None
-    #print('No matches:',schoolname,normname)    
+    #print('No matches:',schoolname,normname)
     C[schoolname+',no-matches'] += 1
     return None
 
@@ -109,27 +109,27 @@ def get_degree_level(degstr):
                'aa': ['aa','as', 'aas'],
                'ma': ['ma','ms', 'msc','am', 'mst','mfa','mph','msw','mia','mpa','msed'
                        'mbe','mssp','mcit','mes','mse','mcp','mpa',
-                       'mpp','mdiv', 'mls'],               
+                       'mpp','mdiv', 'mls'],
                'llb': ['llb','bsl','bl'],
-               'jd': ['jd'], 
+               'jd': ['jd'],
                'llm': ['llm','ml', 'mjs', 'mj'],
                'jsd': ['jsd','sjd'],
-               'phd': ['phd','edd','ded','dma','dphil'],     
+               'phd': ['phd','edd','ded','dma','dphil'],
                'md': ['md','dmd','rn'],
                'mba': ['mba'],
               }
     deg = re.sub(r"[^a-z]+", '', degstr.lower())
     for k in degdict:
         if deg in degdict[k]:
-            return k    
-    
+            return k
+
     if deg.startswith('b'):
         return 'ba'
     if deg.startswith('m'):
         return 'ma'
-    print(degstr+' not in degdict.')    
+    print(degstr+' not in degdict.')
     return ''
-               
+
 def get_party(partystr):
     partydict =  dict([(v,k) for (k,v) in [('d', 'Democrat'),
         ('r', 'Republican'),
@@ -139,7 +139,7 @@ def get_party(partystr):
         ('f', 'Federalist'),
         ('w', 'Whig'),
         ('j', 'Jeffersonian Republican')]])
-    return partydict[partystr]   
+    return partydict[partystr]
 
 def get_appointer(appointstr):
     return appointstr
@@ -154,8 +154,8 @@ def get_suffix(suffstr):
     if pd.isnull(suffstr):
         return ''
     else:
-        return suffdict[suffstr]    
-              
+        return suffdict[suffstr]
+
 def get_races(str_race):
     racedict =  {'White': 'w',
              'Black': 'b',
@@ -163,24 +163,24 @@ def get_races(str_race):
              'African Am.': 'b',
              'American Indian': 'i',
              'Alaska Native': 'i',
-             'Asian': 'a',            
+             'Asian': 'a',
              'Asian American': 'a',
              'Asian Am.': 'a',
              'Native Hawaiian':'p',
-             'Pacific Islander': 'p',            
-             'Pacific Isl.': 'p',  
+             'Pacific Islander': 'p',
+             'Pacific Isl.': 'p',
              'Pac. Isl.': 'p',
              'Hispanic': 'h',
-             'Latino': 'h'}  
+             'Latino': 'h'}
     if '/' in str_race:
         rawraces = [x.strip() for x in str_race.split('/')]
     else:
         rawraces = [str_race]
     races = []
-    for rawrace in rawraces: 
+    for rawrace in rawraces:
         races.append(racedict[rawrace])
     return races
-        
+
 def get_aba(abastr):
     abadict =  dict([(v,k) for (k,v) in [('ewq', 'Exceptionally Well Qualified'),
         ('wq', 'Well Qualified'),
@@ -191,9 +191,7 @@ def get_aba(abastr):
         return None
     aba = abadict[abastr]
     return aba
-    
 
-select_data = pd.read_excel('/vagrant/flp/columbia_data/judges/stateyeardata.xlsx',0)
 
 def get_select(state,year):
     select_dict = {'P': 'e_part',
@@ -203,6 +201,6 @@ def get_select(state,year):
                'M': 'a_gov'
               }
     return 'P'
- 
-    
- 
+
+
+
--- cl/people_db/import_judges/populate_state_judges.py
+++ cl/people_db/import_judges/populate_state_judges.py
@@ -1,74 +1,83 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
 
 from datetime import date
 
 from cl.corpus_importer.import_columbia.parse_opinions import get_court_object
-from cl.people_db.models import Person, Position, Education, Race, PoliticalAffiliation, Source, ABARating
-from cl.people_db.import_judges.judge_utils import get_school, process_date, get_select, get_races, get_party, get_appointer, get_suffix
+from cl.people_db.models import Person, Position, Education, Race, \
+    PoliticalAffiliation, Source, ABARating
+from cl.people_db.import_judges.judge_utils import get_school, process_date, \
+    get_races, get_party, get_appointer, get_suffix
+
 
 def make_state_judge(item, testing=False):
     """Takes the state judge data <item> and associates it with a Judge object.
-    
+
     Saves the judge to the DB.
     """
 
-    date_dob, date_granularity_dob = process_date(item['birthyear'], 
-                                                  item['birthmonth'], 
-                                                  item['birthday']) 
-    date_dod, date_granularity_dod = process_date(item['deathyear'], 
-                                                  item['deathmonth'], 
-                                                  item['deathday'])  
-    
+    date_dob, date_granularity_dob = process_date(item['birthyear'],
+                                                  item['birthmonth'],
+                                                  item['birthday'])
+    date_dod, date_granularity_dod = process_date(item['deathyear'],
+                                                  item['deathmonth'],
+                                                  item['deathday'])
+
     if item['firstname'] == '':
         return
-    
-    check = Person.objects.filter(name_first=item['firstname'], name_last=item['lastname'], date_dob=date_dob)
+
+    check = Person.objects.filter(name_first=item['firstname'],
+                                  name_last=item['lastname'], date_dob=date_dob)
     if len(check) > 0:
-        print('Warning: ' + item['firstname'] + ' ' + item['lastname'] + ' ' + str(date_dob) + ' exists.') 
-        person = check[0]          
+        print(
+            'Warning: ' + item['firstname'] + ' ' + item[
+                'lastname'] + ' ' + str(
+                    date_dob) + ' exists.')
+        person = check[0]
     else:
-        
+
         person = Person(
-            cl_id = item['cl_id'],
-            name_first = item['firstname'],
-            name_middle = item['midname'],
-            name_last = item['lastname'],
-            name_suffix = get_suffix(item['suffname']),
-            gender = item['gender'],        
-            date_dob = date_dob,
-            date_granularity_dob = date_granularity_dob,
-            date_dod = date_dod,
-            date_granularity_dod = date_granularity_dod                         
+                cl_id=item['cl_id'],
+                name_first=item['firstname'],
+                name_middle=item['midname'],
+                name_last=item['lastname'],
+                name_suffix=get_suffix(item['suffname']),
+                gender=item['gender'],
+                date_dob=date_dob,
+                date_granularity_dob=date_granularity_dob,
+                date_dod=date_dod,
+                date_granularity_dod=date_granularity_dod
         )
-        
+
         if not testing:
             person.save()
-        
+
     courtid = get_court_object(item['court'] + ' of ' + item['state'])
-    
+
     if courtid is None:
         raise
-  
+
     # assign start date
-    date_start, date_granularity_start = process_date(item['startyear'], 
-                                                      item['startmonth'], 
+    date_start, date_granularity_start = process_date(item['startyear'],
+                                                      item['startmonth'],
                                                       item['startday'])
-    date_termination, date_granularity_termination = process_date(item['endyear'], 
-                                                                  item['endmonth'], 
-                                                                  item['endday'])    
-    
+    date_termination, date_granularity_termination = process_date(
+            item['endyear'],
+            item['endmonth'],
+            item['endday'])
+
     judgeship = Position(
-        person = person,
-        court_id = courtid,
-        position_type = 'jud',
-        date_start = date_start,
-        date_granularity_start = date_granularity_start,
-        date_termination = date_termination,
-        date_granularity_termination = date_granularity_termination,        
-        #how_selected = get_select(courtid,item['startyear']),
-        termination_reason = item['howended']
+            person=person,
+            court_id=courtid,
+            position_type='jud',
+            date_start=date_start,
+            date_granularity_start=date_granularity_start,
+            date_termination=date_termination,
+            date_granularity_termination=date_granularity_termination,
+            # how_selected = get_select(courtid,item['startyear']),
+            termination_reason=item['howended']
     )
-    
+
     if not testing:
         judgeship.save()
 
@@ -81,36 +90,36 @@ def make_state_judge(item, testing=False):
             school = get_school(coll)
             if school is not None:
                 college = Education(
-                    person = person,       
-                    school = school,
-                    degree_level = 'ba',
-                )   
-                if not testing:    
+                        person=person,
+                        school=school,
+                        degree_level='ba',
+                )
+                if not testing:
                     college.save()
-    
+
     if not pd.isnull(item['lawschool']):
         if ';' in item['lawschool']:
             lschools = [x.strip() for x in item['lawschool'].split(';')]
         else:
             lschools = [item['lawschool'].strip()]
-        
+
         for L in lschools:
-            lschool = get_school(L)    
+            lschool = get_school(L)
             if lschool is not None:
                 lawschool = Education(
-                    person = person,
-                    school = lschool,
-                    degree_level = 'jd',
-                    )
-                if not testing:    
+                        person=person,
+                        school=lschool,
+                        degree_level='jd',
+                )
+                if not testing:
                     lawschool.save()
-        
+
     # iterate through job variables and add to career if applicable
-    for jobvar in ['prevjudge','prevprivate','prevpolitician','prevprof',
+    for jobvar in ['prevjudge', 'prevprivate', 'prevpolitician', 'prevprof',
                    'postjudge', 'postprivate', 'postpolitician', 'postprof']:
         if pd.isnull(item[jobvar]) or item[jobvar] == 0:
             continue
-        position_type = None                     
+        position_type = None
         if 'judge' in jobvar:
             position_type = 'jud'
         elif 'private' in jobvar:
@@ -118,10 +127,10 @@ def make_state_judge(item, testing=False):
         elif 'politician' in jobvar:
             position_type = 'legis'
         elif 'prof' in jobvar:
-            position_type = 'prof'            
-            
+            position_type = 'prof'
+
         job_start = None
-        job_end = None          
+        job_end = None
         if 'prev' in jobvar:
             job_start = date_start.year - 1
             job_end = date_start.year - 1
@@ -129,27 +138,27 @@ def make_state_judge(item, testing=False):
             if date_termination is None:
                 continue
             job_start = date_termination.year + 1
-            job_end = date_termination.year + 1    
-            
+            job_end = date_termination.year + 1
+
         job = Position(
-            person = person,
-            position_type = position_type,
-            date_start = date(job_start, 1, 1),
-            date_granularity_start = '%Y',
-            date_termination = date(job_end,1,1),
-            date_granularity_termination = '%Y'
-        )       
-        if not testing:   
+                person=person,
+                position_type=position_type,
+                date_start=date(job_start, 1, 1),
+                date_granularity_start='%Y',
+                date_termination=date(job_end, 1, 1),
+                date_granularity_termination='%Y'
+        )
+        if not testing:
             job.save()
-    
+
     if not pd.isnull(item['politics']):
         politics = PoliticalAffiliation(
-            person = person,
-            political_party = item['politics']
-        )  
-        if not testing: 
+                person=person,
+                political_party=item['politics']
+        )
+        if not testing:
             politics.save()
-    
+
     if not pd.isnull(item['links']):
         links = item['links']
         if ';' in links:
@@ -158,24 +167,9 @@ def make_state_judge(item, testing=False):
             urls = [links]
         for url in urls:
             source = Source(
-                person = person,
-                notes = item['notes'],
-                url = url
+                    person=person,
+                    notes=item['notes'],
+                    url=url
             )
-            if not testing: 
-                source.save()                 
-
-#if __name__ == '__main__':
-import pandas as pd
-import numpy as np
-textfields = ['firstname','midname','lastname','gender',
-           'howended']
-df = pd.read_excel('/vagrant/flp/columbia_data/judges/supreme-court-judgebios-2016-02-27.xlsx', 0)    
-for x in textfields:
-    df[x] = df[x].replace(np.nan,'',regex=True)
-for i, row in df.iterrows():   
-    make_state_judge(dict(row), testing=False)
-
-#df = pd.read_excel('/vagrant/flp/columbia_data/judges/iac-judgebios-2016-01-19.xlsx', 0)   
-#for i, row in df.iterrows():    
-#    make_state_judge(dict(row), testing=True)
+            if not testing:
+                source.save()
