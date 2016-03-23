7ff01b8c2facd6d0ace30b7924c610bdff613629
Michael Lissner <mlissner@michaeljaylissner.com>
2016-03-23 10:34:49 -0700

    Makes viz crawler not send an error when it can't find a page.

--- cl/visualizations/tasks.py
+++ cl/visualizations/tasks.py
@@ -43,9 +43,11 @@ def get_title(self, referer_id):
 
     html_tree = html.fromstring(r.text)
     try:
-        title = getattr(html_tree.xpath('//title')[0], 'text', '').strip()
-    except IndexError as exc:
-        raise self.retry(exc=exc, countdown=countdown)
+        title = getattr(html_tree.xpath('//title')[0], 'text', '')
+        if title is not None:
+            title = title.strip()
+    except IndexError:
+        title = ''
 
     if title:
         referer.page_title = trunc(
@@ -66,4 +68,7 @@ def get_title(self, referer_id):
             # Create an exception to catch.
             raise Exception("Couldn't get title from HTML")
         except Exception as exc:
+            if self.request.retries >= self.max_retries:
+                # We couldn't get the title. Let it go.
+                return
             raise self.retry(exc=exc, countdown=countdown)
