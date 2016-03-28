50395b8b163db3197a464973bfa0935c3ebac918
Michael Lissner <mlissner@michaeljaylissner.com>
2016-03-28 15:42:10 -0700

    New version processes 50 at a time.

--- cl/corpus_importer/management/commands/cl_download_recap.py
+++ cl/corpus_importer/management/commands/cl_download_recap.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import pandas as pd
+from celery.task import TaskSet
 from django.core.management import BaseCommand
 from django.conf import settings
 
@@ -34,30 +35,48 @@ def load_csv():
 
     The resulting file is tab separated, and pandas will handle that just fine.
     """
-    return pd.read_csv(CSV_PATH, delimiter='\t', dtype={
+    with open(CSV_PATH, 'r') as f:
+        line_count = sum(1 for line in f) - 1
+    data = pd.read_csv(CSV_PATH, delimiter='\t', dtype={
         'casenum': object,
         'docnum': object,
         'court': object,
     })
+    return data, line_count
 
 
-def make_download_tasks(data):
+def make_download_tasks(data, line_count):
     """For every item in the CSV, send it to Celery for processing"""
     previous_casenum = None
+    subtasks = []
+    completed = 0
     for index, item in data.iterrows():
+        last_item = (line_count == completed + 1)
         if item['casenum'] != previous_casenum:
             # New case, get the docket before getting the pdf
             logger.info("New docket found with casenum: %s" % item['casenum'])
             previous_casenum = item['casenum']
             filename = get_docket_filename(item['court'], item['casenum'])
             url = get_docketxml_url(item['court'], item['casenum'])
-            download_recap_item.delay(url, filename)
+            subtasks.append(download_recap_item.subtask((url, filename)))
 
         # Get the document
         filename = get_document_filename(item['court'], item['casenum'],
                                          item['docnum'], item['subdocnum'])
         url = get_pdf_url(item['court'], item['casenum'], filename)
-        download_recap_item.delay(url, filename)
+        subtasks.append(download_recap_item.subtask((url, filename)))
+
+        # Every fifty items send the subtasks to Celery.
+        if (len(subtasks) >= 50) or last_item:
+            msg = ("Sent %s subtasks to celery. We have processed %s "
+                   "rows so far." % (len(subtasks), completed))
+            logger.info(msg)
+            print msg
+            job = TaskSet(tasks=subtasks)
+            job.apply_async().join()
+            subtasks = []
+
+        completed += 1
 
 
 class Command(BaseCommand):
@@ -65,5 +84,5 @@ class Command(BaseCommand):
             'Output is sent to the log.')
 
     def handle(self, *args, **kwargs):
-        data = load_csv()
-        make_download_tasks(data)
+        data, line_count = load_csv()
+        make_download_tasks(data, line_count)
