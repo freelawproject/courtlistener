b4c8bfdccf6b53890cb71f7f84f5a6c601aaf4e1
Michael Lissner <mlissner@michaeljaylissner.com>
2016-04-20 11:38:38 -0700

    Gotta do the right court field.

--- cl/people_db/import_judges/assign_authors.py
+++ cl/people_db/import_judges/assign_authors.py
@@ -5,40 +5,41 @@ Created on Fri Mar 18 18:27:09 2016
 @author: elliott
 """
 
-from cl.lib.import_lib import find_person
 from cl.corpus_importer.import_columbia.parse_judges import find_judges
+from cl.lib.import_lib import find_person
 from cl.search.models import OpinionCluster
+
 
 def assign_authors(testing=False):
 
     clusters = OpinionCluster.objects.exclude(judges='')
-    
+
     for cluster in clusters:
-      
-        judges = find_judges(cluster.judges)    
-        
+
+        judges = find_judges(cluster.judges)
+
         if len(judges) == 0:
             continue
-    
+
         candidates = []
-        for judge in judges:        
-            candidates.append(find_person(judge, 
-                                          cluster.court_id, 
-                                          case_date=cluster.date_filed))        
+        for judge in judges:
+            candidates.append(find_person(judge,
+                                          cluster.docket.court_id,
+                                          case_date=cluster.date_filed))
         candidates = [c for c in candidates if c is not None]
-        
+
         opinion = cluster.opinions[0]
-        
+
         if len(candidates) == 1:
-            opinion.author = candidates[0]    
+            opinion.author = candidates[0]
             print(cluster,'-- author assigned -- ',candidates[0])
         elif len(candidates) > 1:
             opinion.panel = candidates
             print(cluster,'-- panel assigned  -- ',candidates)
         else:
             print(cluster,'-- no match -- ',cluster.judges)
-        
+
         if not testing:
-            opinion.save()  
-        
-            
+            opinion.save()
+
+
