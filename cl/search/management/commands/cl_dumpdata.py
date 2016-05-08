db0fed759a84d934733be7bd171a42b7c02bbcac
Dave Voutila <voutilad@gmail.com>
2016-05-08 07:59:20 -0400

    added try/except, added OpinionsCited support

--- cl/search/management/commands/cl_dumpdata.py
+++ cl/search/management/commands/cl_dumpdata.py
@@ -6,7 +6,7 @@ import random
 import time
 from django.core import serializers
 from django.core.management.base import BaseCommand
-from cl.search.models import Docket, OpinionCluster, Opinion
+from cl.search.models import Docket, OpinionCluster, Opinion, OpinionsCited
 
 
 class Command(BaseCommand):
@@ -48,28 +48,43 @@ class Command(BaseCommand):
         cluster_pks = OpinionCluster.objects.filter(sub_opinions__in=pks) \
                                     .values_list('id', flat=True)
 
-        self.stdout.write('Writing Opinions to opinions.json...')
-        with open('opinions.json', 'w') as stream:
-            serializers.serialize(
-                fmt,
-                Opinion.objects.filter(id__in=pks),
-                stream=stream
-            )
+        try:
 
-        self.stdout.write('Writing OpinionClusters to clusters.json...')
-        with open('clusters.json', 'w') as stream:
-            serializers.serialize(
-                fmt,
-                OpinionCluster.objects.filter(id__in=cluster_pks),
-                stream=stream
-            )
+            self.stdout.write('Writing Opinions to opinions.json...')
+            with open('opinions.json', 'w') as stream:
+                serializers.serialize(
+                    fmt,
+                    Opinion.objects.filter(id__in=pks),
+                    stream=stream
+                )
 
-        self.stdout.write('Writing Dockets to dockets.json...')
-        with open('dockets.json', 'w') as stream:
-            serializers.serialize(
-                fmt,
-                Docket.objects.filter(clusters__in=cluster_pks),
-                stream=stream
-            )
+            self.stdout.write('Writing OpinionsCited to citings.json...')
+            with open('cited.json', 'w') as stream:
+                serializers.serialize(
+                    fmt,
+                    OpinionsCited.objects.filter(
+                        citing_opinion__in=pks | cited_opinion__in=pks
+                    ),
+                    stream=stream
+                )
 
-        self.stdout.write('Done!')
+            self.stdout.write('Writing OpinionClusters to clusters.json...')
+            with open('clusters.json', 'w') as stream:
+                serializers.serialize(
+                    fmt,
+                    OpinionCluster.objects.filter(id__in=cluster_pks),
+                    stream=stream
+                )
+
+            self.stdout.write('Writing Dockets to dockets.json...')
+            with open('dockets.json', 'w') as stream:
+                serializers.serialize(
+                    fmt,
+                    Docket.objects.filter(clusters__in=cluster_pks),
+                    stream=stream
+                )
+
+            self.stdout.write('Done!')
+
+        except Exception as e:
+            self.stderr.write('Failed to serialize: %s' % (e,))
