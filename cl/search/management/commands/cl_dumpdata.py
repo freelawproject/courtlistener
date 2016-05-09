781b99a87afba31c9943ff78526d431c9fc8b198
Dave Voutila <voutilad@gmail.com>
2016-05-09 13:18:20 -0400

    Update to cl_dumpdata: added citations support, refactored (#504)
    * initial logic stub for dumping data objects.
    
    * hardcoded basic logic for selecting and dumping data.
    
    * set to use format argument, removed unused argument. updated help.
    
    * removed order_by("?") timebomb, replaced with some random sampling of pks
    
    * purged all()'s, seeding random
    
    * added try/except, added OpinionsCited support
    
    * wrapped boolean filter logic in Q() to fix bug
    
    * refactored into smaller methods. manually tested for json and xml.

--- cl/search/management/commands/cl_dumpdata.py
+++ cl/search/management/commands/cl_dumpdata.py
@@ -4,9 +4,19 @@
 import calendar
 import random
 import time
+import traceback
 from django.core import serializers
 from django.core.management.base import BaseCommand
-from cl.search.models import Docket, OpinionCluster, Opinion
+from django.db.models.query_utils import Q
+from cl.search.models import Docket, OpinionCluster, Opinion, OpinionsCited
+
+
+SUPPORTED_MODELS = (
+    Docket,
+    OpinionCluster,
+    Opinion,
+    OpinionsCited
+)
 
 
 class Command(BaseCommand):
@@ -34,42 +44,85 @@ class Command(BaseCommand):
         fmt = options['format']
 
         self.stdout.write(
-            'Generating dump of up to %s randomly selected Opinions' % n
+            'Generating dump of up to %s randomly selected Opinions...' % n
         )
+
+        pks, cluster_pks = self._select_pks(n)
+        if n > len(pks):
+            self.stdout.write(
+                ' n > number of Opinions, serializing all (n=%s)' % len(pks)
+            )
+
+        try:
+            self._serialize(Opinion, fmt, Q(id__in=pks))
+
+            self._serialize(
+                OpinionsCited,
+                fmt,
+                Q(citing_opinion__in=pks) | Q(cited_opinion__in=pks)
+            )
+
+            self._serialize(OpinionCluster, fmt, Q(id__in=cluster_pks))
+            self._serialize(Docket, fmt, Q(clusters__in=cluster_pks))
+
+            self.stdout.write('Done!')
+
+        except Exception as e:
+            self.stderr.write('Failed to serialize: %s' % (e,))
+            if options['traceback']:
+                traceback.print_exc()
+
+    @staticmethod
+    def _select_pks(sample_size):
+        """
+        Select a random sampling of Opinions from the database.
+
+        Attributes:
+            sample_size -- number of Opinions to sample
+        """
+        n = sample_size
         pk_qs = Opinion.objects.values_list('id', flat=True)
-        if pk_qs.count() < n:
+        if pk_qs.count() < sample_size:
             n = pk_qs.count()
 
         random.seed(calendar.timegm(time.gmtime()))
-        pks = random.sample(
-            pk_qs,
-            n
-        )
+        pks = random.sample(pk_qs, n)
         cluster_pks = OpinionCluster.objects.filter(sub_opinions__in=pks) \
                                     .values_list('id', flat=True)
 
-        self.stdout.write('Writing Opinions to opinions.json...')
-        with open('opinions.json', 'w') as stream:
+        return pks, cluster_pks
+
+    def _serialize(self, model, format, filter):
+        """
+        Helper method for performing the serialization to file of CL models.
+
+        Attributes:
+            model -- supported CourtListener model class
+            format -- supported Django serizliation format ('json', 'xml'...)
+            filter -- Django QuerySet filter
+        """
+        if not model in SUPPORTED_MODELS:
+            raise ModelTypeError(model)
+
+        modelname = model.__name__
+        filename = '%s.%s' % (modelname, format)
+        self.stdout.write(' writing %ss to %s...' % (modelname, filename))
+
+        with open(filename, 'w') as stream:
             serializers.serialize(
-                fmt,
-                Opinion.objects.filter(id__in=pks),
+                format,
+                model.objects.filter(filter),
                 stream=stream
             )
 
-        self.stdout.write('Writing OpinionClusters to clusters.json...')
-        with open('clusters.json', 'w') as stream:
-            serializers.serialize(
-                fmt,
-                OpinionCluster.objects.filter(id__in=cluster_pks),
-                stream=stream
-            )
 
-        self.stdout.write('Writing Dockets to dockets.json...')
-        with open('dockets.json', 'w') as stream:
-            serializers.serialize(
-                fmt,
-                Docket.objects.filter(clusters__in=cluster_pks),
-                stream=stream
-            )
+class ModelTypeError(TypeError):
+    """
+    Exception for providing invalid CourtListener model types when serializing
+    """
 
-        self.stdout.write('Done!')
+    def __init__(self, model):
+        self.model = model
+
+    def __str__(self):
+        return '%s is not a valid CourtListnener model' % (self.model,)
