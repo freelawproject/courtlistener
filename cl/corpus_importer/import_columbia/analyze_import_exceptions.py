c7b52eca62bde74bdbb962b191da16cefa71f930
Michael Lissner <mlissner@michaeljaylissner.com>
2016-06-28 11:12:52 -0700

    Tweaks

--- cl/corpus_importer/import_columbia/analyze_import_exceptions.py
+++ cl/corpus_importer/import_columbia/analyze_import_exceptions.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import re
+
 """
 Created on Thu Jun 16 14:31:21 2016
 
@@ -16,17 +20,16 @@ for line in open('import_columbia_unknown_exceptions2.log'):
         i = line.find('opinions/') + len('opinions/')
         j = line.rfind("'")
         currfile = line[i:j]
-        
+
     if 'Error:' in line:
         file_lists[line].append(currfile)
-        #print(line)
-        
+        # print(line)
+
 for k, v in file_lists.items():
     for x in v:
-        print((x+'\n'),end="", file=open(k[:5]+'.txt','at'))
+        print((x + '\n'), end="", file=open(k[:5] + '.txt', 'at'))
 
-import re
-    
+
 f = open('import_columbia_known_exceptions2.log')
 
 cite_tab = Counter()
@@ -43,19 +46,18 @@ for line in f:
 
     if 'Failed to get a citation' in line:
         seg = line.split("'")[1]
-        
-        newseg = re.sub('\d','#',seg)
-        #print(newseg)
-        #if 'Ct. Sup.' not in newseg:
+
+        newseg = re.sub('\d', '#', seg)
+        # print(newseg)
+        # if 'Ct. Sup.' not in newseg:
         #    if 'Ohio App.' not in newseg:
         #        if 'Okla. Cr.' not in newseg:
         cite_tab[newseg] += 1
-    
+
     if 'Failed to find a court ID' in line:
         seg = line.split('"')[1]
-        
-        courtid_tab[court,seg] += 1
 
+        courtid_tab[court, seg] += 1
 
 cite_tab.most_common()
 
