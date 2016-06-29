8d3c9b1bfaa7d224a25ebf7353d615c58cef4268
Michael Lissner <mlissner@michaeljaylissner.com>
2016-06-29 13:57:12 -0700

    Rewrites the parser to stop it from changing the string in intermediate steps.

--- cl/corpus_importer/import_columbia/parse_opinions.py
+++ cl/corpus_importer/import_columbia/parse_opinions.py
@@ -348,23 +348,24 @@ def get_court_object(raw_court, file_path):
         if re.search(regex, raw_court):
             return value
 
-    # this messes up for, e.g. 'St. Louis', but works for all others
-    if '.' in raw_court and 'St.' not in raw_court:
+    # this messes up for, e.g. 'St. Louis', and 'U.S. Circuit Court, but works
+    # for all others
+    if '.' in raw_court and not any([s in raw_court for s in ['St.', 'U.S']]):
         j = raw_court.find('.')
-        raw_court = raw_court[:j]
+        r = raw_court[:j]
 
         for regex, value in state_pairs:
-            if re.search(regex, raw_court):
+            if re.search(regex, r):
                 return value
 
     # we need the comma to successfully match Superior Courts, the name of which
     # comes after the comma
     if ',' in raw_court and 'Superior Court' not in raw_court:
         j = raw_court.find(',')
-        raw_court = raw_court[:j]
+        r = raw_court[:j]
 
         for regex, value in state_pairs:
-            if re.search(regex, raw_court):
+            if re.search(regex, r):
                 return value
 
     folder = file_path.split('/documents')[0]
