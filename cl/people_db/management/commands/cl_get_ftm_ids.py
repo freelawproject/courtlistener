41b464e0a40eee09a02bdd2a46a9fc64edecece5
Michael Lissner <mlissner@michaeljaylissner.com>
2016-07-23 13:55:53 -0400

    Final version of the FTM script, for deployment (hopefully).

--- cl/people_db/management/commands/cl_get_ftm_ids.py
+++ cl/people_db/management/commands/cl_get_ftm_ids.py
@@ -102,80 +102,8 @@ def print_stats(match_stats, candidate_eid_lists):
           "against." % ftm_judge_count)
 
 
-def update_judges(candidate_id_map, debug):
-    """Iterate over the judges and update their information."""
-    msg = '# Beginning process of updating judges. #'
-    print("\n\n%s\n%s\n%s" % ('#' * len(msg), msg, '#' * len(msg)))
-
-    match_stats = defaultdict(int)
-    for person in Person.objects.all():
-        print("\nDoing: %s" % person)
-        judge_matches = []
-        for position in person.positions.all():
-            if not position.is_judicial_position:
-                # We only care about judicial positions.
-                continue
-            if position.court is None:
-                # If the position wasn't in a court, punt.
-                continue
-            if position.court.pk not in courtid2statelevel:
-                # Either not a state court or the Court field is None.
-                continue
-            if courtid2statelevel[position.court.pk][1] != 'H':
-                # We only want high courts
-                continue
-
-            judges_in_court = candidate_id_map[position.court.pk]
-
-            # Check last name
-            position_matches = []
-            for judge in judges_in_court:
-                # Match name by lowercasing and comparing the last, first name
-                # combo.
-                judge_name = judge['name'].lower()
-                person_name = ('%s, %s' % (person.name_last,
-                                           person.name_first)).lower()
-                if judge_name.startswith(person_name):
-                    position_matches.append(judge)
-
-            for position_match in position_matches:
-                existing_judge_match_eids = [j['eid'] for j in judge_matches]
-                if position_match['eid'] not in existing_judge_match_eids:
-                    # add the item.
-                    judge_matches.append(position_match)
-
-        # Save if there's only one unique match across all positions.
-        if len(judge_matches) == 1:
-            print("  Found one match: %s" % judge_matches[0])
-            match_stats[1] += 1
-            if person.is_alias:
-                person = person.is_alias_of
-                if person.ftm_eid:
-                    print("After redirecting to alias, found an existing value.")
-                    continue
-
-            if person.ftm_eid:
-                print("Found an existing value. Could be from the alias?")
-                continue
-
-            person.ftm_eid = judge_matches[0]['eid']
-            person.ftm_total_received = judge_matches[0]['total']
-            if not debug:
-                person.save()
-        elif len(judge_matches) == 0:
-            match_stats[0] += 1
-            print("  Found no matches.")
-        elif len(judge_matches) > 1:
-            match_stats[len(judge_matches)] += 1
-            print("  Found multiple matches: %s" % judge_matches)
-
-    print_stats(match_stats, candidate_id_map)
-
-
 def update_judges_by_solr(candidate_id_map, debug):
-    """An alternate attempt at updating judges, by looking each one up in
-    Solr.
-    """
+    """Update judges by looking up each entity from FTM in Solr."""
     conn = SolrInterface(settings.SOLR_PEOPLE_URL, mode='r')
     match_stats = defaultdict(int)
     # These IDs are ones that cannot be updated due to being identified as
@@ -253,11 +181,5 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        self.debug = options['debug']
-        self.options = options
-
-        # Run the requested method.
         candidate_id_map = make_dict_of_ftm_eids(options['dont_use_pickle'])
-        #update_judges(candidate_id_map, debug=options['debug'])
-
         update_judges_by_solr(candidate_id_map, debug=options['debug'])
