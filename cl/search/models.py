19d4742d6b157e8c6980bd9b5998288216431c19
Michael Lissner <mlissner@michaeljaylissner.com>
2016-08-19 21:41:47 -0700

    Lengthens another field.

--- cl/search/models.py
+++ cl/search/models.py
@@ -216,7 +216,7 @@ class Docket(models.Model):
     )
     cause = models.CharField(
         help_text="The cause for the case.",
-        max_length=200,
+        max_length=500,  # Was 200
         blank=True,
     )
     nature_of_suit = models.CharField(
