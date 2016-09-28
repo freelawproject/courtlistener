b6242ed8e5a83fda8bd2eefa82b5673d7cba2eda
Michael Lissner <mlissner@michaeljaylissner.com>
2016-09-28 11:58:06 -0700

    Switches solr update tasks to use scorched instead of sunburnt.
    (New version supports everything we need.)

--- cl/search/tasks.py
+++ cl/search/tasks.py
@@ -1,3 +1,4 @@
+import scorched
 import socket
 
 from django.conf import settings
@@ -5,7 +6,6 @@ from django.conf import settings
 from cl.audio.models import Audio
 from cl.celery import app
 from cl.lib.search_index_utils import InvalidDocumentError
-from cl.lib import sunburnt
 from cl.lib.sunburnt import SolrError
 from cl.people_db.models import Person
 from cl.search.models import Opinion, OpinionCluster, RECAPDocument
@@ -22,7 +22,7 @@ def add_or_update_items(items, solr_url=settings.SOLR_OPINION_URL):
     this is only used by the update_index command, and we want to get the
     objects in the task, not in its caller.
     """
-    si = sunburnt.SolrInterface(solr_url, mode='w')
+    si = scorched.SolrInterface(solr_url, mode='w')
     if hasattr(items, "items") or not hasattr(items, "__iter__"):
         # If it's a dict or a single item make it a list
         items = [items]
@@ -55,7 +55,7 @@ def add_or_update_items(items, solr_url=settings.SOLR_OPINION_URL):
 
 @app.task
 def add_or_update_opinions(item_pks, force_commit=True):
-    si = sunburnt.SolrInterface(settings.SOLR_OPINION_URL, mode='w')
+    si = scorched.SolrInterface(settings.SOLR_OPINION_URL, mode='w')
     try:
         si.add([item.as_search_dict() for item in
                 Opinion.objects.filter(pk__in=item_pks)])
@@ -67,7 +67,7 @@ def add_or_update_opinions(item_pks, force_commit=True):
 
 @app.task
 def add_or_update_audio_files(item_pks, force_commit=True):
-    si = sunburnt.SolrInterface(settings.SOLR_AUDIO_URL, mode='w')
+    si = scorched.SolrInterface(settings.SOLR_AUDIO_URL, mode='w')
     try:
         si.add([item.as_search_dict() for item in
                 Audio.objects.filter(pk__in=item_pks)])
@@ -79,7 +79,7 @@ def add_or_update_audio_files(item_pks, force_commit=True):
 
 @app.task
 def add_or_update_people(item_pks, force_commit=True):
-    si = sunburnt.SolrInterface(settings.SOLR_PEOPLE_URL, mode='w')
+    si = scorched.SolrInterface(settings.SOLR_PEOPLE_URL, mode='w')
     try:
         si.add([item.as_search_dict() for item in
                 Person.objects.filter(pk__in=item_pks)])
@@ -91,7 +91,7 @@ def add_or_update_people(item_pks, force_commit=True):
 
 @app.task
 def add_or_update_recap_document(item_pks, force_commit=True):
-    si = sunburnt.SolrInterface(settings.SOLR_RECAP_URL, mode='w')
+    si = scorched.SolrInterface(settings.SOLR_RECAP_URL, mode='w')
     try:
         si.add([item.as_search_dict() for item in
                 RECAPDocument.objects.filter(pk__in=item_pks)])
@@ -103,9 +103,9 @@ def add_or_update_recap_document(item_pks, force_commit=True):
 
 @app.task
 def delete_items(items, solr_url, force_commit=False):
-    si = sunburnt.SolrInterface(solr_url, mode='w')
+    si = scorched.SolrInterface(solr_url, mode='w')
     try:
-        si.delete(list(items))
+        si.delete_by_ids(list(items))
         if force_commit:
             si.commit()
     except SolrError, exc:
@@ -114,7 +114,7 @@ def delete_items(items, solr_url, force_commit=False):
 
 @app.task
 def add_or_update_cluster(pk, force_commit=True):
-    si = sunburnt.SolrInterface(settings.SOLR_OPINION_URL, mode='w')
+    si = scorched.SolrInterface(settings.SOLR_OPINION_URL, mode='w')
     try:
         si.add([item.as_search_dict() for item in
                 OpinionCluster.objects.get(pk=pk).sub_opinions.all()])
