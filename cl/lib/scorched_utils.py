8f90c85ec7c6ee06382533da7b3ef1087a3d1578
Michael Lissner <mlissner@michaeljaylissner.com>
2016-10-07 14:10:20 -0700

    Fixes to scorched libs.

--- cl/lib/scorched_utils.py
+++ cl/lib/scorched_utils.py
@@ -28,9 +28,11 @@ class ExtraSolrInterface(SolrInterface):
 class ExtraSolrSearch(SolrSearch):
     """Base class for common search options management"""
     option_modules = ('query_obj', 'filter_obj', 'paginator',
-                      'more_like_this', 'highlighter', 'faceter',
-                      'sorter', 'facet_querier', 'field_limiter',
-                      'extra')
+                      'more_like_this', 'highlighter', 'postings_highlighter',
+                      'faceter', 'grouper', 'sorter', 'facet_querier',
+                      'debugger', 'spellchecker', 'requesthandler',
+                      'field_limiter', 'parser', 'pivoter', 'facet_ranger',
+                      'term_vectors', 'stat', 'extra')
 
     def _init_common_modules(self):
         super(ExtraSolrSearch, self)._init_common_modules()
@@ -44,15 +46,17 @@ class ExtraSolrSearch(SolrSearch):
     _count = None
     def count(self):
         if self._count is None:
-            # We haven't gotten the count yet. Get it.
-            if self.paginator.rows is not None:
-                total_results = self.paginator.rows
+            # We haven't gotten the count yet. Get it. Clone self for this
+            # query or else we'll set rows=0 for remainder.
+            newself = self.clone().add_extra(rows=0)
+            r = newself.execute()
+            if r.groups:
+                total = getattr(r.groups, r.group_field)['ngroups']
             else:
-                r = self.paginate(rows=0).execute()
-                total_results = r.result.numFound
-                if self.paginator.start is not None:
-                    total_results -= self.paginator.start
-            self._count = total_results
+                total = r.result.numFound
+
+            # Set the cache
+            self._count = total
         return self._count
 
 
