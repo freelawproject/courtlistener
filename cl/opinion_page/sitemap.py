14039940bac3df6a0681576f89c020b576588d9c
Michael Lissner <mlissner@michaeljaylissner.com>
2016-11-16 14:44:21 -0800

    Refactors sitemaps to make them much simpler.

--- cl/opinion_page/sitemap.py
+++ cl/opinion_page/sitemap.py
@@ -1,46 +1,13 @@
 from django.conf import settings
-from django.http import HttpResponse
-from django.template import loader
-from django.utils.encoding import smart_str
 
-from cl.lib import sunburnt
-from cl.sitemap import items_per_sitemap, opinion_solr_params
+from cl.sitemap import make_sitemap_solr_params, make_solr_sitemap
 
 
 def opinion_sitemap_maker(request):
-    conn = sunburnt.SolrInterface(settings.SOLR_OPINION_URL, mode='r')
-    page = request.GET.get("p", '1')
-    opinion_solr_params['start'] = (int(page) - 1) * items_per_sitemap
-
-    search_results_object = conn.raw_query(**opinion_solr_params).execute()
-
-    # Translate Solr object into something Django's template can use
-    urls = []
-    for result in search_results_object:
-        url_strs = [
-            'https://www.courtlistener.com%s' % result['absolute_url'],
-            'https://www.courtlistener.com%sauthorities/' %
-            result['absolute_url']
-        ]
-        if result.get('local_path') and result.get('local_path') != '':
-            url_strs.append(
-                'https://www.courtlistener.com/%s' % result['local_path'])
-
-        sitemap_item = {}
-        for url_str in url_strs:
-            sitemap_item['location'] = url_str
-            sitemap_item['changefreq'] = 'yearly'
-            sitemap_item['lastmod'] = result['timestamp']
-            if any(s in url_str for s in
-                   ['authorities', 'pdf', 'doc', 'wpd']):
-                sitemap_item['priority'] = '0.3'
-            else:
-                sitemap_item['priority'] = '0.5'
-            urls.append(dict(sitemap_item))
-
-    xml = smart_str(loader.render_to_string('sitemap.xml', {'urlset': urls}))
-    # These links contain case names, so they should get crawled but not
-    # indexed
-    response = HttpResponse(xml, content_type='application/xml')
-    response['X-Robots-Tag'] = 'noindex, noodp, noarchive, noimageindex'
-    return response
+    return make_solr_sitemap(
+        request,
+        settings.SOLR_OPINION_URL,
+        make_sitemap_solr_params('dateFiled asc', 'o_sitemap'),
+        'yearly',
+        ['pdf', 'doc', 'wpd'],
+    )
--- cl/sitemap.py
+++ cl/sitemap.py
@@ -2,31 +2,68 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.template import loader
+from django.utils.encoding import smart_str
 from django.views.decorators.cache import never_cache
 
 from cl.lib import sunburnt
+from cl.lib.scorched_utils import ExtraSolrInterface
 
 items_per_sitemap = 250
-opinion_solr_params = {
-    'q': '*',
-    'rows': items_per_sitemap,
-    'start': 0,
-    'fl': ','.join([
-        'absolute_url',
-        'dateFiled',
-        'local_path',
-        'citeCount',
-        'timestamp',
-    ]),
-    'sort': 'dateFiled asc',
-    'caller': 'opinion_sitemap_maker',
-}
+
 index_solr_params = {
     'q': '*',
     'rows': '0',  # just need the count
     'start': '0',
     'caller': 'sitemap_index',
 }
+
+
+def make_sitemap_solr_params(sort, caller):
+    return {
+        'q': '*',
+        'rows': items_per_sitemap,
+        'start': 0,
+        'fl': ','.join([
+            'absolute_url',
+            # Not all indexes have this field, but it causes no errors.
+            'local_path',
+            'timestamp',
+        ]),
+        'sort': sort,
+        'caller': caller,
+    }
+
+
+def make_solr_sitemap(request, solr_url, params, changefreq,
+                      low_priority_pages):
+    solr = ExtraSolrInterface(solr_url)
+    page = int(request.GET.get('p', 1))
+    params['start'] = (page - 1) * items_per_sitemap
+    results = solr.query().add_extra(**params).execute()
+
+    urls = []
+    cl = 'https://www.courtlistener.com'
+    for result in results:
+        url_strs = ['%s%s' % (cl, result['absolute_url'])]
+        if result.get('local_path') and \
+                not result['local_path'].endswith('.xml'):
+            url_strs.append('%s/%s' % (cl, result['local_path']))
+
+        item = {}
+        for url_str in url_strs:
+            item['location'] = url_str
+            item['changefreq'] = changefreq
+            item['lastmod'] = result['timestamp']
+            if any(s in url_str for s in low_priority_pages):
+                item['priority'] = '0.3'
+            else:
+                item['priority'] = '0.5'
+            urls.append(item.copy())
+
+    xml = smart_str(loader.render_to_string('sitemap.xml', {'urlset': urls}))
+    response = HttpResponse(xml, content_type='application/xml')
+    response['X-Robots-Tag'] = 'noindex, noodp, noarchive, noimageindex'
+    return response
 
 
 @never_cache
