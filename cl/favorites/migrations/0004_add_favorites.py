ef695431e9fbefe6332c51d25729842540c1c886
Michael Lissner <mlissner@michaeljaylissner.com>
2016-11-18 09:06:20 -0800

    Adds the favorites migration.

---
+++ cl/favorites/migrations/0004_add_favorites.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('search', '0042_make_pacer_doc_id_nullable'),
+        ('favorites', '0003_auto_20160331_1116'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='favorite',
+            name='docket_id',
+            field=models.ForeignKey(verbose_name=b'the docket that is favorited', blank=True, to='search.Docket', null=True),
+        ),
+        migrations.AddField(
+            model_name='favorite',
+            name='recap_doc_id',
+            field=models.ForeignKey(verbose_name=b'the RECAP document that is favorited', blank=True, to='search.RECAPDocument', null=True),
+        ),
+        migrations.AlterUniqueTogether(
+            name='favorite',
+            unique_together=set([('cluster_id', 'user'), ('audio_id', 'user'), ('recap_doc_id', 'user'), ('docket_id', 'user')]),
+        ),
+    ]
