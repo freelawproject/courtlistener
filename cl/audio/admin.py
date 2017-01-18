ca72fedd373cd9ed8c0184eb4e628995fb336311
Michael Lissner <mlissner@michaeljaylissner.com>
2017-01-18 15:27:49 -0800

    Makes the panel a raw ID in the audio admin page. Else it loads 8000 judges in a picker.

--- cl/audio/admin.py
+++ cl/audio/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 
 
 class AudioAdmin(admin.ModelAdmin):
-    raw_id_fields = ('docket',)
+    raw_id_fields = (
+        'docket',
+        'panel',
+    )
     readonly_fields = (
         'date_created',
         'date_modified',
