1df4daa7a331fc39c85756c4b278e312df8c709d
Dave Voutila <voutilad@gmail.com>
2017-01-24 21:11:53 -0500

    fixed issues where some model instances triggered indexing to collection1 core. fixed a pep8 typo. removed external CDN uri for font-awesome.

--- cl/search/tests.py
+++ cl/search/tests.py
@@ -149,7 +149,7 @@ class ModelTest(TestCase):
             cf = ContentFile(StringIO.StringIO('blah').read())
             o.file_with_date = datetime.date(1899, 1, 1)
             o.local_path.save('file_name.pdf', cf, save=False)
-            o.save(index=True)
+            o.save(index=False)
         except ValueError as e:
             raise ValueError("Unable to save a case older than 1900. Did you "
                              "try to use `strftime`...again?")
