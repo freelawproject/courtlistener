443c459abc83e66ce6d00c1b085f7329abe93bdf
Michael Lissner <mlissner@michaeljaylissner.com>
2017-01-26 16:53:31 -0800

    Switches to logging and makes it docket-centric instead of doc-centric.

--- cl/cleanup/management/commands/cleanup_636_pacer_leading_zeros.py
+++ cl/cleanup/management/commands/cleanup_636_pacer_leading_zeros.py
@@ -1,4 +1,5 @@
 # https://github.com/freelawproject/courtlistener/issues/636
+import logging
 import os
 
 from django.core.management import BaseCommand
@@ -8,8 +9,10 @@ from cl.corpus_importer.tasks import download_recap_item
 from cl.lib.pacer import PacerXMLParser
 from cl.lib.recap_utils import get_ia_document_url_from_path, \
     get_local_document_url_from_path
-from cl.search.models import RECAPDocument
+from cl.search.models import RECAPDocument, Docket
 from cl.scrapers.tasks import get_page_count
+
+logger = logging.getLogger(__name__)
 
 
 class CleanupPacerXMLParser(PacerXMLParser):
@@ -26,9 +29,9 @@ class CleanupPacerXMLParser(PacerXMLParser):
             # Only touch things where the new value leads with a zero.
             return None
         else:
-            print "  Doing docket_entry: %s, document_number, " \
-                  "%s and attachment number: %s" % (docket_entry, entry_number,
-                                                    attachment_number)
+            logger.info("  Doing docket_entry: %s, document_number, "
+                        "%s and attachment number: %s" %
+                        (docket_entry, entry_number, attachment_number))
         old_entry_number = int(entry_number)
 
         try:
@@ -37,16 +40,17 @@ class CleanupPacerXMLParser(PacerXMLParser):
                 document_number=old_entry_number,
                 attachment_number=attachment_number or None,
             )
-            print "    Found item."
+            logger.info("    Found item.")
         except RECAPDocument.DoesNotExist:
-            print "    Failed to find item."
+            logger.info("    Failed to find item.")
             return None
 
         d.document_number = entry_number
         if d.is_available:
             new_ia = get_ia_document_url_from_path(
                 self.path, entry_number, attachment_number)
-            print "    Updating IA URL from %s to %s" % (d.filepath_ia, new_ia)
+            logger.info("    Updating IA URL from %s to %s" %
+                        (d.filepath_ia, new_ia))
             d.filepath_ia = new_ia
 
             if not os.path.isfile(d.filepath_local.path):
@@ -57,34 +61,34 @@ class CleanupPacerXMLParser(PacerXMLParser):
                     get_local_document_url_from_path(self.path, entry_number,
                                                      attachment_number),
                 )
-                print "    Updating local path from %s to %s" % (
-                    d.filepath_local, new_local_path)
+                logger.info("    Updating local path from %s to %s" %
+                            (d.filepath_local, new_local_path))
                 d.filepath_local = new_local_path
                 filename = d.filepath_ia.rsplit('/', 1)[-1]
-                print "    Downloading item with filename %s" % filename
+                logger.info("    Downloading item with filename %s" % filename)
                 download_recap_item(d.filepath_ia, filename)
             else:
-                print "    File already on disk. Punting."
+                logger.info("    File already on disk. Punting.")
                 return None
 
             if d.page_count is None:
-                print "    Getting page count."
+                logger.info("    Getting page count.")
                 extension = d.filepath_local.path.split('.')[-1]
                 d.page_count = get_page_count(d.filepath_local.path, extension)
         else:
-            print "    Item not available in RECAP. Punting."
+            logger.info("    Item not available in RECAP. Punting.")
             return None
 
         if not debug:
             try:
                 d.save(do_extraction=True, index=True)
-                print "    Item saved successfully at " \
-                      "https://www.courtlistener.com%s" % d.get_absolute_url()
+                logger.info("    Item saved at https://www.courtlistener.com%s"
+                            % d.get_absolute_url())
             except IntegrityError:
-                print "    Integrity error while saving."
+                logger.info("    Integrity error while saving.")
                 return None
         else:
-            print "    No save requested in debug mode."
+            logger.info("    No save requested in debug mode.")
 
         return d
 
@@ -97,16 +101,17 @@ class Command(BaseCommand):
         parser.set_defaults(debug=False)
 
     def handle(self, *args, **options):
-        docs = RECAPDocument.objects.filter(ocr_status=RECAPDocument.OCR_FAILED)
-        for doc in docs:
-            docket_path = doc.docket_entry.docket.filepath_local.path
+        dockets = Docket.objects.filter(
+            docket_entries__recap_documents__ocr_status=RECAPDocument.OCR_FAILED)
+        for docket in dockets:
+            docket_path = docket.filepath_local.path
             try:
-                print "Doing docket at: %s" % docket_path
+                logger.info("Doing docket at: %s" % docket_path)
                 pacer_doc = CleanupPacerXMLParser(docket_path)
             except IOError:
-                print "Couldn't find docket at: %s" % docket_path
+                logger.info("Couldn't find docket at: %s" % docket_path)
             else:
                 _ = pacer_doc.make_documents(
-                    doc.docket_entry.docket,
+                    docket,
                     debug=options['debug'],
                 )
