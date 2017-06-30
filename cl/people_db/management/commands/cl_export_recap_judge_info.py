23678599eed3f18fe329c5eb330d3b5806338ba9
Michael Lissner <mlissner@michaeljaylissner.com>
2017-06-30 15:58:08 -0700

    Moves the judge name normalization to Juriscraper.

--- cl/people_db/management/commands/cl_export_recap_judge_info.py
+++ cl/people_db/management/commands/cl_export_recap_judge_info.py
@@ -5,137 +5,11 @@ import pandas
 from django.core.management import BaseCommand
 from django.db.models import Q
 
-from juriscraper.lib.string_utils import titlecase
 from pandas import to_pickle
 from unidecode import unidecode
+from juriscraper.lib.judge_parsers import normalize_judge_string
 
 from cl.search.models import Court, Docket
-
-titles = [
-    'judge', 'magistrate', 'district', 'chief', 'senior',
-    'bankruptcy', 'mag.', 'magistrate-judge', 'mag/judge', 'mag',
-    'visiting', 'special', 'senior-judge', 'master', 'u.s.magistrate',
-]
-blacklist = [
-    'a998', 'agb', 'am', 'ca', 'cet', 'ch.', 'cla', 'clerk', 'cp', 'cvb', 'db',
-    'debt-magistrate', 'discovery', 'dj', 'docket', 'duty', 'duty', 'ec', 'eck',
-    'general', 'grc', 'gs', 'hhl', 'hon', 'honorable', 'inactive', 'jne', 'jv',
-    'kec', 'law', 'lc', 'llh', 'lq', 'maryland', 'mediator', 'merged', 'mj',
-    'mmh', 'msh', 'mwd', 'no', 'none', 'prisoner', 'pslc', 'pro', 'pso', 'pt',
-    'rmh', 'se', 'sf', 'successor', 'u.s.', 'tjc', 'unassigned', 'unassigned2',
-    'unassigneddj', 'unknown', 'us', 'usdc', 'vjdistrict',
-]
-
-judge_normalizers = {
-    # Generic Judge & Bankruptcy Judge we merge together for simplicity.
-    '': 'jud',
-    'Judge': 'jud',
-    'Judge Judge': 'jud',
-    'District Judge': 'jud',
-    'Visiting Judge': 'jud',
-    'Bankruptcy': 'jud',
-    'Bankruptcy Judge': 'jud',
-
-    # Chief (normalize to jud for now, due to low sample size)
-    'Chief': 'jud',
-    'Chief Judge': 'jud',
-    'Chief District Judge': 'jud',
-
-    # Magistrate
-    'Mag': 'mag',
-    'Mag Judge': 'mag',
-    'mag/judge': 'mag',
-    'Magistrate': 'mag',
-    'Magistrate Judge': 'mag',
-    'Magistrate-Judge': 'mag',
-    'Magistrate Judge Mag': 'mag',
-    'Magistrate Judge Magistrate': 'mag',
-    'Magistrate Judge Magistrate Judge': 'mag',
-
-    # Chief Magistrate (normalize to magistrate for now, due to low sample size)
-    'Chief Magistrate': 'mag',
-    'Chief Magistrate Judge': 'mag',
-
-    # Senior
-    'Senior Judge': 'ret-senior-jud',
-    'Senior-Judge': 'ret-senior-jud',
-
-    # Special Master
-    'Special Master': 'spec-m',
-    'Chief Special Master': 'c-spec-m',
-}
-
-
-def normalize_judge_titles(title):
-    """Normalize judge titles
-
-    Take in a string like "Magistrate Judge" and return the normalized
-    abbreviation from the POSITION_TYPES variable. Assumes that input is
-    titlecased.
-
-    Also normalizes things like:
-     - District Judge --> Judge
-     - Blank --> Judge
-     - Bankruptcy Judge --> Judge
-    """
-    return judge_normalizers.get(title, 'UNKNOWN: %s' % title)
-
-
-def normalize_judge_names(name):
-    """Cleans up little issues with people's names"""
-    out = []
-    words = name.split()
-    for i, w in enumerate(words):
-        # Michael J Lissner --> Michael J. Lissner
-        if len(w) == 1 and w.isalpha():
-            w = '%s.' % w
-
-        # Michael Lissner Jr --> Michael Lissner Jr.
-        if w.lower() in ['jr', 'sr']:
-            w = '%s.' % w
-
-        # J. Michael Lissner --> Michael Lissner
-        # J. G. Lissner --> J. G. Lissner
-        if i == 0 and w.lower() in ['j.', 'j']:
-            next_word = words[i + 1]
-            if not any([len(next_word) == 2 and next_word.endswith('.'),
-                        len(next_word) == 1]):
-                # Drop the word.
-                continue
-        out.append(w)
-
-    return ' '.join(out)
-
-
-def split_name_title(judge):
-    """Split a value from PACER and return the title and name"""
-    judge = judge.replace(',', '')
-    words = judge.lower().split()
-
-    # Nuke bad junk (punct., j, blacklist, etc.)
-    clean_words = []
-    for i, w in enumerate(words):
-        if any(['(' in w,
-                ')' in w,
-                w.startswith('-'),
-                w.startswith('~'),
-                (len(w) > 2 and '.' in w and w not in ['jr.', 'sr.']),
-                w in blacklist]):
-            continue
-        clean_words.append(w)
-
-    title_words = []
-    name_words = []
-    for w in clean_words:
-        if w in titles:
-            title_words.append(w)
-        else:
-            name_words.append(w)
-
-    title = normalize_judge_titles(titlecase(' '.join(title_words)))
-    name = normalize_judge_names(titlecase(' '.join(name_words)))
-
-    return name, title
 
 
 class Command(BaseCommand):
@@ -191,7 +65,7 @@ class Command(BaseCommand):
                     if not judge:
                         continue
 
-                    name, title = split_name_title(unidecode(judge))
+                    name, title = normalize_judge_string(unidecode(judge))
                     if not name:
                         continue
                     if name not in out[court.pk]:
