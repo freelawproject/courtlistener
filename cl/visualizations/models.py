2897147fbf6bba342bd16490e18a8770eac5b088
Michael Lissner <mlissner@michaeljaylissner.com>
2017-07-10 10:52:08 -0700

    Should fix #706 via improved string/unicode handling.

--- cl/visualizations/models.py
+++ cl/visualizations/models.py
@@ -304,7 +304,7 @@ class SCOTUSMap(models.Model):
         return json.dumps(j, indent=2)
 
     def __unicode__(self):
-        return '{pk}: {title}'.format(
+        return u'{pk}: {title}'.format(
             pk=getattr(self, 'pk', None),
             title=self.title
         )
@@ -326,9 +326,9 @@ class SCOTUSMap(models.Model):
                 obj.case_name,
                 obj.case_name_full
             ]
-            return next((_ for _ in case_name_preference if _), "Unknown")
+            return next((_ for _ in case_name_preference if _), u"Unknown")
 
-        return "{start} ({start_year}) to {end} ({end_year})".format(
+        return u"{start} ({start_year}) to {end} ({end_year})".format(
                 start=get_best_case_name(self.cluster_start),
                 start_year=self.cluster_start.date_filed.year,
                 end=get_best_case_name(self.cluster_end),
@@ -339,7 +339,7 @@ class SCOTUSMap(models.Model):
         # Note that the title needs to be made first, so that the slug can be
         # generated from it.
         if not self.title:
-            self.title = trunc(self.make_title(), 200, ellipsis='…')
+            self.title = trunc(self.make_title(), 200, ellipsis=u'…')
 
         if self.published is True and self.date_published is None:
             # First time shared.
@@ -392,7 +392,7 @@ class Referer(models.Model):
     )
 
     def __unicode__(self):
-        return '{pk}: Refers to {map}'.format(
+        return u'{pk}: Refers to {map}'.format(
             pk=getattr(self, 'pk', None),
             map=self.map,
         )
@@ -424,7 +424,7 @@ class JSONVersion(models.Model):
     )
 
     def __unicode__(self):
-        return '<JSONVersion {pk}> for <{map}>'.format(
+        return u'<JSONVersion {pk}> for <{map}>'.format(
             pk=getattr(self, 'pk', None),
             map=self.map,
         )
