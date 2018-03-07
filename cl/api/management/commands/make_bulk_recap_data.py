039ee5d679192acd701e77d27b14a6a6a91bc5bd
Michael Lissner <mlissner@michaeljaylissner.com>
2018-03-06 17:36:57 -0800

    fix(management commands): Makes new bulk command have a required output dir param

--- cl/api/management/commands/make_bulk_recap_data.py
+++ cl/api/management/commands/make_bulk_recap_data.py
@@ -1,6 +1,7 @@
 from django.utils.timezone import now
 
 from cl.api.tasks import write_json_to_disk
+from cl.lib.argparse_types import readable_dir
 from cl.lib.command_utils import VerboseCommand
 from cl.search.api_serializers import FullDocketSerializer
 from cl.search.models import Court, Docket
@@ -9,17 +10,23 @@ from cl.search.models import Court, Docket
 class Command(VerboseCommand):
     help = 'Create the bulk files for all jurisdictions and for "all".'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--output-directory',
+            type=readable_dir,
+            required=True,
+            help='A directory to place the generated data.',
+        )
+
     def handle(self, *args, **options):
         super(Command, self).handle(*args, **options)
         courts = Court.objects.all()
 
         self.stdout.write('Starting bulk file creation.')
         t1 = now()
-        write_json_to_disk(courts, **{
-            'obj_type_str': 'full-dockets',
-            'obj_class': Docket,
-            'court_attr': 'court_id',
-            'serializer': FullDocketSerializer,
-        })
+        write_json_to_disk(courts, obj_type_str='full-dockets',
+                           obj_class=Docket, court_attr='court_id',
+                           serializer=FullDocketSerializer,
+                           bulk_dir=options['output_directory'])
         t2 = now()
         self.stdout.write("Completed in %s" % (t2 - t1))
