3a7e3a271f67a68a9a278d751fb9804cdcc80efa
Michael Lissner <mlissner@michaeljaylissner.com>
2018-04-30 17:38:43 -0700

    feat(rss scrapers): Improved admin display and filtering
    Related to freelawproject/juriscraper#195

--- cl/recap_rss/admin.py
+++ cl/recap_rss/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from cl.recap_rss.models import RssFeedStatus
 
 
-admin.site.register(RssFeedStatus)
+@admin.register(RssFeedStatus)
+class RssFeedStatusAdmin(admin.ModelAdmin):
+    list_filter = ('court',)
+    list_display = ('__str__', 'court', 'status', 'is_sweep')
