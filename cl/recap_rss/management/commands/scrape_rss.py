0676baf3c9166a6136e863ff59a553d4fe26fcd8
Michael Lissner <mlissner@michaeljaylissner.com>
2018-05-04 15:04:42 -0700

    fix(rss scraper): Better pid handling
    This will make it harder to run the same court multiple times at the same time (though still not
    impossible). Also moves the location of the pid file to somewhere any user can write.

--- cl/recap_rss/management/commands/scrape_rss.py
+++ cl/recap_rss/management/commands/scrape_rss.py
@@ -7,10 +7,10 @@ from celery.canvas import chain
 from django.utils.timezone import now, make_aware
 
 from cl.lib.command_utils import VerboseCommand
-from cl.search.models import Court
 from cl.recap_rss.models import RssFeedStatus
 from cl.recap_rss.tasks import check_if_feed_changed, merge_rss_feed_contents, \
     mark_status_successful
+from cl.search.models import Court
 from cl.search.tasks import add_or_update_recap_document
 
 
@@ -21,7 +21,7 @@ class Command(VerboseCommand):
         parser.add_argument(
             '--courts',
             type=str,
-            default='all',
+            default=['all'],
             nargs="*",
             help="The courts that you wish to parse.",
         )
@@ -44,16 +44,19 @@ class Command(VerboseCommand):
     def handle(self, *args, **options):
         super(Command, self).handle(*args, **options)
 
-        if 'all' in options['courts']:
-            # Only allow one script at a time that's crawling everything.
-            fp = open('program.pid', 'w')
-            try:
-                fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
-            except IOError:
-                print("Another instance of this program is running with "
-                      "'--courts all'. Only one instance can crawl all courts "
-                      "at a time.")
-                sys.exit(1)
+        if options['sweep'] is False:
+            # Only allow one script at a time per court combination.
+            # Note that multiple scripts on multiple machines could still be
+            # run.
+            court_str = '-'.join(sorted(options['courts']))
+            with open('/tmp/rss-scraper-%s.pid' % court_str, 'w') as fp:
+                try:
+                    fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                except IOError:
+                    print("Another instance of this program is running with "
+                          "for this combination of courts. Only one instance "
+                          "can crawl these courts at a time: '%s'" % court_str)
+                    sys.exit(1)
 
         # Loop over the PACER sites that have RSS feeds and see if they're
         # ready to do.
@@ -64,7 +67,7 @@ class Command(VerboseCommand):
             ],
             pacer_has_rss_feed=True,
         )
-        if 'all' != options['courts']:
+        if options['courts'] != ['all']:
             courts = courts.filter(pk__in=options['courts'])
 
         iterations_completed = 0
@@ -92,7 +95,7 @@ class Command(VerboseCommand):
                         date_last_build=lincolns_birthday,
                         is_sweep=options['sweep'],
                     )
-                if 'all' in options['courts'] and options['sweep'] is False:
+                if options['courts'] == ['all'] and options['sweep'] is False:
                     # If it's all courts and it's not a sweep, check if we did
                     # it recently.
                     five_minutes_ago = now() - timedelta(minutes=5)
