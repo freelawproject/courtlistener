b3a8b5707568b2794f3799acf85a486fe821a22d
mschwarzer <gitlab@i.mieo.de>
2018-05-09 11:49:25 +0200

    Added "related:" keyword prefix query for recommendations

--- cl/recommendations/migrations/0001_initial.py
+++ cl/recommendations/migrations/0001_initial.py
@@ -7,10 +7,36 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('search', '0062_add_indexes_to_title_section_fields'),
+        ('audio', '0005_auto_20161103_1436'),
+        ('people_db', '0036_remove_unique_constraint_on_party_name'),
+        ('search', '0063_add_pacer_rss_field'),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='AudioRecommendation',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('score', models.DecimalField(max_digits=12, decimal_places=8)),
+                ('recommendation', models.ForeignKey(related_name='recommendation_id', to='audio.Audio')),
+                ('seed', models.ForeignKey(related_name='seed_id', to='audio.Audio')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='DocketRecommendation',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('score', models.DecimalField(max_digits=12, decimal_places=8)),
+                ('recommendation', models.ForeignKey(related_name='recommendation_id', to='search.Docket')),
+                ('seed', models.ForeignKey(related_name='seed_id', to='search.Docket')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
         migrations.CreateModel(
             name='OpinionRecommendation',
             fields=[
@@ -22,5 +48,33 @@ class Migration(migrations.Migration):
             options={
                 'abstract': False,
             },
+        ),
+        migrations.CreateModel(
+            name='PersonRecommendation',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('score', models.DecimalField(max_digits=12, decimal_places=8)),
+                ('recommendation', models.ForeignKey(related_name='recommendation_id', to='people_db.Person')),
+                ('seed', models.ForeignKey(related_name='seed_id', to='people_db.Person')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.AlterUniqueTogether(
+            name='personrecommendation',
+            unique_together=set([('seed', 'recommendation')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='opinionrecommendation',
+            unique_together=set([('seed', 'recommendation')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='docketrecommendation',
+            unique_together=set([('seed', 'recommendation')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='audiorecommendation',
+            unique_together=set([('seed', 'recommendation')]),
         ),
     ]
--- cl/recommendations/models.py
+++ cl/recommendations/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-from cl.search.models import Opinion
+from cl.audio.models import Audio
+from cl.people_db.models import Person
+from cl.search.models import Opinion, Docket
 
 
 class BaseRecommendation(models.Model):
@@ -8,6 +10,7 @@ class BaseRecommendation(models.Model):
     seed = None
     recommendation = None
     score = models.DecimalField(max_digits=12, decimal_places=8)
+    available_models = ['opinion', 'audio', 'person', 'docket']
 
     class Meta:
         abstract = True
@@ -20,8 +23,29 @@ class BaseRecommendation(models.Model):
     def set_score(self, score):
         self.score = score
 
-    def __repr__(self):
-        return self.__str__()
+    def get_recommendations(self, seed=None, seed_id=None, n=10):
+        if seed is not None:
+            seed_id = seed.pk
+        elif seed is None and seed_id is None:
+            raise ValueError('At least "seed" or "seed_id" must be set.')
+
+        recs = []
+        for r in self._default_manager.filter(seed_id=seed_id).order_by('-score')[:n]:
+            recs.append(r.recommendation)
+        return recs
+
+    @staticmethod
+    def get_model_class(name):
+        if name == 'opinion':
+            return OpinionRecommendation
+        elif name == 'audio':
+            return AudioRecommendation
+        elif name == 'person':
+            return PersonRecommendation
+        elif name == 'docket':
+            return DocketRecommendation
+        else:
+            raise ValueError('Unsupported model name: %s' % name)
 
     def __str__(self):
         return 'Recommendation(seed=%s, recommendation=%s, score=%s)' % (self.seed, self.recommendation, self.score)
@@ -31,15 +55,19 @@ class OpinionRecommendation(BaseRecommendation):
     seed = models.ForeignKey(Opinion, related_name='seed_id', on_delete=models.CASCADE)
     recommendation = models.ForeignKey(Opinion, related_name='recommendation_id', on_delete=models.CASCADE)
 
-    @staticmethod
-    def get_recommendations(seed=None, seed_id=None, n=10):
 
-        if seed is not None:
-            seed_id = seed.pk
-        elif seed is None and seed_id is None:
-            raise ValueError('At least "seed" or "seed_id" must be set.')
+class AudioRecommendation(BaseRecommendation):
+    seed = models.ForeignKey(Audio, related_name='seed_id', on_delete=models.CASCADE)
+    recommendation = models.ForeignKey(Audio, related_name='recommendation_id', on_delete=models.CASCADE)
 
-        recs = []
-        for r in OpinionRecommendation.objects.filter(seed_id=seed_id).order_by('-score')[:n]:
-            recs.append(r.recommendation)
-        return recs
+
+class PersonRecommendation(BaseRecommendation):
+    seed = models.ForeignKey(Person, related_name='seed_id', on_delete=models.CASCADE)
+    recommendation = models.ForeignKey(Person, related_name='recommendation_id', on_delete=models.CASCADE)
+
+
+class DocketRecommendation(BaseRecommendation):
+    seed = models.ForeignKey(Docket, related_name='seed_id', on_delete=models.CASCADE)
+    recommendation = models.ForeignKey(Docket, related_name='recommendation_id', on_delete=models.CASCADE)
+
+
