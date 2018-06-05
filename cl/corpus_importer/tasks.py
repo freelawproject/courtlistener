9da668e303316ebd5c325604113b7f7b9c7f9186
John Hawkinson <jhawk@mit.edu>
2018-06-05 10:16:34 -0400

    corpus_importer/tasks.py: non-semantic PEP8 fixes
    pb3:cl jhawk$ flake8 recap/{api_serializers,models,tasks,tests}.py lib/pacer.py  corpus_importer/{tasks,tests}.py search/models.py
    corpus_importer/tasks.py:225:80: E501 line too long (80 > 79 characters)
    corpus_importer/tasks.py:252:80: E501 line too long (80 > 79 characters)
    corpus_importer/tasks.py:316:80: E501 line too long (81 > 79 characters)
    corpus_importer/tasks.py:345:80: E501 line too long (80 > 79 characters)
    corpus_importer/tasks.py:347:80: E501 line too long (80 > 79 characters)
    corpus_importer/tasks.py:384:80: E501 line too long (80 > 79 characters)
    corpus_importer/tasks.py:626:80: E501 line too long (80 > 79 characters)
    corpus_importer/tasks.py:733:1: W391 blank line at end of file

--- cl/corpus_importer/tasks.py
+++ cl/corpus_importer/tasks.py
@@ -222,7 +222,8 @@ def process_free_opinion_result(self, row_pk, cnt):
         self.request.callbacks = None
         return
 
-    return {'result': result, 'rd_pk': rd.pk, 'pacer_court_id': result.court_id}
+    return {'result': result, 'rd_pk': rd.pk,
+            'pacer_court_id': result.court_id}
 
 
 @app.task(bind=True, max_retries=15, interval_start=5, interval_step=5,
@@ -249,7 +250,8 @@ def get_and_process_pdf(self, data, session, row_pk, index=False):
             msg = "Ran into unknown HTTPError. %s. Aborting." % \
                   exc.response.status_code
             logger.error(msg)
-            PACERFreeDocumentRow.objects.filter(pk=row_pk).update(error_msg=msg)
+            PACERFreeDocumentRow.objects.filter(pk=row_pk).update(
+                error_msg=msg)
             self.request.callbacks = None
             return
 
@@ -313,7 +315,8 @@ def upload_pdf_to_ia(self, rd_pk):
             metadata={
                 'title': best_case_name(d),
                 'collection': settings.IA_COLLECTIONS,
-                'contributor': '<a href="https://free.law">Free Law Project</a>',
+                'contributor':
+                    '<a href="https://free.law">Free Law Project</a>',
                 'court': d.court_id,
                 'language': 'eng',
                 'mediatype': 'texts',
@@ -342,9 +345,10 @@ def upload_pdf_to_ia(self, rd_pk):
             increment_failure_count(rd)
             return [exc.response]
         if self.request.retries == self.max_retries:
-            # This exception is also raised when the endpoint is overloaded, but
-            # doesn't get caught in the OverloadedException below due to
-            # multiple processes running at the same time. Just give up for now.
+            # This exception is also raised when the endpoint is
+            # overloaded, but doesn't get caught in the
+            # OverloadedException below due to multiple processes
+            # running at the same time. Just give up for now.
             increment_failure_count(rd)
             return
         raise self.retry(exc=exc)
@@ -379,10 +383,11 @@ def upload_to_ia(identifier, files, metadata=None):
 
         https://internetarchive.readthedocs.io/en/latest/items.html
 
-    This function mirrors the IA library's similar upload function, but builds
-    in retries and various assumptions that make sense. Note that according to
-    emails with IA staff, it is best to maximize the number of files uploaded to
-    an Item at a time, rather than uploading each file in a separate go.
+    This function mirrors the IA library's similar upload function,
+    but builds in retries and various assumptions that make
+    sense. Note that according to emails with IA staff, it is best to
+    maximize the number of files uploaded to an Item at a time, rather
+    than uploading each file in a separate go.
 
     :param identifier: The global identifier within IA for the item you wish to
     work with.
@@ -623,7 +628,8 @@ def get_pacer_doc_by_rd_and_description(self, rd_pk, description_re, session,
             'date_upload': now(),
         },
     )
-    # Replace the description if we have description data. Else fallback on old.
+    # Replace the description if we have description data.
+    # Else fallback on old.
     rd.description = att_found.get('description', '') or rd.description
     if tag is not None:
         tag, _ = Tag.objects.get_or_create(name=tag)
@@ -730,4 +736,3 @@ def get_pacer_doc_id_with_show_case_doc_url(self, rd_pk, session):
         rd.pacer_doc_id = pacer_doc_id
         rd.save()
         logger.info("Successfully saved pacer_doc_id to rd %s" % rd_pk)
-
