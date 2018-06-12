8b52cd98a85e37c347108552d78a94f2f014d99f
Michael Lissner <mlissner@michaeljaylissner.com>
2018-06-12 13:14:21 -0700

    refactor(alert emails): Pulls the email subjects and other data into a separate file
    Follows pattern elsewhere, and makes the code nicer in general.

---
+++ cl/alerts/utils.py
@@ -0,0 +1,8 @@
+emails = {
+    'docket_alert': {
+        'subject': '[CourtListener] New alert for docket: %s',
+    },
+    'search_alert': {
+        'subject': '[CourtListener] New hits for your alerts'
+    }
+}
