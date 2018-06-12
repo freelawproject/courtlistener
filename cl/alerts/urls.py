36e5e00e7906b0820055fd276543d7a1838f2d2c
Michael Lissner <mlissner@michaeljaylissner.com>
2018-06-12 16:19:52 -0700

    feat(docket alerts): URL update to enable the new on/off toggle for docket alerts

--- cl/alerts/urls.py
+++ cl/alerts/urls.py
@@ -6,6 +6,7 @@ from cl.alerts.views import (
     disable_alert,
     edit_alert_redirect,
     enable_alert,
+    toggle_docket_alert,
 )
 
 urlpatterns = [
@@ -14,4 +15,6 @@ urlpatterns = [
     url(r'^alert/delete/confirm/(\d{1,6})/$', delete_alert_confirm),
     url(r'^alert/disable/([a-zA-Z0-9]{40})/$', disable_alert, name='disable_alert'),
     url(r'^alert/enable/([a-zA-Z0-9]{40})/$', enable_alert, name='enable_alert'),
+    url(r'^alert/docket/toggle/$', toggle_docket_alert,
+        name="toggle_docket_alert"),
 ]
