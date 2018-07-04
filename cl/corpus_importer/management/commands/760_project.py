d874534745b06668c3aa8e0d4d22e5960dcddd89
Michael Lissner <mlissner@michaeljaylissner.com>
2018-07-03 17:02:25 -0700

    feat(client work): Adds district court download code
    This is pretty fun code. It goes through a CSV, looks up the case using the docket number,
    downloads the case using the resulting pacer_case_id, saves it all, then makes it all searchable.

--- cl/corpus_importer/management/commands/760_project.py
+++ cl/corpus_importer/management/commands/760_project.py
@@ -6,7 +6,8 @@ from celery.canvas import chain
 from django.conf import settings
 from juriscraper.pacer import PacerSession
 
-from cl.corpus_importer.tasks import get_appellate_docket_by_docket_number
+from cl.corpus_importer.tasks import get_appellate_docket_by_docket_number, \
+    get_pacer_case_id_and_title, get_docket_by_pacer_case_id
 from cl.lib.celery_utils import CeleryThrottle
 from cl.lib.command_utils import VerboseCommand, logger
 from cl.search.tasks import add_or_update_recap_docket
@@ -17,11 +18,14 @@ PACER_PASSWORD = os.environ.get('PACER_PASSWORD', settings.PACER_PASSWORD)
 TAG = 'QAV5K6HU93A67WS6-760'
 
 
-def get_appellate_dockets(options):
-    """Download the appellate dockets described in the CSV."""
+def get_dockets(options):
+    """Download the dockets described in the CSV according to the `tasks`
+    option.
+    """
     f = options['file']
     reader = csv.DictReader(f)
     q = options['queue']
+    task = options['task']
     throttle = CeleryThrottle(queue_name=q)
     session = PacerSession(username=PACER_USERNAME, password=PACER_PASSWORD)
     session.login()
@@ -32,29 +36,51 @@ def get_appellate_dockets(options):
             break
         if row['Too Old'] == 'Yes':
             continue
-        if row['Appellate/District'] != 'Appellate':
+        if row['Appellate/District'].lower() != task:
+            # Only do appellate when appellate, and district when district.
             continue
 
         # All tests pass. Get the docket.
         throttle.maybe_wait()
-        chain(
-            get_appellate_docket_by_docket_number.s(
-                docket_number=row['Cleaned case_No'],
-                court_id=row['fjc_court_id'],
-                cookies=session.cookies,
-                tag=TAG,
-                **{
-                    'show_docket_entries': True,
-                    'show_orig_docket': True,
-                    'show_prior_cases': True,
-                    'show_associated_cases': True,
-                    'show_panel_info': True,
-                    'show_party_atty_info': True,
-                    'show_caption': True,
-                }
-            ).set(queue=q),
-            add_or_update_recap_docket.s().set(queue=q),
-        ).apply_async()
+        if task == 'appellate':
+            chain(
+                get_appellate_docket_by_docket_number.s(
+                    docket_number=row['Cleaned case_No'],
+                    court_id=row['fjc_court_id'],
+                    cookies=session.cookies,
+                    tag=TAG,
+                    **{
+                        'show_docket_entries': True,
+                        'show_orig_docket': True,
+                        'show_prior_cases': True,
+                        'show_associated_cases': True,
+                        'show_panel_info': True,
+                        'show_party_atty_info': True,
+                        'show_caption': True,
+                    }
+                ).set(queue=q),
+                add_or_update_recap_docket.s().set(queue=q),
+            ).apply_async()
+        elif task == 'district':
+            chain(
+                get_pacer_case_id_and_title.s(
+                    docket_number=row['Cleaned case_No'],
+                    court_id=row['fjc_court_id'],
+                    cookies=session.cookies,
+                    case_name=row['Title'],
+                ).set(queue=q),
+                get_docket_by_pacer_case_id.s(
+                    court_id=row['fjc_court_id'],
+                    cookies=session.cookies,
+                    tag=TAG,
+                    **{
+                        'show_parties_and_counsel': True,
+                        'show_terminated_parties': True,
+                        'show_list_of_member_cases': True
+                    }
+                ).set(queue=q),
+                add_or_update_recap_docket.s().set(queue=q),
+            ).apply_async()
 
 
 class Command(VerboseCommand):
@@ -102,15 +128,4 @@ class Command(VerboseCommand):
     def handle(self, *args, **options):
         super(Command, self).handle(*args, **options)
         logger.info("Using PACER username: %s" % PACER_USERNAME)
-
-        if options['task'] == 'appellate':
-            # Isolate the appellate opinions that aren't too old and download
-            # their dockets.
-            get_appellate_dockets(options)
-        elif options['task'] == 'district':
-            raise NotImplementedError("District isn't done yet. Soon.")
-        else:
-            raise argparse.ArgumentTypeError(
-                "Invalid task parameter. Valid parameters are %s" %
-                self.allowed_tasks
-            )
+        get_dockets(options)
