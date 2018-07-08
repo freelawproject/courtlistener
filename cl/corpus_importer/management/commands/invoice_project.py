014c648e0ff986744619234aa03a9d60885e8e24
Michael Lissner <mlissner@michaeljaylissner.com>
2018-07-07 22:45:36 -0700

    feat(client work): Add pagination to the query lookup.
    Getting 20000 results all at once is SLOW. It takes about 15 minutes. I imagine paginating will be
    *much* faster.

--- cl/corpus_importer/management/commands/invoice_project.py
+++ cl/corpus_importer/management/commands/invoice_project.py
@@ -3,6 +3,7 @@ import os
 from celery.canvas import chain
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from juriscraper.pacer import PacerSession
 
 from cl.corpus_importer.tasks import make_attachment_pq_object, \
@@ -23,38 +24,46 @@ def get_attachment_pages(options):
     """Find docket entries that look like invoices and get their attachment
     pages.
     """
+    page_size = 100
     query_string = 'q=document_type%3A"PACER+Document"+description%3Ainvoice&type=r&order_by=score+desc'
     main_query = build_main_query_from_query_string(
         query_string,
-        {'rows': 20000, 'fl': ['id', 'docket_id']},
+        {'rows': page_size, 'fl': ['id', 'docket_id']},
         {'group': False, 'facet': False},
     )
     si = ExtraSolrInterface(settings.SOLR_RECAP_URL, mode='r')
-    results = si.query().add_extra(**main_query).execute()
+    results = si.query().add_extra(**main_query)
 
     q = options['queue']
     recap_user = User.objects.get(username='recap')
     throttle = CeleryThrottle(queue_name=q)
     session = PacerSession(username=PACER_USERNAME, password=PACER_PASSWORD)
     session.login()
-    for i, result in enumerate(results):
-        if i < options['offset']:
-            continue
-        if i >= options['limit'] > 0:
-            break
+    paginator = Paginator(results, page_size)
+    i = 0
+    for page_number in range(1, paginator.num_pages + 1):
+        paged_results = paginator.page(page_number)
+        for result in paged_results.object_list:
+            if i < options['offset']:
+                i += 1
+                continue
+            if i >= options['limit'] > 0:
+                break
 
-        logger.info("Doing row %s: rd: %s", i, result['id'])
-        throttle.maybe_wait()
-        chain(
-            # Query the attachment page and process it
-            get_attachment_page_by_rd.s(
-                result['id'], session.cookies).set(queue=q),
-            # Take that in a new task and make a PQ object
-            make_attachment_pq_object.s(
-                result['id'], recap_user.pk).set(queue=q),
-            # And then process that using the normal machinery.
-            process_recap_attachment.s(tag_name=TAG).set(queue=q),
-        ).apply_async()
+            logger.info("Doing row %s: rd: %s, docket: %s", i, result['id'],
+                        result['docket_id'])
+            throttle.maybe_wait()
+            chain(
+                # Query the attachment page and process it
+                get_attachment_page_by_rd.s(
+                    result['id'], session.cookies).set(queue=q),
+                # Take that in a new task and make a PQ object
+                make_attachment_pq_object.s(
+                    result['id'], recap_user.pk).set(queue=q),
+                # And then process that using the normal machinery.
+                process_recap_attachment.s(tag_name=TAG).set(queue=q),
+            ).apply_async()
+            i += 1
 
 
 def get_documents(options):
