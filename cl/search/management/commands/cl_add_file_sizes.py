7616ba4e9fbf3c6608f59fb1f96d13dff0add306
Michael Lissner <mlissner@michaeljaylissner.com>
2018-07-09 21:01:16 -0700

    fix(file size script): Adds exception handling (figured we'd have this commit).

--- cl/search/management/commands/cl_add_file_sizes.py
+++ cl/search/management/commands/cl_add_file_sizes.py
@@ -10,7 +10,13 @@ class Command(VerboseCommand):
         super(Command, self).handle(*args, **options)
         qs = RECAPDocument.objects.filter(is_available=True)
         for i, rd in enumerate(queryset_generator(qs)):
-            rd.file_size = rd.filepath_local.size
+            try:
+                rd.file_size = rd.filepath_local.size
+            except OSError as e:
+                if e.errno != 2:
+                    # Problem other than No such file or directory.
+                    raise
+                continue
             rd.save()
             if i % 1000 == 0:
                 logger.info("Completed %s items", i)
