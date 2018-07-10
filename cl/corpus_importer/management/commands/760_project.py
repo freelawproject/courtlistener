5b8a6cfd612037af92b20b3ee324a95c1b8cb2e6
Michael Lissner <mlissner@michaeljaylissner.com>
2018-07-09 23:34:19 -0700

    feat(client work): Adds code for getting attachment pages

--- cl/corpus_importer/management/commands/760_project.py
+++ cl/corpus_importer/management/commands/760_project.py
@@ -4,12 +4,16 @@ import os
 
 from celery.canvas import chain
 from django.conf import settings
+from django.contrib.auth.models import User
 from juriscraper.pacer import PacerSession
 
 from cl.corpus_importer.tasks import get_appellate_docket_by_docket_number, \
-    get_pacer_case_id_and_title, get_docket_by_pacer_case_id
+    get_pacer_case_id_and_title, get_docket_by_pacer_case_id, \
+    get_attachment_page_by_rd, make_attachment_pq_object
 from cl.lib.celery_utils import CeleryThrottle
 from cl.lib.command_utils import VerboseCommand, logger
+from cl.recap.tasks import process_recap_attachment
+from cl.search.models import RECAPDocument
 from cl.search.tasks import add_or_update_recap_docket
 
 PACER_USERNAME = os.environ.get('PACER_USERNAME', settings.PACER_USERNAME)
@@ -84,12 +88,40 @@ def get_dockets(options):
             ).apply_async()
 
 
+def get_district_attachment_pages(options):
+    """Get the attachment page information for all of the items on the dockets
+
+    :param options: The options returned by argparse.
+    :type options: dict
+    """
+    q = options['queue']
+    recap_user = User.objects.get(username='recap')
+    throttle = CeleryThrottle(queue_name=q)
+    session = PacerSession(username=PACER_USERNAME, password=PACER_PASSWORD)
+    session.login()
+    rd_pks = RECAPDocument.objects.filter(
+                    tags__name=TAG).values_list('pk', flat=True)
+    for i, rd_pk in enumerate(rd_pks):
+        if i < options['offset']:
+            continue
+        if i >= options['limit'] > 0:
+            break
+        logger.info("Doing item %s: %s", i, rd_pk)
+        throttle.maybe_wait()
+        chain(
+            get_attachment_page_by_rd.s(rd_pk, session.cookies).set(queue=q),
+            make_attachment_pq_object.s(rd_pk, recap_user.pk).set(queue=q),
+            process_recap_attachment.s(tag_name=TAG).set(queue=q),
+        ).apply_async()
+
+
 class Command(VerboseCommand):
     help = "Look up dockets from a spreadsheet for a client and download them."
 
     allowed_tasks = [
         'appellate',
         'district',
+        'district_attachments',
     ]
 
     def add_arguments(self, parser):
@@ -129,4 +161,8 @@ class Command(VerboseCommand):
     def handle(self, *args, **options):
         super(Command, self).handle(*args, **options)
         logger.info("Using PACER username: %s" % PACER_USERNAME)
-        get_dockets(options)
+        if options['task'] in ['district', 'appellate']:
+            get_dockets(options)
+        elif options['task'] == 'district_attachments':
+            get_district_attachment_pages(options)
+
