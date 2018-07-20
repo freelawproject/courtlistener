0a3a240f1b53b887faf0425d48a8c6e78140508c
Michael Lissner <mlissner@michaeljaylissner.com>
2018-07-20 10:50:12 -0700

    fix(opinion downloader): Provides longer soft timeout and retries for SoftTimeLimitExceeded excs
    We've had some issues with some courts not getting completed properly. It seems to be correlated
    with the largest courts making it a doubly bad problem. I think the issue here is that these courts
    just have a lot of content and their reports are very slow. Doubling the timeout and allowing
    retries should help.

--- cl/corpus_importer/tasks.py
+++ cl/corpus_importer/tasks.py
@@ -85,7 +85,7 @@ def download_recap_item(self, url, filename, clobber=False):
                 shutil.copyfile(tmp.name, location)
 
 
-@app.task(bind=True, max_retries=2, soft_time_limit=60)
+@app.task(bind=True, max_retries=2, soft_time_limit=120)
 def get_and_save_free_document_report(self, court_id, start, end, cookies):
     """Download the Free document report and save it to the DB.
 
@@ -108,8 +108,10 @@ def get_and_save_free_document_report(self, court_id, start, end, cookies):
         if self.request.retries == self.max_retries:
             return PACERFreeDocumentLog.SCRAPE_FAILED
         raise self.retry(exc=exc, countdown=5)
-    except SoftTimeLimitExceeded:
-        return PACERFreeDocumentLog.SCRAPE_FAILED
+    except SoftTimeLimitExceeded as exc:
+        if self.request.retries == self.max_retries:
+            return PACERFreeDocumentLog.SCRAPE_FAILED
+        raise self.retry(exc=exc, countdown=5)
 
     try:
         results = report.data
