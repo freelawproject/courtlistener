50e8808785b9a36c6884803ea69ace35fb6a6ace
Michael Lissner <mlissner@michaeljaylissner.com>
2018-08-02 11:53:09 -0700

    fix(recap sitemaps): Adds aggressive caching to the sitemaps
    This will cache our sitemaps for an entire week. That's a long cache time, but it should be OK since
    these don't change much. The next commit changes the sitemaps to be ordered by PK instead of by
    dateFiled, which should make these more consistent as the DB changes.
    
    Partially addresses #855.

--- cl/opinion_page/sitemap.py
+++ cl/opinion_page/sitemap.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.views.decorators.cache import cache_page
 
 from cl.sitemap import make_sitemap_solr_params, make_solr_sitemap
 
@@ -14,11 +15,12 @@ def opinion_sitemap_maker(request):
     )
 
 
+@cache_page(60 * 60 * 24 * 7)  # A week
 def recap_sitemap_maker(request):
     return make_solr_sitemap(
         request,
         settings.SOLR_RECAP_URL,
-        make_sitemap_solr_params('dateFiled asc', 'r_sitemap'),
+        make_sitemap_solr_params('docket_id asc', 'r_sitemap'),
         'weekly',
         [],
         'docket_absolute_url',
