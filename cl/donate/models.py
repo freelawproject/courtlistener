2ee9d89af2c1d81dd9c303b592aac406486361fa
Michael Lissner <mlissner@michaeljaylissner.com>
2018-08-08 15:39:27 -0700

    feat(donations): New models for monthly subscriptions

--- cl/donate/models.py
+++ cl/donate/models.py
@@ -2,6 +2,21 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class PROVIDERS(object):
+    DWOLLA = 'dwolla'
+    PAYPAL = 'paypal'
+    CREDIT_CARD = 'cc'
+    CHECK = 'check'
+    BITCOIN = 'bitcoin'
+    NAMES = (
+        (DWOLLA, 'Dwolla'),
+        (PAYPAL, 'PayPal'),
+        (CREDIT_CARD, 'Credit Card'),
+        (CHECK, 'Check'),
+        (BITCOIN, 'Bitcoin'),
+    )
+
+
 class Donation(models.Model):
     # These statuses are shown on the profile page. Be warned.
     AWAITING_PAYMENT = 0
@@ -24,18 +39,6 @@ class Donation(models.Model):
         (FAILED, 'Failed'),
         (RECLAIMED_REFUNDED, 'Reclaimed/Refunded'),
         (CAPTURED, 'Captured'),
-    )
-    DWOLLA = 'dwolla'
-    PAYPAL = 'paypal'
-    CREDIT_CARD = 'cc'
-    CHECK = 'check'
-    BITCOIN = 'bitcoin'
-    PROVIDERS = (
-        (DWOLLA, 'Dwolla'),
-        (PAYPAL, 'PayPal'),
-        (CREDIT_CARD, 'Credit Card'),
-        (CHECK, 'Check'),
-        (BITCOIN, 'Bitcoin'),
     )
     donor = models.ForeignKey(
         User,
@@ -65,14 +68,16 @@ class Donation(models.Model):
     )
     payment_provider = models.CharField(
         max_length=50,
-        choices=PROVIDERS,
+        choices=PROVIDERS.NAMES,
         default=None,
     )
     payment_id = models.CharField(
-        'Internal ID used during a transaction',
+        help_text='Internal ID used during a transaction (used by PayPal and '
+                  'Stripe).',
         max_length=64,
     )
     transaction_id = models.CharField(
+        help_text="The ID of a transaction made in PayPal.",
         max_length=64,
         null=True,
         blank=True,
@@ -94,3 +99,41 @@ class Donation(models.Model):
 
     class Meta:
         ordering = ['-date_created']
+
+
+class MonthlyDonation(models.Model):
+    """The metadata needed to associate a monthly donation with a user."""
+    donor = models.ForeignKey(
+        User,
+        help_text="The user that made the donation",
+        related_name="monthly_donations",
+    )
+    date_modified = models.DateTimeField(
+        auto_now=True,
+        db_index=True,
+    )
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True
+    )
+    enabled = models.BooleanField(
+        help_text="Is this monthly donation enabled?",
+        default=True,
+    )
+    payment_provider = models.CharField(
+        max_length=50,
+        choices=PROVIDERS.NAMES,
+    )
+    monthly_donation_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+    monthly_donation_day = models.SmallIntegerField(
+        help_text="The day of the month that the monthly donation should be "
+                  "processed.",
+    )
+    stripe_customer_id = models.CharField(
+        help_text="The ID of the Stripe customer object that we use to charge "
+                  "credit card users each month.",
+        max_length=200,
+    )
