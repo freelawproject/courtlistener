4ab2ddc657c24d6f318086b5e34226c834259a3d
Michael Lissner <mlissner@michaeljaylissner.com>
2018-09-13 23:38:28 -0700

    feat(bot detectors): Refactors to create a base bot detector; adds a open graph bot detector
    Currently supports Facebook and Twitter

--- cl/lib/bot_detector.py
+++ cl/lib/bot_detector.py
@@ -1,12 +1,26 @@
+def base_bot_matcher(request, known_bots):
+    """Detect if a request's user agent is in a list of user agents
+
+    Matching is done by seeing if any of the items in the list of known_bots
+    is in the user agent for the request provided.
+    """
+    ua = request.META.get('HTTP_USER_AGENT', "Testing U-A")
+    for bot in known_bots:
+        if bot in ua.lower():
+            return True
+    return False
+
+
 def is_bot(request):
     """Checks if the thing making a request is a crawler."""
     known_bots = ['baiduspider', 'bingbot', 'dotbot', 'googlebot',
                   'kaloogabot', 'ia_archiver', 'msnbot', 'slurp',
                   'speedy spider', 'teoma', 'twiceler', 'yandexbot', 'yodaobot']
-    user_agent = request.META.get('HTTP_USER_AGENT', "Testing U-A")
-    for bot in known_bots:
-        if bot in user_agent.lower():
-            return True
+    return base_bot_matcher(request, known_bots)
 
-    return False
+
+def is_og_bot(request):
+    """Check if it's a bot that understands opengraph / twitter cards"""
+    known_bots = ['twitterbot', 'facebookexternalhit']
+    return base_bot_matcher(request, known_bots)
 
