0676c23dcdfa42c2e6c8dbb5f86ffe1eba21ce41
Michael Lissner <mlissner@michaeljaylissner.com>
2018-09-14 16:18:51 -0700

    fix(thumbnails): Replace thumbnail pipeline
    This took too long to figure out. We had imagemagick in here, but the versions on my laptop and on
    the server were creating horrid thumbnails. The version on @jhwak's laptop, meanwhile did a fine job
    of it. Anyway, at @jhawk's suggestion, this switches the tool for this job to pdftoppm, which does
    a *much* better job, and does so consistently.
    
    But, all is not so easy. Though it does a great job making thumbnails, it has a bit of a weird API:
    
     - It can't be used with tempfiles because it doesn't accept output file names. Instead it accepts
       a prefix, which it then tacks its own junk onto the end of. But if you leave off the output file
       name, it'll send its output to stdout, which we can capture. So that's what we do instead. Fine.
    
     - It doesn't let you scale to 'x width with auto scaled height'. Instead, you can only tell it the
       max you want any dimension to be, so we do that instead. Fine. @jhawk thinks using the -r
       resolution flag would be better, but they're similar ideas.
    
    Anyway, this should produce better thumbs than imagemagick.

--- cl/people_db/tasks.py
+++ cl/people_db/tasks.py
@@ -1,15 +1,14 @@
 import subprocess
-from tempfile import NamedTemporaryFile
 
-from django.core.files import File
+from django.core.files.base import ContentFile
 
 from cl.celery import app
 from cl.people_db.models import FinancialDisclosure
 from cl.search.models import THUMBNAIL_STATUSES, RECAPDocument
 
 
-def make_png_thumbnail_for_instance(pk, InstanceClass, file_attr, width,
-                                    tmp_prefix):
+def make_png_thumbnail_for_instance(pk, InstanceClass, file_attr,
+                                    max_dimension):
     """Abstract function for making a thumbnail for a PDF
 
     See helper functions below for how to use this in a simple way.
@@ -17,35 +16,38 @@ def make_png_thumbnail_for_instance(pk, InstanceClass, file_attr, width,
     :param pk: The PK of the item to make a thumbnail for
     :param InstanceClass: The class of the instance
     :param file_attr: The attr where the PDF is located on the item
-    :param width: The width in pixels of the desired thumb
-    :param tmp_prefix: The prefix to use for the temporary file used during
-    thumbnail creation.
+    :param max_dimension: The longest you want any edge to be
     """
     item = InstanceClass.objects.get(pk=pk)
-    with NamedTemporaryFile(prefix=tmp_prefix, suffix=".png") as tmp:
-        convert = [
-            'convert',
-            # Only do the first page.
-            '%s[0]' % getattr(item, file_attr).path,
-            '-thumbnail', '%s' % width,
-            # This and the next line handle transparency problems
-            '-background', 'white',
-            '-alpha', 'remove',
-            tmp.name,
-        ]
-        p = subprocess.Popen(convert, close_fds=True, stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE, universal_newlines=True)
-        stdout, stderr = p.communicate()
-        if p.returncode != 0:
-            item.thumbnail_status = THUMBNAIL_STATUSES.FAILED
-            item.save()
-            return item.pk
+    command = [
+        'pdftoppm',
+        '-jpeg',
+        getattr(item, file_attr).path,
+        # Start and end on the first page
+        '-f', '1',
+        '-l', '1',
+        # Set the max dimension (generally the height). Alas, we can't just
+        # set the width, so this is our only hope.
+        '-scale-to', str(max_dimension),
+    ]
 
-        item.thumbnail_status = THUMBNAIL_STATUSES.COMPLETE
-        filename = '%s.thumb.%sw.png' % (pk, width)
-        item.thumbnail.save(filename, File(tmp))
-
+    # Note that pdftoppm adds things like -01.png to the end of whatever
+    # filename you give it, which makes using a temp file difficult. But,
+    # if you don't give it an output file, it'll send the result to stdout,
+    # so that's why we are capturing it here.
+    p = subprocess.Popen(command, close_fds=True, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    if p.returncode != 0:
+        item.thumbnail_status = THUMBNAIL_STATUSES.FAILED
+        item.save()
         return item.pk
+
+    item.thumbnail_status = THUMBNAIL_STATUSES.COMPLETE
+    filename = '%s.thumb.%s.jpeg' % (pk, max_dimension)
+    item.thumbnail.save(filename, ContentFile(stdout))
+
+    return item.pk
 
 
 @app.task
@@ -54,8 +56,7 @@ def make_financial_disclosure_thumbnail_from_pdf(pk):
         pk=pk,
         InstanceClass=FinancialDisclosure,
         file_attr='filepath',
-        width=350,
-        tmp_prefix='financial_disclosure_',
+        max_dimension=350,
     )
 
 
@@ -65,8 +66,7 @@ def make_recap_document_thumbnail_from_pdf(pk):
         pk=pk,
         InstanceClass=RECAPDocument,
         file_attr='filepath_local',
-        width=700,
-        tmp_prefix='recap_thumb_',
+        max_dimension=1068,
     )
 
 
