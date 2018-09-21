c7541929fce740a25fc83044fbc6af2a51f11bae
Michael Lissner <mlissner@michaeljaylissner.com>
2018-09-21 11:19:37 -0700

    feat(citations): Better error reporting for bad citations

--- cl/search/management/commands/populate_citation_table.py
+++ cl/search/management/commands/populate_citation_table.py
@@ -56,7 +56,8 @@ class Command(VerboseCommand):
                             disambiguate=False,
                         )[0]
                     except IndexError:
-                        print("Errored out on: %s" % citation_str)
+                        print("Errored out on: %s in %s" % (citation_str,
+                                                            cluster.pk))
                         exit(1)
                     try:
                         Citation.objects.create(
