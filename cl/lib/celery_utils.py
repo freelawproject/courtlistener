9c227bde5b569c23a883519888bd553974aa5590
Michael Lissner <mlissner@michaeljaylissner.com>
2018-10-22 17:04:57 -0700

    style(celery): Automated whitespace cleanup

--- cl/lib/celery_utils.py
+++ cl/lib/celery_utils.py
@@ -13,22 +13,22 @@ DEFAULT_PRIORITY_STEPS = [0, 3, 6, 9]
 
 def make_queue_name_for_pri(queue, pri):
     """Make a queue name for redis
-    
+
     Celery uses PRIORITY_SEP to separate different priorities of tasks into
     different queues in Redis. Each queue-priority combination becomes a key in
     redis with names like:
-    
+
      - batch1\x06\x163 <-- P3 queue named batch1
-     
-    There's more information about this in Github, but it doesn't look like it 
+
+    There's more information about this in Github, but it doesn't look like it
     will change any time soon:
-     
+
       - https://github.com/celery/kombu/issues/422
-      
+
     In that ticket the code below, from the Flower project, is referenced:
-    
+
       - https://github.com/mher/flower/blob/master/flower/utils/broker.py#L135
-        
+
     :param queue: The name of the queue to make a name for.
     :param pri: The priority to make a name with.
     :return: A name for the queue-priority pair.
@@ -41,7 +41,7 @@ def make_queue_name_for_pri(queue, pri):
 
 def get_queue_length(queue_name='celery'):
     """Get the number of tasks in a celery queue.
-    
+
     :param queue_name: The name of the queue you want to inspect.
     :return: the number of items in the queue.
     """
@@ -60,10 +60,10 @@ class CeleryThrottle(object):
 
     def __init__(self, min_items=100, min_wait=0, queue_name='celery'):
         """Create a throttle to prevent celery run aways.
-        
-        :param min_items: The minimum number of items that should be enqueued. 
-        A maximum of 2× this number may be created. This minimum value is not 
-        guaranteed and so a number slightly higher than your max concurrency 
+
+        :param min_items: The minimum number of items that should be enqueued.
+        A maximum of 2× this number may be created. This minimum value is not
+        guaranteed and so a number slightly higher than your max concurrency
         should be used. Note that this number includes all tasks unless you use
         a specific queue for your processing.
         :param min_wait: The minimum amount of time that should be waited every
@@ -100,13 +100,13 @@ class CeleryThrottle(object):
 
     def maybe_wait(self):
         """Stall the calling function or let it proceed, depending on the queue.
-        
-        The idea here is to check the length of the queue as infrequently as 
-        possible while keeping the number of items in the queue as closely 
+
+        The idea here is to check the length of the queue as infrequently as
+        possible while keeping the number of items in the queue as closely
         between self.min and self.max as possible.
-        
-        We do this by immediately enqueueing self.max items. After that, we 
-        monitor the queue to determine how quickly it is processing items. Using 
+
+        We do this by immediately enqueueing self.max items. After that, we
+        monitor the queue to determine how quickly it is processing items. Using
         that rate we wait an appropriate amount of time or immediately press on.
         """
         self.last_processed_count += 1
