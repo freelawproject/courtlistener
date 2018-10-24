0e747ca8c54695b780ea7ce0f7f0d8edf72f1296
David Xia <dxia@spotify.com>
2018-10-24 12:06:52 -0400

    Start automatically running tests on circleci.com
    We'll start small with just testing one module, `cl.corpus_importer`.
    
    Motivation: there's no CI/CD for this project right now. Humans forget to run
    the test suite before merging.  This can lead to bugs getting deployed to
    production.  Let's use circleci.com, a CI/CD platform that's free for open
    source projects, to run tests. We'll use circleci.com instead of travis-ci.com
    because Circle supports using custom Docker images as the test executor.
    [Travis doesn't](https://github.com/travis-ci/travis-ci/issues/7726).
    
    There are lots of dependencies that need to be
    installed before the tests in this repo can be run.
    
    `.circleci/Dockerfile` creates a Docker image that has test dependencies
    preinstalled to speed up test executions. Installing all the test dependencies
    at testing time would take nearly ten minutes. So we pre-install the
    dependencies since they change infrequently.
    
    For now we're just running a small set of tests to prove this works.
    I'm not sure if the test files are structured according to Django
    best practices.

---
+++ .circleci/configure-courtlistener.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p /var/log/courtlistener
+
+# Install required debian packages.
+apt-get update -qq && apt-get install -y --no-install-recommends \
+  build-essential \
+  git \
+  libpq-dev \
+  libxml2-dev \
+  libxslt1-dev \
+  python-dev \
+  python-pip
+
+# Install, create, and activate a virtualenv.
+pip install virtualenv
+virtualenv ~/virtualenvs/courtlistener
+source ~/virtualenvs/courtlistener/bin/activate
+
+pip install -r /tmp/requirements.txt
+pip install -r /tmp/requirements-test.txt
+# This dependency isn't in the requirements file for some reason.
+pip install git+https://github.com/freelawproject/judge-pics@master
