12b0dde520c5a1dfb9eb06cf051f6520f9f61955
Michael Lissner <mlissner@michaeljaylissner.com>
2019-02-12 17:29:00 -0800

    feat(payment urls): Move logic to urls by allowing template_name variables and named urls

--- cl/donate/urls.py
+++ cl/donate/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from cl.donate.paypal import process_paypal_callback, donate_paypal_cancel
 from cl.donate.stripe_helpers import process_stripe_callback
-from cl.donate.views import donate, donate_complete, make_check_donation, \
+from cl.donate.views import donate, payment_complete, make_check_donation, \
     toggle_monthly_donation, cc_payment
 from cl.users.views import view_donations
 
@@ -10,20 +10,36 @@ urlpatterns = [
     # Donations & payments
     url(r'^donate/$', donate, name="donate"),
     url(r'^pay/$', cc_payment, name="cc_payment"),
+    url(
+        r'^donate/complete/$',
+        payment_complete,
+        {
+            'template_name': 'donate_complete.html',
+        },
+        name='donate_complete'
+    ),
+    url(
+        r'^pay/complete/$',
+        payment_complete,
+        {
+            'template_name': 'payment_complete.html',
+        },
+        name='payment_complete'
+    ),
+
 
     # Paypal
-    url(r'^donate/paypal/complete/$', donate_complete, name='paypal_complete'),
-    url(r'^donate/paypal/cancel/$', donate_paypal_cancel),
-    url(r'^donate/callbacks/paypal/$', process_paypal_callback),
+    url(r'^donate/paypal/cancel/$', donate_paypal_cancel,
+        name='paypal_cancel'),
+    url(r'^donate/callbacks/paypal/$', process_paypal_callback,
+        name='paypal_callback'),
 
     # Stripe
-    url(r'^donate/stripe/complete/$', donate_complete, name='stripe_complete'),
     url(r'^donate/callbacks/stripe/$', process_stripe_callback,
         name='stripe_callback'),
 
     # Checks
     url(r'^donate/check/$', make_check_donation, name='make_check_donation'),
-    url(r'^donate/check/complete/$', donate_complete, name='check_complete'),
 
     # Profile page
     url(r'^profile/donations/$', view_donations, name='profile_donations'),
