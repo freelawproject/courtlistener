a12a541391f03e37157277eac0a0c6e6e100f3bf
Michael Lissner <mlissner@michaeljaylissner.com>
2019-08-15 12:29:53 -0400

    feat(models): Create AbstractPDF model as a base class for our PDF models
    This is a small refactoring so that we have a common base for PDF related functionality. Things like
    OCR, thumbnails, etc. All of this became needed because we have the parallel pressures from Claims
    history content and from LASC.

--- cl/search/models.py
+++ cl/search/models.py
@@ -16,7 +16,8 @@ from cl.custom_filters.templatetags.text_filters import best_case_name
 from cl.lib import fields
 from cl.lib.date_time import midnight_pst
 from cl.lib.model_helpers import make_upload_path, make_recap_path, \
-    make_recap_pdf_path, make_recap_thumb_path, make_docket_number_core
+    make_docket_number_core
+from cl.lib.models import AbstractPDF
 from cl.lib.search_index_utils import InvalidDocumentError, null_map, \
     normalize_search_dicts
 from cl.lib.storage import IncrementingFileSystemStorage
@@ -52,17 +53,6 @@ SOURCES = (
     ('ZCR', 'columbia merged with court and resource.org'),
     ('ZL', 'columbia merged with lawbox'),
 )
-
-
-class THUMBNAIL_STATUSES(object):
-    NEEDED = 0
-    COMPLETE = 1
-    FAILED = 2
-    NAMES = (
-        (NEEDED, "Thumbnail needed"),
-        (COMPLETE, "Thumbnail completed successfully"),
-        (FAILED, 'Unable to generate thumbnail'),
-    )
 
 
 class OriginatingCourtInformation(models.Model):
@@ -886,26 +876,6 @@ class DocketEntry(models.Model):
 
 
 class AbstractPacerDocument(models.Model):
-    OCR_COMPLETE = 1
-    OCR_UNNECESSARY = 2
-    OCR_FAILED = 3
-    OCR_NEEDED = 4
-    OCR_STATUSES = (
-        (OCR_COMPLETE, "OCR Complete"),
-        (OCR_UNNECESSARY, "OCR Not Necessary"),
-        (OCR_FAILED, "OCR Failed"),
-        (OCR_NEEDED, "OCR Needed"),
-    )
-    date_created = models.DateTimeField(
-        help_text="The date the file was imported to Local Storage.",
-        auto_now_add=True,
-        db_index=True,
-    )
-    date_modified = models.DateTimeField(
-        help_text="Timestamp of last update.",
-        auto_now=True,
-        db_index=True,
-    )
     date_upload = models.DateTimeField(
         help_text="upload_date in RECAP. The date the file was uploaded to "
                   "RECAP. This information is provided by RECAP.",
@@ -937,62 +907,6 @@ class AbstractPacerDocument(models.Model):
         null=True,
         default=False,
     )
-    sha1 = models.CharField(
-        help_text="The ID used for a document in RECAP",
-        max_length=40,  # As in RECAP
-        blank=True,
-    )
-    page_count = models.IntegerField(
-        help_text="The number of pages in the document, if known",
-        blank=True,
-        null=True,
-    )
-    file_size = models.IntegerField(
-        help_text="The size of the file in bytes, if known",
-        blank=True,
-        null=True,
-    )
-    filepath_local = models.FileField(
-        help_text="The path of the file in the local storage area.",
-        upload_to=make_recap_pdf_path,
-        storage=IncrementingFileSystemStorage(),
-        max_length=1000,
-        db_index=True,
-        blank=True,
-    )
-    filepath_ia = models.CharField(
-        help_text="The URL of the file in IA",
-        max_length=1000,
-        blank=True,
-    )
-    ia_upload_failure_count = models.SmallIntegerField(
-        help_text="Number of times the upload to the Internet Archive failed.",
-        null=True,
-        blank=True,
-    )
-    thumbnail = models.FileField(
-        help_text="A thumbnail of the first page of the document",
-        upload_to=make_recap_thumb_path,
-        storage=IncrementingFileSystemStorage(),
-        null=True,
-        blank=True,
-    )
-    thumbnail_status = models.SmallIntegerField(
-        help_text="The status of the thumbnail generation",
-        choices=THUMBNAIL_STATUSES.NAMES,
-        default=THUMBNAIL_STATUSES.NEEDED,
-    )
-    plain_text = models.TextField(
-        help_text="Plain text of the document after extraction using "
-                  "pdftotext, wpd2txt, etc.",
-        blank=True,
-    )
-    ocr_status = models.SmallIntegerField(
-        help_text="The status of OCR processing on this item.",
-        choices=OCR_STATUSES,
-        null=True,
-        blank=True,
-    )
     is_free_on_pacer = models.NullBooleanField(
         help_text="Is this item freely available as an opinion on PACER?",
         db_index=True,
@@ -1006,7 +920,7 @@ class AbstractPacerDocument(models.Model):
         abstract = True
 
 
-class RECAPDocument(AbstractPacerDocument):
+class RECAPDocument(AbstractPacerDocument, AbstractPDF):
     """
         The model for Docket Documents and Attachments.
     """
@@ -1478,7 +1392,7 @@ class Claim(models.Model):
     )
 
 
-class ClaimHistory(AbstractPacerDocument):
+class ClaimHistory(AbstractPacerDocument, AbstractPDF):
     DOCKET_ENTRY = 1
     CLAIM_ENTRY = 2
     CLAIM_TYPES = (
