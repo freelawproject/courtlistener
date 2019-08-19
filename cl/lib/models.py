7610c51b2a984d1cae40f4aa377755b74963dd2a
vvilliam <wpalin@gmail.com>
2019-08-19 10:54:30 -0400

    Add LASCPDF Content type
    (Feat) Create LASCPDF and JSON file types.
    
    Add LASCPDF Content type
    Refactor JSON content type to LASCJSON
    Add AbstractDocument Base class.
    
    This code will be substantially changed when merged into Master

--- cl/lib/models.py
+++ cl/lib/models.py
@@ -11,19 +11,24 @@ from cl.lib.storage import UUIDFileSystemStorage
 
 
 class UPLOAD_TYPE:
-    CASE_JSON = 1
-    CASE_HTML = 2
+    JSON = 1
+    HTML = 2
     PDF = 3
 
     NAMES = (
-        (CASE_JSON, 'JSON Response'),
-        (CASE_HTML, 'HTML attachment page'),
+        (JSON, 'JSON'),
+        (HTML, 'HTML'),
         (PDF, 'PDF'),
     )
 
 def make_json_data_path(instance, filename):
     # return make_path('recap-data', filename)
     return make_path('json-data', filename)
+
+def make_pdf_path(instance, filename):
+    # return make_path('recap-data', filename)
+    return make_path('pdf-data', filename)
+
 
 def make_path(root, filename):
     d = now()
@@ -35,12 +40,8 @@ def make_path(root, filename):
         filename,
     )
 
-class JSONFile(models.Model):
-    """This is a simple object for holding original JSON content from any court api
+class AbstractDocument(models.Model):
 
-    We will use this maintain a copy of all json acquired from LASC which is important
-    in the event we lose our database.
-    """
 
     date_created = models.DateTimeField(
         help_text="The time when this item was created",
@@ -52,16 +53,32 @@ class JSONFile(models.Model):
         auto_now=True,
         db_index=True,
     )
+    upload_type = models.SmallIntegerField(
+        help_text="The type of object that is uploaded",
+        choices=UPLOAD_TYPE.NAMES,
+    )
+
+    class Meta:
+        abstract = True
+
+    pass
+
+
+class LASCJSON(AbstractDocument):
+    """This is a simple object for holding original JSON content from any court api
+
+    We will use this maintain a copy of all json acquired from LASC which is important
+    in the event we lose our database.
+    """
+
     filepath = models.FileField(
         help_text="The path of the original json file.",
         upload_to=make_json_data_path,
         storage=UUIDFileSystemStorage(),
         max_length=150,
     )
-    upload_type = models.SmallIntegerField(
-        help_text="The type of object that is uploaded",
-        choices=UPLOAD_TYPE.NAMES,
-    )
+
+
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
@@ -74,3 +91,30 @@ class JSONFile(models.Model):
     def print_file_contents(self):
         print(self.file_contents)
 
+
+class LASCPDF(AbstractDocument):
+    """This is a simple object for holding original JSON content from any court api
+
+    We will use this maintain a copy of all json acquired from LASC which is important
+    in the event we lose our database.
+    """
+
+    filepath = models.FileField(
+        help_text="The path of the original json file.",
+        upload_to=make_pdf_path,
+        storage=UUIDFileSystemStorage(),
+        max_length=150,
+    )
+
+
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+    @property
+    def file_contents(self):
+        with open(self.filepath.path, 'r') as f:
+            return f.read().decode('utf-8')
+
+    def print_file_contents(self):
+        print(self.file_contents)
