239e7c9e1dfb86a5ffcc9668d0fcc12ebf4250f6
vvilliam <wpalin@gmail.com>
2019-08-29 19:44:09 -0400

    refactor (CSSAdminMixin): Add Mixin to lasc.admin.py
    Add import for CSSAdminMixin to admin
    Add CSSMixin to Base class for admin

--- cl/lasc/admin.py
+++ cl/lasc/admin.py
@@ -1,17 +1,11 @@
 from django.contrib import admin
-from django.forms import TextInput, Textarea
-from django.db import models
-
+from cl.lib.admin import CSSAdminMixin
 from cl.lasc.models import Action, CrossReference, Docket, DocumentFiled, \
     DocumentImage, LASCJSON, LASCPDF, Party, Proceeding, \
     QueuedCase, QueuedPDF, TentativeRuling
 
 
-class Base(admin.ModelAdmin):
-    formfield_overrides = {
-        models.CharField: {'widget': TextInput(attrs={'size':'20'})},
-        models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
-    }
+class Base(admin.ModelAdmin, CSSAdminMixin):
 
     readonly_fields = (
         'date_created',
