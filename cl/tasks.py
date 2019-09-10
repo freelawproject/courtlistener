514e08580f7e29a76060f67fba3172cf9bb2622b
Michael Lissner <mlissner@michaeljaylissner.com>
2019-09-10 14:35:32 -0700

    fix(lasc): Remove another orphan file

--- cl/tasks.py
+++
@@ -1,6 +0,0 @@
-# from celery import Celery
-#
-# # app = Celery('tasks', broker='pyamqp://guest@localhost//')
-# # app = Celery('tasks', broker='redis://guest@localhost//')
-# # app = Celery('tasks', broker='redis://localhost:6379/0')
-#
