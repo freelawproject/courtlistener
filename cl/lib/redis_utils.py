21a3376c083df845582d6f19d12f58c307ed554f
Michael Lissner <mlissner@michaeljaylissner.com>
2019-09-26 23:05:34 -0700

    docs(lib): Better docstring for make_redis_interface

--- cl/lib/redis_utils.py
+++ cl/lib/redis_utils.py
@@ -6,6 +6,7 @@ def make_redis_interface(db_name):
     """Create a redis connection object
 
     :param db_name: The name of the database to use, as defined in our settings
+    :return StrictRedis interface using django settings
     """
     return redis.StrictRedis(host=settings.REDIS_HOST,
                              port=settings.REDIS_PORT,
