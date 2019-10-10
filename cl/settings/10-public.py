a15d8f3569c3f37450fc98fab60f1ba63d1a4c25
vvilliam <wpalin@gmail.com>
2019-10-10 12:50:35 -0400

    Chore (Public Settings) Alphabetize Storages in settings

--- cl/settings/10-public.py
+++ cl/settings/10-public.py
@@ -81,8 +81,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
-    'django_filters',
     'storages',
+    'django_filters',
 
     # CourtListener Apps
     'cl.alerts',
