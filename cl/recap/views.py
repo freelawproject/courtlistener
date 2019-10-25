65001a985d650ef180d7f38173806e8d191c550e
Michael Lissner <mlissner@michaeljaylissner.com>
2019-10-24 17:32:32 -0700

    feat(recap fetch): New view for the Pacer fetch API

--- cl/recap/views.py
+++ cl/recap/views.py
@@ -3,9 +3,12 @@ from rest_framework.viewsets import ModelViewSet
 from cl.api.utils import RECAPUploaders, LoggingMixin, RECAPUsersReadOnly, \
     BigPagination
 from cl.recap.api_serializers import ProcessingQueueSerializer, \
-    PacerDocIdLookUpSerializer, FjcIntegratedDatabaseSerializer
-from cl.recap.filters import ProcessingQueueFilter, FjcIntegratedDatabaseFilter
-from cl.recap.models import ProcessingQueue, FjcIntegratedDatabase
+    PacerDocIdLookUpSerializer, FjcIntegratedDatabaseSerializer, \
+    PacerFetchQueueSerializer
+from cl.recap.filters import ProcessingQueueFilter, \
+    FjcIntegratedDatabaseFilter, PacerFetchQueueFilter
+from cl.recap.models import ProcessingQueue, FjcIntegratedDatabase, \
+    PacerFetchQueue
 from cl.recap.tasks import process_recap_upload
 from cl.search.filters import RECAPDocumentFilter
 from cl.search.models import RECAPDocument
@@ -23,6 +26,19 @@ class PacerProcessingQueueViewSet(LoggingMixin, ModelViewSet):
     def perform_create(self, serializer):
         pq = serializer.save(uploader=self.request.user)
         process_recap_upload(pq)
+
+
+class PacerFetchRequestViewSet(LoggingMixin, ModelViewSet):
+    queryset = PacerFetchQueue.objects.all().order_by('-id')
+    serializer_class = PacerFetchQueueSerializer
+    filter_class = PacerFetchQueueFilter
+    ordering_fields = (
+        'date_created', 'date_modified', 'date_completed',
+    )
+
+    def perform_create(self, serializer):
+        fq = serializer.save(user=self.request.user)
+        #do_pacer_fetch(fq)
 
 
 class PacerDocIdLookupViewSet(LoggingMixin, ModelViewSet):
