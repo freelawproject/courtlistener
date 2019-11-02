1df26d1fadeb2574f2b4c187cb58fd7584908c77
vvilliam <wpalin@gmail.com>
2019-11-01 22:33:59 -0400

    bug (document_proc.): Unwind env variable in antiword doc processing
    Tests failed with env added to subprocess.  Unwound to original code.
    All other tests succeeded

--- cl/lib/document_processors.py
+++ cl/lib/document_processors.py
@@ -130,12 +130,8 @@ def extract_from_doc(path):
     https://stackoverflow.com/questions/13404116/subprocess-popen-command-antiword-produces-different-output-in-shell-vs-web-a
     """
 
-    new_env = dict(os.environ)  # Copy current environment
-    new_env['LANG'] = 'en_US.UTF-8'
-
     process = subprocess.Popen(['antiword', path, '-i', '1'], shell=False,
-                               stdout=subprocess.PIPE, stderr=DEVNULL,
-                               env=new_env)
+                               stdout=subprocess.PIPE, stderr=DEVNULL)
     content, err = process.communicate()
     content = content.decode('utf-8', errors='ignore')
     return content, err
