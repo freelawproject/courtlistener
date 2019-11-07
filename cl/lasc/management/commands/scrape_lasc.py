ddc6f062f3860148801dc2c379f5f06d841ee2d0
vvilliam <wpalin@gmail.com>
2019-11-07 11:00:08 -0500

    fix(scrape_lasc): Remove Queryset Generator

--- cl/lasc/management/commands/scrape_lasc.py
+++ cl/lasc/management/commands/scrape_lasc.py
@@ -108,9 +108,9 @@ def process_pdf_queue(options):
 
     :return: None
     """
-    pdf_pks = queryset_generator(QueuedPDF.objects.all()
-                                 .order_by('-document_id')
-                                 .values_list('pk', flat=True))
+    pdf_pks = QueuedPDF.objects.all()\
+                    .order_by('-document_id')\
+                    .values_list('pk', flat=True)
     q = options['queue']
     throttle = CeleryThrottle(queue_name=q)
     for pdf_pk in pdf_pks:
