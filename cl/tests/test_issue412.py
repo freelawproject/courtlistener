63907a8a43812afc1fb7c70cecc8d2099d00c125
David Cook <divergentdave@gmail.com>
2019-12-02 17:19:36 -0600

    refactor(tests): send_keys('\n') => submit() (#1069)
    It seems that geckodriver doesn't submit forms when send_keys() is
    passed a line feed, but ChromeDriver does. Use submit() throughout for
    future compatibility, if needed.

--- cl/tests/test_issue412.py
+++ cl/tests/test_issue412.py
@@ -152,7 +152,7 @@ class AudioBlockedFromSearchEnginesTest(BaseSeleniumTest):
         # She lands on the advanced search screen for OA, and does a wildcard
         # search.
         searchbox = self.browser.find_element_by_id('id_q')
-        searchbox.send_keys('\n')
+        searchbox.submit()
 
         # The SERP updates and she selects the one she knows is blocked
         blocked_argument = self.browser.find_element_by_link_text(
@@ -177,7 +177,7 @@ class AudioBlockedFromSearchEnginesTest(BaseSeleniumTest):
         # She lands on the advanced search screen for OA, and does a wildcard
         # search.
         searchbox = self.browser.find_element_by_id('id_q')
-        searchbox.send_keys('\n')
+        searchbox.submit()
 
         # The SERP updates and she selects the one she knows is blocked
         self.click_link_for_new_page('Blocked Oral Argument (Test 2015)')
@@ -199,7 +199,7 @@ class AudioBlockedFromSearchEnginesTest(BaseSeleniumTest):
         # She lands on the advanced search screen for OA, and does a wildcard
         # search.
         searchbox = self.browser.find_element_by_id('id_q')
-        searchbox.send_keys('\n')
+        searchbox.submit()
 
         # The SERP updates and she selects the one she knows is blocked
         blocked_argument = self.browser.find_element_by_link_text(
