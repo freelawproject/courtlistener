7420d82ab1f26efda7b39657d65b60c60738e043
Michael Lissner <mlissner@michaeljaylissner.com>
2019-12-20 16:48:30 -0800

    refactor(citations): Simplify citation mapping function
    Also: Correct the lexis and west parameters.

--- cl/citations/utils.py
+++ cl/citations/utils.py
@@ -7,21 +7,14 @@ def map_reporter_db_cite_type(citation_type):
     :param citation_type: A value from REPORTERS['some-key']['cite_type']
     :return: A value from the search.models.Citation object
     """
-    if citation_type == "specialty":
-        model_citation_type = Citation.SPECIALTY
-    elif citation_type == "federal":
-        model_citation_type = Citation.FEDERAL
-    elif citation_type == "state":
-        model_citation_type = Citation.STATE
-    elif citation_type == "state_regional":
-        model_citation_type = Citation.STATE_REGIONAL
-    elif citation_type == "neutral":
-        model_citation_type = Citation.NEUTRAL
-    elif citation_type == "lexis":
-        model_citation_type = Citation.LEXIS
-    elif citation_type == "west":
-        model_citation_type = Citation.WEST
-    elif citation_type == "scotus_early":
-        model_citation_type = Citation.SCOTUS_EARLY
-    else:
-        model_citation_type = None
+    citation_map = {
+        "specialty": Citation.SPECIALTY,
+        "federal": Citation.FEDERAL,
+        "state": Citation.STATE,
+        "state_regional": Citation.STATE_REGIONAL,
+        "neutral": Citation.NEUTRAL,
+        "specialty_lexis": Citation.LEXIS,
+        "specialty_west": Citation.WEST,
+        "scotus_early": Citation.SCOTUS_EARLY,
+    }
+    return citation_map.get(citation_type)
