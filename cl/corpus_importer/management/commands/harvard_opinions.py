f89c7fc0d975d59ce83bded47778d33676e88b5a
vvilliam <wpalin@gmail.com>
2020-01-08 13:46:21 -0500

    fix(harvard_opinions) Minor fix for per curiam extraction

--- cl/corpus_importer/management/commands/harvard_opinions.py
+++ cl/corpus_importer/management/commands/harvard_opinions.py
@@ -326,7 +326,7 @@ def parse_harvard_opinions(reporter, volume):
                 for elem in [op.find("author")]:
                     [x.extract() for x in elem.find_all("page-number")]
 
-                author_tag_str = op.find("author").text.strip(":")
+                author_tag_str = titlecase(op.find("author").text.strip(":"))
                 author_str = titlecase("".join(find_judge_names(author_tag_str)))
                 per_curiam = True if author_tag_str == "Per Curiam" else False
                 # If Per Curiam is True set author string to Per Curiam
