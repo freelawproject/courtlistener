7f00f9e814a1b5b9fd19cc353339916973bd62a0
vvilliam <wpalin@gmail.com>
2020-01-09 13:46:09 -0500

    fix(search) Remove useless sql migration

--- cl/search/migrations/0090_update_opinion_sha1.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
---
--- Alter field sha1 on opinion
---
-ALTER TABLE "search_opinion" ALTER COLUMN "sha1" SET DEFAULT '';
-ALTER TABLE "search_opinion" ALTER COLUMN "sha1" DROP DEFAULT;
-COMMIT;
