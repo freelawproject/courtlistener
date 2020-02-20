ff874ec6dc8858ec1416b0e643f064dedfa9e6d8
Matt Dahl <matt.dahl.2013@gmail.com>
2020-02-19 21:32:53 -0500

    feat(citator): Adds helper function for calculating cluster depth data.

--- cl/citations/utils.py
+++ cl/citations/utils.py
@@ -1,4 +1,7 @@
-from cl.search.models import Citation
+from django.db.models import Sum
+from django.apps import (
+    apps,
+)  # Must use apps.get_model() to avoid circular import issue
 
 
 def map_reporter_db_cite_type(citation_type):
@@ -7,6 +10,7 @@ def map_reporter_db_cite_type(citation_type):
     :param citation_type: A value from REPORTERS['some-key']['cite_type']
     :return: A value from the search.models.Citation object
     """
+    Citation = apps.get_model("search.Citation")
     citation_map = {
         "specialty": Citation.SPECIALTY,
         "federal": Citation.FEDERAL,
@@ -18,3 +22,22 @@ def map_reporter_db_cite_type(citation_type):
         "scotus_early": Citation.SCOTUS_EARLY,
     }
     return citation_map.get(citation_type)
+
+
+def get_citation_depth_between_clusters(citing_cluster_pk, cited_cluster_pk):
+    """OpinionsCited objects exist as relationships between Opinion objects,
+    but we often want access to citation depth information between
+    OpinionCluster objects. This helper method assists in doing the necessary
+    DB lookup.
+
+    :param citing_cluster_pk: The primary key of the citing OpinionCluster
+    :param cited_cluster_pk: The primary key of the cited OpinionCluster
+    :return: The sum of all the depth fields of the OpinionsCited objects
+        associated with the Opinion objects associated with the given
+        OpinionCited objects
+    """
+    OpinionsCited = apps.get_model("search.OpinionsCited")
+    return OpinionsCited.objects.filter(
+        citing_opinion__cluster__pk=citing_cluster_pk,
+        cited_opinion__cluster__pk=cited_cluster_pk,
+    ).aggregate(depth=Sum("depth"))["depth"]
