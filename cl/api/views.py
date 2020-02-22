1eefb0a3ad756f4cfa4b82dbfd9a313ebf46c321
Michael Lissner <mlissner@michaeljaylissner.com>
2020-02-21 17:39:42 -0800

    feat(alerts): Add new estimator api endpoint
    We'll use this from the front end when folks make alerts to provide an estimate
    of how many alerts they can expect. If it's too darned high, we'll block them
    from creating the alert, particularly in RECAP, where such alerts will cause
    issues.

--- cl/api/views.py
+++ cl/api/views.py
@@ -1,11 +1,21 @@
+import logging
+
 from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from rest_framework import status
 
 from cl.lib import sunburnt
-from cl.lib.search_utils import build_coverage_query, build_court_count_query
+from cl.lib.search_utils import (
+    build_coverage_query,
+    build_court_count_query,
+    get_solr_interface,
+    build_alert_estimation_query,
+)
+from cl.search.forms import SearchForm
 from cl.search.models import Court
+
+logger = logging.getLogger(__name__)
 
 
 def annotate_courts_with_counts(courts, court_count_tuples):
@@ -132,8 +142,44 @@ def coverage_data(request, version, court):
         total_docs += count
 
     return JsonResponse(
-        {"annual_counts": annual_counts, "total": total_docs,}, safe=True
+        {"annual_counts": annual_counts, "total": total_docs}, safe=True
     )
+
+
+def get_result_count(request, version, day_count):
+    """Get the count of results for the past `day_count` number of days
+
+    GET parameters will be a complete search string
+
+    :param request: The Django request object
+    :param version: The API version number (ignored for now, but there for
+    later)
+    :param day_count: The number of days to average across. More is slower.
+    :return: A JSON object with the number of hits during the last day_range
+    period.
+    """
+    search_form = SearchForm(request.GET.copy())
+    if not search_form.is_valid():
+        return JsonResponse({"error": "Invalid SearchForm"}, safe=True)
+
+    cd = search_form.cleaned_data
+    try:
+        si = get_solr_interface(cd)
+    except NotImplementedError:
+        logger.error(
+            "Tried getting solr connection for %s, but it's not "
+            "implemented yet",
+            cd["type"],
+        )
+        raise
+
+    response = (
+        si.query()
+        .add_extra(**build_alert_estimation_query(cd, int(day_count)))
+        .execute()
+    )
+
+    return JsonResponse({"count": response.result.numFound}, safe=True)
 
 
 def deprecated_api(request, v):
