dcecde9552c8db76467c78b6474b87da2f18097c
Michael Lissner <mlissner@michaeljaylissner.com>
2020-04-30 14:32:05 -0700

    fix(viz): Broken merge

--- cl/visualizations/api_serializers.py
+++ cl/visualizations/api_serializers.py
@@ -1,18 +1,10 @@
-from django.db.models import Q
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
-from cl.api.utils import HyperlinkedModelSerializerWithId
 from cl.visualizations.models import SCOTUSMap
 from cl.search.models import OpinionCluster
-from cl.recap.models import ProcessingQueue
 
-<<<<<<< HEAD
+
 class VisualizationSerializer(serializers.ModelSerializer):
-=======
-
-class CreateVisualizationSerializer(serializers.ModelSerializer):
->>>>>>> 3ae07497bb981edf56c3edbf2ae21fffbcda38ba
-    user = serializers.HiddenField(default=serializers.CurrentUserDefault(),)
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     title = serializers.CharField(max_length=200)
     cluster_start = serializers.PrimaryKeyRelatedField(
         queryset=OpinionCluster.objects.all(),
