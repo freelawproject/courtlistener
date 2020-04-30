3ae07497bb981edf56c3edbf2ae21fffbcda38ba
Nick Sethi <nick@sethipc.com>
2020-04-30 12:09:19 -0700

    remove cluster validations, extraneous serializer, and re-include private-05.example

--- cl/visualizations/api_views.py
+++ cl/visualizations/api_views.py
@@ -2,11 +2,12 @@ from rest_framework import viewsets
 
 from cl.api.utils import LoggingMixin
 
-from cl.visualizations.api_serializers import VisualizationSerializer
+from cl.visualizations.api_serializers import CreateVisualizationSerializer
 
 from cl.visualizations.models import SCOTUSMap
 
+
 class VisualizationViewSet(LoggingMixin, viewsets.ModelViewSet):
     queryset = SCOTUSMap.objects.all()
-    serializer_class = VisualizationSerializer
+    serializer_class = CreateVisualizationSerializer
     ordering_fields = ("id", "date_created", "date_modified", "user")
