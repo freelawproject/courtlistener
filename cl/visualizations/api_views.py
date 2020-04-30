44aec3c8d308cd5bd5a10bc985b9ca0e315bf319
Michael Lissner <mlissner@michaeljaylissner.com>
2020-04-30 14:38:01 -0700

    fix(api): Rename import

--- cl/visualizations/api_views.py
+++ cl/visualizations/api_views.py
@@ -2,12 +2,12 @@ from rest_framework import viewsets
 
 from cl.api.utils import LoggingMixin
 
-from cl.visualizations.api_serializers import CreateVisualizationSerializer
+from cl.visualizations.api_serializers import VisualizationSerializer
 
 from cl.visualizations.models import SCOTUSMap
 
 
 class VisualizationViewSet(LoggingMixin, viewsets.ModelViewSet):
     queryset = SCOTUSMap.objects.all()
-    serializer_class = CreateVisualizationSerializer
+    serializer_class = VisualizationSerializer
     ordering_fields = ("id", "date_created", "date_modified", "user")
