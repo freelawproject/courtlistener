65103f1aacf586e47397869251eff4426809b41b
Nick Sethi <nick@sethipc.com>
2020-04-30 11:57:01 -0700

    black formatting on previous commit

--- cl/visualizations/api_views.py
+++ cl/visualizations/api_views.py
@@ -2,20 +2,11 @@ from rest_framework import viewsets
 
 from cl.api.utils import LoggingMixin
 
-from cl.visualizations.api_serializers import (
-  VisualizationSerializer
-)
+from cl.visualizations.api_serializers import VisualizationSerializer
 
-from cl.visualizations.models import (
-  SCOTUSMap
-)
+from cl.visualizations.models import SCOTUSMap
 
-class VisualizationViewSet(LoggingMixin, viewsets.ModelViewSet) :
-  queryset = SCOTUSMap.objects.all()
-  serializer_class = VisualizationSerializer
-  ordering_fields = (
-    "id",
-    "date_created",
-    "date_modified",
-    "user"
-  )
+class VisualizationViewSet(LoggingMixin, viewsets.ModelViewSet):
+    queryset = SCOTUSMap.objects.all()
+    serializer_class = VisualizationSerializer
+    ordering_fields = ("id", "date_created", "date_modified", "user")
