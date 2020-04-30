156b317b4399d060d28446d344f6a36ec399687e
Michael Lissner <mlissner@michaeljaylissner.com>
2020-04-30 14:38:40 -0700

    fix(api): Only allow SCOTUS cases via the API

--- cl/visualizations/api_serializers.py
+++ cl/visualizations/api_serializers.py
@@ -10,14 +10,19 @@ class VisualizationSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault(),)
     title = serializers.CharField(max_length=200)
     cluster_start = serializers.PrimaryKeyRelatedField(
-        queryset=OpinionCluster.objects.all(),
+        queryset=OpinionCluster.objects.filter(docket__court_id="scotus"),
         style={"base_template": "select.html"},
     )
     cluster_end = serializers.PrimaryKeyRelatedField(
-        queryset=OpinionCluster.objects.all(),
+        queryset=OpinionCluster.objects.filter(docket__court_id="scotus"),
         style={"base_template": "select.html"},
     )
 
     class Meta:
         model = SCOTUSMap
+        # TODO: Do we just want all fields?
+        # TODO: What about nested JSON data?
+        # TODO: What about security?
+        # TODO: What about validation?
+        # TODO: fields looks like it could just be "__all__"
         fields = ["user", "title", "cluster_start", "cluster_end"]
