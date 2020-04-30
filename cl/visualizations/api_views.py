26a2e758c2219cd8928b25d1f54bf6146e94151c
Michael Lissner <mlissner@michaeljaylissner.com>
2020-04-30 16:49:34 -0700

    feat(api): New views for JSONVersions and Visualizations
    Mostly normal stuff. The one trick that was needed was to override the
    create function. This seems like overkill, but the issue is that if
    you have generating code in your perform_create that can fail,
    there's no particularly good way to throw an error from that code. In
    this instance we build the network in our perform_create function.
    Doing that can throw a couple different errors, namely, too_many_nodes
    or too_few_nodes.
    
    To know that those errors happened we override create, which can
    actually return appropriate responses according to the error messages.

--- cl/visualizations/api_views.py
+++ cl/visualizations/api_views.py
@@ -1,13 +1,56 @@
-from rest_framework import viewsets
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from cl.api.utils import LoggingMixin
+from cl.visualizations.api_serializers import (
+    VisualizationSerializer,
+    JSONVersionSerializer,
+)
+from cl.visualizations.models import SCOTUSMap, JSONVersion
+from cl.visualizations.network_utils import reverse_endpoints_if_needed
+from cl.visualizations.utils import build_visualization
 
-from cl.visualizations.api_serializers import VisualizationSerializer
 
-from cl.visualizations.models import SCOTUSMap
+class JSONViewSet(LoggingMixin, ModelViewSet):
+    serializer_class = JSONVersionSerializer
+    ordering_fields = ("id", "date_created", "date_modified")
+
+    def get_queryset(self):
+        return JSONVersion.objects.filter(
+            map__user=self.request.user
+        ).order_by("-id")
 
 
-class VisualizationViewSet(LoggingMixin, viewsets.ModelViewSet):
-    queryset = SCOTUSMap.objects.all()
+class VisualizationViewSet(LoggingMixin, ModelViewSet):
     serializer_class = VisualizationSerializer
     ordering_fields = ("id", "date_created", "date_modified", "user")
+
+    def get_queryset(self):
+        return SCOTUSMap.objects.filter(user=self.request.user).order_by("-id")
+
+    def create(self, request, *args, **kwargs):
+        # Override create by copying from source so we can throw 403's
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        status = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        if status != "success":
+            return Response(
+                {"error": status}, status=HTTP_400_BAD_REQUEST, headers=headers
+            )
+        else:
+            return Response(
+                serializer.data, status=HTTP_201_CREATED, headers=headers
+            )
+
+    def perform_create(self, serializer):
+        cd = serializer.validated_data
+        start, end = reverse_endpoints_if_needed(
+            cd["cluster_start"], cd["cluster_end"],
+        )
+        viz = serializer.save(
+            user=self.request.user, cluster_start=start, cluster_end=end
+        )
+        status, viz = build_visualization(viz, end)
+        return status
