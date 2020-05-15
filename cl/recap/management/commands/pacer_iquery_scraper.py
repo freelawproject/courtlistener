5237d2ebf5a6e0ff696354c4b30462cddbe36adb
Michael Lissner <mlissner@michaeljaylissner.com>
2020-05-15 12:08:55 -0700

    feat(iquery): Add logging, throttling, and queue argument

--- cl/recap/management/commands/pacer_iquery_scraper.py
+++ cl/recap/management/commands/pacer_iquery_scraper.py
@@ -1,14 +1,14 @@
 # coding=utf-8
 from __future__ import print_function
 
-import json
 import re
 
 import requests
 from django.conf import settings
 
 from cl.alerts.models import DocketAlert
-from cl.lib.command_utils import VerboseCommand
+from cl.lib.celery_utils import CeleryThrottle
+from cl.lib.command_utils import VerboseCommand, logger
 from cl.scrapers.tasks import update_docket_info_iquery
 
 
@@ -45,8 +45,28 @@ def get_dockets():
 class Command(VerboseCommand):
     help = "Scrape PACER iquery report"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--queue",
+            default="batch1",
+            help="The celery queue where the tasks should be processed.",
+        )
+
     def handle(self, *args, **options):
         super(Command, self).handle(*args, **options)
         docket_list = get_dockets()
-        for item in docket_list:
-            update_docket_info_iquery.delay(item)
+        logger.info(
+            "iQuery crawling starting up. Will crawl %s dockets",
+            len(docket_list),
+        )
+        queue = options["queue"]
+        throttle = CeleryThrottle(queue_name=queue)
+        for i, item in enumerate(docket_list):
+            throttle.maybe_wait()
+
+            if i % 500 == 0:
+                logger.info("Sent %s items to celery for crawling so far.")
+
+            update_docket_info_iquery.apply_async(args=(item,), queue=queue)
+
+        logger.info("Done!")
