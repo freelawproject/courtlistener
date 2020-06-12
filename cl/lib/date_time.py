bd5c0b7f0cb58e4f59b6a48491d53049242ed507
Michael Lissner <mlissner@michaeljaylissner.com>
2020-06-12 16:07:47 -0700

    fox(alerts): Fix timezone errors in alert tests
    Tricky one, but the length docstrings should explain it. Basically,
    django does a fair bit of date conversions when you put datetimes in
    the DB, and I was calling date() a lot, which also does tz
    conversions, and it was a bit of a mess.
    
    The new path forward is that we compare dates, after they're
    normalized to the server's timezone. This should prevent the tests
    from failing at certain times of day. I tested this locally while
    tinkering with my laptop's time, so I think it's in good shape.

--- cl/lib/date_time.py
+++ cl/lib/date_time.py
@@ -1,6 +1,10 @@
+# coding=utf-8
 import pytz
 
 from datetime import datetime, time
+
+from django.conf import settings
+from django.utils.timezone import is_aware
 
 
 def midnight_pst(d):
@@ -8,3 +12,28 @@ def midnight_pst(d):
     pst = pytz.timezone("US/Pacific")
     d = datetime.combine(d, time()).replace(tzinfo=pst)
     return d
+
+
+def dt_as_local_date(dt):
+    """Convert a datetime to a localized date
+
+    Datetimes are stored in the DB in UTC. Dates are handled a bit differently:
+
+      1. If a tz-aware datetime is added to a date field in the DB, it's
+         converted to UTC, then the time and tz info is removed, leaving only
+         the date.
+
+      2. If a date is added to the DB, it's just added as is (no conversion is
+         needed or indeed possible).
+
+    This function takes a datetime — typically in UTC — and converts it to a
+    date that's in the server's localtime.
+
+    This is useful for comparing dates in the DB to datetimes in the DB (not
+    that I recommend it).
+
+    :param dt: A tz-aware datetime object to convert
+    :returns A date object
+    """
+    assert is_aware(dt), "dt must be a timezone-aware datetime object"
+    return dt.astimezone(pytz.timezone(settings.TIME_ZONE)).date()
