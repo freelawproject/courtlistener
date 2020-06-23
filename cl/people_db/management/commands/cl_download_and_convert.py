b5ce419349d4423a6164f8e0ed16e41194c03a45
aekey1 <aekey1@student.gsu.edu>
2020-06-23 13:47:40 -0400

    refactor(cl_download_and_convert): Remove break in loop
    Remove break from loop in iterate_over_aws()

--- cl/people_db/management/commands/cl_download_and_convert.py
+++ cl/people_db/management/commands/cl_download_and_convert.py
@@ -177,8 +177,6 @@ class FD(object):
                 #     },
                 # )
 
-                break
-
             try:
                 kwargs["ContinuationToken"] = resp["NextContinuationToken"]
             except KeyError:
