5ebcdac1fa838a9d528840cc5e0a60e1dec133c8
vvilliam <wpalin@gmail.com>
2020-07-17 13:30:31 -0400

    fix(api.tests): Fix api tests
    Remove tenn judges from testing db loading
    Reset test count query to 1

--- cl/api/tests.py
+++ cl/api/tests.py
@@ -490,7 +490,7 @@ class DRFJudgeApiFilterTests(TestCase, FilteringCountTestCase):
         # Note the exclamation mark. In a URL this would look like
         # "?judge!=1". Fun stuff.
         self.q["person!"] = 2
-        self.assertCountInResults(18)  # Bill
+        self.assertCountInResults(1)  # Bill
 
 
 class DRFRecapApiFilterTests(TestCase, FilteringCountTestCase):
