2ffb49fa39a0bcedb1b8ab9cd38fb637bfbec8f7
Michael Lissner <mlissner@michaeljaylissner.com>
2020-07-21 22:57:07 -0700

    fix(recap): Ensure that we get pacer_doc_ids
    Fixes: #1342

--- cl/search/filters.py
+++ cl/search/filters.py
@@ -225,6 +225,7 @@ class RECAPDocumentFilter(FilterSet):
             "date_upload": DATETIME_LOOKUPS,
             "document_type": ["exact"],
             "document_number": ["exact", "gte", "gt", "lte", "lt"],
+            # Parameter required in view.
             "pacer_doc_id": ["exact", "in"],
             "is_available": ["exact"],
             "sha1": ["exact"],
