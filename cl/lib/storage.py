4060dc8d7cc14b7a49f62260abed39a555d2171f
vvilliam <wpalin@gmail.com>
2020-07-22 16:08:59 -0400

    feat(storage): Add _ to duplicate fn saves for AWS MEDIA STORAGE

--- cl/lib/storage.py
+++ cl/lib/storage.py
@@ -44,7 +44,7 @@ class IncrementingFileSystemStorage(FileSystemStorage):
 
 class UUIDFileSystemStorage(FileSystemStorage):
     """Implements a simple UUID file system storage.
-    
+
     Useful when you don't care what the name of the file is, but you want it to
     be unique. Keeps the path from upload_to param and the extension of the
     original file.
@@ -66,3 +66,18 @@ class AWSMediaStorage(S3Boto3Storage):
     AWS_DEFAULT_ACL = settings.AWS_DEFAULT_ACL
     file_overwrite = True
     custom_domain = False
+
+    def get_available_name(self, name, max_length=None):
+        dir_name, file_name = os.path.split(name)
+        file_root, file_ext = os.path.splitext(file_name)
+        # If the filename already exists, add an underscore and a number (before
+        # the file extension, if one exists) to the filename until the generated
+        # filename doesn't exist.
+        count = itertools.count(1)
+        while self.exists(name):
+            # file_ext includes the dot.
+            name = os.path.join(
+                dir_name, "%s_%s%s" % (file_root, next(count), file_ext)
+            )
+
+        return name
