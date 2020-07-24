e6a9f52831c22ef40fcbbe02e097c43a817456b0
Michael Lissner <mlissner@michaeljaylissner.com>
2020-07-23 17:04:49 -0700

    feat(models): Add generic notes field to admin
    #1316

--- cl/lib/admin.py
+++ cl/lib/admin.py
@@ -1,6 +1,16 @@
+from django.contrib.contenttypes.admin import GenericTabularInline
+
+from cl.lib.models import Note
+
+
 class AdminTweaksMixin(object):
     class Media:
         css = {
             "all": ("css/admin.css",),
         }
         js = ("js/admin.js",)
+
+
+class NotesInline(GenericTabularInline):
+    model = Note
+    extra = 1
