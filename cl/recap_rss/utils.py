849960736ef0261780a9cf41e823a98c704a57be
Michael Lissner <mlissner@michaeljaylissner.com>
2020-07-24 17:29:28 -0700

    feat(pacer): Begin storing PACER RSS data

---
+++ cl/recap_rss/utils.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+emails = {
+    "changed_rss_feed": {
+        "subject": "PACER RSS feed changed for: %s",
+        "body": "Dear admin:\n\n%s's RSS feed has changed:\n\n"
+        " - %s\n"
+        " + %s\n\n"
+        "You should probably tell the court this is unacceptable.",
+        "from": settings.DEFAULT_FROM_EMAIL,
+        "to": [a[1] for a in settings.ADMINS],
+    },
+}
