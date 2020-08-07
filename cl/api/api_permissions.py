1c527f38967e75ecd65b3d8245d68250f36b523d
Michael Lissner <mlissner@michaeljaylissner.com>
2020-08-07 15:54:45 -0700

    feat(api): Generalize the IsOwner API permission

--- cl/api/api_permissions.py
+++ cl/api/api_permissions.py
@@ -1,10 +1,10 @@
 from rest_framework import permissions
 
 
-class IsParentVisualizationOwner(permissions.BasePermission):
-    """Only allow changes to JSON data from the vis's owner."""
+class IsOwner(permissions.BasePermission):
+    """Only allow changes to visualizations by its owner"""
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.map.user == request.user
+        return obj.user == request.user
