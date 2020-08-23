e10c3d47dbb083b98ce35181504af89a6df23324
Nick Sethi <nick@sethipc.com>
2020-08-22 23:51:19 -0700

    add typescript

---
+++ cl/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
+
--- cl/babel.config.js
+++ cl/babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   // cache babel config by environment
   api.cache.using(() => process.env.NODE_ENV)
 
-  const presets = ['@babel/preset-env', '@babel/preset-react']
+  const presets = ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
   const plugins = [
     !api.env('production') && 'react-refresh/babel'
   ]
