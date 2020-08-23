3f985287687e0b4af18476b6441fd917908a5567
Nick Sethi <nick@sethipc.com>
2020-08-23 15:19:57 -0700

    splitchunks in webpack, remove old js

--- cl/babel.config.js
+++ cl/babel.config.js
@@ -1,13 +1,12 @@
 module.exports = function (api) {
   // cache babel config by environment
-  api.cache.using(() => process.env.NODE_ENV)
+  api.cache.using(() => process.env.NODE_ENV);
 
-  const presets = ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
-  const plugins = [
-    !api.env('production') && 'react-refresh/babel'
-  ]
+  const presets = ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'];
+  const plugins = [!api.env('production') && 'react-refresh/babel'].filter(Boolean);
   return {
     plugins,
-    presets
-  }
-}
+    presets,
+  };
+};
+
