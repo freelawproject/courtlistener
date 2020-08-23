13bdb5e974f488774b0d7ead437a733473f68cae
Nick Sethi <nick@sethipc.com>
2020-08-22 22:40:51 -0700

    add webpack to django

--- cl/webpack.config.js
+++ cl/webpack.config.js
@@ -1,68 +1,38 @@
-const path = require('path');
-const webpack = require('webpack');
-const BundleTracker = require('webpack-bundle-tracker');
-const CompressionPlugin = require('compression-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const BundleTracker = require("webpack-bundle-tracker");
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
-
-const baseOutput = {
-  path: path.resolve('./assets/bundles/'),
-  filename: '[name]-[hash].js',
-};
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  context: __dirname,
   mode: isDevelopment ? 'development' : 'production',
+  context: __dirname,
 
   entry: [
-    './assets/react/index', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+    'webpack-dev-server/client?http://localhost:3000',
+    "./assets/js/index", // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
   ],
-  output: isDevelopment
-    ? // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
-      { ...baseOutput, publicPath: 'http://localhost:3000/assets/bundles/' }
-    : { ...baseOutput },
-
-  optimization: {
-    removeAvailableModules: true,
-    splitChunks: {
-      chunks: 'all',
-      name: 'vendor',
-    },
+  output: {
+    path: path.resolve("./assets/bundles/"),
+    filename: "[name]-[hash].js",
+    // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
+    publicPath: 'http://localhost:3000/assets/bundles/',
   },
+
   plugins: [
     isDevelopment && new ReactRefreshWebpackPlugin(),
-    new CompressionPlugin(),
-    new BundleTracker({ filename: './webpack-stats.json' }),
+    new BundleTracker({ filename: "./webpack-stats.json" }),
   ].filter(Boolean),
 
   module: {
     rules: [
-      {
-        test: /\.(jsx|ts|tsx)?$/,
-        include: path.join(__dirname, 'assets', 'react'),
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" }, // to transform JSX into JS
     ],
   },
 
   resolve: {
-    modules: ['node_modules'],
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    modules: ["node_modules", "bower_components"],
+    extensions: [".js", ".jsx"],
   },
-  devServer: {
-    compress: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-      'Accept-Encoding': 'gzip',
-    },
-    port: 3000,
-  },
-  devtool: 'cheap-module-source-map',
 };
