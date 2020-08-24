665481688581536010bee61f0a61085e318a93ff
Nick Sethi <nick@sethipc.com>
2020-08-24 16:48:31 -0700

    fix item loading, get csrftoken from dom, apply bs3 styles

--- cl/webpack.config.js
+++ cl/webpack.config.js
@@ -50,6 +50,12 @@ module.exports = {
   },
   devServer: {
     compress: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
     port: 3000,
   },
+  devtool: 'cheap-module-source-map',
 };
