ac02c54240bc5efcb7da017e7c339f7b27038ee3
vvilliam <wpalin@gmail.com>
2020-08-25 11:43:48 -0400

    refactor(import_tn): Change to input-file to input-dir containing pdfs and json data

--- cl/corpus_importer/management/commands/import_tn.py
+++ cl/corpus_importer/management/commands/import_tn.py
@@ -1,14 +1,16 @@
 import json
+from glob import glob
 
 from cl.lib.command_utils import VerboseCommand
 import logging
 
-def import_tn_corpus(filepath, log, skip_until):
+
+def import_tn_corpus(log, skip_until, dir):
     """Import TN Corpus
 
-    :param filepath: Location of pre-processed json
     :param log: Should we should logs
     :param skip_until: Label ID, if any, to process first
+    :param dir: Location of pre-processed json
     :return: None
     """
     ready = False if skip_until else True
@@ -17,20 +19,28 @@ def import_tn_corpus(filepath, log, skip_until):
         logging.getLogger().setLevel(logging.INFO)
 
     logging.info("Starting import")
+    filepath = "%s/data.json" % dir
     tn_corpus = json.loads(open(filepath, "r").read())
 
     if not ready:
         case = [x for x in tn_corpus if x["label"] == int(skip_until)][0]
-        logging.info("Skipping until case %s labeled: %s", case['title'], case['label'])
+        logging.info(
+            "Skipping until case %s labeled: %s", case["title"], case["label"]
+        )
 
-    for case in sorted(tn_corpus, key=lambda x:x['label']):
-        if case['label'] == int(skip_until):
+    for case in sorted(tn_corpus, key=lambda x: x["label"]):
+        if case["label"] == int(skip_until):
             ready = True
         if not ready:
             continue
-
-        logging.info("Processing label:%s for case:%s", case['label'], case['title'])
-
+        logging.info(
+            "Processing label:%s for case:%s", case["label"], case["title"]
+        )
+        pdf_path = [
+            x
+            for x in glob("%s/%s/*.pdf" % (dir, case["label"]))
+            if "stamped" not in x
+        ][0]
 
 
 class Command(VerboseCommand):
@@ -44,16 +54,23 @@ class Command(VerboseCommand):
             # required=True,
         )
         parser.add_argument(
+            "--input-dir",
+            help="The directory containing all the PDFs.",
+            default="utk_workerscomp",
+            # required=True,
+        )
+        parser.add_argument(
             "--log",
             action="store_true",
             default=False,
             help="Determine feedback level.",
         )
         parser.add_argument(
-            "--skip-until",
-            help="Skip until to process",
+            "--skip-until", default=False, help="Skip until to process",
         )
 
     def handle(self, *args, **options):
         super(Command, self).handle(*args, **options)
-        import_tn_corpus(options['input_file'], options['log'], options["skip_until"])
+        import_tn_corpus(
+            options["log"], options["skip_until"], options["input_dir"],
+        )
