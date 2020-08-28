08f910d04193ae7708ab172691f906d464e5c375
Michael Lissner <mlissner@michaeljaylissner.com>
2020-08-27 17:17:32 -0700

    feat(client): New method for getting the relevant dockets

--- cl/corpus_importer/management/commands/buchwald_project.py
+++ cl/corpus_importer/management/commands/buchwald_project.py
@@ -1,17 +1,28 @@
 import os
 from argparse import RawTextHelpFormatter
 
+from celery import chain
 from django.conf import settings
 from juriscraper.pacer import PacerSession
 
+from cl.corpus_importer.management.commands.everything_project import (
+    NOS_EXCLUSIONS,
+)
 from cl.lib.celery_utils import CeleryThrottle
 from cl.lib.command_utils import VerboseCommand, logger
-from cl.corpus_importer.tasks import make_docket_by_iquery
+from cl.corpus_importer.tasks import (
+    make_docket_by_iquery,
+    get_docket_by_pacer_case_id,
+)
+from cl.search.models import Docket
+from cl.search.tasks import add_or_update_recap_docket
+
 
 PACER_USERNAME = os.environ.get("PACER_USERNAME", settings.PACER_USERNAME)
 PACER_PASSWORD = os.environ.get("PACER_PASSWORD", settings.PACER_PASSWORD)
 
-TAG = "ljNwRSfxpxnCceEoBBIb"
+NYSD_TAG = "ljNwRSfxpxnCceEoBBIb"
+BUCKWALD_TAG = "GhkcARCmMnPeDrXqqKTX"
 
 
 def add_all_nysd_to_cl(options):
@@ -44,28 +55,83 @@ def add_all_nysd_to_cl(options):
             )
             session.login()
 
-        logger.info("Doing pacer_case_id: %s", pacer_case_id)
         throttle.maybe_wait()
+        logger.info("Doing pacer_case_id: %s", pacer_case_id)
         make_docket_by_iquery.apply_async(
-            args=("nysd", pacer_case_id, session.cookies, [TAG]),
+            args=("nysd", pacer_case_id, session.cookies, [NYSD_TAG]),
             queue=q,
         )
 
 
+def get_dockets(options):
+    """Get the dockets by the particular judge now that we have run iquery
+    for all of the cases in the jurisdiction, and now that we have
+    """
+    q = options["queue"]
+    throttle = CeleryThrottle(queue_name=q)
+    session = PacerSession(username=PACER_USERNAME, password=PACER_PASSWORD)
+    session.login()
+
+    buchwald_id = 450
+    ds = (
+        Docket.objects.filter(
+            court_id="nysd", assigned_to_id=buchwald_id, tag__name=NYSD_TAG
+        )
+        .exclude(idb_data__nature_of_suit__in=NOS_EXCLUSIONS)
+        .exclude(idb_data__isnull=True)
+    )
+    for i, d in enumerate(ds):
+        if i < options["skip_until"]:
+            continue
+        if i >= options["limit"] > 0:
+            break
+
+        if i % 5000 == 0:
+            # Re-authenticate just in case the auto-login mechanism isn't
+            # working.
+            session = PacerSession(
+                username=PACER_USERNAME, password=PACER_PASSWORD
+            )
+            session.login()
+
+        throttle.maybe_wait()
+        logger.info("%s: Doing docket with pk: %s", i, d.pk)
+        chain(
+            get_docket_by_pacer_case_id.s(
+                data={"pacer_case_id": d.pacer_case_id},
+                court_id=d.court_id,
+                cookies=session.cookies,
+                docket_pk=d.pk,
+                tag_names=[BUCKWALD_TAG],
+                **{
+                    "show_parties_and_counsel": True,
+                    "show_terminated_parties": True,
+                    "show_list_of_member_cases": False,
+                }
+            ).set(queue=q),
+            add_or_update_recap_docket.s().set(queue=q),
+        ).apply_async()
+
+
 class Command(VerboseCommand):
-    help = u"""Get all relevant cases by a Judge Buchwald in SDNY
+    help = """Get all relevant cases by a Judge Buchwald in SDNY
 
 1. Start with the earliest case in 2013 (13-cv-00001, pacer_case_id 405990)
-2. For every case between that and today (1:20-cv-06900, pacer_case_id 543051),
+1. For every case between that and today (1:20-cv-06900, pacer_case_id 543051),
    run the iquery page and parse the docket number.
-3. If the judge matches (Naomi Reice Buchwald, NRB), check the NOS in the FJC
+1. Use merge_idb_into_dockets to link IDB data with data from iquery look ups
+1. If the judge matches (Naomi Reice Buchwald, NRB), check the NOS in the FJC
    DB.
-4. If the NOS is valid, get and tag the docket.
+1. If the NOS is valid, get and tag the docket.
 
 Checks:
  - Buchwald has been at that court the whole time.
  - The FJC data is up to date.
- -
+
+Limitations:
+ - In multi-defendant criminal cases, this may fail (OK b/c this project
+   limited to civil)
+
 """
 
     def create_parser(self, *args, **kwargs):
@@ -103,6 +169,11 @@ Checks:
     def handle(self, *args, **options):
         logger.info("Using PACER username: %s" % PACER_USERNAME)
         if options["task"] == "iquery":
+            # Run iquery for all of the cases in the district during the time
+            # period.
             add_all_nysd_to_cl(options)
+        elif options["task"] == "dockets":
+            # Get dockets for all of the relevant cases
+            get_dockets(options)
         else:
             print("Unknown task: %s" % options["task"])
