c8775b92afa208e1189136dd1f9aacfa1384615a
vvilliam <wpalin@gmail.com>
2020-09-16 15:13:26 -0400

    style(scraper/tests.py): Black style

--- cl/scrapers/tests.py
+++ cl/scrapers/tests.py
@@ -482,7 +482,7 @@ class WeirdBinariesTest(TestCase):
             if ".pdf" in filepath:
                 content = response["content"]
             else:
-                content = response['content'][0]
+                content = response["content"][0]
             self.assertIn(
                 test_string,
                 content.lower(),
