3297091a384046a463827dd17e7a7d504a48466e
vvilliam <wpalin@gmail.com>
2020-09-16 15:11:09 -0400

    refactor(scrapers/tasks): Remove subprocess methods
    Refactor subprocess into our new BTE container

--- cl/scrapers/tasks.py
+++ cl/scrapers/tasks.py
@@ -4,37 +4,26 @@
 import os
 import logging
 import random
-import subprocess
 import traceback
 import uuid
-from tempfile import NamedTemporaryFile
 
 import eyed3
 import requests
-from PyPDF2 import PdfFileReader
-from PyPDF2.utils import PdfReadError
 from django.apps import apps
 from django.conf import settings
 from django.core.files.base import ContentFile
 from django.db import IntegrityError
-from django.utils.encoding import (
-    smart_text,
-    DjangoUnicodeDecodeError,
-    force_text,
-)
 from django.utils.timezone import now
 from eyed3 import id3
-from lxml.etree import XMLSyntaxError
-from lxml.html.clean import Cleaner
 from seal_rookery import seals_data, seals_root
 
 from cl.audio.models import Audio
+from cl.celery import app
 from cl.audio.utils import get_audio_binary
 from cl.celery_init import app
 from cl.citations.tasks import find_citations_for_opinion_by_pks
 from cl.custom_filters.templatetags.text_filters import best_case_name
 from cl.lib.juriscraper_utils import get_scraper_object_by_name
-from cl.lib.mojibake import fix_mojibake
 from cl.lib.pacer import map_cl_to_pacer_id
 from cl.lib.pacer_session import get_or_cache_pacer_cookies
 from cl.lib.recap_utils import needs_ocr
@@ -49,217 +38,65 @@ from cl.search.models import Opinion, RECAPDocument, Docket
 from cl.search.tasks import add_items_to_solr
 from juriscraper.pacer import PacerSession, CaseQuery
 
-DEVNULL = open("/dev/null", "w")
-
 logger = logging.getLogger(__name__)
 
 
-def send_file_to_bte(filepath, do_ocr=False):
-    """Send file to extract doc content
+def make_pdftotext_process(path):
+    """Helper function to call make_pdftotext_process method
+
+    :param path:
+    :return:
+    """
+    with open(path, "rb") as file:
+        f = file.read()
+    return requests.post(
+        "http://cl-binary-transformers-and-extractors:80/make_pdftotext_process",
+        files={"file": (os.path.basename(path), f)},
+    ).json()
+
+
+def get_page_count(path):
+    """Helper method to call get_page_count.
+
+    :param path: Location of file
+    :return: dictionary containing page count.
+    """
+    with open(path, "rb") as file:
+        f = file.read()
+    return requests.post(
+        "http://cl-binary-transformers-and-extractors:80/get_page_count",
+        files={"file": (os.path.basename(path), f)},
+    ).json()
+
+
+def process_doc(path, do_ocr=False):
+    """Helper method to call task extract doc content
+
+    :param path:
+    :param do_ocr:
+    :return:
+    """
+    with open(path, "rb") as file:
+        f = file.read()
+    return requests.post(
+        "http://cl-binary-transformers-and-extractors:80/extract_doc_content",
+        files={"file": (os.path.basename(path), f)},
+        params={"do_ocr": do_ocr},
+    ).json()
+
+
+def send_file_to_convert_audio(filepath):
+    """This is a helper function to call convert_audio_file.
 
     :param filepath:
-    :param do_ocr:
     :return:
     """
     with open(filepath, "rb") as file:
         f = file.read()
     return requests.post(
-        "http://cl-binary-transformers-and-extractors:80/extract_doc_content",
+        "http://cl-binary-transformers-and-extractors:80/convert_audio_file",
         files={"file": (os.path.basename(filepath), f)},
-        params={"do_ocr": do_ocr},
     )
-
-
-
-def get_clean_body_content(content):
-    """Parse out the body from an html string, clean it up, and send it along."""
-    cleaner = Cleaner(
-        style=True, remove_tags=["a", "body", "font", "noscript", "img"]
-    )
-    try:
-        return cleaner.clean_html(content)
-    except XMLSyntaxError:
-        return (
-            "Unable to extract the content from this file. Please try "
-            "reading the original."
-        )
-
-
-def extract_from_doc(path):
-    """Extract text from docs.
-
-    We use antiword to pull the text out of MS Doc files.
-    """
-    process = subprocess.Popen(
-        ["antiword", path, "-i", "1"],
-        shell=False,
-        stdout=subprocess.PIPE,
-        stderr=DEVNULL,
-    )
-    content, err = process.communicate()
-    return content.decode(), err
-
-
-def extract_from_docx(path):
-    """Extract text from docx files
-
-    We use docx2txt to pull out the text. Pretty simple.
-    """
-    process = subprocess.Popen(
-        ["docx2txt", path, "-"],
-        shell=False,
-        stdout=subprocess.PIPE,
-        stderr=DEVNULL,
-    )
-    content, err = process.communicate()
-    return content.decode(), err
-
-
-def extract_from_html(path):
-    """Extract from html.
-
-    A simple wrapper to go get content, and send it along.
-    """
-    try:
-        with open(path, "rb") as f:
-            content = f.read()
-        content = get_clean_body_content(content)
-        encodings = ["utf-8", "ISO8859", "cp1252"]
-        for encoding in encodings:
-            try:
-                content = force_text(content, encoding=encoding)
-            except DjangoUnicodeDecodeError:
-                continue
-            else:
-                return content, False
-
-        # Fell through, therefore unable to decode the string.
-        return "", True
-    except:
-        return "", True
-
-
-def make_pdftotext_process(path):
-    """Make a subprocess to hand to higher-level code."""
-    return subprocess.Popen(
-        ["pdftotext", "-layout", "-enc", "UTF-8", path, "-"],
-        shell=False,
-        stdout=subprocess.PIPE,
-        stderr=DEVNULL,
-    )
-
-
-def extract_from_pdf(path, opinion, do_ocr=False):
-    """Extract text from pdfs.
-
-    Here, we use pdftotext. If that fails, try to use tesseract under the
-    assumption it's an image-based PDF. Once that is complete, we check for the
-    letter e in our content. If it's not there, we try to fix the mojibake
-    that ca9 sometimes creates.
-    """
-    process = make_pdftotext_process(path)
-    content, err = process.communicate()
-    content = content.decode()
-    if content.strip() == "" and do_ocr:
-        success, content = extract_by_ocr(path)
-        if success:
-            opinion.extracted_by_ocr = True
-        elif content == "" or not success:
-            content = "Unable to extract document content."
-    elif "e" not in content:
-        # It's a corrupt PDF from ca9. Fix it.
-        content = fix_mojibake(content)
-    return content, err
-
-
-def extract_from_txt(path):
-    """Extract text from plain text files: A fool's errand.
-
-    Unfortunately, plain text files lack encoding information, so we have to
-    guess. We could guess ascii, but we may as well use a superset of ascii,
-    cp1252, and failing that try utf-8, ignoring errors. Most txt files we
-    encounter were produced by converting wpd or doc files to txt on a
-    Microsoft box, so assuming cp1252 as our first guess makes sense.
-
-    May we hope for a better world.
-    """
-    try:
-        err = False
-        with open(path, "rb") as f:
-            data = f.read()
-        try:
-            # Alas, cp1252 is probably still more popular than utf-8.
-            content = smart_text(data, encoding="cp1252")
-        except DjangoUnicodeDecodeError:
-            content = smart_text(data, encoding="utf-8", errors="ignore")
-    except:
-        err = True
-        content = ""
-    return content, err
-
-
-def extract_from_wpd(path, opinion):
-    """Extract text from a Word Perfect file
-
-    Yes, courts still use these, so we extract their text using wpd2html. Once
-    that's done, we pull out the body of the HTML, and do some minor cleanup
-    on it.
-    """
-    process = subprocess.Popen(
-        ["wpd2html", path], shell=False, stdout=subprocess.PIPE, stderr=DEVNULL
-    )
-    content, err = process.communicate()
-
-    content = get_clean_body_content(content)
-    content = content.decode()
-
-    if "not for publication" in content.lower():
-        opinion.precedential_status = "Unpublished"
-
-    return content, err
-
-
-def convert_file_to_txt(path):
-    tesseract_command = ["tesseract", path, "stdout", "-l", "eng"]
-    p = subprocess.Popen(
-        tesseract_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    return p.communicate()[0].decode()
-
-
-def get_page_count(path, extension):
-    """Get the number of pages, if appropriate mimetype.
-
-    :param path: A path to a binary (pdf, wpd, doc, txt, html, etc.)
-    :param extension: The extension of the binary.
-    :return: The number of pages if possible, else return None
-    """
-    if extension == "pdf":
-        try:
-            reader = PdfFileReader(path)
-            return int(reader.getNumPages())
-        except (
-            IOError,
-            ValueError,
-            TypeError,
-            KeyError,
-            AssertionError,
-            PdfReadError,
-        ):
-            # IOError: File doesn't exist. My bad.
-            # ValueError: Didn't get an int for the page count. Their bad.
-            # TypeError: NumberObject has no attribute '__getitem__'. Ugh.
-            # KeyError, AssertionError: assert xrefstream["/Type"] == "/XRef". WTF?
-            # PdfReadError: Something else. I have no words.
-            pass
-    elif extension == "wpd":
-        # Best solution appears to be to dig into the binary format
-        pass
-    elif extension == "doc":
-        # Best solution appears to be to dig into the XML of the file
-        # itself: http://stackoverflow.com/a/12972502/64911
-        pass
-    return None
 
 
 def update_document_from_text(opinion):
@@ -315,31 +152,11 @@ def extract_doc_content(pk, do_ocr=False, citation_jitter=False):
     path = opinion.local_path.path
 
     extension = path.split(".")[-1]
-    if extension == "doc":
-        content, err = extract_from_doc(path)
-    elif extension == "docx":
-        content, err = extract_from_docx(path)
-    elif extension == "html":
-        content, err = extract_from_html(path)
-    elif extension == "pdf":
-        content, err = extract_from_pdf(path, opinion, do_ocr)
-    elif extension == "txt":
-        content, err = extract_from_txt(path)
-    elif extension == "wpd":
-        content, err = extract_from_wpd(path, opinion)
-    else:
-        print(
-            "*****Unable to extract content due to unknown extension: %s "
-            "on opinion: %s****" % (extension, opinion)
-        )
-        return
-
-    assert isinstance(
-        content, str
-    ), "content must be of type str, not %s" % type(content)
-
+    response = process_doc(path, do_ocr=do_ocr)
+    content = response["content"]
+    success = response["success"]
     # Do page count, if possible
-    opinion.page_count = get_page_count(path, extension)
+    opinion.page_count = get_page_count(path)["pg_count"]
 
     # Do blocked status
     if extension in ["html", "wpd"]:
@@ -351,9 +168,7 @@ def extract_doc_content(pk, do_ocr=False, citation_jitter=False):
         opinion.cluster.date_blocked = now()
 
     update_document_from_text(opinion)
-
-    if err:
-        print(err)
+    if not success:
         print(
             "****Error extracting text from %s: %s****" % (extension, opinion)
         )
@@ -400,13 +215,15 @@ def extract_recap_pdf(pks, skip_ocr=False, check_if_needed=True):
             processed.append(pk)
             continue
         path = rd.filepath_local.path
-        process = make_pdftotext_process(path)
-        content, err = process.communicate()
+        response = make_pdftotext_process(path)
+        content = response["content"]
 
         if needs_ocr(content):
             if not skip_ocr:
                 # probably an image PDF. Send it to OCR.
-                success, content = extract_by_ocr(path)
+                response = process_doc(path, do_ocr=True)
+                content = response["content"]
+                success = response["success"]
                 if success:
                     rd.ocr_status = RECAPDocument.OCR_COMPLETE
                 elif content == "" or not success:
@@ -424,80 +241,6 @@ def extract_recap_pdf(pks, skip_ocr=False, check_if_needed=True):
         processed.append(pk)
 
     return processed
-
-
-def rasterize_pdf(path, destination):
-    """Convert the PDF into a multipage Tiff file.
-
-    This function uses ghostscript for processing and borrows heavily from:
-
-        https://github.com/jbarlow83/OCRmyPDF/blob/636d1903b35fed6b07a01af53769fea81f388b82/ocrmypdf/ghostscript.py#L11
-
-    """
-    # gs docs, see: http://ghostscript.com/doc/7.07/Use.htm
-    # gs devices, see: http://ghostscript.com/doc/current/Devices.htm
-    #
-    # Compression is a trade off. It takes twice as long to convert PDFs, but
-    # they're about 1-2% the size of the uncompressed version. They take about
-    # 30% of the RAM when Tesseract processes them. See:
-    # https://github.com/tesseract-ocr/tesseract/issues/431#issuecomment-250549208
-    gs = [
-        "gs",
-        "-dQUIET",  # Suppress printing routine info
-        "-dSAFER",  # Lock down the filesystem to only files on command line
-        "-dBATCH",  # Exit after finishing file. Don't wait for more commands.
-        "-dNOPAUSE",  # Don't pause after each page
-        "-sDEVICE=tiffgray",
-        "-sCompression=lzw",
-        "-r300x300",  # Set the resolution to 300 DPI.
-        "-o",
-        destination,
-        path,
-    ]
-    p = subprocess.Popen(
-        gs,
-        close_fds=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True,
-    )
-    stdout, stderr = p.communicate()
-    return stdout, stderr, p.returncode
-
-
-def cleanup_ocr_text(txt):
-    """Do some basic cleanup to make OCR text better.
-
-    Err on the side of safety. Don't make fixes that could cause other issues.
-
-    :param txt: The txt output from the OCR engine.
-    :return: Txt output, cleaned up.
-    """
-    simple_replacements = (
-        ("Fi|ed", "Filed"),
-        (" Il ", " II "),
-    )
-    for replacement in simple_replacements:
-        txt = txt.replace(replacement[0], replacement[1])
-    return txt
-
-
-@app.task
-def extract_by_ocr(path: str) -> (bool, str):
-    """Extract the contents of a PDF using OCR."""
-    fail_msg = (
-        "Unable to extract the content from this file. Please try "
-        "reading the original."
-    )
-    with NamedTemporaryFile(prefix="ocr_", suffix=".tiff") as tmp:
-        out, err, returncode = rasterize_pdf(path, tmp.name)
-        if returncode != 0:
-            return False, fail_msg
-
-        txt = convert_file_to_txt(tmp.name)
-        txt = cleanup_ocr_text(txt)
-
-    return True, txt
 
 
 def set_mp3_meta_data(audio_obj, mp3_path):
@@ -592,32 +335,9 @@ def process_audio_file(pk):
     """
     af = Audio.objects.get(pk=pk)
     tmp_path = os.path.join("/tmp", "audio_" + uuid.uuid4().hex + ".mp3")
-    av_path = get_audio_binary()
-    av_command = [
-        av_path,
-        "-i",
-        af.local_path_original_file.path,
-        "-ar",
-        "22050",  # sample rate (audio samples/s) of 22050Hz
-        "-ab",
-        "48k",  # constant bit rate (sample resolution) of 48kbps
-        tmp_path,
-    ]
-    try:
-        _ = subprocess.check_output(av_command, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        print(
-            "%s failed command: %s\nerror code: %s\noutput: %s\n%s"
-            % (
-                av_path,
-                av_command,
-                e.returncode,
-                e.output,
-                traceback.format_exc(),
-            )
-        )
-        raise
-
+    r = send_file_to_convert_audio(af.local_path_original_file.path)
+    with open(tmp_path, "w") as mp3:
+        mp3.write(r.content)
     set_mp3_meta_data(af, tmp_path)
     try:
         with open(tmp_path, "rb") as mp3:
