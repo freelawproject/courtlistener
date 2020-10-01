f8c3036719867b820e059ffc8b797bd2f643edca
Drew Silcock <drew.silcock@stfc.ac.uk>
2020-10-01 21:14:54 +0100

    Starting number with '0' is invalid.
    I didn't think this was a Python 2 vs. 3 thing but hey.

--- cl/lib/sunburnt/test_schema.py
+++ cl/lib/sunburnt/test_schema.py
@@ -21,32 +21,32 @@ not_utc = pytz.timezone("Etc/GMT-3")
 
 samples_from_pydatetimes = {
     "2009-07-23T03:24:34.000376Z": [
-        datetime.datetime(2009, 07, 23, 3, 24, 34, 376),
-        datetime.datetime(2009, 07, 23, 3, 24, 34, 376, pytz.utc),
+        datetime.datetime(2009, 7, 23, 3, 24, 34, 376),
+        datetime.datetime(2009, 7, 23, 3, 24, 34, 376, pytz.utc),
     ],
     "2009-07-23T00:24:34.000376Z": [
-        not_utc.localize(datetime.datetime(2009, 07, 23, 3, 24, 34, 376)),
-        datetime.datetime(2009, 07, 23, 0, 24, 34, 376, pytz.utc),
+        not_utc.localize(datetime.datetime(2009, 7, 23, 3, 24, 34, 376)),
+        datetime.datetime(2009, 7, 23, 0, 24, 34, 376, pytz.utc),
     ],
     "2009-07-23T03:24:34Z": [
-        datetime.datetime(2009, 07, 23, 3, 24, 34),
-        datetime.datetime(2009, 07, 23, 3, 24, 34, tzinfo=pytz.utc),
+        datetime.datetime(2009, 7, 23, 3, 24, 34),
+        datetime.datetime(2009, 7, 23, 3, 24, 34, tzinfo=pytz.utc),
     ],
     "2009-07-23T00:24:34Z": [
-        not_utc.localize(datetime.datetime(2009, 07, 23, 3, 24, 34)),
-        datetime.datetime(2009, 07, 23, 0, 24, 34, tzinfo=pytz.utc),
+        not_utc.localize(datetime.datetime(2009, 7, 23, 3, 24, 34)),
+        datetime.datetime(2009, 7, 23, 0, 24, 34, tzinfo=pytz.utc),
     ],
 }
 
 if HAS_MX_DATETIME:
     samples_from_mxdatetimes = {
         "2009-07-23T03:24:34.000376Z": [
-            mx.DateTime.DateTime(2009, 07, 23, 3, 24, 34.000376),
-            datetime.datetime(2009, 07, 23, 3, 24, 34, 376, pytz.utc),
+            mx.DateTime.DateTime(2009, 7, 23, 3, 24, 34.000376),
+            datetime.datetime(2009, 7, 23, 3, 24, 34, 376, pytz.utc),
         ],
         "2009-07-23T03:24:34Z": [
-            mx.DateTime.DateTime(2009, 07, 23, 3, 24, 34),
-            datetime.datetime(2009, 07, 23, 3, 24, 34, tzinfo=pytz.utc),
+            mx.DateTime.DateTime(2009, 7, 23, 3, 24, 34),
+            datetime.datetime(2009, 7, 23, 3, 24, 34, tzinfo=pytz.utc),
         ],
     }
 
@@ -54,13 +54,13 @@ if HAS_MX_DATETIME:
 samples_from_strings = {
     # These will not have been serialized by us, but we should deal with them
     "2009-07-23T03:24:34Z": datetime.datetime(
-        2009, 07, 23, 3, 24, 34, tzinfo=pytz.utc
+        2009, 7, 23, 3, 24, 34, tzinfo=pytz.utc
     ),
     "2009-07-23T03:24:34.1Z": datetime.datetime(
-        2009, 07, 23, 3, 24, 34, 100000, pytz.utc
+        2009, 7, 23, 3, 24, 34, 100000, pytz.utc
     ),
     "2009-07-23T03:24:34.123Z": datetime.datetime(
-        2009, 07, 23, 3, 24, 34, 123000, pytz.utc
+        2009, 7, 23, 3, 24, 34, 123000, pytz.utc
     ),
 }
 
