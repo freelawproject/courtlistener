52a0bd9b706bed203ddf9ab79b8d481075760ff4
vvilliam <wpalin@gmail.com>
2020-10-09 11:59:08 -0400

    fix(crypto): Update sha1_activation_key py3 encoding

--- cl/lib/crypto.py
+++ cl/lib/crypto.py
@@ -78,8 +78,8 @@ def sha1_activation_key(s):
     :param s: The data to use with the salt to make the activation key
     :return: A SHA1 activation key
     """
-    salt = hashlib.sha1(str(random.random())).hexdigest()[:5]
-    activation_key = hashlib.sha1(salt + s).hexdigest()
+    salt = hashlib.sha1(str(random.random()).encode()).hexdigest()[:5]
+    activation_key = hashlib.sha1((salt + s).encode()).hexdigest()
     return activation_key
 
 
