990c8cb253718f9d7622d7b5864470453c958736
vvilliam <wpalin@gmail.com>
2020-10-09 11:11:48 -0400

    fix(scraper/tests): Update ExtensionIDTests
    Read file to binary to avoid utf8 incompatability issues with files not encoded in it.

--- cl/scrapers/tests.py
+++ cl/scrapers/tests.py
@@ -145,28 +145,28 @@ class ExtensionIdentificationTest(TestCase):
         self.path = os.path.join(settings.MEDIA_ROOT, "test", "search")
 
     def test_wpd_extension(self):
-        with open(os.path.join(self.path, "opinion_wpd.wpd"), "r") as f:
+        with open(os.path.join(self.path, "opinion_wpd.wpd"), "rb") as f:
             data = f.read()
         self.assertEqual(get_extension(data), ".wpd")
 
     def test_pdf_extension(self):
         with open(
-            os.path.join(self.path, "opinion_pdf_text_based.pdf"), "r"
+            os.path.join(self.path, "opinion_pdf_text_based.pdf"), "rb"
         ) as f:
             data = f.read()
         self.assertEqual(get_extension(data), ".pdf")
 
     def test_doc_extension(self):
-        with open(os.path.join(self.path, "opinion_doc.doc"), "r") as f:
+        with open(os.path.join(self.path, "opinion_doc.doc"), "rb") as f:
             data = f.read()
         self.assertEqual(get_extension(data), ".doc")
 
     def test_html_extension(self):
-        with open(os.path.join(self.path, "opinion_html.html"), "r") as f:
+        with open(os.path.join(self.path, "opinion_html.html"), "rb") as f:
             data = f.read()
         self.assertEqual(get_extension(data), ".html")
 
-        with open(os.path.join(self.path, "not_wpd.html"), "r") as f:
+        with open(os.path.join(self.path, "not_wpd.html"), "rb") as f:
             data = f.read()
         self.assertEqual(get_extension(data), ".html")
 
