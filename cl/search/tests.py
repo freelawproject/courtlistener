ff9ccb8df5acbbd66cc170c6f279b90a9d1b9abc
vvilliam <wpalin@gmail.com>
2020-10-08 20:28:48 -0400

    fix(test_more_like_this_opinion_detail_no_filter): Py2->3 encoding update

--- cl/search/tests.py
+++ cl/search/tests.py
@@ -725,10 +725,10 @@ class RelatedSearchTest(IndexedSolrTestCase):
 
         # Test for click tracking order
         self.assertTrue(
-            r.content.index(
+            r.content.decode().index(
                 "'clickRelated_mlt_seed%i', %i," % (seed_pk, expected_first_pk)
             )
-            < r.content.index(
+            < r.content.decode().index(
                 "'clickRelated_mlt_seed%i', %i,"
                 % (seed_pk, expected_second_pk)
             ),
