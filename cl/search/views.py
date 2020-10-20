b7c203e10b971b88b19a4f47f5e2f3cdbdf51bf8
vvilliam <wpalin@gmail.com>
2020-10-20 11:33:36 -0400

    fix(search.views): Close solr connections

--- cl/search/views.py
+++ cl/search/views.py
@@ -131,6 +131,7 @@ def do_search(
         try:
             si = get_solr_interface(cd)
         except NotImplementedError:
+            si.conn.http_connection.close()
             logger.error(
                 "Tried getting solr connection for %s, but it's not "
                 "implemented yet",
@@ -154,11 +155,13 @@ def do_search(
                     # Original query
                     cd["q"].replace(related_prefix_match.group("pfx"), ""),
                 )
+                si.conn.http_connection.close()
             else:
                 # Regular search queries
                 results = si.query().add_extra(
                     **build_main_query(cd, facet=facet)
                 )
+                si.conn.http_connection.close()
 
             paged_results = paginate_cached_solr_results(
                 get_params, cd, results, rows, cache_key
