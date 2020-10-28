630df7b95f831bf7327b415960f412add53f4009
Michael Lissner <mlissner@michaeljaylissner.com>
2020-10-28 12:26:58 -0700

    feat(apache): Add tests to our WSGI test script

--- docker/apache/wsgi-configs/python_version_test.py
+++ docker/apache/wsgi-configs/python_version_test.py
@@ -1,6 +1,6 @@
 # Use this test to ensure that your python binary and virtualenvs are set up
 # properly.
-
+import locale
 import sys
 
 
@@ -11,6 +11,24 @@ def application(environ, start_response):
     output += u"sys.version = %s\n" % repr(sys.version)
     output += u"sys.prefix = %s\n" % repr(sys.prefix)
     output += u"sys.path = %s\n" % repr(sys.path)
+    output += u"locale.getlocale() = %s\n" % repr(locale.getlocale())
+    output += u"locale.getdefaultlocale() = %s\n" % repr(
+        locale.getdefaultlocale()
+    )
+    output += u"sys.getfilesystemencoding() = %s\n" % repr(
+        sys.getfilesystemencoding()
+    )
+    output += u"sys.getdefaultencoding() = %s\n" % repr(
+        sys.getdefaultencoding()
+    )
+    output += u"locale.getpreferredencoding(False): %s\n" % repr(
+        locale.getpreferredencoding(False)
+    )
+
+    output += "\n\n"
+    output += "mod_wsgi.process_group = %s" % repr(
+        environ["mod_wsgi.process_group"]
+    )
 
     response_headers = [
         ("Content-type", "text/plain"),
