fdf03dbee0d1617cac60d1b788aefec8053b4318
Michael Lissner <mlissner@michaeljaylissner.com>
2020-11-03 23:49:28 -0800

    feat(urls): Remove ancient validation utils
    (We'll use DNS for this in the future.)

--- cl/simple_pages/views.py
+++ cl/simple_pages/views.py
@@ -344,24 +344,6 @@ def robots(request):
     return response
 
 
-def validate_for_bing(request):
-    return HttpResponse(
-        '<?xml version="1.0"?><users><user>8BA95D8EAA744379D80D9F70847EA156</user></users>'
-    )
-
-
-def validate_for_google(request):
-    return HttpResponse(
-        "google-site-verification: googleef3d845637ccb353.html"
-    )
-
-
-def validate_for_google2(request):
-    return HttpResponse(
-        "google-site-verification: google646349975c2495b6.html"
-    )
-
-
 def validate_for_wot(request):
     return HttpResponse("bcb982d1e23b7091d5cf4e46826c8fc0")
 
