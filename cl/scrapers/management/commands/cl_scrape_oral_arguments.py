085027ed3bb6ac16559e3fa293b7ab18aaea9c98
Michael Lissner <mlissner@michaeljaylissner.com>
2020-11-08 08:23:14 -0800

    fix(scraper): Remove adapter from binary downloader
    It's never overridden in Juriscraper, it's a protected method, and
    it's about to get killed in Juriscraper.

--- cl/scrapers/management/commands/cl_scrape_oral_arguments.py
+++ cl/scrapers/management/commands/cl_scrape_oral_arguments.py
@@ -120,7 +120,6 @@ class Command(cl_scrape_opinions.Command):
                 msg, r = get_binary_content(
                     item["download_urls"],
                     site.cookies,
-                    site._get_adapter_instance(),
                     method=site.method,
                 )
                 if msg:
--- cl/scrapers/utils.py
+++ cl/scrapers/utils.py
@@ -87,13 +87,12 @@ def get_extension(content):
     return fixes.get(extension, extension).lower()
 
 
-def get_binary_content(download_url, cookies, adapter, method="GET"):
+def get_binary_content(download_url, cookies, method="GET"):
     """Downloads the file, covering a few special cases such as invalid SSL
     certificates and empty file errors.
 
     :param download_url: The URL for the item you wish to download.
     :param cookies: Cookies that might be necessary to download the item.
-    :param adapter: An HTTPAdapter for use when getting content.
     :param method: The HTTP method used to get the item, or "LOCAL" to get an
     item during testing
     :return: Two values. The first is a msg indicating any errors encountered.
@@ -117,7 +116,6 @@ def get_binary_content(download_url, cookies, adapter, method="GET"):
             # Note that we do a GET even if site.method is POST. This is
             # deliberate.
             s = requests.session()
-            s.mount("https://", adapter)
             headers = {"User-Agent": "CourtListener"}
 
             r = s.get(
