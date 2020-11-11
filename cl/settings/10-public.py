5ce125040860fb738804a3d2ceb9fd9e89feb26d
Michael Lissner <mlissner@michaeljaylissner.com>
2020-11-10 17:12:52 -0800

    refactor(settings): More readable timeout setting

--- cl/settings/10-public.py
+++ cl/settings/10-public.py
@@ -204,9 +204,9 @@ else:
     CELERY_BROKER_TRANSPORT_OPTIONS = {
         # This is the length of time a task will wait to be acknowledged by a
         # worker. This value *must* be greater than the largest ETA/countdown
-        # that a task may be assigned with, or else it will be run over and over
-        # in a loop. Our countdowns never tend to exceed one hour.
-        "visibility_timeout": 21600,  # six hours
+        # that a task may be assigned with, or else it will be run over and
+        # over in a loop. Our countdowns never tend to exceed one hour.
+        "visibility_timeout": (60 * 60 * 6),  # six hours
     }
 
 CELERY_BROKER_URL = "redis://%s:%s/%s" % (
