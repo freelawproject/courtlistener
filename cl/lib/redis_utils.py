4d9f93e44f32af91260302a9588cd4fc9005707b
Michael Lissner <mlissner@michaeljaylissner.com>
2020-11-10 17:29:40 -0800

    refactor(redis): Switch out deprecated class and add type hints

--- cl/lib/redis_utils.py
+++ cl/lib/redis_utils.py
@@ -2,13 +2,13 @@ import redis
 from django.conf import settings
 
 
-def make_redis_interface(db_name):
+def make_redis_interface(db_name: str) -> redis.Redis:
     """Create a redis connection object
 
     :param db_name: The name of the database to use, as defined in our settings
-    :return StrictRedis interface using django settings
+    :return Redis interface using django settings
     """
-    return redis.StrictRedis(
+    return redis.Redis(
         host=settings.REDIS_HOST,
         port=settings.REDIS_PORT,
         db=settings.REDIS_DATABASES[db_name],
