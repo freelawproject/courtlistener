51c953aebbe9bc0e74d9ac71ae7d0bb4c5b13082
Michael Lissner <mlissner@michaeljaylissner.com>
2020-11-15 09:50:54 -0800

    fix(utils): Send text to StringIO, not bytes
    Fixes: COURTLISTENER-MT

--- cl/lib/solr_core_admin.py
+++ cl/lib/solr_core_admin.py
@@ -41,7 +41,7 @@ def get_solr_core_status(core="all", url=settings.SOLR_HOST):
         )
 
     try:
-        solr_config = lxml.etree.parse(io.StringIO(r.content))
+        solr_config = lxml.etree.parse(io.StringIO(r.text))
     except lxml.etree.XMLSyntaxError as e:
         raise SolrError("Invalid XML in schema:\n%s" % e.args[0])
 
