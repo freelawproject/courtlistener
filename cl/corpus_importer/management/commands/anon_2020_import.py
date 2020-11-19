4dfa9ee6335786e72295f701f61668e8c31375f6
vvilliam <wpalin@gmail.com>
2020-11-19 13:45:47 -0500

    feat(anon_2020_import): Update import code
    Change methods to merge, merge+ or add new opinions to our database.

--- cl/corpus_importer/management/commands/anon_2020_import.py
+++ cl/corpus_importer/management/commands/anon_2020_import.py
@@ -2,7 +2,7 @@ import json
 import re
 from datetime import datetime
 from glob import iglob
-from typing import Optional, Tuple, Dict, List, AnyStr
+from typing import Optional, Dict, List, Tuple
 
 from bs4 import BeautifulSoup as bs4
 from django.db import transaction
@@ -18,31 +18,6 @@ from cl.search.models import Opinion, OpinionCluster, Docket, Citation
 from cl.search.tasks import add_items_to_solr
 
 cnt = CaseNameTweaker()
-
-
-def validate_dt(date_str: str) -> Tuple[datetime.date, bool]:
-    """Validate datetime string.
-
-    Check if the date string is only year-month or year.
-    If partial date string, make date string the first of the month
-    and mark the date as an estimate.
-
-    If unable to validate date return an empty string, True tuple.
-
-    :param date_str: a date string we receive from the harvard corpus.
-    :returns: Tuple of date obj or date obj estimate.
-    and boolean indicating estimated date or actual date.
-    """
-    date_approx = False
-    add_ons = ["", "-15", "-07-01"]
-    for add_on in add_ons:
-        try:
-            date_obj = datetime.strptime(date_str + add_on, "%Y-%m-%d").date()
-            break
-        except ValueError:
-            # Failed parsing at least once, ∴ an approximate date
-            date_approx = True
-    return date_obj, date_approx
 
 
 def find_cites(case_data: Dict[str, str]) -> List[FoundCitation]:
@@ -62,20 +37,114 @@ def find_cites(case_data: Dict[str, str]) -> List[FoundCitation]:
     return found_citations
 
 
-def should_we_add_opinion(cluster_id: int) -> bool:
-    """Check if we previously added this document.
+def merge_or_add_opinions(
+    cluster_id: int,
+    html_str: str,
+    data: Dict[str, Optional[str, int]],
+    date_argued: datetime.date,
+    date_filed: datetime.date,
+    case_names: Dict[str, str],
+    status: str,
+    docket_number: str,
+    found_citations: List[FoundCitation],
+):
+    """Merge opinions if applicable.
 
-    If we find the citation in our system, we check if the anon-2020 DB html
-    has been added to the system previously.
+    If opinion not in system, merge or add to cluster.
+    If opinion is on system came from harvard, add new opinion to cluster, else
+    we merge new opinion data into scraped opinion.
 
-    :param cluster_id: ID of any cluster opinion found.
-    :return: Should we had the opinion to found cluster.
+    :param cluster_id: Opinion Cluster id.
+    :param html_str: HTML opinion to add.
+    :param data: Case data to import.
+    :param date_argued: Date case was argued.
+    :param date_filed: Date case was filed.
+    :return: None.
     """
-    return (
+    does_exist = (
         Opinion.objects.filter(cluster_id=cluster_id)
         .exclude(html_anon_2020="")
         .exists()
     )
+    if does_exist:
+        logger.info(f"Opinion already in database at {cluster_id}")
+        return
+
+    logger.info(f"Starting merger of opinions in cluster {cluster_id}.")
+
+    cluster = OpinionCluster.objects.get(pk=cluster_id)
+    docket = cluster.docket
+
+    # Dates are uniformly good in our dataset
+    # validation and is_approx not needed
+
+    # Merge docket information
+    docket.source = docket.source + docket.ANON_2020
+    docket.date_argued = date_argued or docket.date_argued
+    docket.docket_number = docket_number or docket.docket_number
+    docket.case_name_short = (
+        case_names["case_name_short"] or docket.case_name_short
+    )
+    docket.case_name = case_names["case_name"] or docket.case_name
+    docket.case_name_full = (
+        case_names["case_name_full"] or docket.case_name_full
+    )
+
+    # Merge cluster information
+
+    cluster.date_filed = date_filed or cluster.date_filed
+    cluster.precedential_status = status or cluster.precedential_status
+    cluster.attorneys = data["representation"] or cluster.attorneys
+    cluster.disposition = data["summary_disposition"] or cluster.disposition
+    cluster.summary = data["summary_court"] or cluster.summary
+    cluster.history = data["history"] or cluster.history
+    cluster.other_dates = data["date_standard"] or cluster.other_dates
+    cluster.cross_reference = (
+        data["history_docket_numbers"] or cluster.cross_reference
+    )
+    cluster.correction = data["publication_status_note"] or cluster.correction
+    if data["judges"]:
+        cluster.judges = (
+            data["judges"].replace("{", "").replace("}", "") or cluster.judges
+        )
+    cluster.case_name_short = (
+        case_names["case_name_short"] or cluster.case_name_short
+    )
+    cluster.case_name = case_names["case_name"] or cluster.case_name
+    cluster.case_name_full = (
+        case_names["case_name_full"] or cluster.case_name_full
+    )
+
+    docket.save()
+    cluster.save()
+
+    # Add citations to cluster if applicable
+    for citation in found_citations:
+        Citation.objects.get_or_create(
+            volume=citation.volume,
+            reporter=citation.reporter,
+            page=citation.page,
+            type=map_reporter_db_cite_type(
+                REPORTERS[citation.canonical_reporter][0]["cite_type"]
+            ),
+            cluster_id=cluster.id,
+        )
+
+    # Merge with scrape or add opinion to cluster with harvard
+    if OpinionCluster.objects.get(pk=cluster_id).source == "C":
+        opinion = Opinion.objects.get(cluster_id=cluster_id)
+        logger.info("Merge with Harvard data")
+        opinion.html_anon_2020 = html_str
+    else:
+        op = Opinion(
+            cluster_id=cluster.id,
+            type=Opinion.COMBINED,
+            html_anon_2020=html_str,
+            extracted_by_ocr=False,
+        )
+        op.save()
+
+    logger.info(f"Finished merging opinion in cluster {cluster_id}.")
 
 
 def check_publication_status(found_cites: List[Citation]) -> str:
@@ -96,26 +165,6 @@ def check_publication_status(found_cites: List[Citation]) -> str:
     return "Unpublished"
 
 
-def add_only_opinion(soup, cluster_id) -> None:
-    """Add opinion to the cluster object.
-
-    This is only run if we are already in the system and just need
-    to add the anon 2020 db html to our cluster.
-
-    :param soup: bs4 html object of opinion.
-    :param cluster_id: Cluster ID for the opinion to save.
-    :return:None.
-    """
-    html_str = soup.find("div", {"class": "container"}).decode_contents()
-    op = Opinion(
-        cluster_id=cluster_id,
-        type=Opinion.COMBINED,
-        html_anon_2020=html_str,
-        extracted_by_ocr=False,
-    )
-    op.save()
-
-
 def attempt_cluster_lookup(citations: List[FoundCitation]) -> Optional[int]:
     """Check if the citation in our database.
 
@@ -125,17 +174,22 @@ def attempt_cluster_lookup(citations: List[FoundCitation]) -> Optional[int]:
     :return: Cluster id for citations.
     """
     for citation in citations:
-        cite = Citation.objects.get(
+        cite_exists = Citation.objects.filter(
             reporter=citation.reporter,
             page=citation.page,
             volume=citation.volume,
-        )
-        if cite:
-            return cite.cluster_id
+        ).exists()
+        if cite_exists:
+            citation = Citation.objects.get(
+                reporter=citation.reporter,
+                page=citation.page,
+                volume=citation.volume,
+            )
+            return citation.cluster_id
     return None
 
 
-def do_case_name(soup, data) -> Dict[str, str]:
+def do_case_name(soup, data: Dict[str, Optional[str, int]]) -> Dict[str, str]:
     """Extract and normalize the case name
 
     :param soup: bs4 html object of opinion.
@@ -153,15 +207,36 @@ def do_case_name(soup, data) -> Dict[str, str]:
     }
 
 
-def find_court_id(data) -> AnyStr:
+def find_court_id(court_str: str) -> str:
     """Extract cl court id from court name
 
-    :param data: The full json data dict
+    :param court_str: The raw court name
     :return: The cl court id for associated tax court.
     """
-    if data["court"] == "United States Board of Tax Appeals":
+    if court_str == "United States Board of Tax Appeals":
         return "bta"
     return "tax"
+
+
+def process_dates(
+    data: Dict[str, Optional[str, int]]
+) -> Tuple[datetime.date, datetime.date()]:
+    """Process date argued and date filed
+
+    Dates in this dataset fall into two categories, argued and filed/decided.
+    We use date standard as the key for filed and/or decided.
+    :param data:
+    :return:
+    """
+    date_argued = date_filed = None
+    if data["date_argued"]:
+        date_argued = datetime.strptime(data["date_argued"], "%Y-%m-%d").date()
+    if data["date_standard"]:
+        date_filed = datetime.strptime(
+            data["date_standard"], "%Y-%m-%d"
+        ).date()
+
+    return date_argued, date_filed
 
 
 def import_anon_2020_db(
@@ -184,64 +259,50 @@ def import_anon_2020_db(
     """
     directories = iglob(f"{import_dir}/*/????-*.json")
     for dir in directories:
-        logger.info(f"Importing case at: {dir}")
+        logger.info(f"Importing case id: {dir.split('/')[-2]}")
         if skip_until:
             if skip_until in dir:
-                continue
-            skip_until = False
+                skip_until = False
+            continue
 
-        # Prepare data and HTML
+        # Prepare data and html
         with open(dir, "rb") as f:
             data = json.load(f)
         with open(dir.replace("json", "html"), "rb") as f:
             soup = bs4(f.read(), "html.parser")
 
+        case_names = do_case_name(soup, data)
+        court_id = find_court_id(data["court"])
+        date_argued, date_filed = process_dates(data)
+        docket_number = trunc(
+            data["docket_number"],
+            length=5000,
+            ellipsis="...",
+        )
+        html_str = soup.find("div", {"class": "container"}).decode_contents()
         found_cites = find_cites(data)
+        status = check_publication_status(found_cites)
+
         if found_cites is not None:
             cluster_id = attempt_cluster_lookup(found_cites)
             if cluster_id is not None:
-                add_opinion = should_we_add_opinion(cluster_id)
-                if add_opinion:
-                    logger.info(f"Adding opinion to cluster {cluster_id}.")
-                    add_only_opinion(soup, cluster_id)
-                else:
-                    logger.info(f"Opinion in system at {cluster_id}.")
+                merge_or_add_opinions(
+                    cluster_id,
+                    html_str,
+                    data,
+                    date_argued,
+                    date_filed,
+                    case_names,
+                    status,
+                    docket_number,
+                    found_cites,
+                )
                 continue
-
-        # xxx move court stuff to function
-        if data["court"] == "United States Tax Court":
-            court_id = "tax"
-        else:
-            court_id = "bta"
-
         with transaction.atomic():
             logger.info(
                 "Creating docket for: %s", found_cites[0].base_citation()
             )
 
-            # XXX move this into validate function
-            try:
-                date_argued, is_approximate = validate_dt(data["date_argued"])
-            except:
-                # xxx add exception type
-                date_argued, is_approximate = None, None
-
-            try:
-                date_filed, is_approximate = validate_dt(data["date_filed"])
-            except:
-                # xxx naked exceptions in rare circumstances only
-                date_filed, is_approximate = validate_dt(data["date_standard"])
-
-            # xxx do all parsing, normalizing, etc, up front, then to all
-            #     merging next, so stuff isn't intermixed.
-
-            case_names = do_case_name(soup, data)
-            status = check_publication_status(found_cites)
-            docket_number = trunc(
-                data["docket_number"],
-                length=5000,
-                ellipsis="...",
-            )
             docket = Docket.objects.create(
                 **case_names,
                 docket_number=docket_number,
@@ -252,13 +313,13 @@ def import_anon_2020_db(
             )
 
             logger.info("Add cluster for: %s", found_cites[0].base_citation())
+            judges = data["judges"] or ""
             cluster = OpinionCluster(
                 **case_names,
                 precedential_status=status,
                 docket_id=docket.id,
                 source=docket.ANON_2020,
                 date_filed=date_filed,
-                date_filed_is_approximate=is_approximate,
                 attorneys=data["representation"] or "",
                 disposition=data["summary_disposition"] or "",
                 summary=data["summary_court"] or "",
@@ -266,7 +327,7 @@ def import_anon_2020_db(
                 other_dates=data["date_standard"] or "",
                 cross_reference=data["history_docket_numbers"] or "",
                 correction=data["publication_status_note"] or "",
-                judges=data["judges"].replace("{", "").replace("}", "") or "",
+                judges=judges.replace("{", "").replace("}", "") or "",
             )
             cluster.save(index=False)
 
@@ -274,7 +335,7 @@ def import_anon_2020_db(
                 logger.info(
                     "Adding citation for: %s", citation.base_citation()
                 )
-                Citation.objects.create(
+                Citation.objects.get_or_create(
                     volume=citation.volume,
                     reporter=citation.reporter,
                     page=citation.page,
@@ -283,10 +344,6 @@ def import_anon_2020_db(
                     ),
                     cluster_id=cluster.id,
                 )
-
-            html_str = soup.find(
-                "div", {"class": "container"}
-            ).decode_contents()
 
             op = Opinion(
                 cluster_id=cluster.id,
@@ -298,7 +355,9 @@ def import_anon_2020_db(
 
             if make_searchable:
                 add_items_to_solr.delay([op.pk], "search.Opinion")
-        logger.info("Finished: %s", found_cites[0].base_citation())
+        logger.info(
+            f"Finished importing cluster {cluster.id}; {found_cites[0].base_citation()}"
+        )
 
 
 class Command(VerboseCommand):
