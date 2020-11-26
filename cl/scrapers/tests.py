43dc43e11f4d561d1b53b7ccbbf69eafa0d8d718
vvilliam <wpalin@gmail.com>
2020-11-25 19:45:20 -0500

    test(scraper.tests): Add testing for new BTE call

--- cl/scrapers/tests.py
+++ cl/scrapers/tests.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 
 from django.conf import settings
-from django.test import TestCase, override_settings
+from django.test import TestCase
 from django.utils.timezone import now
 
 from cl.audio.models import Audio
@@ -20,6 +20,9 @@ from cl.scrapers.tasks import (
     process_audio_file,
 )
 from cl.scrapers.test_assets import test_opinion_scraper, test_oral_arg_scraper
+from cl.scrapers.transformer_extractor_utils import (
+    convert_and_clean_audio,
+)
 from cl.scrapers.utils import get_extension
 from cl.search.models import Court, Opinion
 
@@ -457,4 +460,12 @@ class AudioFileTaskTest(TestCase):
             delta=5,
             msg="We should end up with the proper duration of about %s. "
             "Instead we got %s." % (expected_duration, measured_duration),
+        )
+
+    def test_BTE_audio_conversion(self):
+        """Can we convert wav to audio and update the metadata"""
+        af = Audio.objects.get(pk=1)
+        resp = convert_and_clean_audio(af)
+        self.assertEqual(
+            resp.status_code, 200, msg="Unsuccessful audio conversion"
         )
