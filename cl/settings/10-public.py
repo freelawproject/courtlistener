0d4bbb7e614a9412b8e483fc5db41a9b283809c5
vvilliam <wpalin@gmail.com>
2020-11-26 10:21:23 -0500

    style(10-public): Black

--- cl/settings/10-public.py
+++ cl/settings/10-public.py
@@ -594,7 +594,4 @@ CLOUDFRONT_DOMAIN = ""
 ####################################
 
 BTE_HOST = "http://cl-binary-transformers-and-extractors:5050"
-BTE_URLS = {
-    "convert_audio": f"{BTE_HOST}/convert/audio"
-}
-
+BTE_URLS = {"convert_audio": f"{BTE_HOST}/convert/audio"}
