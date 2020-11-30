e5941df28c81b8c383bc0e7317623e335cfa192d
vvilliam <wpalin@gmail.com>
2020-11-30 10:24:21 -0500

    docs(transformer_extractor_utils): Update doc strings and type hints

--- cl/scrapers/transformer_extractor_utils.py
+++ cl/scrapers/transformer_extractor_utils.py
@@ -1,16 +1,21 @@
 import json
+from typing import Dict
 
 import requests
 from django.conf import settings
 from django.core.serializers import serialize
 
 
-def make_audio_post_params(af):
+def make_audio_post_params(af) -> Dict:
     """Make audio object parameters used in conversion process.
+
+    BTE requires court and docket information to process the audio. This
+    serializes the required information as JSON so we can post it to
+    the BTE container.
 
     :param af: Audio file.
     :return: Audio data in json format
-    :type: JSON
+    :type: dict
     """
     af_dict = json.loads(serialize("json", [af]))[0]["fields"]
     docket_dict = json.loads(serialize("json", [af.docket]))[0]["fields"]
@@ -20,12 +25,12 @@ def make_audio_post_params(af):
     return {"audio_obj": json.dumps(af_dict)}
 
 
-def convert_and_clean_audio(audio_obj):
+def convert_and_clean_audio(audio_obj) -> requests.Response:
     """Convert audio file to MP3 w/ metadata and image.
 
     :param audio_obj: Audio file object
-    :return: Converted audio
-    :type: JSON response
+    :return: Request response containing converted audio file w/ duration info.
+    :type: requests.Response
     """
     with open(audio_obj.local_path_original_file.path, "rb") as af:
         audio_file = {"file": ("audio_file", af.read())}
