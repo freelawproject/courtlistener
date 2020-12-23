81792196a8983a91169d7be9c1c5d55568de95c3
vvilliam <wpalin@gmail.com>
2020-12-23 16:28:06 -0500

    feat(cl.disclosures.tasks): Init disclosure tasks file
    Transfer make_financial_disclosure_thumbnail_from_pdf to disclosures from people_db

---
+++ cl/disclosures/tasks.py
@@ -0,0 +1,14 @@
+from django.apps import apps
+
+from cl.celery_init import app
+from cl.people_db.tasks import make_png_thumbnail_for_instance
+
+
+@app.task
+def make_financial_disclosure_thumbnail_from_pdf(pk):
+    make_png_thumbnail_for_instance(
+        pk=pk,
+        InstanceClass=apps.get_model('disclosures', 'FinancialDisclosure'),
+        file_attr="filepath",
+        max_dimension=350,
+    )
