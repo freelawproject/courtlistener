dc2970f7e86e3fa9909e5d251292a4b0a37637cf
vvilliam <wpalin@gmail.com>
2021-01-12 17:05:59 -0500

    tests(disclosure.tests): Add new tests for API generation

--- cl/disclosures/tests.py
+++ cl/disclosures/tests.py
@@ -2,7 +2,9 @@ import json
 import os
 
 from django.conf import settings
+from django.contrib.auth.models import Permission, User
 from django.test import TestCase
+from django.urls import reverse
 
 from cl.disclosures.models import (
     FinancialDisclosure,
@@ -51,4 +53,151 @@ class DisclosureIngestionTests(TestCase):
             non_investments.count() == 2,
             "Should have 2 ingested non-investments, not %s"
             % non_investments.count(),
+        )
+
+
+class DisclosureAPIAccessTests(TestCase):
+
+    fixtures = [
+        "authtest_data.json",
+        "disclosure.json",
+        "judge_judy.json",
+    ]
+
+    def setUp(self):
+        u = User.objects.get(pk=1001)
+        ps = Permission.objects.filter(codename="has_disclosure_api_access")
+        u.user_permissions.add(*ps)
+        self.assertTrue(
+            self.client.login(username="pandora", password="password")
+        )
+
+    def test_basic_disclosure_api_query(self):
+        """Can we query the financial disclosures?"""
+        url = reverse("financial-disclosure-list", kwargs={"version": "v3"})
+        self.assertEqual(
+            url, "/api/rest/v3/financial-disclosures/", msg="API URL fails"
+        )
+        # 4 of the queries are from the setup
+        with self.assertNumQueries(13):
+            r = self.client.get(url)
+            self.assertEqual(r.status_code, 200, msg="API failed.")
+            self.assertEqual(r.json()["count"], 2, msg="Wrong API count.")
+
+    def test_gift_disclosure_api(self):
+        """Can we query the gifts?"""
+        url = reverse("gift-list", kwargs={"version": "v3"})
+        self.assertEqual(url, "/api/rest/v3/gifts/", msg="API URL fails")
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, 200, msg="API failed.")
+        self.assertEqual(r.json()["count"], 1, msg="Wrong API count")
+
+    def test_investments_api(self):
+        """Can we query the investments?"""
+        url = reverse("investment-list", kwargs={"version": "v3"})
+        self.assertEqual(url, "/api/rest/v3/investments/", msg="API URL fails")
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, 200, msg="API failed.")
+        self.assertEqual(r.json()["count"], 9, msg="Wrong API count")
+
+    def test_unauthorized_user(self):
+        """Can a regular user access forbidden content?"""
+        self.client.logout()
+        url = reverse("gift-list", kwargs={"version": "v3"})
+        r = self.client.get(url)
+        self.assertEqual(
+            r.status_code, 401, msg="Unauthorized content exposed."
+        )
+
+    def test_access_to_content_outside_authorization(self):
+        """Can a admin user access forbidden content?"""
+        self.client.login(username="admin", password="admin")
+        url = reverse("gift-list", kwargs={"version": "v3"})
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, 200, msg="Admin lacking access.")
+
+
+class DisclosureAPITests(TestCase):
+
+    fixtures = [
+        "authtest_data.json",
+        "disclosure.json",
+        "judge_judy.json",
+    ]
+
+    def setUp(self):
+        u = User.objects.get(pk=1001)
+        ps = Permission.objects.filter(codename="has_disclosure_api_access")
+        u.user_permissions.add(*ps)
+        self.assertTrue(
+            self.client.login(username="pandora", password="password")
+        )
+        self.q = dict()
+
+    def test_basic_disclosure_api_query(self):
+        """Can we query the financial disclosure API?"""
+        self.path = reverse("disclosure-list", kwargs={"version": "v3"})
+        self.assertEqual(
+            self.path, "/api/rest/v3/disclosures/", msg="API URL fails"
+        )
+        r = self.client.get(self.path)
+        self.assertEqual(r.status_code, 200, msg="API failed.")
+        self.assertEqual(r.json()["count"], 2, msg="Wrong API count")
+
+    def test_disclosure_position_api(self):
+        """Can we query the financial disclosure API?"""
+        self.path = reverse(
+            "disclosure-position-list", kwargs={"version": "v3"}
+        )
+        r = self.client.get(self.path)
+        self.assertEqual(r.status_code, 200, msg="API failed.")
+        self.assertEqual(r.json()["count"], 2, msg="Wrong API count")
+
+    def test_investment_filtering(self):
+        """Can we filter investments by transaction value codes?"""
+        self.path = reverse("investment-list", kwargs={"version": "v3"})
+        self.q = {"transaction_value_code": "M"}
+        r = self.client.get(self.path, self.q)
+        self.assertEqual(r.json()["count"], 1, msg="Wrong Investment filter")
+
+    def test_exact_filtering_by_id(self):
+        """Can we filter investments by id?"""
+        self.path = reverse("investment-list", kwargs={"version": "v3"})
+        self.q = {"id": 878}
+        r = self.client.get(self.path, self.q)
+        self.assertEqual(
+            r.json()["count"], 1, msg="Investment filtering by id failed."
+        )
+
+    def test_filtering_description_by_text(self):
+        """Can we filter by description partial text?"""
+        self.path = reverse("investment-list", kwargs={"version": "v3"})
+        self.q = {"description__startswith": "Harris Bank"}
+        r = self.client.get(self.path, self.q)
+        self.assertEqual(
+            r.json()["count"], 2, msg="Investment filtering by id failed."
+        )
+
+    def test_filter_investments_by_redaction(self):
+        """Can we filter investments by redaction boolean?"""
+        self.path = reverse("investment-list", kwargs={"version": "v3"})
+        self.q = {"redacted": True}
+        r = self.client.get(self.path, self.q)
+        self.assertEqual(
+            r.json()["count"],
+            3,
+            msg="Investment filtering by redactions failed.",
+        )
+
+    def test_filter_disclosures_by_person_id(self):
+        """Can we filter disclosures by person id?"""
+        self.path = reverse(
+            "financial-disclosure-list", kwargs={"version": "v3"}
+        )
+        self.q = {"person": 2}
+        r = self.client.get(self.path, self.q)
+        self.assertEqual(
+            r.json()["count"],
+            1,
+            msg="Incorrect disclosures found.",
         )
