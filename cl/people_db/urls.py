0a667461dd5837d3dea89cebae82cd1e4573a34d
vvilliam <wpalin@gmail.com>
2021-01-12 17:02:14 -0500

    feat(disclosure.urls): Move disclosure URLS to disclosure app

--- cl/people_db/urls.py
+++ cl/people_db/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import url
 
 from cl.people_db.views import (
-    financial_disclosures_fileserver,
-    financial_disclosures_for_somebody,
-    financial_disclosures_home,
     view_person,
 )
 
@@ -12,26 +9,5 @@ urlpatterns = [
         r"^person/(?P<pk>\d+)/(?P<slug>[^/]*)/$",
         view_person,
         name="view_person",
-    ),
-    url(
-        r"^person/(?P<pk>\d+)/(?P<slug>[^/]*)/financial-disclosures/$",
-        financial_disclosures_for_somebody,
-        name="financial_disclosures_for_somebody",
-    ),
-    # Serve the PDFs, TIFFS, and thumbnails
-    url(
-        r"^person/"
-        r"(?P<pk>\d+)/"
-        r"(?P<slug>[^/]*)/"
-        r"(?P<filepath>financial-disclosures/"
-        r"(?:thumbnails/)?"
-        r".+\.(?:pdf|tiff|png))$",
-        financial_disclosures_fileserver,
-        name="financial_disclosures_fileserver",
-    ),
-    url(
-        r"^financial-disclosures/$",
-        financial_disclosures_home,
-        name="financial_disclosures_home",
     ),
 ]
