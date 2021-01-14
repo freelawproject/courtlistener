b1c2e24cabb5413c862e324aaaff7f0e7d3827d6
Michael Lissner <mlissner@michaeljaylissner.com>
2021-01-14 12:55:11 -1000

    refactor(api): DRY setup class

--- cl/disclosures/tests.py
+++ cl/disclosures/tests.py
@@ -56,8 +56,7 @@ class DisclosureIngestionTests(TestCase):
         )
 
 
-class DisclosureAPIAccessTests(TestCase):
-
+class LoggedInDisclosureTestCase(TestCase):
     fixtures = [
         "authtest_data.json",
         "disclosure.json",
@@ -71,23 +70,22 @@ class DisclosureAPIAccessTests(TestCase):
         self.assertTrue(
             self.client.login(username="pandora", password="password")
         )
+        self.q = dict()
 
+
+class DisclosureAPIAccessTests(LoggedInDisclosureTestCase):
     def test_basic_disclosure_api_query(self):
         """Can we query the financial disclosures?"""
         url = reverse("financial-disclosure-list", kwargs={"version": "v3"})
-        self.assertEqual(
-            url, "/api/rest/v3/financial-disclosures/", msg="API URL fails"
-        )
         # 4 of the queries are from the setup
         with self.assertNumQueries(13):
             r = self.client.get(url)
-            self.assertEqual(r.status_code, 200, msg="API failed.")
-            self.assertEqual(r.json()["count"], 2, msg="Wrong API count.")
+        self.assertEqual(r.status_code, 200, msg="API failed.")
+        self.assertEqual(r.json()["count"], 2, msg="Wrong API count.")
 
     def test_gift_disclosure_api(self):
         """Can we query the gifts?"""
         url = reverse("gift-list", kwargs={"version": "v3"})
-        self.assertEqual(url, "/api/rest/v3/gifts/", msg="API URL fails")
         r = self.client.get(url)
         self.assertEqual(r.status_code, 200, msg="API failed.")
         self.assertEqual(r.json()["count"], 1, msg="Wrong API count")
@@ -95,7 +93,6 @@ class DisclosureAPIAccessTests(TestCase):
     def test_investments_api(self):
         """Can we query the investments?"""
         url = reverse("investment-list", kwargs={"version": "v3"})
-        self.assertEqual(url, "/api/rest/v3/investments/", msg="API URL fails")
         r = self.client.get(url)
         self.assertEqual(r.status_code, 200, msg="API failed.")
         self.assertEqual(r.json()["count"], 9, msg="Wrong API count")
@@ -117,29 +114,10 @@ class DisclosureAPIAccessTests(TestCase):
         self.assertEqual(r.status_code, 200, msg="Admin lacking access.")
 
 
-class DisclosureAPITests(TestCase):
-
-    fixtures = [
-        "authtest_data.json",
-        "disclosure.json",
-        "judge_judy.json",
-    ]
-
-    def setUp(self):
-        u = User.objects.get(pk=1001)
-        ps = Permission.objects.filter(codename="has_disclosure_api_access")
-        u.user_permissions.add(*ps)
-        self.assertTrue(
-            self.client.login(username="pandora", password="password")
-        )
-        self.q = dict()
-
+class DisclosureAPITests(LoggedInDisclosureTestCase):
     def test_basic_disclosure_api_query(self):
         """Can we query the financial disclosure API?"""
         self.path = reverse("disclosure-list", kwargs={"version": "v3"})
-        self.assertEqual(
-            self.path, "/api/rest/v3/disclosures/", msg="API URL fails"
-        )
         r = self.client.get(self.path)
         self.assertEqual(r.status_code, 200, msg="API failed.")
         self.assertEqual(r.json()["count"], 2, msg="Wrong API count")
