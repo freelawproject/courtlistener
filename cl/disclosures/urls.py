381362313b9506e73ba083933e1bdfe76e6a9197
Michael Lissner <mlissner@michaeljaylissner.com>
2021-01-14 12:56:01 -1000

    feat(views): Yank fileserving class (S3, baby!)

--- cl/disclosures/urls.py
+++ cl/disclosures/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from cl.disclosures.views import (
     financial_disclosures_for_somebody,
     financial_disclosures_home,
-    financial_disclosures_fileserver,
 )
 
 urlpatterns = [
@@ -11,17 +10,6 @@ urlpatterns = [
         r"^person/(?P<pk>\d+)/(?P<slug>[^/]*)/financial-disclosures/$",
         financial_disclosures_for_somebody,
         name="financial_disclosures_for_somebody",
-    ),
-    # Serve the PDFs, TIFFS, and thumbnails
-    url(
-        r"^person/"
-        r"(?P<pk>\d+)/"
-        r"(?P<slug>[^/]*)/"
-        r"(?P<filepath>financial-disclosures/"
-        r"(?:thumbnails/)?"
-        r".+\.(?:pdf|tiff|png))$",
-        financial_disclosures_fileserver,
-        name="financial_disclosures_fileserver",
     ),
     url(
         r"^financial-disclosures/$",
