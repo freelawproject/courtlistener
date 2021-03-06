05d20f7d9823f52fe135a65f6452dc0ab7114407
Michael Lissner <mlissner@michaeljaylissner.com>
2021-03-05 16:13:38 -1000

    fix(tasks): Slow down the throttle
    We are doing so many retries right now that we're filling our disks
    with logs. SLOOOW down.

--- cl/corpus_importer/tasks.py
+++ cl/corpus_importer/tasks.py
@@ -375,7 +375,7 @@ def get_and_save_free_document_report(
 
 
 @app.task(bind=True, max_retries=5, ignore_result=True)
-@throttle_task("2/s", key="court_id")
+@throttle_task("2/s", key="court_id", jitter=(5, 10))
 def process_free_opinion_result(
     self,
     row_pk: int,
@@ -1053,7 +1053,7 @@ def filter_docket_by_tags(
     interval_step=5 * 60,
     ignore_result=True,
 )
-@throttle_task("2/s", key="court_id", jitter=(2, 15))
+@throttle_task("2/s", key="court_id", jitter=(5, 10))
 def make_docket_by_iquery(
     self,
     court_id: str,
