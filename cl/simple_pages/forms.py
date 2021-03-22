b13399598c246d96353faf3741be54d849cb15ba
Michael Lissner <mlissner@michaeljaylissner.com>
2021-03-22 15:48:08 -0700

    feat(contact): Add basic captcha technology

--- cl/simple_pages/forms.py
+++ cl/simple_pages/forms.py
@@ -1,4 +1,8 @@
+import re
+from typing import Dict
+
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class ContactForm(forms.Form):
@@ -22,3 +26,32 @@ class ContactForm(forms.Form):
     message = forms.CharField(
         min_length=20, widget=forms.Textarea(attrs={"class": "form-control"})
     )
+
+    quiz = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "autocomplete": "off"},
+        ),
+    )
+
+    def clean_quiz(self) -> Dict[str, str]:
+        data = self.cleaned_data["quiz"]
+        if data.strip().lower() != "blue":
+            raise ValidationError("Please say the sky is blue.")
+        return data
+
+    def clean(self) -> None:
+        cleaned_data = super().clean()
+        subject = cleaned_data["phone_number"]
+        message = cleaned_data["message"]
+        regex = re.compile(
+            r"remov(e|al)|take down request|opt out|de-index|delete link"
+            r"|no ?index|block (from)?search engines?|block pages",
+            re.I,
+        )
+        if re.match(regex, subject) and "http" not in message.lower():
+            msg = (
+                "This appears to be a removal request, but you did not "
+                "include a link. You must include a link for a request to be "
+                "valid."
+            )
+            self.add_error("message", msg)
