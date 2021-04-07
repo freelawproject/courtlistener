89971513407168fd9b2d557e82dfb6a2793a844a
Michael Lissner <mlissner@michaeljaylissner.com>
2021-04-06 17:20:49 -0700

    refactor(storage): Remove duplication and add typing

--- cl/lib/storage.py
+++ cl/lib/storage.py
@@ -1,45 +1,61 @@
 import itertools
 import os
 import uuid
+from typing import Optional
 
 from django.conf import settings
-from django.core.files.storage import FileSystemStorage
+from django.core.files.storage import FileSystemStorage, Storage
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class IncrementingFileSystemStorage(FileSystemStorage):
-    """This maintains the old behavior of get_available_name that was available
+def get_name_by_incrementing(
+    instance: Storage,
+    name: str,
+    max_length: Optional[int] = None,
+) -> str:
+    """Generate usable file name for storage iterating if needed.
+
+    Returns a filename that is available in the storage mechanism,
+    taking the provided filename into account.
+
+    This maintains the old behavior of get_available_name that was available
     prior to Django 1.5.9. This behavior increments the file name by adding _1,
     _2, etc., but was removed because incrementing the file names in this
-    manner created a security vector if users were able to upload files.
+    manner created a security vector if users were able to upload (many) files.
 
     We are only able to use it in places where users are not uploading files,
-    and we are instead creating them programatically (for example, via a
+    and we are instead creating them programmatically (for example, via a
     scraper).
 
     For more detail, see:
 
     https://docs.djangoproject.com/en/1.8/releases/1.5.9/#file-upload-denial-of-service
+
+    :param instance: The instance of the storage class being used
+    :param max_length: The name will not exceed max_length, if provided
+    :param name: File name of the object being saved.
+    :return: The filepath
     """
+    dir_name, file_name = os.path.split(name)
+    file_root, file_ext = os.path.splitext(file_name)
+    count = itertools.count(1)
+    while instance.exists(name):
+        # file_ext includes the dot.
+        name = os.path.join(
+            dir_name, "%s_%s%s" % (file_root, next(count), file_ext)
+        )
+    return name
 
-    def get_available_name(self, name, max_length=None):
-        """
-        Returns a filename that's free on the target storage system, and
-        available for new content to be written to.
-        """
-        dir_name, file_name = os.path.split(name)
-        file_root, file_ext = os.path.splitext(file_name)
-        # If the filename already exists, add an underscore and a number (before
-        # the file extension, if one exists) to the filename until the generated
-        # filename doesn't exist.
-        count = itertools.count(1)
-        while self.exists(name):
-            # file_ext includes the dot.
-            name = os.path.join(
-                dir_name, "%s_%s%s" % (file_root, next(count), file_ext)
-            )
 
-        return name
+class IncrementingFileSystemStorage(FileSystemStorage):
+    """A storage class that increments the filename."""
+
+    def get_available_name(
+        self,
+        name: str,
+        max_length: Optional[int] = None,
+    ) -> str:
+        return get_name_by_incrementing(self, name, max_length)
 
 
 class UUIDFileSystemStorage(FileSystemStorage):
@@ -50,39 +66,27 @@ class UUIDFileSystemStorage(FileSystemStorage):
     original file.
     """
 
-    def get_available_name(self, name, max_length=None):
+    def get_available_name(
+        self,
+        name: str,
+        max_length: Optional[int] = None,
+    ) -> str:
         dir_name, file_name = os.path.split(name)
         _, file_ext = os.path.splitext(file_name)
         return os.path.join(dir_name, uuid.uuid4().hex + file_ext)
 
 
 class AWSMediaStorage(S3Boto3Storage):
-    """Implements AWS file system storage.
-
-    We use AWSMediaStorage to upload LASC PDF files to AWS.
-    """
+    """Implements AWS file system storage with a few overrides"""
 
     location = ""
     AWS_DEFAULT_ACL = settings.AWS_DEFAULT_ACL
     file_overwrite = True
     custom_domain = False
 
-    def get_available_name(self, name, max_length=None):
-        """Generate usable file name for storage iterating if needed.
-
-        Returns a filename that is available in the storage mechanism,
-        possibly taking the provided filename into account
-
-        :param max_length: The name will not exceed max_length, if provided
-        :param name: File name of the object being saved.
-        :return: The filepath
-        """
-        dir_name, file_name = os.path.split(name)
-        file_root, file_ext = os.path.splitext(file_name)
-        count = itertools.count(1)
-        while self.exists(name):
-            # file_ext includes the dot.
-            name = os.path.join(
-                dir_name, "%s_%s%s" % (file_root, next(count), file_ext)
-            )
-        return name
+    def get_available_name(
+        self,
+        name: str,
+        max_length: Optional[int] = None,
+    ) -> str:
+        return get_name_by_incrementing(self, name, max_length)
