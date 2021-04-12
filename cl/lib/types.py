89c807ed14c59d00df2c9902a9d5c60e01428cbe
Michael Lissner <mlissner@michaeljaylissner.com>
2021-04-12 14:34:34 -0700

    feat(typing): Add typeddict for emails

--- cl/lib/types.py
+++ cl/lib/types.py
@@ -1,3 +1,5 @@
+from typing import List, TypedDict
+
 from django.http import HttpRequest
 
 from cl.users.models import User
@@ -5,3 +7,10 @@ from cl.users.models import User
 
 class AuthenticatedHttpRequest(HttpRequest):
     user: User
+
+
+class EmailType(TypedDict, total=False):
+    subject: str
+    body: str
+    from_email: str
+    to: List[str]
