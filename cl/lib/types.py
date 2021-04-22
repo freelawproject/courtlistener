5b59e7d2f3d237040d36a80b2e8bea53692cd28e
Michael Lissner <mlissner@michaeljaylissner.com>
2021-04-22 13:52:40 -0700

    feat(typing): Add types for search_utils

--- cl/lib/types.py
+++ cl/lib/types.py
@@ -1,8 +1,10 @@
-from typing import List, TypedDict
+from typing import Any, Dict, List, TypedDict, Union
 
 from django.http import HttpRequest
 
 from cl.users.models import User
+
+CleanData = Dict[str, Any]
 
 
 class AuthenticatedHttpRequest(HttpRequest):
@@ -14,3 +16,60 @@ class EmailType(TypedDict, total=False):
     body: str
     from_email: str
     to: List[str]
+
+
+# fmt: off
+SearchParam = TypedDict(
+    "SearchParam",
+    {
+        "q": str,
+        "fq": List[str],
+
+        # Pagination & ordering
+        "rows": int,
+        "sort": str,
+
+        # Faceting
+        "facet": str,
+        "facet.field": str,
+        "facet.mincount": int,
+        "facet.range": str,
+        "facet.range.start": str,
+        "facet.range.end": str,
+        "facet.range.gap": str,
+
+        # Highlighting
+        "hl": str,
+        "hl.fl": str,
+        "fl": str,
+        "f.text.hl.snippets": str,
+        "f.text.hl.maxAlternateFieldLength": str,
+        "f.text.hl.alternateField": str,
+
+        # Grouping
+        "group": str,
+        "group.ngroups": str,
+        "group.limit": int,
+        "group.field": str,
+        "group.sort": str,
+
+        # Boosting
+        "boost": str,
+        "qf": str,
+        "pf": str,
+        "ps": Union[float, str],
+
+        # More Like This
+        "mlt": str,
+        "mlt.fl": str,
+        "mlt.maxqt": int,
+        "mlt.mintf": int,
+        "mlt.minwl": int,
+        "mlt.maxwl": int,
+        "mlt.maxdf": int,
+
+        "caller": str,
+    },
+    total=False,
+)
+# fmt: on
