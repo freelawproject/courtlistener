f9cdfea3da7c498c0d9e6295927c9f825b4ebc9d
vvilliam <wpalin@gmail.com>
2021-05-05 16:48:38 -0400

    feat(index.tsx): Add browserRouter
    Add routing for each react component
    Add Markdown Page
    Tag List Page to routing

--- cl/assets/react/index.tsx
+++ cl/assets/react/index.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import TagSelect from './TagSelect';
+import TagList from './TagList';
+import {TagMarkdown} from "./TagPage";
+
+
+const App = () => {
+  var root = document.getElementById('react-root');
+  const data = JSON.parse(JSON.stringify(root!.dataset));
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/docket">
+          <TagSelect {... data} />
+        </Route>
+        <Route exact path={`/tags/:userName/`}>
+          <TagList userId={data.requestedUserId} userName={data.requestedUser} isPageOwner={data.isPageOwner} />
+        </Route>
+        <Route path={`/tags/:userName/:id`}>
+          <TagMarkdown {... data}/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <TagSelect />
-  </React.StrictMode>,
+    <App/>
+  </React.StrictMode>
+  ,
   document.getElementById('react-root')
 );
