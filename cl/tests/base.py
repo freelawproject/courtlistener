2475be9261a56424643cd98df6a03db7323a5b6f
David Cook <divergentdave@gmail.com>
2021-05-18 21:45:02 -0500

    refactor(tests): Reuse browser session in class

--- cl/tests/base.py
+++ cl/tests/base.py
@@ -83,9 +83,11 @@ class BaseSeleniumTest(StaticLiveServerTestCase):
         # Set host to externally accessible web server address
         cls.host = socket.gethostbyname(socket.gethostname())
 
+        cls.browser = cls._create_browser()
+        cls.browser.implicitly_wait(5)
+
     def setUp(self) -> None:
-        self.browser = self._create_browser()
-        self.browser.implicitly_wait(5)
+        self.reset_browser()
         self._update_index()
 
     def reset_browser(self) -> None:
@@ -96,8 +98,13 @@ class BaseSeleniumTest(StaticLiveServerTestCase):
             filename = type(self).__name__ + "-selenium.png"
             print("\nSaving screenshot: %s" % (filename,))
             self.browser.save_screenshot("/tmp/" + filename)
-        self.browser.quit()
         self._teardown_test_solr()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        super(BaseSeleniumTest, cls).tearDownClass()
+
+        cls.browser.quit()
 
     @retry(AssertionError, tries=3, delay=0.25, backoff=1)
     def assert_text_in_node(self, text: str, tag_name: str) -> None:
