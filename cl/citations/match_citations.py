f044b6c7e5a58cebcfbcfff84075388a000ca7b0
Matt Dahl <matt.dahl.2013@gmail.com>
2021-05-24 19:26:59 -0700

    feat(citator): Implements eyecite.resolve_citations() API.

--- cl/citations/match_citations.py
+++ cl/citations/match_citations.py
@@ -2,27 +2,32 @@
 # encoding utf-8
 
 from datetime import date, datetime
-from typing import Dict, List, Optional, Tuple, Union, cast
+from typing import Dict, Iterable, List, Optional, Tuple, Union
 
 from django.conf import settings
+from eyecite import resolve_citations
 from eyecite.models import (
     CitationBase,
     FullCaseCitation,
-    IdCitation,
-    NonopinionCitation,
+    Resource,
     ShortCaseCitation,
     SupraCitation,
 )
+from eyecite.test_factories import case_citation
 from eyecite.utils import strip_punct
 
 from cl.custom_filters.templatetags.text_filters import best_case_name
 from cl.lib.scorched_utils import ExtraSolrInterface, ExtraSolrSearch
-from cl.lib.types import SearchParam
+from cl.lib.types import SearchParam, SupportedCitationType
 from cl.search.models import Opinion
 
 DEBUG = True
 
 QUERY_LENGTH = 10
+
+NO_MATCH_RESOURCE = Resource(
+    case_citation(0, source_text="UNMATCHED_CITATION")
+)
 
 
 def build_date_range(start_year: int, end_year: int) -> str:
@@ -46,12 +51,12 @@ def make_name_param(defendant: str, plaintiff: str = None) -> Tuple[str, int]:
 def reverse_match(
     conn: ExtraSolrInterface,
     results: List[ExtraSolrSearch],
-    citing_doc: Opinion,
+    citing_opinion: Opinion,
 ) -> List[ExtraSolrSearch]:
     """Uses the case name of the found document to verify that it is a match on
     the original.
     """
-    params: SearchParam = {"fq": ["id:%s" % citing_doc.pk]}
+    params: SearchParam = {"fq": ["id:%s" % citing_opinion.pk]}
     for result in results:
         case_name, length = make_name_param(result["caseName"])
         # Avoid overly long queries
@@ -71,8 +76,8 @@ def reverse_match(
 def case_name_query(
     conn: ExtraSolrInterface,
     params: SearchParam,
-    citation: CitationBase,
-    citing_doc: Opinion,
+    citation: SupportedCitationType,
+    citing_opinion: Opinion,
 ) -> List[ExtraSolrSearch]:
     query, length = make_name_param(citation.defendant, citation.plaintiff)
     params["q"] = "caseName:(%s)" % query
@@ -87,13 +92,15 @@ def case_name_query(
             # For 1 result, make sure case name of match actually appears in
             # citing doc. For multiple results, use same technique to
             # potentially narrow down
-            return reverse_match(conn, new_results, citing_doc)
+            return reverse_match(conn, new_results, citing_opinion)
             # Else, try again
         results = new_results
     return results
 
 
-def get_years_from_reporter(citation: CitationBase) -> Tuple[int, int]:
+def get_years_from_reporter(
+    citation: SupportedCitationType,
+) -> Tuple[int, int]:
     """Given a citation object, try to look it its dates in the reporter DB"""
     start_year = 1750
     end_year = date.today().year
@@ -107,15 +114,17 @@ def get_years_from_reporter(citation: CitationBase) -> Tuple[int, int]:
     return start_year, end_year
 
 
-def match_citation(
-    citation: CitationBase, citing_doc: Opinion = None
+def search_db_for_fullcitation(
+    full_citation: FullCaseCitation,
 ) -> List[ExtraSolrSearch]:
     """For a citation object, try to match it to an item in the database using
     a variety of heuristics.
-
     Returns:
       - a Solr Result object with the results, or an empty list if no hits
     """
+    if not hasattr(full_citation, "citing_opinion"):
+        full_citation.citing_opinion = None
+
     # TODO: Create shared solr connection for all queries
     si = ExtraSolrInterface(settings.SOLR_OPINION_URL, mode="r")
     main_params: SearchParam = {
@@ -125,42 +134,50 @@ def match_citation(
         ],
         "caller": "citation.match_citations.match_citation",
     }
-    if citing_doc is not None:
+    if full_citation.citing_opinion is not None:
         # Eliminate self-cites.
-        main_params["fq"].append("-id:%s" % citing_doc.pk)
+        main_params["fq"].append("-id:%s" % full_citation.citing_opinion.pk)
     # Set up filter parameters
-    if citation.year:
-        start_year = end_year = citation.year
+    if full_citation.year:
+        start_year = end_year = full_citation.year
     else:
-        start_year, end_year = get_years_from_reporter(citation)
-        if citing_doc is not None and citing_doc.cluster.date_filed:
-            end_year = min(end_year, citing_doc.cluster.date_filed.year)
-
+        start_year, end_year = get_years_from_reporter(full_citation)
+        if (
+            full_citation.citing_opinion is not None
+            and full_citation.citing_opinion.cluster.date_filed
+        ):
+            end_year = min(
+                end_year, full_citation.citing_opinion.cluster.date_filed.year
+            )
     main_params["fq"].append(
         "dateFiled:%s" % build_date_range(start_year, end_year)
     )
 
-    if citation.court:
-        main_params["fq"].append("court_exact:%s" % citation.court)
+    if full_citation.court:
+        main_params["fq"].append("court_exact:%s" % full_citation.court)
 
     # Take 1: Use a phrase query to search the citation field.
-    main_params["fq"].append('citation:("%s")' % citation.base_citation())
+    main_params["fq"].append('citation:("%s")' % full_citation.base_citation())
     results = si.query().add_extra(**main_params).execute()
     si.conn.http_connection.close()
     if len(results) == 1:
         return results
     if len(results) > 1:
-        if citing_doc is not None and citation.defendant:
-            # Refine using defendant, if there is one
-            results = case_name_query(si, main_params, citation, citing_doc)
-        return results
+        if (
+            full_citation.citing_opinion is not None
+            and full_citation.defendant
+        ):  # Refine using defendant, if there is one
+            results = case_name_query(
+                si, main_params, full_citation, full_citation.citing_opinion
+            )
+            return results
 
     # Give up.
     return []
 
 
 def filter_by_matching_antecedent(
-    citation_matches: List[Opinion],
+    opinion_candidates: Iterable[Opinion],
     antecedent_guess: Optional[str],
 ) -> Optional[Opinion]:
     if not antecedent_guess:
@@ -169,116 +186,82 @@ def filter_by_matching_antecedent(
     antecedent_guess = strip_punct(antecedent_guess)
     candidates: List[Opinion] = []
 
-    for cm in citation_matches:
-        if antecedent_guess in best_case_name(cm.cluster):
-            candidates.append(cm)
+    for o in opinion_candidates:
+        if antecedent_guess in best_case_name(o.cluster):
+            candidates.append(o)
 
     # Remove duplicates and only accept if one candidate remains
     candidates = list(set(candidates))
     return candidates[0] if len(candidates) == 1 else None
 
 
-def get_citation_matches(
-    citing_opinion: Opinion,
-    citations: List[CitationBase],
-) -> List[Opinion]:
-    """For a list of Citation objects (e.g., FullCitations, SupraCitations,
-    IdCitations, etc.), try to match them to Opinion objects in the database
-    using a variety of heuristics.
-
-    Returns:
-      - a list of Opinion objects, as matched to citations
-    """
-    citation_matches: List[Opinion] = []  # List of matches to return
-    was_matched: bool = (
-        False  # Whether the previous citation match was successful
+def resolve_fullcase_citation(
+    full_citation: FullCaseCitation,
+) -> Union[Opinion, Resource]:
+    db_search_results: List[ExtraSolrSearch] = search_db_for_fullcitation(
+        full_citation
     )
 
-    for citation in citations:
-        matched_opinion = None
-
-        # If the citation is to a non-opinion document, we currently cannot
-        # match these.
-        if isinstance(citation, NonopinionCitation):
+    # If there is one search result, try to return it
+    if len(db_search_results) == 1:
+        result_id = db_search_results[0]["id"]
+        try:
+            return Opinion.objects.get(pk=result_id)
+        except (Opinion.DoesNotExist, Opinion.MultipleObjectsReturned):
             pass
 
-        # If the citation is an id citation, just resolve it to the opinion
-        # that was matched immediately prior (so long as the previous match
-        # was successful).
-        elif isinstance(citation, IdCitation):
-            if was_matched:
-                matched_opinion = citation_matches[-1]
+    # If no Opinion can be matched, just return a placeholder object
+    return NO_MATCH_RESOURCE
 
-        # If the citation is a supra citation, try to resolve it to one of
-        # the citations that has already been matched
-        elif isinstance(citation, SupraCitation):
-            # The only clue we have to help us with resolution is the guess
-            # of what the supra citation's antecedent is, so we try to
-            # match that string to one of the known case names of the
-            # already matched opinions. However, because case names might
-            # look alike, matches using this heuristic may not be unique.
-            # If no match, or more than one match, is found, then the supra
-            # reference is effectively dropped.
-            matched_opinion = filter_by_matching_antecedent(
-                citation_matches, citation.antecedent_guess
-            )
 
-        # Likewise, if the citation is a short form citation, try to resolve it
-        # to one of the citations that has already been matched
-        elif isinstance(citation, ShortCaseCitation):
-            # We first try to match by using the reporter and volume number.
-            # However, because matches made using this heuristic may not be
-            # unique, we then refine by using the antecedent guess and only
-            # accept the match if there is a single unique candidate. This
-            # refinement may still fail (because the guess could be
-            # meaningless), in which case the citation is not resolvable and
-            # is dropped.
-            candidates = []
-            for cm in citation_matches:
-                for c in cm.cluster.citations.all():
-                    if (
-                        citation.reporter == c.reporter
-                        and citation.volume == str(c.volume)
-                    ):
-                        candidates.append(cm)
+def resolve_shortcase_citation(
+    short_citation: ShortCaseCitation,
+    resolved_full_cites: Dict[FullCaseCitation, Opinion],
+) -> Optional[Opinion]:
+    candidates: List[Opinion] = []
+    for full_citation, opinion in resolved_full_cites.items():
+        for c in opinion.cluster.citations.all():
+            if (
+                short_citation.reporter == c.reporter
+                and short_citation.volume == str(c.volume)
+            ):
+                candidates.append(opinion)
 
-            candidates = list(set(candidates))  # Remove duplicate matches
-            if len(candidates) == 1:
-                # Accept the match!
-                matched_opinion = candidates[0]
-            else:
-                matched_opinion = filter_by_matching_antecedent(
-                    candidates, citation.antecedent_guess
-                )
+    # Remove duplicates
+    candidates = list(set(candidates))
 
-        # Otherwise, the citation is just a regular citation, so try to match
-        # it directly to an opinion
-        elif isinstance(citation, FullCaseCitation):
-            matches = match_citation(citation, citing_doc=citing_opinion)
+    # Only accept if one candidate remains
+    if len(candidates) == 1:
+        return candidates[0]
 
-            if len(matches) == 1:
-                match_id = matches[0]["id"]
-                try:
-                    matched_opinion = Opinion.objects.get(pk=match_id)
-                except Opinion.DoesNotExist:
-                    # No Opinions returned. Press on.
-                    pass
-                except Opinion.MultipleObjectsReturned:
-                    # Multiple Opinions returned. Press on.
-                    pass
-            else:
-                # No match found for citation
-                pass
+    # Otherwise, try to refine further using the antecedent guess
+    else:
+        return filter_by_matching_antecedent(
+            candidates, short_citation.antecedent_guess
+        )
 
-        # If an opinion was successfully matched, add it to the list and
-        # set the match fields on the original citation object so that they
-        # can later be used for generating inline html
-        if matched_opinion:
-            was_matched = True
-            citation_matches.append(matched_opinion)
-            citation.match_url = matched_opinion.cluster.get_absolute_url()
-            citation.match_id = matched_opinion.pk
-        else:
-            was_matched = False
 
-    return citation_matches
+def resolve_supra_citation(
+    supra_citation: SupraCitation,
+    resolved_full_cites: Dict[FullCaseCitation, Opinion],
+) -> Optional[Opinion]:
+    return filter_by_matching_antecedent(
+        resolved_full_cites.values(), supra_citation.antecedent_guess
+    )
+
+
+def do_resolve_citations(
+    citations: List[CitationBase], citing_opinion: Opinion
+) -> Dict[Union[Opinion, Resource], List[SupportedCitationType]]:
+    # Set the citing opinion on FullCaseCitation objects for later matching
+    for c in citations:
+        if type(c) is FullCaseCitation:
+            c.citing_opinion = citing_opinion
+
+    # Call and return eyecite's resolve_citations() function
+    return resolve_citations(
+        citations=citations,
+        resolve_fullcase_citation=resolve_fullcase_citation,
+        resolve_shortcase_citation=resolve_shortcase_citation,
+        resolve_supra_citation=resolve_supra_citation,
+    )
--- cl/citations/tasks.py
+++ cl/citations/tasks.py
@@ -1,6 +1,5 @@
-from collections import Counter
 from http.client import ResponseNotReady
-from typing import List, Set, Tuple
+from typing import Dict, List, Set, Tuple
 
 from django.db import transaction
 from django.db.models import F
@@ -12,7 +11,8 @@ from cl.citations.annotate_citations import (
     create_cited_html,
     get_and_clean_opinion_text,
 )
-from cl.citations.match_citations import get_citation_matches
+from cl.citations.match_citations import do_resolve_citations
+from cl.lib.types import SupportedCitationType
 from cl.search.models import Opinion, OpinionCluster, OpinionsCited
 from cl.search.tasks import add_items_to_solr
 
@@ -24,8 +24,8 @@ PARALLEL_DISTANCE = 6
 
 @app.task
 def identify_parallel_citations(
-    citations: List[CitationBase],
-) -> Set[Tuple[CitationBase, ...]]:
+    citations: List[SupportedCitationType],
+) -> Set[Tuple[SupportedCitationType, ...]]:
     """Work through a list of citations and identify ones that are physically
     near each other in the document.
 
@@ -72,31 +72,32 @@ def find_citations_for_opinion_by_pks(
     :param index: Whether to add the item to Solr
     :return: None
     """
-    opinions = Opinion.objects.filter(pk__in=opinion_pks)
+    opinions: List[Opinion] = Opinion.objects.filter(pk__in=opinion_pks)
     for opinion in opinions:
         # Memoize parsed versions of the opinion's text
         get_and_clean_opinion_text(opinion)
 
         # Extract the citations from the opinion's text
-        citations = get_citations(opinion.cleaned_text)
+        citations: List[CitationBase] = get_citations(opinion.cleaned_text)
 
         # If no citations are found, continue
         if not citations:
             continue
 
-        # Match all those different citation objects to Opinion objects, using
-        # a variety of heuristics.
+        # Resolve all those different citation objects to Opinion objects,
+        # using a variety of heuristics.
         try:
-            citation_matches = get_citation_matches(opinion, citations)
+            citation_resolutions: Dict[
+                Opinion, List[SupportedCitationType]
+            ] = do_resolve_citations(citations, opinion)
         except ResponseNotReady as e:
             # Threading problem in httplib, which is used in the Solr query.
             raise self.retry(exc=e, countdown=2)
 
-        # Consolidate duplicate matches, keeping a counter of how often each
-        # match appears (so we know how many times an opinion cites another).
-        # keys = cited opinion
-        # values = number of times that opinion is cited
-        grouped_matches = Counter(citation_matches)
+        # Generate the citing opinion's new HTML with inline citation links
+        opinion.html_with_citations = create_cited_html(
+            opinion, citation_resolutions
+        )
 
         # Increase the citation count for the cluster of each matched opinion
         # if that cluster has not already been cited by this opinion. First,
@@ -106,12 +107,9 @@ def find_citations_for_opinion_by_pks(
             "pk", flat=True
         )
         opinion_ids_to_update = set()
-        for matched_opinion in grouped_matches:
-            if matched_opinion.pk not in all_cited_opinions:
-                opinion_ids_to_update.add(matched_opinion.pk)
-
-        # Generate the citing opinion's new HTML with inline citation links
-        opinion.html_with_citations = create_cited_html(opinion, citations)
+        for _opinion in citation_resolutions.keys():
+            if _opinion.pk not in all_cited_opinions:
+                opinion_ids_to_update.add(_opinion.pk)
 
         # Finally, commit these changes to the database in a single
         # transcation block. Trigger a single Solr update as well, if
@@ -137,10 +135,10 @@ def find_citations_for_opinion_by_pks(
                 [
                     OpinionsCited(
                         citing_opinion_id=opinion.pk,
-                        cited_opinion_id=matched_opinion.pk,
-                        depth=grouped_matches[matched_opinion],
+                        cited_opinion_id=_opinion.pk,
+                        depth=len(_citations),
                     )
-                    for matched_opinion in grouped_matches
+                    for _opinion, _citations in citation_resolutions.items()
                 ]
             )
 
