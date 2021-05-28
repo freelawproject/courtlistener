08061a257d4b6a235e9d5415cee6e9f82cc5081b
Michael Lissner <mlissner@michaeljaylissner.com>
2021-05-28 13:06:41 -0700

    feat(db): Trim migrations
    This removes 331 files, adds 3, and modifies 19. When you get this,
    you'll need to run:
    
    $ ./manage.py migrate --fake
    
    That should bring your DB into alignment with these migrations, and
    you should get a nice speed boost in your tests.
    
    Fixes: 1694

--- cl/lib/migration_utils.py
+++ cl/lib/migration_utils.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
 
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.management import create_permissions
 from django.core.management import call_command
 from django.core.serializers import base, python
+from django.utils import timezone
+from rest_framework.authtoken.models import Token
 
 
 def load_migration_fixture(apps, schema_editor, fixture, app_label):
@@ -26,8 +30,8 @@ def load_migration_fixture(apps, schema_editor, fixture, app_label):
     # Save the old _get_model() function
     old_get_model = python._get_model
 
-    # Define new _get_model() function here, which utilizes the apps argument to
-    # get the historical version of a model. This piece of code is directly
+    # Define new _get_model() function here, which utilizes the apps argument
+    # to get the historical version of a model. This piece of code is directly
     # stolen from django.core.serializers.python._get_model, unchanged.
     def _get_model(model_identifier):
         try:
@@ -37,7 +41,7 @@ def load_migration_fixture(apps, schema_editor, fixture, app_label):
                 "Invalid model identifier: '%s'" % model_identifier
             )
 
-    # Replace the _get_model() function on the module so loaddata can utilize it
+    # Replace the _get_model() function on the module so loaddata can use it
     python._get_model = _get_model
 
     try:
@@ -46,3 +50,35 @@ def load_migration_fixture(apps, schema_editor, fixture, app_label):
     finally:
         # Restore old _get_model() function
         python._get_model = old_get_model
+
+
+def make_new_user(apps, schema_editor, username, email, permission_codenames):
+    User = apps.get_model(
+        "auth",
+        "User",
+    )
+    UserProfile = apps.get_model("users", "UserProfile")
+    Permission = apps.get_model("auth", "Permission")
+    new_user = User.objects.create(
+        username=username,
+        email=email,
+        password=make_password(None),  # Unusable password
+        date_joined=timezone.now(),
+    )
+    UserProfile.objects.create(
+        user=new_user,
+        email_confirmed=True,
+    )
+
+    # Give the user permissions
+    # https://stackoverflow.com/a/41564061/64911
+    for app_config in apps.get_app_configs():
+        app_config.models_module = True
+        create_permissions(app_config, verbosity=0)
+        app_config.models_module = None
+
+    for codename in permission_codenames:
+        p = Permission.objects.get(codename=codename)
+        new_user.user_permissions.add(p)
+
+    Token.objects.get_or_create(user_id=new_user.id)
--- cl/recap/migrations/0024_idb_updates.sql
+++
@@ -1,5 +0,0 @@
-BEGIN;
-ALTER TABLE "recap_fjcintegrateddatabase" DROP COLUMN "case_name" CASCADE;
-ALTER TABLE "recap_fjcintegrateddatabase" DROP COLUMN "pacer_case_id" CASCADE;
-
-COMMIT;
