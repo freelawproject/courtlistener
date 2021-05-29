0723d0ee8c209d2037744500ae663dd015a0b2f1
Michael Lissner <mlissner@michaeljaylissner.com>
2021-05-28 17:02:39 -0700

    fix(models): Uses non-deprecated field
    This is a bit annoying. Django says the old way is deprecated.
    django-stubs doesn't know about the new way. This uses the new way
    while ignoring the typing error.
    
    See: https://github.com/typeddjango/django-stubs/issues/439
    
    Fixes: #1698

--- cl/api/models.py
+++ cl/api/models.py
@@ -2,7 +2,6 @@ from http import HTTPStatus
 
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.contrib.postgres import fields
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -70,7 +69,7 @@ class WebhookEvent(AbstractDateTimeModel):
         "created.",
         choices=HttpStatusCodes.choices,
     )
-    content = fields.JSONField(
+    content = models.JSONField(  # type: ignore
         help_text="The content of the outgoing body in the POST request."
     )
     response = models.TextField(
