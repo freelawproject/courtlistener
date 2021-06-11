3001b39bea2c0811f1670a43f45be36c5af277ba
Michael Lissner <mlissner@michaeljaylissner.com>
2021-06-11 15:39:11 -0700

    feat(opinions): Proacively set blocked status

--- cl/lib/string_utils.py
+++ cl/lib/string_utils.py
@@ -1,28 +1,5 @@
 import re
-from typing import Optional, Tuple
-
-
-def anonymize(s: str) -> Tuple[str, bool]:
-    """Anonymizes private information.
-
-    Converts SSNs, EINs, and A-Numbers to X's. Reports whether a modification
-    was made, as a boolean.
-
-    For more information about A-Numbers, see:
-      https://www.uscis.gov/tools/glossary/number
-
-    SSNs can be much more complicated than the implementation here. For
-    details, see:
-      http://rion.io/2013/09/10/validating-social-security-numbers-through-regular-expressions-2/
-    """
-    ssn_re = re.compile(r"\b(\d{3}-\d{2}-\d{4})\b", flags=re.VERBOSE)
-    ein_re = re.compile(r"\b(\d{2}-\d{7})\b")
-    a_number_re = re.compile(r"\b(A\d{8,9})\b")
-    s, ssn_count = re.subn(ssn_re, r"XXX-XX-XXXX", s)
-    s, ien_count = re.subn(ein_re, r"XX-XXXXXXX", s)
-    s, a_number_count = re.subn(a_number_re, r"AXXXXXXXX", s)
-    modified = bool(ssn_count + ien_count + a_number_count)
-    return s, modified
+from typing import Optional
 
 
 def trunc(s: str, length: int, ellipsis: Optional[str] = None) -> str:
