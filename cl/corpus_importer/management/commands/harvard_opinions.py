4677658555f5f24c12d8523ec5f199c2661eed2c
vvilliam <wpalin@gmail.com>
2021-08-09 18:49:16 -0400

    feat(harvard_opinions): Update Harvard opinions to ingest Washington State opinions
    Add court-id argument for finicky parsing
    Add code to identify previously imported opinions to avoid duplicates
    Refactor citation adding into its own function.
    Add more type hints
    Fix Typos

--- cl/corpus_importer/management/commands/harvard_opinions.py
+++ cl/corpus_importer/management/commands/harvard_opinions.py
@@ -5,13 +5,15 @@ import difflib
 import itertools
 import json
 import os
-from datetime import datetime
+import re
+from datetime import datetime, timedelta
 from glob import glob
+from typing import List, Dict, Optional, Any
 
 from bs4 import BeautifulSoup
 from django.conf import settings
 from django.db import transaction
-from django.db.utils import OperationalError
+from django.db.utils import OperationalError, IntegrityError
 from eyecite.find_citations import get_citations
 from juriscraper.lib.diff_tools import normalize_phrase
 from juriscraper.lib.string_utils import CaseNameTweaker, harmonize, titlecase
@@ -85,7 +87,8 @@ def filepath_list(reporter, volume):
 
 
 def check_for_match(new_case, possibilities):
-    """
+    """Check for matches based on case names
+
     This code is a variation of get_closest_match_index used in juriscraper.
     It checks if the case name we are trying to add matches any duplicate
     citation cases already in the system.
@@ -136,14 +139,16 @@ def skip_processing(citation, case_name, file_path):
         if check_for_match(case_name, case_names) is not None:
 
             for found_filepath in found_filepaths:
-                if found_filepath == file_path or found_filepath == "":
+                if found_filepath == file_path:
                     # Check if all same citations are Harvard imports
                     # If all Harvard data - match on file_path
                     # If no match assume different case
                     logger.info(f"Looks like we already have {case_name}.")
                     return True
 
-        logger.info("Duplicate cite string but appears to be a new case")
+        logger.info(
+            f"Duplicate cite string but appears to be a new case {case_name}"
+        )
     return False
 
 
@@ -197,8 +202,9 @@ def parse_extra_fields(soup, fields, long_field=False):
     return data_set
 
 
-def parse_harvard_opinions(reporter, volume, make_searchable):
-    """
+def parse_harvard_opinions(options: Dict[str, Any]) -> None:
+    """Parse Harvard Opinions
+
     Parse downloaded CaseLaw Corpus from internet archive and add them to our
     database.
 
@@ -209,11 +215,16 @@ def parse_harvard_opinions(reporter, volume, make_searchable):
 
     If neither is provided, code will cycle through all downloaded files.
 
-    :param volume: The volume (int) of the reporters (optional) (ex 10)
-    :param reporter: Reporter string as slugify'd (optional) (tc) for T.C.
-    :param make_searchable: Boolean to indicate saving to solr
+    :param options: The command line options including (reporter,
+    volume court_id and make_searchable)
     :return: None
     """
+
+    reporter = options["reporter"]
+    volume = options["volume"]
+    make_searchable = options["make_searchable"]
+    court_id = options["court_id"]
+
     if not reporter and volume:
         logger.error("You provided a volume but no reporter. Exiting.")
         return
@@ -223,10 +234,11 @@ def parse_harvard_opinions(reporter, volume, make_searchable):
             ["https://archive.org/download", file_path.split("/", 9)[-1]]
         )
 
-        if OpinionCluster.objects.filter(
-            filepath_json_harvard=file_path
-        ).exists():
-            logger.info(f"Skipping - already in system {ia_download_url}")
+        oc = OpinionCluster.objects.filter(filepath_json_harvard=file_path)
+        if len(oc) > 0:
+            logger.info(
+                f"Skipping {oc[0].id} - already in system {ia_download_url}"
+            )
             continue
 
         try:
@@ -255,14 +267,30 @@ def parse_harvard_opinions(reporter, volume, make_searchable):
             continue
 
         # TODO: Generalize this to handle all court types somehow.
-        court_id = match_court_string(
-            data["court"]["name"],
-            state=True,
-            federal_appeals=True,
-            federal_district=True,
-        )
+        if not options["court_id"]:
+            # Sometimes courts can't match just one court
+            # This is used to alleviate certain circumstances
+            court_id = match_court_string(
+                data["court"]["name"],
+                state=True,
+                federal_appeals=True,
+                federal_district=True,
+            )
+        # Handle partial dates by adding -01v to YYYY-MM dates
+        date_filed, is_approximate = validate_dt(data["decision_date"])
+        case_body = data["casebody"]["data"]
 
-        soup = BeautifulSoup(data["casebody"]["data"], "lxml")
+        previously_imported_case = find_previously_imported_cases(
+            court_id, date_filed, case_body, data["docket_number"]
+        )
+        if previously_imported_case:
+            with transaction.atomic():
+                add_citations(
+                    data["citations"], cluster_id=previously_imported_case.id
+                )
+            continue
+
+        soup = BeautifulSoup(case_body, "lxml")
 
         # Some documents contain images in the HTML
         # Flag them for a later crawl by using the placeholder '[[Image]]'
@@ -283,12 +311,7 @@ def parse_harvard_opinions(reporter, volume, make_searchable):
             )
         )
         judges = titlecase(judges)
-        docket_string = (
-            data["docket_number"]
-            .replace("Docket No.", "")
-            .replace("Docket Nos.", "")
-            .strip()
-        )
+        docket_string = data["docket_number"].strip()
 
         short_fields = ["attorneys", "disposition", "otherdate", "seealso"]
 
@@ -304,7 +327,9 @@ def parse_harvard_opinions(reporter, volume, make_searchable):
         long_data = parse_extra_fields(soup, long_fields, True)
 
         with transaction.atomic():
-            logger.info("Adding docket for: %s", citation.base_citation())
+            logger.info(
+                f"Adding docket for {case_name}: {citation.base_citation()}"
+            )
             docket = Docket(
                 case_name=case_name,
                 case_name_short=case_name_short,
@@ -328,8 +353,6 @@ def parse_harvard_opinions(reporter, volume, make_searchable):
                         data["docket_number"],
                         long_data["correction"],
                     )
-            # Handle partial dates by adding -01v to YYYY-MM dates
-            date_filed, is_approximate = validate_dt(data["decision_date"])
 
             logger.info("Adding cluster for: %s", citation.base_citation())
             cluster = OpinionCluster(
@@ -356,57 +379,249 @@ def parse_harvard_opinions(reporter, volume, make_searchable):
             cluster.save(index=False)
 
             logger.info("Adding citation for: %s", citation.base_citation())
-            Citation.objects.create(
-                volume=citation.volume,
-                reporter=citation.reporter,
-                page=citation.page,
-                type=map_reporter_db_cite_type(
-                    REPORTERS[citation.canonical_reporter][0]["cite_type"]
-                ),
-                cluster_id=cluster.id,
-            )
-            new_op_pks = []
-            for op in soup.find_all("opinion"):
-                # This code cleans author tags for processing.
-                # It is particularly useful for identifiying Per Curiam
-                for elem in [op.find("author")]:
-                    if elem is not None:
-                        [x.extract() for x in elem.find_all("page-number")]
-
-                auth = op.find("author")
-                if auth is not None:
-                    author_tag_str = titlecase(auth.text.strip(":"))
-                    author_str = titlecase(
-                        "".join(extract_judge_last_name(author_tag_str))
-                    )
-                else:
-                    author_str = ""
-                    author_tag_str = ""
-
-                per_curiam = True if author_tag_str == "Per Curiam" else False
-                # If Per Curiam is True set author string to Per Curiam
-                if per_curiam:
-                    author_str = "Per Curiam"
-
-                op_type = map_opinion_type(op.get("type"))
-                opinion_xml = str(op)
-                logger.info("Adding opinion for: %s", citation.base_citation())
-                op = Opinion(
-                    cluster_id=cluster.id,
-                    type=op_type,
-                    author_str=author_str,
-                    xml_harvard=opinion_xml,
-                    per_curiam=per_curiam,
-                    extracted_by_ocr=True,
-                )
-                # Don't index now; do so later if desired
-                op.save(index=False)
-                new_op_pks.append(op.pk)
+            add_citations(data["citations"], cluster.id)
+            new_op_pks = add_opinions(soup, cluster.id, citation)
 
         if make_searchable:
             add_items_to_solr.delay(new_op_pks, "search.Opinion")
 
         logger.info("Finished: %s", citation.base_citation())
+
+
+def add_citations(cites: List, cluster_id: int) -> None:
+    """Add citations to OpinionClusters
+
+    :param cites: Harvard Citation data
+    :param cluster_id: Cluster of found opinion in DB
+    :return:
+    """
+    for cite in cites:
+        citation = get_citations(cite["cite"])
+        if not citation:
+            continue
+
+        # Because of non-canonical reporters this code breaks for states like
+        # Washington.  This is a temporary solution.
+        if not citation[0].canonical_reporter:
+            reporter_type = Citation.STATE
+        else:
+            reporter_type = map_reporter_db_cite_type(
+                REPORTERS[citation[0].canonical_reporter][0]["cite_type"]
+            )
+
+        Citation.objects.get_or_create(
+            volume=citation[0].volume,
+            reporter=citation[0].reporter,
+            page=citation[0].page,
+            type=reporter_type,
+            cluster_id=cluster_id,
+        )
+
+
+def add_opinions(
+    soup: BeautifulSoup, cluster_id: int, citation: Citation
+) -> List[int]:
+    """Add opinions to Cluster
+
+    :param soup: The bs4 representaion of the case data xml
+    :param cluster_id: The cluster ID
+    :param citation: Citation object
+    :return: Opinion IDs in a list
+    """
+    new_op_pks = []
+    for op in soup.find_all("opinion"):
+        # This code cleans author tags for processing.
+        # It is particularly useful for identifying Per curiam
+        for elem in [op.find("author")]:
+            if elem is not None:
+                [x.extract() for x in elem.find_all("page-number")]
+
+        auth = op.find("author")
+        if auth is not None:
+            author_tag_str = titlecase(auth.text.strip(":"))
+            author_str = titlecase(
+                "".join(extract_judge_last_name(author_tag_str))
+            )
+        else:
+            author_str = ""
+            author_tag_str = ""
+
+        per_curiam = True if author_tag_str == "Per Curiam" else False
+        # If Per Curiam is True set author string to Per Curiam
+        if per_curiam:
+            author_str = "Per Curiam"
+
+        op_type = map_opinion_type(op.get("type"))
+        opinion_xml = str(op)
+        logger.info("Adding opinion for: %s", citation.base_citation())
+        op = Opinion(
+            cluster_id=cluster_id,
+            type=op_type,
+            author_str=author_str,
+            xml_harvard=opinion_xml,
+            per_curiam=per_curiam,
+            extracted_by_ocr=True,
+        )
+        # Don't index now; do so later if desired
+        op.save(index=False)
+        new_op_pks.append(op.pk)
+    return new_op_pks
+
+
+def get_opinion_content(cluster):
+    """Get the opinions content for a cluster object
+
+    :param cluster: Cluster ID for a set of opinions
+    :return: Combined opinion text
+    """
+    opinions = []
+    for op in Opinion.objects.filter(cluster_id=cluster.id):
+        if len(op.html_with_citations) > 1:
+            opinions.append(op.html_with_citations)
+        elif len(op.html_columbia) > 1:
+            opinions.append(op.html_columbia)
+        elif len(op.plain_text) > 1:
+            opinions.append(op.plain_text)
+        elif len(op.xml_harvard) > 1:
+            opinions.append(op.xml_harvard)
+    op = " ".join(opinions)
+    soup = BeautifulSoup(op, features="html.parser")
+    return soup.text
+
+
+def clean_docket_number(docket_number: str) -> str:
+    """Strip non-numeric content from docket numbers
+
+    :param docket_number: Case docket number
+    :return: A stripped down docket number.
+    """
+
+    docket_number = re.sub("Department.*", "", docket_number)
+    docket_number = re.sub("Nos?. ", "", docket_number)
+    return docket_number
+
+
+def clean_body_content(case_body: str) -> str:
+    """Strip all non alphanumeric characters
+
+    :param case_body: Opinion text
+    :return:Opinion text with only alphanumeric characters
+    """
+    soup = BeautifulSoup(case_body, "lxml")
+    return re.sub(r"[^[a-zA-Z]", "", soup.text)
+
+
+def match_based_text(
+    case_body: str, possible_cases: List, docket_number: str
+) -> Optional[OpinionCluster]:
+    """Compare CL text to Harvard content to establish duplicates
+
+    :param case_body: Harvard case body to compare
+    :param possible_cases: List of opinions to check against
+    :param docket_number: The docket number
+    :return: OpinionCluster or None
+    """
+    harvard_characters = clean_body_content(case_body)
+    for case in possible_cases:
+        cl_case_body = get_opinion_content(case)
+        cl_characters = clean_body_content(cl_case_body)
+        if (
+            len(harvard_characters) / len(cl_characters) < 0.3
+            or len(harvard_characters) / len(cl_characters) > 3
+        ):
+            # Content too dissimilar in length to compare
+            continue
+
+        percent_match = compare_documents(harvard_characters, cl_characters)
+        if percent_match < 45:
+            continue
+        if len(harvard_characters) < 500:
+            if not percent_match in range(98, 102):
+                continue
+            clean_docket = clean_docket_number(docket_number)
+            if clean_docket not in case.docket.docket_number:
+                continue
+        return case
+    return None
+
+
+def find_previously_imported_cases(
+    court_id: str,
+    date_filed: datetime.date,
+    case_body: str,
+    docket_number: str,
+) -> Optional[OpinionCluster]:
+    """Check if opinion is in Courtlistener
+
+    :param court_id: Court ID
+    :param date-filed: The date filed
+    :param case_body: Date of opinion
+    :param docket_number: The docket number
+    :return:
+    """
+    possible_cases = OpinionCluster.objects.filter(
+        date_filed=date_filed,
+        docket__court_id=court_id,
+    ).order_by("id")
+    month = timedelta(days=31)
+    broad_search = OpinionCluster.objects.filter(
+        date_filed__range=[date_filed - month, date_filed + month],
+        docket__court_id=court_id,
+    ).order_by("id")
+
+    match = match_based_text(case_body, possible_cases, docket_number)
+    if not match:
+        possible_cases = [
+            case for case in broad_search if case.date_filed != date_filed
+        ]
+        match = match_based_text(case_body, possible_cases, docket_number)
+    return match
+
+
+def compare_documents(harvard_characters: str, cl_characters: str) -> int:
+    """Compare Harvard text to CL opinion text
+
+    This code iterates over two opinions logging similar stretches and then
+    returns a percentage of the total overlapping characters
+
+    :param harvard_characters: The stripped down opinion text from Harvard
+    :param cl_characters: The stripped down opinion text on Courtlistener
+    :return: Percentage (as integer) overlapping content
+    """
+
+    start, stop, count = 0, 0, 0
+    max_string = ""
+    hit = False
+    while start < len(harvard_characters):
+        if start > len(harvard_characters) or stop > len(harvard_characters):
+            break
+        stop += 1
+        if harvard_characters[start:stop] in cl_characters:
+            if len(harvard_characters) - start < len(max_string):
+                percent_match = int(
+                    100
+                    * (
+                        count
+                        / min([len(harvard_characters), len(cl_characters)])
+                    )
+                )
+                return percent_match
+            max_string = harvard_characters[start:stop]
+            hit = True
+        else:
+            if hit == True:
+                # Only count strings 10 characters or longer
+                if len(max_string) > 10:
+                    count += len(max_string)
+                hit = False
+            start = stop + 1
+            stop = start + 1
+    if len(max_string) > 10:
+        count += len(max_string)
+    percent_match = int(
+        100 * (count / min([len(harvard_characters), len(cl_characters)]))
+    )
+    return percent_match
 
 
 class MissingDocumentError(Exception):
@@ -429,12 +644,20 @@ class Command(VerboseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             "--volume",
+            type=str,
             help="Volume number. If none provided, "
             "code will cycle through all volumes of reporter on IA.",
         )
         parser.add_argument(
             "--reporter",
+            type=str,
             help="Reporter abbreviation as saved on IA.",
+            required=False,
+        )
+        parser.add_argument(
+            "--court-id",
+            type=str,
+            help="The CL Court ID",
             required=False,
         )
 
@@ -446,7 +669,4 @@ class Command(VerboseCommand):
         )
 
     def handle(self, *args, **options):
-        reporter = options["reporter"]
-        volume = options["volume"]
-        make_searchable = options["make_searchable"]
-        parse_harvard_opinions(reporter, volume, make_searchable)
+        parse_harvard_opinions(options)
