85e3f1cd1c913b5e3a5ec6934358abb1f7f7544c
Nick Sethi <nicholas.sethi@gmail.com>
2021-08-20 14:59:10 -0700

    add ca_state_courts

---
+++ cl/search/migrations/0003_load_ca_state_court_data.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+
+import sys
+
+from django.db import migrations, models
+
+from cl.lib.migration_utils import load_migration_fixture
+
+
+def load_fixture(apps, schema_editor):
+    if 'test' in sys.argv:
+        return None
+    else:
+        fixture = 'cal_state_court_data'
+    load_migration_fixture(apps, schema_editor, fixture, 'search')
+
+
+def unload_fixture(apps, schema_editor):
+    return None
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('search', '0002_load_initial_data'),
+    ]
+
+    operations = [
+        migrations.RunPython(load_fixture, reverse_code=unload_fixture),
+    ]
