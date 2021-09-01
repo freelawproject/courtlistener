10525c66ee6c34c2129a4418b2ad69b06d6d6022
Nick Sethi <nicholas.sethi@gmail.com>
2021-08-31 22:48:34 -0700

    cleanup, move to management command

--- cl/people_db/import_judges/ca_judges_import_helpers.py
+++ cl/people_db/import_judges/ca_judges_import_helpers.py
@@ -14,6 +14,11 @@ from cl.search.models import Docket
 
 
 def load_json_file(file_name):
+    """Loads Json File
+
+    :param file_name: string
+    :return json
+    """
     dir_path = os.path.dirname(os.path.realpath(__file__))
     with open(f"{dir_path}/{file_name}") as f:
         deserialized = json.load(f)
@@ -21,11 +26,24 @@ def load_json_file(file_name):
 
 
 def get_middle_name(last_first_middle_name, first_name, last_name):
-    # lfm in format of Abbe, John Q.
-    # Aranda, Benjamin J. III
-    # Cherniss, Sidney A., Jr.
-    # Nelson, Mark G., Sr.
-    # replace first and last names with empty strings
+    """Extracts middle name from the last_first_middle string
+
+    Given that we have a first name, and a last name, we can
+    subtract both of those from the string and return only the
+    remaining text.
+
+    The last_first_middle name is the in format of:
+    Abbe, John Q.
+    Aranda, Benjamin J. III
+    Cherniss, Sidney A., Jr.
+    Nelson, Mark G., Sr.
+
+    :param last_first_middle_name: string
+    :param first_name: string
+    :param last_name: string
+    :return string
+    """
+
     without_first = last_first_middle_name.replace(first_name, "")
     without_first_and_last = without_first.replace(last_name, "")
 
@@ -40,6 +58,12 @@ def get_middle_name(last_first_middle_name, first_name, last_name):
 
 
 def get_how_selected(jud_exp_pending_status):
+    """Maps pending status to the Selection Method Enum
+
+    :param jud_exp_pending_status: string
+    :return enum / string
+    """
+
     if jud_exp_pending_status == "Appointed":
         # SELECTION_METHODS['Appointment']
         # need to check to make sure legislatures don't appoint
@@ -53,6 +77,12 @@ def get_how_selected(jud_exp_pending_status):
 
 
 def get_appointer(jud_exp_pending_sub_type):
+    """Extracts Appointer Name from the pending sub_type
+
+    :param jud_exp_pending_sub_type: string
+    :return string
+    """
+
     # if type is 'Unknown' or 'Board of Supv' or 'Consolidation' or '(Blanks)'
     # or 'Chief Judge' it is an edge case
     edge_cases = [
@@ -66,10 +96,14 @@ def get_appointer(jud_exp_pending_sub_type):
     if jud_exp_pending_sub_type in edge_cases:
         return ""
 
+    # TODO: LOOKUP APPOINTER FOREIGN KEY
+    # search positions for the appointer's name
+    # handle stupid multiple "Brown" govs
     return jud_exp_pending_sub_type
 
 
-# def get_termination_date_and_reason(position):
+# TODO: WRITE THIS FUNCTION
+# def get_date_termination_and_termination_reason(position):
 # status = position["experienceStatus"]
 # reason = position["experienceReason"]
 # date = position["experienceStatusEffectiveDate"]
@@ -77,7 +111,20 @@ def get_appointer(jud_exp_pending_sub_type):
 
 
 def find_judge(lfm, positions, counties):
-    # from the positions, grab the name in the format "last, first middle"
+    """Find Judge in db
+
+    Using the lookup_judge_by_full_name function we can pass in a
+    humanname string in the last_first_middle format.
+
+    We thus iterate over the courts, and try to find the judge. If we
+    find one, we break and return the judge. If no judge is found, we
+    return None
+
+    :param lfm: string
+    :param positions: PositionJson[]
+    :param counties: CountiesJson {[countyName]: countyAbbreviation}[]
+    :return Judge or None
+    """
 
     # iterate over the positions and for each
     courts = []
@@ -97,17 +144,28 @@ def find_judge(lfm, positions, counties):
     # else keep going
     # return none if nothing found
     if judge is None:
-        print(f"Unable to find a judge for name {lfm}")
+        logging.info(f"Unable to find a judge for name {lfm}")
     else:
-        print(f"Found judge with id {judge.id}")
+        logging.info(f"Found judge with id {judge.id}")
     return judge
 
 
 def find_or_create_judge(judgeJson, counties):
+    """Find Judge in db
+
+    Check to see if a judge exists by running the find_judge function.
+    If no judge is returned, go ahead and create the judge and return
+    the Model instance.
+
+    :param judgeJson: JudgeJson
+    :param counties: CountiesJson {[countyName]: countyAbbreviation}[]
+    :return Judge
+    """
+
     positions = judgeJson["positions"]
     # from the positions, grab the name in the format "last, first middle"
     lfm = positions[0]["lastFirstMiddleName"]
-    print(f"Searching for judge with last, first middle {lfm}")
+    logging.info(f"Searching for judge with last, first middle {lfm}")
     judge = find_judge(lfm, positions, counties)
     # if no judge, create it
     if judge is None:
@@ -117,10 +175,10 @@ def find_or_create_judge(judgeJson, counties):
         name_middle = get_middle_name(lfm, name_first, name_last)
         date_dod = first_position["deceasedDate"]
 
-        print(f"name_first: {name_first}")
-        print(f"name_last: {name_last}")
-        print(f"name_middle: {name_middle}")
-        print(f"date_dod: {date_dod}")
+        logging.info(f"name_first: {name_first}")
+        logging.info(f"name_last: {name_last}")
+        logging.info(f"name_middle: {name_middle}")
+        logging.info(f"date_dod: {date_dod}")
 
         # judge = Person(
         #   name_first=name_first
@@ -133,21 +191,29 @@ def find_or_create_judge(judgeJson, counties):
     return judge
 
 
-# def parse_name(last_first_middle, first, last):
-# examples
-
 def find_court(position, counties):
-    # Association
-    # County Counsel
-    # Court Not CA Judiciary [i.e., Federal]
-    # Court of Appeal
-    # JCC Agency
-    # Justice Court
-    # Law Firm
-    # Municipal Court
-    # Non-Judicial Other
-    # Superior Court
-    # Supreme Court
+    """Lookup the court based on the court name and type
+
+    Using the orgType and orgName we search for the corresponding
+    court and return the court's pk
+
+    OrgTypes
+    Association
+    County Counsel
+    Court Not CA Judiciary [i.e., Federal]
+    Court of Appeal
+    JCC Agency
+    Justice Court
+    Law Firm
+    Municipal Court
+    Non-Judicial Other
+    Superior Court
+    Supreme Court
+
+    :param position: PositionJson
+    :param counties: CountiesJson
+    :return string (court pk)
+    """
 
     org_type = position["orgType"]
     org_name = position["orgName"]
@@ -191,12 +257,18 @@ def find_court(position, counties):
 
 
 def lookup_court_abbr(org_name, counties):
-    # given an org_name for a court that is county based
-    # i.e., Municipal, Superior, Justice
-    # e.g., "Butte Justice Court", "Los Angeles Municipal Court", "Santa Clara Superior Court"
-    # split by space and if 2nd to last is "Superior" or "Justice" or "Municipal"
-    # and last is "Court"
-    # find the corresponding court in the abbreviations
+    """Lookup the abbreviation for a county court
+
+    Given an org_name for a court that is county based
+    i.e., Municipal, Superior, Justice
+    e.g., "Butte Justice Court", "Los Angeles Municipal Court", "Santa Clara Superior Court"
+    split by space and if 2nd to last is "Superior" or "Justice" or "Municipal"
+    and last is "Court"
+
+    :param org_name: string
+    :param counties: CountyJSON
+    """
+
     state_courts = ["Justice", "Municipal", "Superior"]
     parts = org_name.split(" ")
     if parts[-1] == "Court" and parts[-2] in state_courts:
@@ -207,6 +279,12 @@ def lookup_court_abbr(org_name, counties):
 
 
 def lookup_appellate_court_district(fourth_word):
+    """Get a number from an ordinal
+
+    :param fourth_word: string
+    :return string
+    """
+
     appellate_courts = {
         "First": "1",
         "Second": "2",
@@ -219,60 +297,22 @@ def lookup_appellate_court_district(fourth_word):
     return lookup
 
 
-"""
-  Shape of Loaded File JSON
-  {
-    count: string,
-    judges: Judge[]
-  }
-"""
+# TODO: WRITE THIS FUNCTION
+def get_position_type(jobTitle):
+    """Map the provided jobTitle string to the Position Type Enum
 
-"""
-  Shape of Judge JSON
-  {
-    fullName: string
-    positions: Position[]
-  }
-"""
+    :param jobTitle: one of the following possible values:
+        Court of Appeal Administrative Presiding Justice
+        Court of Appeal Associate Justice
+        Court of Appeal Presiding Justice
+        Justice Court Judge
+        Municipal Court Judge
+        Superior Court Assistant Presiding Judge
+        Superior Court Judge
+        Superior Court Presiding Judge
+        Superior Court Supervising Judge
+        Supreme Court Associate Justice
+        Supreme Court Chief Justice
 
-"""
-  Shape of Position JSON
-  All properties are strings
-  {
-    id,
-    salutation,
-    jobTitle,
-    title,
-    fullName,
-    lastFirstMiddleName,
-    firstName,
-    lastName,
-    judicialIndicator,
-    orgType,
-    orgName,
-    orgDivision,
-    experienceStatus,
-    experienceStatusReason,
-    experienceStatusEffectiveDate,
-    expId,
-    barAdmissionDate,
-    deceasedDate,
-    judicialPositionId,
-    judicialPositionJobClass,
-    judicialPositionJobTitle,
-    judicialPositionOrgType,
-    judicialPositionOrgName,
-    judicialPositionLocationName,
-    judicialExperienceDivsionName,
-    judicialPositionOrgCounty,
-    judicialPositionActiveDate,
-    judicialPositionInactiveDate,
-    judicialExperiencePendingStatus,
-    judicialExperiencePendingSubType,
-    JudicialExperienceInactiveStatus,
-    judicialExperienceAppointmentDate,
-    judicialExperienceActiveDate,
-    judicialExperienceInactiveDate,
-    judicialExperienceTermEndDate,
-  }
-"""
+    :return Position enum / string
+    """
