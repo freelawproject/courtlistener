b96e23dca456f578bcfd1dc6595d2d74ddfb9a0d
William Palin <flooie@users.noreply.github.com>
2021-10-04 15:55:08 -0400

    feat(main): Update main.js

--- cl/assets/static-global/js/react/main.js
+++ cl/assets/static-global/js/react/main.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "89dcf7499d0db3dc614d";
+/******/ 	var hotCurrentHash = "c7ea82894e8a27a2d70d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -1136,6 +1136,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _disclosure_page_css__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./disclosure-page.css */ "./assets/react/disclosure-page.css");
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
 /* harmony import */ var _disclosure_models__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./_disclosure_models */ "./assets/react/_disclosure_models.ts");
+/* harmony import */ var _disclosure_helpers__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./_disclosure_helpers */ "./assets/react/_disclosure_helpers.ts");
 
 
 
@@ -1147,15 +1148,6 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-var getIndex = function getIndex(value, arr) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === value.toString()) {
-      return i;
-    }
-  }
-
-  return 0;
-};
 
 var TableNavigation = function TableNavigation(disclosures) {
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_4___default.a.Fragment, null, MainSection(disclosures));
@@ -1164,7 +1156,7 @@ var TableNavigation = function TableNavigation(disclosures) {
 var MainSection = function MainSection(disclosures) {
   var years = disclosures['years'].split(",");
   var doc_ids = disclosures['ids'].split(",");
-  var is_admin = disclosures['admin'] == "True" ? true : false;
+  var is_admin = disclosures['admin'] == "True";
   var judge_name = disclosures['judge'];
 
   var _React$useState = react__WEBPACK_IMPORTED_MODULE_4___default.a.useState(""),
@@ -1180,29 +1172,12 @@ var MainSection = function MainSection(disclosures) {
   var _useParams = Object(react_router_dom__WEBPACK_IMPORTED_MODULE_8__["useParams"])(),
       judge_id = _useParams.judge_id;
 
-  var search = window.location.search;
-  var params = new URLSearchParams(search);
-  var optional_doc_id = params.get('id');
-  var start;
-  var index;
+  var year_index = Object(_disclosure_helpers__WEBPACK_IMPORTED_MODULE_10__["fetch_year_index"])(years, doc_ids);
 
-  if (optional_doc_id == null) {
-    start = years[0];
-    index = 0;
-  } else {
-    index = getIndex(optional_doc_id, doc_ids);
-    start = years[index];
-  }
-
-  var _React$useState5 = react__WEBPACK_IMPORTED_MODULE_4___default.a.useState(start),
+  var _React$useState5 = react__WEBPACK_IMPORTED_MODULE_4___default.a.useState(year_index[0]),
       _React$useState6 = _babel_runtime_helpers_slicedToArray__WEBPACK_IMPORTED_MODULE_3___default()(_React$useState5, 2),
       year = _React$useState6[0],
       setYear = _React$useState6[1];
-
-  var _React$useState7 = react__WEBPACK_IMPORTED_MODULE_4___default.a.useState(index),
-      _React$useState8 = _babel_runtime_helpers_slicedToArray__WEBPACK_IMPORTED_MODULE_3___default()(_React$useState7, 2),
-      id = _React$useState8[0],
-      setId = _React$useState8[1];
 
   var fetchDisclosure = /*#__PURE__*/function () {
     var _ref = _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_2___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee(year, index) {
@@ -1264,7 +1239,6 @@ var MainSection = function MainSection(disclosures) {
 
             case 7:
               response = _context2.sent;
-              // console.log("resp", response['results'])
               setJudge(response['results']);
               _context2.next = 14;
               break;
@@ -1288,7 +1262,7 @@ var MainSection = function MainSection(disclosures) {
   }();
 
   if (data == '') {
-    fetchDisclosure(year, id);
+    fetchDisclosure(year, year_index[1]);
   }
 
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("div", null, data != "" && data.has_been_extracted == true ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("div", {
@@ -1316,38 +1290,6 @@ var MainSection = function MainSection(disclosures) {
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("h3", {
     className: "text-center"
   }, " Loading ...")));
-};
-
-var convertTD = function convertTD(value, table, key) {
-  if (value == -1) {
-    return "[Failed OCR]";
-  }
-
-  if (table == "Investments" && key == "transaction_value_code" && value) {
-    return "".concat(_disclosure_models__WEBPACK_IMPORTED_MODULE_9__["GROSS_VALUE"][value], " (").concat(value, ")");
-  }
-
-  if (table == "Debts" && key == "value_code" && value) {
-    return "".concat(_disclosure_models__WEBPACK_IMPORTED_MODULE_9__["GROSS_VALUE"][value], " (").concat(value, ")");
-  }
-
-  if (table == "Investments" && key == "income_during_reporting_period_code" && value) {
-    return "".concat(_disclosure_models__WEBPACK_IMPORTED_MODULE_9__["INCOME_GAIN"][value], " (").concat(value, ")");
-  }
-
-  if (table == "Investments" && key == "gross_value_method" && value) {
-    return "".concat(_disclosure_models__WEBPACK_IMPORTED_MODULE_9__["VALUATION_METHODS"][value], " (").concat(value, ")");
-  }
-
-  if (table == "Investments" && key == "gross_value_code" && value) {
-    return "".concat(_disclosure_models__WEBPACK_IMPORTED_MODULE_9__["GROSS_VALUE"][value], " (").concat(value, ")");
-  }
-
-  if (table == "Investments" && key == "transaction_gain_code" && value) {
-    return "".concat(_disclosure_models__WEBPACK_IMPORTED_MODULE_9__["INCOME_GAIN"][value], " (").concat(value, ")");
-  }
-
-  return value;
 };
 
 var TableMaker = function TableMaker(data, key, is_admin) {
@@ -1384,10 +1326,12 @@ var TableMaker = function TableMaker(data, key, is_admin) {
       key: entry.id,
       className: ""
     }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("td", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("a", {
+      title: "Go to PDF",
       href: url + "#page=" + entry.page_number
     }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("i", {
       className: "fa fa-file-text-o gray"
-    })), entry.redacted == true ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("i", {
+    })), "\xA0", entry.redacted == true ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("i", {
+      title: "Redaction present in row",
       className: "fa fa-file-excel-o black"
     }) : ""), is_admin == true ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("td", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("a", {
       href: "/admin/disclosures/".concat(key.replaceAll("_", "").slice(0, -1), "/").concat(entry.id, "/")
@@ -1398,7 +1342,7 @@ var TableMaker = function TableMaker(data, key, is_admin) {
           key = _ref6[0],
           value = _ref6[1];
 
-      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("td", null, convertTD(entry[value], title, value));
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("td", null, Object(_disclosure_helpers__WEBPACK_IMPORTED_MODULE_10__["convertTD"])(entry[value], title, value));
     }));
   })))) : "");
 };
@@ -2480,6 +2424,76 @@ var TagSelect = function TagSelect(_ref) {
 
 /***/ }),
 
+/***/ "./assets/react/_disclosure_helpers.ts":
+/*!*********************************************!*\
+  !*** ./assets/react/_disclosure_helpers.ts ***!
+  \*********************************************/
+/*! exports provided: convertTD, fetch_year_index */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "convertTD", function() { return convertTD; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "fetch_year_index", function() { return fetch_year_index; });
+/* harmony import */ var _disclosure_models__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./_disclosure_models */ "./assets/react/_disclosure_models.ts");
+
+var convertTD = function convertTD(value, table, key) {
+  if (value == -1) {
+    return "[Failed OCR]";
+  }
+
+  if (table == "Investments" && key == "transaction_value_code" && value) {
+    return "".concat(_disclosure_models__WEBPACK_IMPORTED_MODULE_0__["GROSS_VALUE"][value], " (").concat(value, ")");
+  }
+
+  if (table == "Debts" && key == "value_code" && value) {
+    return "".concat(_disclosure_models__WEBPACK_IMPORTED_MODULE_0__["GROSS_VALUE"][value], " (").concat(value, ")");
+  }
+
+  if (table == "Investments" && key == "income_during_reporting_period_code" && value) {
+    return "".concat(_disclosure_models__WEBPACK_IMPORTED_MODULE_0__["INCOME_GAIN"][value], " (").concat(value, ")");
+  }
+
+  if (table == "Investments" && key == "gross_value_method" && value) {
+    return "".concat(_disclosure_models__WEBPACK_IMPORTED_MODULE_0__["VALUATION_METHODS"][value], " (").concat(value, ")");
+  }
+
+  if (table == "Investments" && key == "gross_value_code" && value) {
+    return "".concat(_disclosure_models__WEBPACK_IMPORTED_MODULE_0__["GROSS_VALUE"][value], " (").concat(value, ")");
+  }
+
+  if (table == "Investments" && key == "transaction_gain_code" && value) {
+    return "".concat(_disclosure_models__WEBPACK_IMPORTED_MODULE_0__["INCOME_GAIN"][value], " (").concat(value, ")");
+  }
+
+  return value;
+};
+
+var getIndex = function getIndex(value, arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === value.toString()) {
+      return i;
+    }
+  }
+
+  return 0;
+};
+
+var fetch_year_index = function fetch_year_index(years, doc_ids) {
+  var search = window.location.search;
+  var params = new URLSearchParams(search);
+  var optional_doc_id = params.get('id');
+  var index = 0;
+
+  if (optional_doc_id != null) {
+    index = getIndex(optional_doc_id, doc_ids);
+  }
+
+  return [years[index], index];
+};
+
+/***/ }),
+
 /***/ "./assets/react/_disclosure_models.ts":
 /*!********************************************!*\
   !*** ./assets/react/_disclosure_models.ts ***!
@@ -3109,7 +3123,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _node_modules_css_loader_dist_cjs_js_disclosure_page_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !../../node_modules/css-loader/dist/cjs.js!./disclosure-page.css */ "./node_modules/css-loader/dist/cjs.js!./assets/react/disclosure-page.css");
 
-
+            
 
 var options = {};
 
@@ -3246,7 +3260,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _node_modules_css_loader_dist_cjs_js_tag_page_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !../../node_modules/css-loader/dist/cjs.js!./tag-page.css */ "./node_modules/css-loader/dist/cjs.js!./assets/react/tag-page.css");
 
-
+            
 
 var options = {};
 
