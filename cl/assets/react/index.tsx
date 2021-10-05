33af67070dc262b2c1831b45855d0731913e0986
William Palin <flooie@users.noreply.github.com>
2021-10-05 13:53:15 -0400

    chore(DisclosureList): Typing Typescript

--- cl/assets/react/index.tsx
+++ cl/assets/react/index.tsx
@@ -29,7 +29,6 @@ const App = () => {
         <Route path={`/financial-disclosures/:judge_id/:slug/`}>
           <DisclosureViewer {...data}  />
         </Route>
-
       </Switch>
     </BrowserRouter>
   );
