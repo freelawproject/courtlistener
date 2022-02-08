f8766bdee25fa8be0b108d6344bc007b39329e95
Alberto <albertisfu@gmail.com>
2022-02-08 14:46:46 -0600

    fix(docket): improve buy on parcer modal implementation

--- cl/opinion_page/static/js/buy_pacer_modal.js
+++ cl/opinion_page/static/js/buy_pacer_modal.js
@@ -1,62 +1,32 @@
 $(document).ready(function () {
-
-
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-          var cookies = document.cookie.split(';');
-          for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-              break;
-            }
-          }
-        }
-        return cookieValue;
-      }
-
-    
-    $("#open_buy_pacer_modal").on("click", function (e) {
-
+    $(".open_buy_pacer_modal").on("click", function (e) {
         //Modal clicked
-
-
-        var recap_installed = getCookie('recap_install_plea');
-        //check if recap is installed or ctrl or shift key pressed
-        if (recap_installed || e.metaKey || e.shiftKey){
+        //check if ctrl or shift key pressed
+        if (e.metaKey || e.shiftKey){
             //prevent modal from opening, go directly to href link
             e.stopPropagation();
-
         }
-        
         else{
             //otherwise open modal and concatenate pacer URL to button
             var pacer_url = $(this).attr('href');
             $("#modal-buy-pacer #pacer_url").attr("href", pacer_url);
         }
-
-
     });
 
   //////////////////////////
   //  Modal Cookie Handling//
   //////////////////////////
   $('#pacer_url').on("click", function () {
-
+    //on open parcer external url, set buy_on_pacer_modal cookie for 30 days
     let that = $(this);
     let duration = parseInt(that.data('duration'), 10);
     let cookie_name = that.data('cookie-name');
     let date = new Date();
-    date.setTime(date.getTime() + (duration * 24 * 60 * 60 * 1000));
+    date.setTime(date.getTime() + (duration * 24 * 60 * 60 * 1000)); //30 days
     let expires = "; expires=" + date.toGMTString();
     document.cookie = cookie_name + "=" + 'true' + expires + "; path=/";
 
     ///Close Modal
     $("#modal-buy-pacer ").modal('toggle');
-    
   });
-
-
 });
