c5d61565a90f6d563c964229ef9c0aee56a2d236
Alberto <albertisfu@gmail.com>
2022-03-10 13:51:49 -0600

    feat(email): Handle SNS notifications for emails sent

--- cl/users/models.py
+++ cl/users/models.py
@@ -1,7 +1,8 @@
+import logging
 import re
 from datetime import datetime, timedelta
 from decimal import Decimal
-from typing import Dict
+from typing import Dict, List
 
 from django.contrib.auth.models import User
 from django.core.exceptions import FieldError
@@ -10,6 +11,11 @@ from django.db.models import Sum
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.timezone import now
+from django_ses.signals import (
+    bounce_received,
+    complaint_received,
+    delivery_received,
+)
 from localflavor.us.models import USStateField
 
 from cl.api.utils import invert_user_logs
@@ -174,6 +180,69 @@ class UserProfile(models.Model):
         verbose_name_plural = "user profiles"
 
 
+class EmailFlag(models.Model):
+    BAN = "ban"
+    FLAG = "flag"
+    FLAGS_TYPES = (
+        (BAN, "Email ban"),
+        (FLAG, "Email flag"),
+    )
+    email_address = models.EmailField(
+        help_text="Email address flagged.",
+    )
+    flag_type = models.CharField(
+        help_text="The flag type assigned.",
+        choices=FLAGS_TYPES,
+        max_length=5,
+    )
+    flag = models.CharField(
+        help_text="The flag assigned to email address.",
+        max_length=25,
+        blank=True,
+    )
+    reason = models.CharField(
+        help_text="The notification subtype",
+        max_length=50,
+        blank=True,
+    )
+    date_created = models.DateTimeField(
+        help_text="The moment when the item was created.",
+        auto_now_add=True,
+    )
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["email_address"]),
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.get_flag_type_display()} for {self.email_address}"
+
+
+class BackoffEvent(models.Model):
+    email_address = models.EmailField(
+        help_text="Email address under backoff event.",
+    )
+    retry_counter = models.SmallIntegerField(
+        help_text="The retry counter for exponential backoff events.",
+    )
+    next_retry_date = models.DateTimeField(
+        help_text="The datetime for next retry.",
+    )
+    date_created = models.DateTimeField(
+        help_text="The moment when the item was created.",
+        auto_now_add=True,
+    )
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["email_address"]),
+        ]
+
+    def __str__(self) -> str:
+        return f"Backoff event for {self.email_address}"
+
+
 def generate_recap_email(user_profile: UserProfile, append: int = None) -> str:
     username = user_profile.user.username
     recap_email_header = re.sub(r"[^0-9a-zA-Z]+", ".", username) + str(
@@ -190,8 +259,179 @@ def generate_recap_email(user_profile: UserProfile, append: int = None) -> str:
     return recap_email
 
 
+def handle_hard_bounce(
+    message_id: str, bounceSubType: str, recipient_emails: List[str]
+) -> None:
+    """Handle a hard bounce notification received from SNS
+    :param message_id: The unique message id assigned by Amazon SES
+    :param bounceSubType: The subtype of the bounce, as determined
+     by Amazon SES
+    :param recipient_emails: a list of email addresses one per recipient
+     to whom the bounce notification pertains
+    :return: None
+    """
+    pass
+
+
+def handle_soft_bounce(
+    message_id: str, bounceSubType: str, recipient_emails: List[str]
+) -> None:
+    """Handle a soft bounce notification received from SNS
+    :param message_id: The unique message id assigned by Amazon SES
+    :param bounceSubType: The subtype of the bounce, as determined
+     by Amazon SES
+    :param recipient_emails: a list of email addresses one per recipient
+     to whom the bounce notification pertains
+    :return: None
+    """
+
+    back_off_events = ["Undetermined", "General", "MailboxFull"]
+    small_only_events = ["MessageTooLarge", "AttachmentRejected"]
+
+    MAX_RETRY_COUNTER = 5
+    INITIAL_HOURS = 2
+
+    for email in recipient_emails:
+        if bounceSubType in back_off_events:
+            # Handle events that must trigger a backoff event
+            backoff_event = BackoffEvent.objects.filter(
+                email_address=email,
+            )
+            if not backoff_event.exists():
+                # Create a backoff event for an email address if not exists
+                # Initialize retry_counter and next_retry_date
+                next_retry_date = now() + timedelta(hours=INITIAL_HOURS)
+                BackoffEvent.objects.create(
+                    email_address=email,
+                    retry_counter=0,
+                    next_retry_date=next_retry_date,
+                )
+            else:
+                # If a previous backoff event exists
+                retry_counter = backoff_event[0].retry_counter
+                next_retry_date = backoff_event[0].next_retry_date
+
+                # Check if waiting period expired
+                if next_retry_date <= now():
+                    if retry_counter >= MAX_RETRY_COUNTER:
+                        # Check if backoff event has reached
+                        # max number of retries, if so ban email address
+                        EmailFlag.objects.create(
+                            email_address=email,
+                            flag_type="ban",
+                            flag="max_retry_reached",
+                            reason=bounceSubType,
+                        )
+                    else:
+                        # If max number of retries has not been reached,
+                        # update backoff event, update retry_counter
+                        new_retry_counter = retry_counter + 1
+                        # Update new_next_retry_date exponentially
+                        new_next_retry_date = now() + timedelta(
+                            hours=pow(INITIAL_HOURS, new_retry_counter + 1)
+                        )
+                        BackoffEvent.objects.filter(
+                            email_address=email,
+                        ).update(
+                            retry_counter=new_retry_counter,
+                            next_retry_date=new_next_retry_date,
+                        )
+
+        elif bounceSubType in small_only_events:
+            # Handle events that must trigger a small_email_only event
+            small_only_exists = EmailFlag.objects.filter(
+                email_address=email,
+                flag_type="flag",
+                flag="small_email_only",
+            ).exists()
+            # Create a small_email_only flag for email address
+            if not small_only_exists:
+                EmailFlag.objects.create(
+                    email_address=email,
+                    flag_type="flag",
+                    flag="small_email_only",
+                    reason=bounceSubType,
+                )
+        else:
+            # Handle other unexpected bounceSubType events, log a warning
+            logging.warning(
+                f"Unexpected {bounceSubType} soft bounce for {email}"
+            )
+
+
+def handle_complaint(message_id: str, recipient_emails: List[str]) -> None:
+    """Handle a complaint notification received from SNS
+    :param message_id: The unique message id assigned by Amazon SES
+    :param recipient_emails: a list of email addresses one per recipient
+     to whom the complaint notification pertains
+    :return: None
+    """
+    pass
+
+
+def handle_delivery(message_id: str, recipient_emails: List[str]) -> None:
+    """Handle a delivery notification received from SNS
+    :param message_id: The unique message id assigned by Amazon SES
+    :param recipient_emails: a list of email addresses one per recipient
+     to whom the delivery notification pertains
+    :return: None
+    """
+    pass
+
+
 @receiver(post_save, sender=UserProfile)
 def assign_recap_email(sender, instance=None, created=False, **kwargs) -> None:
     if created:
         instance.recap_email = generate_recap_email(instance)
         instance.save()
+
+
+@receiver(bounce_received)
+def bounce_handler(sender, mail_obj, bounce_obj, raw_message, *args, **kwargs):
+    """Receiver function to handle bounce notifications sent by Amazon SES via
+    SESEventWebhookView
+    """
+    if bounce_obj:
+        message_id = mail_obj["messageId"]
+        bounceType = bounce_obj["bounceType"]
+        bounceSubType = bounce_obj["bounceSubType"]
+        bouncedRecipients = bounce_obj["bouncedRecipients"]
+        recipient_emails = [
+            email["emailAddress"] for email in bouncedRecipients
+        ]
+        # If bounceType is Permanent, handle a hard bounce
+        # If bounceType is Transient, handle a soft bounce
+        if bounceType == "Permanent":
+            handle_hard_bounce(message_id, bounceSubType, recipient_emails)
+        elif bounceType == "Transient":
+            handle_soft_bounce(message_id, bounceSubType, recipient_emails)
+
+
+@receiver(complaint_received)
+def complaint_handler(
+    sender, mail_obj, complaint_obj, raw_message, *args, **kwargs
+):
+    """Receiver function to handle complaint notifications sent by
+    Amazon SES via SESEventWebhookView
+    """
+
+    if complaint_obj:
+        message_id = mail_obj["messageId"]
+        complainedRecipients = complaint_obj["complainedRecipients"]
+        recipient_emails = [
+            email["emailAddress"] for email in complainedRecipients
+        ]
+        handle_complaint(message_id, recipient_emails)
+
+
+@receiver(delivery_received)
+def delivery_handler(
+    sender, mail_obj, delivery_obj, raw_message, *args, **kwargs
+):
+    """Receiver function to handle delivery notifications sent by
+    Amazon SES via SESEventWebhookView
+    """
+    if delivery_obj:
+        message_id = mail_obj["messageId"]
+        recipient_emails = mail_obj["destination"]
+        handle_delivery(message_id, recipient_emails)
--- cl/users/tests.py
+++ cl/users/tests.py
@@ -1,18 +1,23 @@
+import json
+import os
 from datetime import timedelta
+from unittest import mock
 
+from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
 from django.core import mail
 from django.test import Client
 from django.urls import reverse
 from django.utils.http import urlsafe_base64_encode
 from django.utils.timezone import now
+from django_ses import signals
 from rest_framework.status import HTTP_200_OK
 from selenium.webdriver.common.by import By
 from timeout_decorator import timeout_decorator
 
 from cl.tests.base import SELENIUM_TIMEOUT, BaseSeleniumTest
 from cl.tests.cases import LiveServerTestCase, TestCase
-from cl.users.models import UserProfile
+from cl.users.models import BackoffEvent, EmailFlag, UserProfile
 
 
 class UserTest(LiveServerTestCase):
@@ -329,3 +334,337 @@ class LiveUserTest(BaseSeleniumTest):
             self.browser.current_url,
             f"{self.live_server_url}{reverse('password_reset_complete')}",
         )
+
+
+class SNSWebhookTest(TestCase):
+    def setUp(self) -> None:
+        test_dir = os.path.join(
+            settings.INSTALL_ROOT, "cl", "users", "test_assets"
+        )
+        with open(os.path.join(test_dir, "general_soft_bounce.json")) as file:
+            self.soft_bounce_asset = json.loads(file.read())
+        with open(os.path.join(test_dir, "msg_large_bounce.json")) as file:
+            self.soft_bounce_msg_large_asset = json.loads(file.read())
+        with open(os.path.join(test_dir, "cnt_rejected_bounce.json")) as file:
+            self.soft_bounce_cnt_rejected_asset = json.loads(file.read())
+        with open(os.path.join(test_dir, "hard_bounce.json")) as file:
+            self.hard_bounce_asset = json.loads(file.read())
+        with open(os.path.join(test_dir, "complaint.json")) as file:
+            self.complaint_asset = json.loads(file.read())
+        with open(os.path.join(test_dir, "delivery.json")) as file:
+            self.delivery_asset = json.loads(file.read())
+
+    def send_signal(self, test_asset, event_name, signal) -> None:
+        """Function to dispatch signal that mocks a SNS notification event
+        :param test_asset: the json object that contains notification
+        :param event_name: the signal event name
+        :param signal: the signal corresponding to the event
+        :return: None
+        """
+        # Prepare parameters
+        raw = json.dumps(test_asset)
+        notification = json.loads(raw)
+        message = json.loads(notification["Message"])
+        mail_obj = message.get("mail")
+        event_obj = message.get(event_name, {})
+
+        # Send signal
+        signal_kwargs = dict(
+            sender=self,
+            mail_obj=mail_obj,
+            raw_message=raw,
+        )
+        signal_kwargs["%s_obj" % event_name] = event_obj
+        signal.send(**signal_kwargs)
+
+    @mock.patch("cl.users.models.handle_hard_bounce")
+    def test_hard_bounce_signal(self, mock_hard_bounce) -> None:
+        """This test checks if handle_hard_bounce function is called
+        when a hard bounce event is received
+        """
+        # Trigger a hard bounce event
+        self.send_signal(
+            self.hard_bounce_asset, "bounce", signals.bounce_received
+        )
+        # Check if handle_hard_bounce is called
+        mock_hard_bounce.assert_called()
+
+    @mock.patch("cl.users.models.handle_soft_bounce")
+    def test_soft_bounce_signal(self, mock_soft_bounce) -> None:
+        """This test checks if handle_soft_bounce function is called
+        when a soft bounce event is received
+        """
+        # Trigger a soft bounce event
+        self.send_signal(
+            self.soft_bounce_asset, "bounce", signals.bounce_received
+        )
+        # Check if handle_soft_bounce is called
+        mock_soft_bounce.assert_called()
+
+    @mock.patch("cl.users.models.handle_complaint")
+    def test_complaint_signal(self, mock_complaint) -> None:
+        """This test checks if handle_complaint function is called
+        when a complaint event is received
+        """
+        # Trigger a complaint event
+        self.send_signal(
+            self.complaint_asset, "complaint", signals.complaint_received
+        )
+        # Check if handle_complaint is called
+        mock_complaint.assert_called()
+
+    @mock.patch("cl.users.models.handle_delivery")
+    def test_delivery_signal(self, mock_delivery) -> None:
+        """This test checks if handle_delivery function is called
+        when a delivery event is received
+        """
+        # Trigger a delivery event
+        self.send_signal(
+            self.delivery_asset, "delivery", signals.delivery_received
+        )
+
+        # Check if handle_delivery is called
+        mock_delivery.assert_called()
+
+    def test_handle_soft_bounce_create_small_only(self) -> None:
+        """This test checks if a small_email_only flag is created for
+        an email address if it doesn't exist previously when a
+        small_email_only soft bounce is received
+        """
+
+        email_flag_exists = EmailFlag.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+            flag_type="flag",
+            flag="small_email_only",
+        ).exists()
+        self.assertEqual(email_flag_exists, False)
+
+        # Trigger a small_email_only soft bounce
+        self.send_signal(
+            self.soft_bounce_msg_large_asset, "bounce", signals.bounce_received
+        )
+
+        email_flag_exists = EmailFlag.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+            flag_type="flag",
+            flag="small_email_only",
+        ).exists()
+        # Check if small_email_only was created
+        self.assertEqual(email_flag_exists, True)
+
+    def test_handle_soft_bounce_small_only_exist(self) -> None:
+        """This test checks if a small_email_only flag is not created for
+        an email address if it exists previously when a
+        small_email_only soft bounce is received
+        """
+        # Create a small_email_only flag
+        email_flag_count = EmailFlag.objects.create(
+            email_address="bounce@simulator.amazonses.com",
+            flag_type="flag",
+            flag="small_email_only",
+            reason="MessageTooLarge",
+        )
+        email_flag_count = EmailFlag.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+            flag_type="flag",
+            flag="small_email_only",
+        ).count()
+        self.assertEqual(email_flag_count, 1)
+
+        # Trigger a small_email_only soft bounce
+        self.send_signal(
+            self.soft_bounce_msg_large_asset, "bounce", signals.bounce_received
+        )
+
+        email_flag_count = EmailFlag.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+            flag_type="flag",
+            flag="small_email_only",
+        ).count()
+
+        # Check if new small_email_only was not created
+        self.assertEqual(email_flag_count, 1)
+
+    @mock.patch("cl.users.models.logging")
+    def test_handle_soft_bounce_unexpected(self, mock_logging) -> None:
+        """This test checks if a warning is logged when a
+        unexpected bounceSubType event is received
+        """
+        # Trigger a content_rejected event
+        self.send_signal(
+            self.soft_bounce_cnt_rejected_asset,
+            "bounce",
+            signals.bounce_received,
+        )
+        # Check if warning is logged
+        warning_part_one = "Unexpected ContentRejected soft bounce for "
+        warning_part_two = "bounce@simulator.amazonses.com"
+        mock_logging.warning.assert_called_with(
+            f"{warning_part_one}{warning_part_two}"
+        )
+
+    def test_handle_soft_bounce_create_back_off(self) -> None:
+        """This test checks if a back_off event is created for
+        an email address if it doesn't exist previously when a
+        backoff type soft bounce is received
+        """
+        email_backoff_exists = BackoffEvent.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+        ).exists()
+
+        self.assertEqual(email_backoff_exists, False)
+        # Trigger a backoff notification event
+        self.send_signal(
+            self.soft_bounce_asset, "bounce", signals.bounce_received
+        )
+
+        email_backoff_exists = BackoffEvent.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+        ).exists()
+        # Check if backoff event was created
+        self.assertEqual(email_backoff_exists, True)
+
+    def test_handle_soft_bounce_waiting_back_off(self) -> None:
+        """This test checks if a back_off event exists for
+        an email address and is under waiting period,
+        backoff event is not updated
+        """
+        # Trigger first backoff notification event
+        self.send_signal(
+            self.soft_bounce_asset, "bounce", signals.bounce_received
+        )
+        email_backoff_event = BackoffEvent.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+        )
+        email_backoff_exists = email_backoff_event.exists()
+        # Check if backoff event was created
+        self.assertEqual(email_backoff_exists, True)
+
+        if email_backoff_exists:
+            # Store parameters from backoff event
+            retry_counter_before = email_backoff_event[0].retry_counter
+            next_retry_date_before = email_backoff_event[0].next_retry_date
+
+        # Trigger second backoff notification event
+        self.send_signal(
+            self.soft_bounce_asset, "bounce", signals.bounce_received
+        )
+
+        email_backoff_event_after = BackoffEvent.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+        )
+        # Store parameters after second backoff notification event
+        retry_counter_after = email_backoff_event_after[0].retry_counter
+        next_retry_date_after = email_backoff_event_after[0].next_retry_date
+
+        # Check parameters were not updated
+        self.assertEqual(retry_counter_before, retry_counter_after)
+        self.assertEqual(next_retry_date_before, next_retry_date_after)
+
+    def test_handle_soft_bounce_not_waiting_back_off(self) -> None:
+        """This test checks if a back_off event exists for
+        an email address and is not under waiting period and,
+        max_retry has not been reached, backoff event is updated
+        """
+        # Trigger first backoff notification event
+        self.send_signal(
+            self.soft_bounce_asset, "bounce", signals.bounce_received
+        )
+        # Update next_retry_date to expire waiting time
+        BackoffEvent.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+        ).update(next_retry_date=now() - timedelta(hours=3))
+
+        email_ban_event = BackoffEvent.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+        )
+        # Store parameters after first backoff event
+        retry_counter_before = email_ban_event[0].retry_counter
+        next_retry_date_before = email_ban_event[0].next_retry_date
+        # Trigger second backoff notification event
+        self.send_signal(
+            self.soft_bounce_asset, "bounce", signals.bounce_received
+        )
+        email_ban_event_after = BackoffEvent.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+        )
+        # Store parameters after second backoff notification event
+        retry_counter_after = email_ban_event_after[0].retry_counter
+        next_retry_date_after = email_ban_event_after[0].next_retry_date
+
+        # Check parameters were updated
+        self.assertNotEqual(retry_counter_before, retry_counter_after)
+        self.assertNotEqual(next_retry_date_before, next_retry_date_after)
+
+    def test_handle_soft_bounce_not_max_retry_reached(self) -> None:
+        """This test checks if a back_off event exists for
+        an email address and is not under waiting period and,
+        max_retry has been reached, email address is banned
+        """
+        # Trigger first backoff notification event
+        self.send_signal(
+            self.soft_bounce_asset, "bounce", signals.bounce_received
+        )
+        email_ban_exist = EmailFlag.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+            flag_type="ban",
+        ).exists()
+        # Check email address is not banned
+        self.assertEqual(email_ban_exist, False)
+
+        # Update next_retry_date to expire waiting time and retry_counter to
+        # reach max_retry_counter
+        BackoffEvent.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+        ).update(next_retry_date=now() - timedelta(hours=3), retry_counter=5)
+        # Trigger second backoff notification event
+        self.send_signal(
+            self.soft_bounce_asset, "bounce", signals.bounce_received
+        )
+        email_ban_exist = EmailFlag.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+            flag_type="ban",
+        ).exists()
+        # Check email address is now banned
+        self.assertEqual(email_ban_exist, True)
+
+    def test_handle_soft_bounce_max_retry_reached(self) -> None:
+        """This test checks if the exponential waiting period
+        is computed properly
+        """
+        # Trigger first backoff notification event
+        self.send_signal(
+            self.soft_bounce_asset, "bounce", signals.bounce_received
+        )
+        # Update next_retry_date to expire waiting time and retry_counter to 4
+        BackoffEvent.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+        ).update(next_retry_date=now() - timedelta(hours=3), retry_counter=4)
+
+        email_ban_event = BackoffEvent.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+        )
+        # Store parameter after backoff event update
+        retry_counter_before = email_ban_event[0].retry_counter
+        # Trigger second backoff notification event
+        self.send_signal(
+            self.soft_bounce_asset, "bounce", signals.bounce_received
+        )
+        email_ban_event_after = BackoffEvent.objects.filter(
+            email_address="bounce@simulator.amazonses.com",
+        )
+        # Store parametes after second backoff event update
+        retry_counter_after = email_ban_event_after[0].retry_counter
+        next_retry_date_after = email_ban_event_after[0].next_retry_date
+
+        # Hours expected for the final backoff waiting period
+        expected_waiting_period = 64
+
+        # Obtain waiting period in hours after backoff notification
+        waiting_period = next_retry_date_after - now()
+        actual_waiting_period = round(waiting_period.total_seconds() / 3600)
+
+        # Check retry counter is updated
+        self.assertNotEqual(retry_counter_before, retry_counter_after)
+        # Check expected waiting period equals to computed waiting period
+        self.assertEqual(expected_waiting_period, actual_waiting_period)
