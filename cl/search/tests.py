ce2b1f1b6d30b123d125ed34726ea1c118b74d84
mlnewman <marion.newman@gmail.com>
2022-03-15 22:41:40 -0400

    Add unit tests for advanced search operators.

--- cl/search/tests.py
+++ cl/search/tests.py
@@ -1201,7 +1201,6 @@ class PagerankTest(TestCase):
                 "%s" % (key, pr_results[key], answers[key]),
             )
 
-
 class OpinionSearchFunctionalTest(BaseSeleniumTest):
     """
     Test some of the primary search functionality of CL: searching opinions.
