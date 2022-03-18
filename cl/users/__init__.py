4fe1bcf585c90a58172ecad0e2009eccbd041db9
Alberto <albertisfu@gmail.com>
2022-03-18 09:52:14 -0600

    fix(email): Solves changes requested
    - Added admin files for email models
    - Moved email logic and signals outside of the models
    - Rebased with main, (poetry.lock) conflicts solved
    - Updates and improvements in email models
    - Other Changes requested solved

---
+++ cl/users/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "cl.users.apps.UsersConfig"
--- cl/users/migrations/0005_add_backoff_event.sql
+++ cl/users/migrations/0005_add_backoff_event.sql
@@ -2,9 +2,11 @@ BEGIN;
 --
 -- Create model BackoffEvent
 --
-CREATE TABLE "users_backoffevent" ("id" serial NOT NULL PRIMARY KEY, "email_address" varchar(254) NOT NULL, "retry_counter" smallint NOT NULL, "next_retry_date" timestamp with time zone NOT NULL, "date_created" timestamp with time zone NOT NULL);
+CREATE TABLE "users_backoffevent" ("id" serial NOT NULL PRIMARY KEY, "date_created" timestamp with time zone NOT NULL, "date_modified" timestamp with time zone NOT NULL, "email_address" varchar(254) NOT NULL, "retry_counter" smallint NOT NULL, "next_retry_date" timestamp with time zone NOT NULL);
 --
 -- Create index users_backo_email_a_f71b7f_idx on field(s) email_address of model backoffevent
 --
 CREATE INDEX "users_backo_email_a_f71b7f_idx" ON "users_backoffevent" ("email_address");
+CREATE INDEX "users_backoffevent_date_created_ee8b632d" ON "users_backoffevent" ("date_created");
+CREATE INDEX "users_backoffevent_date_modified_413b2021" ON "users_backoffevent" ("date_modified");
 COMMIT;
