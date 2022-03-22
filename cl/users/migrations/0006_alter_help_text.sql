612d1b55bf23a95dc645fbf03270e2c7e2aa3c10
Alberto <albertisfu@gmail.com>
2022-03-22 13:10:47 -0600

    fix(email): Fixes comments on models

---
+++ cl/users/migrations/0006_alter_help_text.sql
@@ -0,0 +1,20 @@
+BEGIN;
+--
+-- Alter field email_address on backoffevent
+--
+--
+-- Alter field next_retry_date on backoffevent
+--
+--
+-- Alter field email_address on emailflag
+--
+--
+-- Alter field event_sub_type on emailflag
+--
+--
+-- Alter field flag on emailflag
+--
+--
+-- Alter field object_type on emailflag
+--
+COMMIT;
