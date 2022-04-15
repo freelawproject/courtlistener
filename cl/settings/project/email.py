63f26130d4da6254385f5eedb0e572f46fa8ef8b
Alberto <albertisfu@gmail.com>
2022-04-14 22:40:29 -0500

    fix(email): Added new bcc settings

--- cl/settings/project/email.py
+++ cl/settings/project/email.py
@@ -15,6 +15,14 @@ else:
 # Max email attachment to send in bytes, 350KB
 MAX_ATTACHMENT_SIZE = 350_000
 
+# 1: Every message is BCC'ed
+# 0.5: Half of messages are BCC'ed
+# 0.1: 10% of messages are BCC'ed
+# 0: No messages are BCC'ed
+EMAIL_BCC_COPY_RATE = 0.1
+
+BCC_EMAIL_ADDRESS = "bcc@free.law"
+
 SERVER_EMAIL = "CourtListener <noreply@courtlistener.com>"
 DEFAULT_FROM_EMAIL = "CourtListener <noreply@courtlistener.com>"
 DEFAULT_ALERTS_EMAIL = "CourtListener Alerts <alerts@courtlistener.com>"
