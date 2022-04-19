d333a0195d82b666a22cbda72e071d8634e6efda
Michael Lissner <mlissner@michaeljaylissner.com>
2022-04-19 15:17:13 -0700

    feat(monitoring): Add health check url

--- cl/stats/urls.py
+++ cl/stats/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from cl.stats.views import celery_fail, redis_writes, sentry_fail
+from cl.stats.views import celery_fail, health_check, redis_writes, sentry_fail
 
 urlpatterns = [
+    path("monitoring/health-check/", health_check, name="health_check"),
     path("monitoring/redis-writes/", redis_writes, name="check_redis_writes"),
     path("sentry/error/", sentry_fail, name="sentry_fail"),
     path("sentry/celery-fail/", celery_fail, name="celery_fail"),
