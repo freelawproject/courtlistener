b1197b6181ec39608d5b246d6d8fe18fd63695cc
Michael Lissner <mlissner@michaeljaylissner.com>
2022-06-28 17:53:54 -0400

    feat(k8s): Delete nginx and celery files

--- docker/nginx/wsgi-configs/cl_wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ["DJANGO_SETTINGS_MODULE"] = "cl.settings"
-
-sys.path.append("/opt/courtlistener")
-application = get_wsgi_application()
