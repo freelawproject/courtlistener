19f81c256d4780f47706e87d0b513c0b75e4e9a8
Michael Lissner <mlissner@michaeljaylissner.com>
2022-07-05 18:44:36 -0400

    fix(users): Don't delete anybody that ever logged in
    Fixes: #2140

--- cl/users/management/commands/cl_account_management.py
+++ cl/users/management/commands/cl_account_management.py
@@ -8,7 +8,7 @@ from cl.lib.command_utils import VerboseCommand
 from cl.lib.crypto import sha1_activation_key
 from cl.lib.types import EmailType
 from cl.users.models import UserProfile
-from cl.users.utils import emails
+from cl.users.utils import delete_user_assets, emails
 
 
 class Command(VerboseCommand):
@@ -57,29 +57,31 @@ class Command(VerboseCommand):
             print("* NO EMAILS SENT OR ACCOUNTS DELETED *")
             print("**************************************")
 
-    def delete_old_accounts(self):
-        """Find accounts older than roughly two months that have not been
-        confirmed, and delete them. Should be run once a month, or so.
+    def delete_old_accounts(self) -> None:
+        """Delete old, unused accounts
+
+        Delete accounts older than 60 days that lack a confirmed email
+        address and never logged in.
+
+        :return None
         """
         two_months_ago = now() - datetime.timedelta(60)
         unconfirmed_ups = UserProfile.objects.filter(
             email_confirmed=False,
             user__date_joined__lte=two_months_ago,
+            user__last_login=None,
             stub_account=False,
         )
 
         for up in unconfirmed_ups:
+            if self.options["simulate"]:
+                return
+
+            delete_user_assets(up.user)
+            up.user.delete()
+            up.delete()
             if self.options["verbose"]:
                 print(f"User {up.user.username} deleted")
-            if not self.options["simulate"]:
-                # Gather their foreign keys, delete those
-                up.user.alerts.all().delete()
-                up.user.donations.all().delete()
-                up.user.favorites.all().delete()
-
-                # delete the user then the profile.
-                up.user.delete()
-                up.delete()
 
     def notify_unconfirmed_accounts(self):
         """This function will notify people who have not confirmed their
