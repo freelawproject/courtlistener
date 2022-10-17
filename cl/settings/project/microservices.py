ebae16cda00cf47757bd60297125d6a3c61ae917
Alberto Islas <albertisfu@gmail.com>
2022-10-17 09:41:44 -0500

    fix(microservices): Fixes OCR document extraction for opinions
    Also:
    - Tweaked extract_recap_pdf to avoid logging retries to sentry
    - Added a new URL document-extract-ocr with increased timeout for OCR extraction

--- cl/settings/project/microservices.py
+++ cl/settings/project/microservices.py
@@ -16,6 +16,10 @@ MICROSERVICE_URLS = {
         "url": f"{DOCTOR_HOST}/extract/doc/text/",
         "timeout": 60 * 15,
     },
+    "document-extract-ocr": {
+        "url": f"{DOCTOR_HOST}/extract/doc/text/",
+        "timeout": 60 * 90,
+    },
     # Utils Endpoints
     "page-count": {
         "url": f"{DOCTOR_HOST}/utils/page-count/pdf/",
