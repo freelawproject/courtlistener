5458251a73f0e2d94355954bcf0fae56e246eae3
Kevin Ramirez <kvnzavalza@hotmail.com>
2022-12-05 09:08:34 -0600

    fix reporter mismatch warning for harvard opinions importer

--- cl/corpus_importer/management/commands/harvard_opinions.py
+++ cl/corpus_importer/management/commands/harvard_opinions.py
@@ -579,18 +579,31 @@ def add_citations(cites: List[CitationType], cluster_id: int) -> None:
             cite_type_str = citation[0].all_editions[0].reporter.cite_type
             reporter_type = map_reporter_db_cite_type(cite_type_str)
 
-        try:
-            Citation.objects.get_or_create(
-                volume=citation[0].groups["volume"],
-                reporter=citation[0].corrected_reporter(),
-                page=citation[0].groups["page"],
-                type=reporter_type,
-                cluster_id=cluster_id,
-            )
-        except IntegrityError:
-            logger.warning(
-                f"Reporter mismatch for cluster: {cluster_id} on cite: {cite['cite']}"
-            )
+        # We can only get one result due model constraints
+        citation_obj = Citation.objects.filter(
+            volume=citation[0].groups["volume"],
+            reporter=citation[0].corrected_reporter(),
+            page=citation[0].groups["page"],
+            cluster_id=cluster_id,
+        ).first()
+
+        if citation_obj:
+            if citation_obj.type != reporter_type:
+                # Update reporter type if this changed in reporters-db
+                citation_obj.type = reporter_type
+                citation_obj.save()
+        else:
+            try:
+                citation_obj = Citation.objects.create(
+                    volume=citation[0].groups["volume"],
+                    reporter=citation[0].corrected_reporter(),
+                    page=citation[0].groups["page"],
+                    cluster_id=cluster_id,
+                    type=reporter_type
+                )
+            except IntegrityError:
+                logger.warning(
+                    f"Reporter mismatch for cluster: {cluster_id} on cite: {cite['cite']}")
 
 
 def add_opinions(
@@ -1144,7 +1157,7 @@ class Command(VerboseCommand):
             required=False,
             type=_argparse_volumes,
             help="Ex. '2:10' will fetch volumes 2 to 10 inclusive;"
-            "'1:' will start at 1 and to 2000; '5' will do volume 5",
+                 "'1:' will start at 1 and to 2000; '5' will do volume 5",
         )
         parser.add_argument(
             "--reporter",
@@ -1169,7 +1182,7 @@ class Command(VerboseCommand):
             "--location",
             type=str,
             help="The location of the court (if applicable) ex. Florida"
-            "for courts-db differentiation.",
+                 "for courts-db differentiation.",
             required=False,
             default=None,
         )
@@ -1177,13 +1190,13 @@ class Command(VerboseCommand):
             "--make-searchable",
             action="store_true",
             help="Add items to solr as we create opinions. "
-            "Items are not searchable unless flag is raised.",
+                 "Items are not searchable unless flag is raised.",
         )
         parser.add_argument(
             "--bankruptcy",
             action="store_true",
             help="Tells function to use bankruptcy courts for bankruptcy "
-            "cases.",
+                 "cases.",
         )
         parser.add_argument(
             "--no-debug",
