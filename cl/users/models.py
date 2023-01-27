0abadd039e26595585200754199d92f2d4d49049
Kevin Ramirez <kvnzavalza@hotmail.com>
2023-01-27 13:19:44 -0600

    feat(django.contrib.auth.models): Add tracking for django auth models

--- cl/users/models.py
+++ cl/users/models.py
@@ -6,7 +6,7 @@ from typing import Dict
 
 import pghistory
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group, Permission
 from django.contrib.postgres.fields import ArrayField
 from django.core.exceptions import FieldError
 from django.core.mail import EmailMessage, EmailMultiAlternatives
@@ -40,7 +40,9 @@ class BarMembership(models.Model):
         ordering = ["barMembership"]
 
 
-@pghistory.track(pghistory.Snapshot())
+@pghistory.track(
+    pghistory.Snapshot(),
+)
 class UserProfile(models.Model):
     user = models.OneToOneField(
         User,
@@ -131,9 +133,9 @@ class UserProfile(models.Model):
     )
     auto_subscribe = models.BooleanField(
         help_text="If enabled, for every new case that comes in from the "
-        "user's recap.email address, a new docket subscription for the case "
-        "will be created; if disabled we'll ask users if they want to "
-        "subscribe to the case.",
+                  "user's recap.email address, a new docket subscription for the case "
+                  "will be created; if disabled we'll ask users if they want to "
+                  "subscribe to the case.",
         default=True,
     )
 
@@ -285,8 +287,8 @@ class EmailFlag(AbstractDateTimeModel):
     )
     flag_type = models.SmallIntegerField(
         help_text="The flag type assigned, "
-        "Email ban: ban an email address and avoid sending any email. "
-        "Email backoff event: an active backoff event.",
+                  "Email ban: ban an email address and avoid sending any email. "
+                  "Email backoff event: an active backoff event.",
         choices=FLAG_TYPES.TYPES,
         default=FLAG_TYPES.BAN,
     )
@@ -307,7 +309,7 @@ class EmailFlag(AbstractDateTimeModel):
     )
     checked = models.DateTimeField(
         help_text="The datetime the recipient's deliverability was checked"
-        " since the last bounce event.",
+                  " since the last bounce event.",
         blank=True,
         null=True,
     )
@@ -353,9 +355,9 @@ class EmailSent(AbstractDateTimeModel):
     user = models.ForeignKey(
         User,
         help_text="The user that this message is related to in case of users "
-        "change their email address we can send failed email to the user's "
-        "new email address, this is optional in case we send email to an"
-        "email address that doesn't belong to a CL user.",
+                  "change their email address we can send failed email to the user's "
+                  "new email address, this is optional in case we send email to an"
+                  "email address that doesn't belong to a CL user.",
         related_name="emails",
         on_delete=models.CASCADE,
         blank=True,
@@ -518,3 +520,66 @@ def generate_recap_email(user_profile: UserProfile, append: int = None) -> str:
     elif len(user_profiles_with_match) > 0:
         return generate_recap_email(user_profile, (append or 0) + 1)
     return recap_email
+
+
+@pghistory.track(
+    pghistory.Snapshot(),
+)
+class UserProxy(User):
+    """A proxy model class to track auth user model"""
+
+    class Meta:
+        proxy = True
+
+
+@pghistory.track(
+    pghistory.Snapshot(),
+)
+class GroupProxy(Group):
+    """A proxy model class to track auth group model"""
+
+    class Meta:
+        proxy = True
+
+
+@pghistory.track(
+    pghistory.Snapshot(),
+)
+class PermissionProxy(Permission):
+    """A proxy model class to track auth permission model"""
+
+    class Meta:
+        proxy = True
+
+
+@pghistory.track(
+    pghistory.Snapshot(),
+    obj_field=None,
+)
+class GroupPermissions(Group.permissions.through):
+    """A proxy model class to track group permissions m2m relation"""
+
+    class Meta:
+        proxy = True
+
+
+@pghistory.track(
+    pghistory.Snapshot(),
+    obj_field=None,
+)
+class UserGroups(User.groups.through):
+    """A proxy model class to track user groups m2m relation"""
+
+    class Meta:
+        proxy = True
+
+
+@pghistory.track(
+    pghistory.Snapshot(),
+    obj_field=None,
+)
+class UserPermissions(User.user_permissions.through):
+    """A proxy model class to track user permissions m2m relation"""
+
+    class Meta:
+        proxy = True
