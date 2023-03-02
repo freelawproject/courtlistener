d0e2d6605ee96aa0151a5a64edf98bf57204d46e
Michael Lissner <mlissner@michaeljaylissner.com>
2023-03-02 07:31:19 -0800

    fix(typing): Fix improper import

--- cl/alerts/views.py
+++ cl/alerts/views.py
@@ -1,8 +1,13 @@
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed
-from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+    HttpResponseNotAllowed,
+    HttpResponseRedirect,
+)
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
 
