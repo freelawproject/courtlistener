6f3d7cd4e0d99249134f8cb91c50df4725b43987
Kevin Ramirez <kvnzavalza@hotmail.com>
2023-04-07 12:55:49 -0600

    fix(citations): update log message in lexis citation merger

--- cl/citations/management/commands/lexis_citation_merger.py
+++ cl/citations/management/commands/lexis_citation_merger.py
@@ -440,7 +440,7 @@ def find_cases_with_citations(
         # Possible duplicate cases, we still add citations to cases that
         # match the search criteria
         logger.warning(
-            f"(Westlaw) Possible duplicated cases with ids: {','.join(map(str, results_ids))}"
+            f"(Lexis) Possible duplicated cases with ids: {','.join(map(str, results_ids))}"
         )
 
     # Return a queryset of all the possible cases matched by filter and case
