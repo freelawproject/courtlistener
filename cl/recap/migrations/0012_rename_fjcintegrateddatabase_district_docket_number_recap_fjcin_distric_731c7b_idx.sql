16c1e356e1910b3773c5ce0c7af6307dd8300509
ttys0dev <126845556+ttys0dev@users.noreply.github.com>
2023-05-06 17:04:55 -0600

    Update recap migration sql to match production schema

--- cl/recap/migrations/0012_rename_fjcintegrateddatabase_district_docket_number_recap_fjcin_distric_731c7b_idx.sql
+++ cl/recap/migrations/0012_rename_fjcintegrateddatabase_district_docket_number_recap_fjcin_distric_731c7b_idx.sql
@@ -2,5 +2,5 @@ BEGIN;
 --
 -- Rename unnamed index for ('district', 'docket_number') on fjcintegrateddatabase to recap_fjcin_distric_731c7b_idx
 --
-ALTER INDEX "recap_fjcintegrateddatab_district_id_docket_numbe_714d6d64_idx" RENAME TO "recap_fjcin_distric_731c7b_idx";
+ALTER INDEX "recap_fjcintegrateddatabase_district_id_455568623a9da568_idx" RENAME TO "recap_fjcin_distric_731c7b_idx";
 COMMIT;
