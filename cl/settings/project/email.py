8ddaf1bc4fb4af78e5cba3387143e87211188ce5
Eduardo Rosendo <eduardojra96@gmail.com>
2023-06-12 17:57:10 -0400

    feat(setting): Add a new setting to limit the number of sent emails

--- cl/settings/project/email.py
+++ cl/settings/project/email.py
@@ -26,6 +26,9 @@ BACKOFF_THRESHOLD = 36
 # Hours after a backoff event expired to check if there was a new bounce event.
 DELIVERABILITY_THRESHOLD = 2
 
+# Number of emails that can be sent per 24-hour period
+SENT_EMAILS_THRESHOLD = env.int("SENT_EMAILS_THRESHOLD", default=50_000)
+
 SERVER_EMAIL = "CourtListener <noreply@courtlistener.com>"
 DEFAULT_FROM_EMAIL = "CourtListener <noreply@courtlistener.com>"
 DEFAULT_ALERTS_EMAIL = "CourtListener Alerts <alerts@courtlistener.com>"
