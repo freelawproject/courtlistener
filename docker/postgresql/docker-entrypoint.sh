1bfe53721be1b297a813892f5b05a62e9d542703
Alberto Islas <albertisfu@gmail.com>
2023-06-21 18:52:16 -0600

    fix(docker): Fixes postgresql.conf overwriting order

--- docker/postgresql/docker-entrypoint.sh
+++ docker/postgresql/docker-entrypoint.sh
@@ -12,19 +12,5 @@ if [ ! -f $SSL_CERT_DIR/server.crt ]; then
   chmod 600 $SSL_CERT_DIR/server.req $SSL_CERT_DIR/server.crt
 fi
 
-# Run PostgreSQL and capture the PID to wait for it later
-docker-entrypoint.sh postgres &
-POSTGRES_PID=$!
-
-# Wait for PostgreSQL to initialize and the configuration file to appear, so it can be modified.
-while [ ! -f /var/lib/postgresql/data/postgresql.conf ]; do
-  sleep 1
-done
-
-# Update the PostgreSQL configuration to add SSL support and use self-signed SSL certificate
-sed -i "s|^#ssl = off|ssl = on|" /var/lib/postgresql/data/postgresql.conf
-sed -i "s|^#ssl_cert_file =.*|ssl_cert_file = '/var/lib/postgresql/ssl/server.crt'|" /var/lib/postgresql/data/postgresql.conf
-sed -i "s|^#ssl_key_file =.*|ssl_key_file = '/var/lib/postgresql/ssl/server.req'|" /var/lib/postgresql/data/postgresql.conf
-
-# Wait for the PostgreSQL initialization command to complete
-wait $POSTGRES_PID
+# Run PostgreSQL
+docker-entrypoint.sh postgres
