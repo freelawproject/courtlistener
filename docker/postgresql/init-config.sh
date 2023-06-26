b1d34e134634cbb89ad2a39850166c28f6b0a449
Alberto Islas <albertisfu@gmail.com>
2023-06-26 17:00:54 -0600

    fix(docker): Use a pre-generated self-signed certificate
    - Set PGSSLCERT env var in Dockerfile

--- docker/postgresql/init-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Update the PostgreSQL configuration to add SSL support and use self-signed SSL certificate
-sed -i "s|^#ssl = off|ssl = on|" /var/lib/postgresql/data/postgresql.conf
-sed -i "s|^#ssl_cert_file =.*|ssl_cert_file = '/var/lib/postgresql/ssl/server.crt'|" /var/lib/postgresql/data/postgresql.conf
-sed -i "s|^#ssl_key_file =.*|ssl_key_file = '/var/lib/postgresql/ssl/server.req'|" /var/lib/postgresql/data/postgresql.conf
