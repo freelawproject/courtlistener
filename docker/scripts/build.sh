0d4f96993b71ec779f13b2c86a5bc0c8ad3c9eb3
ss108 <samier_saeed@protonmail.com>
2023-07-11 15:33:15 -0700

    rm commented code from shell scripts

--- docker/scripts/build.sh
+++ docker/scripts/build.sh
@@ -3,16 +3,8 @@
 # Build the docker images
 BUILD_ENV=dev
 SHA=`git rev-parse HEAD`
-#DJANGO_DOCKER_IMAGE="localhost:5000/freelawproject/courtlistener:latest-web-dev-${SHA}"
-#CELERY_DOCKER_IMAGE="localhost:5000/freelawproject/courtlistener:latest-celery-${SHA}"
-#export DJANGO_DOCKER_IMAGE CELERY_DOCKER_IMAGE BUILD_ENV
 make -e VERSION=${SHA} -f ../django/Makefile development
-#docker build --target web-dev --file docker/django/Dockerfile -t ${DJANGO_DOCKER_IMAGE} .
-#docker build --target celery --file docker/django/Dockerfile -t ${CELERY_DOCKER_IMAGE} .
-#docker push ${DJANGO_DOCKER_IMAGE}
-#docker push ${CELERY_DOCKER_IMAGE}
 docker network create -d bridge --attachable cl_net_overlay
-#cd docker/courtlistener
 docker-compose \
 	-f ../courtlistener/docker-compose.yml \
        	up -d
