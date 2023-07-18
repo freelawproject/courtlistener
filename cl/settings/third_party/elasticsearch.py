f5d6f722ce0db5cb3776ad338613c8b4a85a0856
Alberto Islas <albertisfu@gmail.com>
2023-07-17 18:27:48 -0600

    fix(elasticsearch): Tweaked the Elasticsearch connection settings for cluster and SSL support.

--- cl/settings/third_party/elasticsearch.py
+++ cl/settings/third_party/elasticsearch.py
@@ -2,11 +2,36 @@ import environ
 
 env = environ.FileAwareEnv()
 
-ELASTICSEARCH_DSL_HOST = env("ELASTICSEARCH_DSL_HOST", default="cl-es")
-ELASTICSEARCH_DSL_PORT = env("ELASTICSEARCH_DSL_PORT", default="9200")
+ELASTICSEARCH_DSL_HOST = env(
+    "ELASTICSEARCH_DSL_HOST",
+    default=[
+        "cl-es:9200",
+    ],
+)
+ELASTICSEARCH_USER = env(
+    "ELASTICSEARCH_USER",
+    default="elastic",
+)
+ELASTICSEARCH_PASSWORD = env(
+    "ELASTICSEARCH_PASSWORD",
+    default="password",
+)
+ELASTICSEARCH_CA_CERT = env(
+    "ELASTICSEARCH_CA_CERT",
+    default="/opt/courtlistener/docker/elastic/ca.crt",
+)
 
 ELASTICSEARCH_DSL = {
-    "default": {"hosts": f"{ELASTICSEARCH_DSL_HOST}:{ELASTICSEARCH_DSL_PORT}"},
+    "default": {
+        "hosts": ELASTICSEARCH_DSL_HOST,
+        "http_auth": (ELASTICSEARCH_USER, ELASTICSEARCH_PASSWORD),
+        "use_ssl": True,
+        "verify_certs": True,
+        "ca_certs": ELASTICSEARCH_CA_CERT,
+        "request_timeout": 30,
+        "retry_on_timeout": True,
+        "max_retries": 5,
+    },
 }
 
 ELASTICSEARCH_NUMBER_OF_SHARDS = env(
