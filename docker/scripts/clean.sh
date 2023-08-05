4d8da5e533bf6ccca5b2f94c5f33eb900817af98
ss108 <samier_saeed@protonmail.com>
2023-08-05 15:41:27 -0700

    actually use this for a clean script lol

--- docker/scripts/clean.sh
+++ docker/scripts/clean.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-# Run this shell script to purge/refresh CL-related Docker images when:
-# 1) Merging `main` into a working branch;
-# 2) Forking off of `main` to start a new working branch 
+# Run this shell script to rm existing CL Docker images and pull the latest, freshest ones. 
+# Use when:
+## 1) Merging `main` into a working branch;
+## 2) Forking off of `main` to start a new working branch 
 # Run it before using the build script. Usage of both of these shell scripts is optional.
 
-# Stop all running Courtlistener cointainers
-docker-compose -f  ../courtlistener/docker-compose.yml  down
+# https://stackoverflow.com/a/39127792/3256077
 
-# Remove any Docker images that are related to Courtlistener
-flp_images=`docker image ls | awk '/^freelawproject/{print $1}'`
-echo $flp_images
-docker image rm --force $flp_images
-docker image prune --force
-
-# Pull fresh copies of the Docker images for CL's various services
-for image in $flp_images; do
-    docker pull $image
-done
+docker-compose -f ../courtlistener/docker-compose.yml stop
+docker-compose -f ../courtlistener/docker-compose.yml rm --force
+docker rmi $(docker-compose -f ../courtlistener/docker-compose.yml config | awk '/freelawproject/{if ($1 == "image:") print $2;}')
+docker-compose -f ../courtlistener/docker-compose.yml pull   
