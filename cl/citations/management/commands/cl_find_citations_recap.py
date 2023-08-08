bf5155d6fb42e424ea8565f9cfeb59fa67eaef70
ss108 <samier_saeed@protonmail.com>
2023-08-08 08:24:10 -0700

    fixed botch indentation in management command

--- cl/citations/management/commands/cl_find_citations_recap.py
+++ cl/citations/management/commands/cl_find_citations_recap.py
@@ -66,115 +66,113 @@ class Command(VerboseCommand):
             help="The celery queue where the tasks should be processed.",
         )
 
-        def update_documents(self, pks: Iterable, queue_name: str):
-            sys.stdout.write(f"Graph size is {self.count:d} nodes.\n")
-            sys.stdout.flush()
-
-            chunk = []
-            chunk_size = 100
-            processed_count = 0
-            throttle = CeleryThrottle(queue_name=queue_name)
-
-            for pk in pks:
-                throttle.maybe_wait()
-                processed_count += 1
-                last_item = self.count == processed_count
-                chunk.append(pk)
-
-                if processed_count % chunk_size == 0 or last_item:
-                    find_citations_and_parantheticals_for_recap_documents.apply_async(
-                        args=(chunk),
-                        queue=queue_name,
-                    )
-                    chunk = []
-
-                self.log_progress(processed_count, pk)
-
-        def handle(self, *args: List[str], **options: OptionsType):
-            super(Command, self).handle(*args, **options)
-            both_list_and_endpoints = options.get("doc_id") is not None and (
-                options.get("start_id") is not None
-                or options.get("end_id") is not None
-                or options.get("filed_after") is not None
-                or options.get("filed_before") is not None
-                or options.get("modified_after") is not None
+    def log_progress(self, processed_count: int, last_pk: int):
+        if processed_count % 1000 == 1:
+            self.t1 = time.time()
+        if processed_count % 1000 == 0:
+            self.t2 = time.time()
+            self.timings.append(self.t2 - self.t1)
+            self.average_per_s = (
+                1000 / sum(self.timings) / float(len(self.timings))
             )
-            no_option = not any(
-                [
-                    options.get("doc_id") is None,
-                    options.get("start_id") is None,
-                    options.get("end_id") is None,
-                    options.get("filed_after") is None,
-                    options.get("filed_before") is None,
-                    options.get("modified_after") is None,
-                    options.get("all") is False,
-                ]
+
+        template = (
+            "\rProcessing items in Celery queue: {:.0%} ({}/{}, "
+            "{:.1f}/s, Last id: {})"
+        )
+
+        sys.stdout.write(
+            template.format(
+                float(processed_count) / self.count,  # Percent
+                processed_count,
+                self.count,
+                self.average_per_s,
+                last_pk,
             )
-            if both_list_and_endpoints or no_option:
-                raise CommandError(
-                    "Please specify either a list of documents, a "
-                    "range of ids, a range of dates, or "
-                    "everything."
+        )
+
+        sys.stdout.flush()
+
+    def update_documents(self, pks: Iterable, queue_name: str):
+        sys.stdout.write(f"Graph size is {self.count:d} nodes.\n")
+        sys.stdout.flush()
+
+        chunk = []
+        chunk_size = 100
+        processed_count = 0
+        throttle = CeleryThrottle(queue_name=queue_name)
+
+        for pk in pks:
+            throttle.maybe_wait()
+            processed_count += 1
+            last_item = self.count == processed_count
+            chunk.append(pk)
+
+            if processed_count % chunk_size == 0 or last_item:
+                find_citations_and_parantheticals_for_recap_documents.apply_async(
+                    args=(chunk),
+                    queue=queue_name,
                 )
+                chunk = []
 
-            self.index = options["index"]
-            query = RECAPDocument.objects.all().order_by("pk")
-            if options.get("doc_id"):
-                query = query.filter(pk__in=options["doc_id"])
-            if options.get("end_id"):
-                query = query.filter(pk__lte=options["end_id"])
-            if options.get("start_id"):
-                query = query.filter(pk__gte=options["start_id"])
+            self.log_progress(processed_count, pk)
 
-            if options.get("filed_after"):
-                query = query.filter(
-                    cluster__date_filed__gte=options["filed_after"]
-                )
+    def handle(self, *args: List[str], **options: OptionsType):
+        super(Command, self).handle(*args, **options)
+        both_list_and_endpoints = options.get("doc_id") is not None and (
+            options.get("start_id") is not None
+            or options.get("end_id") is not None
+            or options.get("filed_after") is not None
+            or options.get("filed_before") is not None
+            or options.get("modified_after") is not None
+        )
+        no_option = not any(
+            [
+                options.get("doc_id") is None,
+                options.get("start_id") is None,
+                options.get("end_id") is None,
+                options.get("filed_after") is None,
+                options.get("filed_before") is None,
+                options.get("modified_after") is None,
+                options.get("all") is False,
+            ]
+        )
+        if both_list_and_endpoints or no_option:
+            raise CommandError(
+                "Please specify either a list of documents, a "
+                "range of ids, a range of dates, or "
+                "everything."
+            )
 
-            if options.get("filed_before"):
-                query = query.filter(
-                    cluster__date_filed__lte=options["filed_before"]
-                )
+        self.index = options["index"]
+        query = RECAPDocument.objects.all().order_by("pk")
+        if options.get("doc_id"):
+            query = query.filter(pk__in=options["doc_id"])
+        if options.get("end_id"):
+            query = query.filter(pk__lte=options["end_id"])
+        if options.get("start_id"):
+            query = query.filter(pk__gte=options["start_id"])
 
-            if options.get("modified_after"):
-                query = query.filter(
-                    date_modified__gte=options["modified_after"]
-                )
+        if options.get("filed_after"):
+            query = query.filter(
+                cluster__date_filed__gte=options["filed_after"]
+            )
 
-            if options.get("all"):
-                query = RECAPDocument.objects.all()
+        if options.get("filed_before"):
+            query = query.filter(
+                cluster__date_filed__lte=options["filed_before"]
+            )
 
-            self.count = query.count()
-            self.average_per_s = 0.0
-            self.timings = []
+        if options.get("modified_after"):
+            query = query.filter(date_modified__gte=options["modified_after"])
 
-            document_pks = query.values_list("pk", flat=True).iterator()
-            queue = cast(str, options["queue"])
-            self.update_documents(document_pks, queue)
+        if options.get("all"):
+            query = RECAPDocument.objects.all()
 
-            def log_progreess(self, processed_count: int, last_pk: int):
-                if processed_count % 1000 == 1:
-                    self.t1 = time.time()
-                if processed_count % 1000 == 0:
-                    self.t2 = time.time()
-                    self.timings.append(self.t2 - self.t1)
-                    self.average_per_s = (
-                        1000 / sum(self.timings) / float(len(self.timings))
-                    )
+        self.count = query.count()
+        self.average_per_s = 0.0
+        self.timings: List[float] = []
 
-                template = (
-                    "\rProcessing items in Celery queue: {:.0%} ({}/{}, "
-                    "{:.1f}/s, Last id: {})"
-                )
-
-                sys.stdout.write(
-                    template.format(
-                        float(processed_count) / self.count,  # Percent
-                        processed_count,
-                        self.count,
-                        self.average_per_s,
-                        last_pk,
-                    )
-                )
-
-                sys.stdout.flush()
+        document_pks = query.values_list("pk", flat=True).iterator()
+        queue = cast(str, options["queue"])
+        self.update_documents(document_pks, queue)
