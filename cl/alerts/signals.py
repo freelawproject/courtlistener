b21c607dae7691b1c198e7238c6fb2ced675ce90
Eduardo Rosendo <eduardojra96@gmail.com>
2023-08-10 11:51:00 -0400

    feat(alerts): Use an early return in signals

--- cl/alerts/signals.py
+++ cl/alerts/signals.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from django.http import QueryDict
@@ -19,6 +20,8 @@ def create_or_update_alert_in_es_index(sender, instance=None, **kwargs):
     """Receiver function that gets called after an Alert instance is saved.
     This method creates or updates an Alert object in the AudioPercolator index
     """
+    if settings.ELASTICSEARCH_DISABLED:
+        return
 
     if f"type={SEARCH_TYPES.ORAL_ARGUMENT}" in instance.query:
         # Make a dict from the query string.
@@ -51,6 +54,9 @@ def remove_alert_from_es_index(sender, instance=None, **kwargs):
     """Receiver function that gets called after an Alert instance is deleted.
     This function removes Alert from the AudioPercolator index.
     """
+    if settings.ELASTICSEARCH_DISABLED:
+        return
+
     # Check if the document exists before deleting it
     if AudioPercolator.exists(id=instance.pk):
         doc = AudioPercolator.get(id=instance.pk)
