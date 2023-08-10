35437b0be03c9ae85e19d457927597ebf33b4fb4
ss108 <samier_saeed@protonmail.com>
2023-08-09 21:24:12 -0700

    rm test script

--- docker/scripts/test.sh
+++
@@ -1,5 +0,0 @@
-
-docker exec cl-doctor pip install x-ray
-docker exec -e SELENIUM_DEBUG=1 -e SELENIUM_TIMEOUT=30 cl-django python /opt/courtlistener/manage.py test cl --verbosity=2 --exclude-tag selenium --parallel
-docker exec -e SELENIUM_DEBUG=1 -e SELENIUM_TIMEOUT=120 cl-django python /opt/courtlistener/manage.py test cl --verbosity=2 --tag selenium --parallel
-
