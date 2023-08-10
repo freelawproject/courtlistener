949b311603bdb02fc35d3ee2bf2422b3aade36bf
Eduardo Rosendo <eduardojra96@gmail.com>
2023-08-10 14:15:18 -0400

    feat(search): Remove the array to register indexes

--- cl/search/es_indices.py
+++ cl/search/es_indices.py
@@ -24,12 +24,3 @@ oral_arguments_percolator_index.settings(
     number_of_replicas=settings.ELASTICSEARCH_NUMBER_OF_REPLICAS,
     analysis=settings.ELASTICSEARCH_DSL["analysis"],
 )
-
-
-# Register ES indices here. In order to create a unique name for each index for
-# testing purposes.
-es_indices_registered = [
-    parenthetical_group_index,
-    oral_arguments_index,
-    oral_arguments_percolator_index,
-]
