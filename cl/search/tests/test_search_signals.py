39999c6cb8820b1ad4b677cc5ff9d16e5f0c78fe
ss108 <samier_saeed@protonmail.com>
2023-09-01 10:03:12 -0700

    fixed test for recap doc save receiver

--- cl/search/tests/test_search_signals.py
+++ cl/search/tests/test_search_signals.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import Mock
 
 from django.db.models.signals import post_save
 
@@ -15,9 +15,10 @@ from cl.tests.cases import SimpleTestCase
 class RECAPDocumentSignalsTests(SimpleTestCase):
     def setUp(self):
         post_save.disconnect(handle_recap_doc_change, sender=RECAPDocument)
+        self.mock_receiver = Mock()
+        post_save.connect(self.mock_receiver, sender=RECAPDocument)
 
-    @patch("cl.search.signals.handle_recap_doc_change")
-    def test_recapdoc_save_emit_signal(self, mock_receiver):
+    def test_recapdoc_save_emit_signal(self):
         recap_doc = RECAPDocumentFactory.create(
             plain_text="In Fisher v. SD Protection Inc., 948 F.3d 593 (2d Cir. 2020), the Second Circuit held that in the context of settlement of FLSA and NYLL cases, which must be approved by the trial court in accordance with Cheeks v. Freeport Pancake House, Inc., 796 F.3d 199 (2d Cir. 2015), the district court abused its discretion in limiting the amount of recoverable fees to a percentage of the recovery by the successful plaintiffs. But also: sdjnfdsjnk. Fisher, 948 F.3d at 597.",
             ocr_status=RECAPDocument.OCR_UNNECESSARY,
@@ -25,4 +26,4 @@ class RECAPDocumentSignalsTests(SimpleTestCase):
         )
 
         recap_doc.save(update_fields=["ocr_status", "plain_text"])
-        self.assertTrue(mock_receiver.called)
+        self.assertTrue(self.mock_receiver.called)
