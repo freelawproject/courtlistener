cfb8e0b29a58326b238f4a5eecf82c3ae5af533f
Eduardo Rosendo <eduardojra96@gmail.com>
2023-09-20 16:42:02 -0400

    feat(search):Tweak the task to update child document by query
    This commit adds the logic to update the position documents using the UpdateByQuery method

--- cl/search/tasks.py
+++ cl/search/tasks.py
@@ -297,12 +297,9 @@ def update_child_documents_by_query(
     """
 
     s = es_document.search()
-    match es_document:
-        case PositionDocument:
-            s = s.query(
-                "parent_id", type="position_document", id=parent_instance.pk
-            )
-            main_doc = PersonDocument.get(id=parent_instance.pk)
+    if es_document is PositionDocument:
+        s = s.query("parent_id", type="position", id=parent_instance.pk)
+        main_doc = PersonDocument.get(id=parent_instance.pk)
 
     client = connections.get_connection()
     ubq = UpdateByQuery(using=client, index=es_document._index._name).query(
@@ -320,8 +317,6 @@ def update_child_documents_by_query(
 
             prepare_method = getattr(main_doc, f"prepare_{field_name}", None)
             if prepare_method:
-                # It needs to be ES doc no the class.
-                # we could get the docket es doc and use their prepare methods...
                 params[field_to_update] = prepare_method(parent_instance)
             else:
                 params[field_to_update] = getattr(
