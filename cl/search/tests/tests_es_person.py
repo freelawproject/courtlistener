15df5dda1857d2e50c07b9971a114cd4a1809eff
Eduardo Rosendo <eduardojra96@gmail.com>
2023-09-20 02:58:56 -0400

    tests(search): Update tests to check fields in position documents

--- cl/search/tests/tests_es_person.py
+++ cl/search/tests/tests_es_person.py
@@ -1223,3 +1223,37 @@ class PeopleSearchTestElasticSearch(
         )
         name_full_reverse = person_2.name_full_reverse
         self.assertEqual(name_full_reverse, pos_doc.appointer)
+        self.assertEqual(name_full_reverse, pos_doc.supervisor)
+        self.assertEqual(name_full_reverse, pos_doc.predecessor)
+
+        self.person_3.dob_city = "Brookyln"
+        self.person_3.save()
+
+        pos_doc = PositionDocument.get(
+            id=PEOPLE_DOCS_TYPE_ID(position_6.pk).POSITION
+        )
+        self.assertEqual("Brookyln", pos_doc.dob_city)
+
+        self.person_3.dob_city = "Brookyln"
+        self.person_3.save()
+
+        pos_doc = PositionDocument.get(
+            id=PEOPLE_DOCS_TYPE_ID(position_6.pk).POSITION
+        )
+        self.assertEqual("Brookyln", pos_doc.dob_city)
+
+        self.person_3.dob_state = "AL"
+        self.person_3.save()
+
+        pos_doc = PositionDocument.get(
+            id=PEOPLE_DOCS_TYPE_ID(position_6.pk).POSITION
+        )
+        self.assertEqual("Alabama", pos_doc.dob_state)
+
+        self.person_3.gender = "m"
+        self.person_3.save()
+
+        pos_doc = PositionDocument.get(
+            id=PEOPLE_DOCS_TYPE_ID(position_6.pk).POSITION
+        )
+        self.assertEqual("Male", pos_doc.gender)
