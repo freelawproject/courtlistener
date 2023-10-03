627e71b2b9f8f886cd9fdf6e17500410b1f3a9d7
Alberto Islas <albertisfu@gmail.com>
2023-10-03 16:25:23 -0300

    fix(search): Fixed migration conflicts after branch update.

--- cl/search/migrations/0023_search_models_update_customers.sql
+++ cl/search/migrations/0023_search_models_update_customers.sql
@@ -1,25 +1,14 @@
 BEGIN;
-
 --
 -- Add field acms_document_guid to claimhistory
 --
 ALTER TABLE "search_claimhistory" ADD COLUMN "acms_document_guid" varchar(64) DEFAULT '' NOT NULL;
 ALTER TABLE "search_claimhistory" ALTER COLUMN "acms_document_guid" DROP DEFAULT;
 --
--- Add field acms_document_guid to claimhistoryevent
---
-ALTER TABLE "search_claimhistoryevent" ADD COLUMN "acms_document_guid" varchar(64) DEFAULT '' NOT NULL;
-ALTER TABLE "search_claimhistoryevent" ALTER COLUMN "acms_document_guid" DROP DEFAULT;
---
 -- Add field acms_document_guid to recapdocument
 --
 ALTER TABLE "search_recapdocument" ADD COLUMN "acms_document_guid" varchar(64) DEFAULT '' NOT NULL;
 ALTER TABLE "search_recapdocument" ALTER COLUMN "acms_document_guid" DROP DEFAULT;
---
--- Add field acms_document_guid to recapdocumentevent
---
-ALTER TABLE "search_recapdocumentevent" ADD COLUMN "acms_document_guid" varchar(64) DEFAULT '' NOT NULL;
-ALTER TABLE "search_recapdocumentevent" ALTER COLUMN "acms_document_guid" DROP DEFAULT;
 --
 -- Alter field pacer_case_id on claimhistory
 --
@@ -31,19 +20,7 @@ ALTER TABLE "search_claimhistory" ALTER COLUMN "pacer_doc_id" TYPE varchar(64);
 CREATE INDEX "search_claimhistory_pacer_doc_id_ddcc4bdf" ON "search_claimhistory" ("pacer_doc_id");
 CREATE INDEX "search_claimhistory_pacer_doc_id_ddcc4bdf_like" ON "search_claimhistory" ("pacer_doc_id" varchar_pattern_ops);
 --
--- Alter field pacer_case_id on claimhistoryevent
---
--- (no-op)
---
--- Alter field pacer_doc_id on claimhistoryevent
---
-ALTER TABLE "search_claimhistoryevent" ALTER COLUMN "pacer_doc_id" TYPE varchar(64);
---
 -- Alter field pacer_case_id on docket
---
--- (no-op)
---
--- Alter field pacer_case_id on docketevent
 --
 -- (no-op)
 --
@@ -52,9 +29,4 @@ ALTER TABLE "search_claimhistoryevent" ALTER COLUMN "pacer_doc_id" TYPE varchar(
 ALTER TABLE "search_recapdocument" ALTER COLUMN "pacer_doc_id" TYPE varchar(64);
 CREATE INDEX "search_recapdocument_pacer_doc_id_e52314d9" ON "search_recapdocument" ("pacer_doc_id");
 CREATE INDEX "search_recapdocument_pacer_doc_id_e52314d9_like" ON "search_recapdocument" ("pacer_doc_id" varchar_pattern_ops);
---
--- Alter field pacer_doc_id on recapdocumentevent
---
-ALTER TABLE "search_recapdocumentevent" ALTER COLUMN "pacer_doc_id" TYPE varchar(64);
-
 COMMIT;
