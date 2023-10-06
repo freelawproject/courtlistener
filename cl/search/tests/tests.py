79f9fdaa7712ddeb807e4ebd81a9080117629500
Eduardo Rosendo <eduardojra96@gmail.com>
2023-10-06 12:32:19 -0400

    tests(opinion): Add command to build ES index for functional tests

--- cl/search/tests/tests.py
+++ cl/search/tests/tests.py
@@ -680,6 +680,7 @@ class OpinionSearchFunctionalTest(AudioTestCase, BaseSeleniumTest):
             user__username="pandora",
             user__password=make_password("password"),
         )
+        self.rebuild_index("search.OpinionCluster")
         super().setUp()
 
     def _perform_wildcard_search(self):
