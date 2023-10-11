49f7405b37d3b7f1fa642484b8da09b74cb13149
William Palin <bill@free.law>
2023-10-11 12:59:29 -0400

    fix(coverage-ops.js): Allow Mobile Charts
    Add javascript to update the data to use court
    id over name on small screens.
    Remove left margin
    Shorten left side group names
    Dynamically choose width based on length of longest label
    Update screen on resize
    
    Automatically hide expanded toc at size change below
    certain threshold

--- cl/simple_pages/static/js/coverage-op.js
+++ cl/simple_pages/static/js/coverage-op.js
@@ -12,16 +12,124 @@ document.body.addEventListener('htmx:configRequest', function (event) {
   }
 });
 
+function calculateWordWidth(word) {
+  //calculate the width we need for the right margin here.
+  const span = document.createElement('text');
+  span.style.fontSize = '12px';
+  span.style.fontFamily = '"Helvetica Neue", Helvetica, Arial, sans-serif;';
+  span.style.visibility = 'hidden';
+  span.style.position = 'absolute';
+  span.textContent = word;
+  document.body.appendChild(span);
+  const width = span.offsetWidth;
+  document.body.removeChild(span);
+  return width;
+}
+
+function make_data_mobile_friendly(data) {
+  // Convert data to mobile chart data
+  // Change labels to ID or short citations
+  if (Array.isArray(data)) {
+    data.forEach((item) => {
+      make_data_mobile_friendly(item);
+    });
+  } else if (typeof data === 'object') {
+    if (data.hasOwnProperty('label')) {
+      data.label = data.id;
+      // console.log(data.label)
+    }
+    for (const key in data) {
+      make_data_mobile_friendly(data[key]);
+    }
+  }
+}
+
+function abbreviate_group_names(data) {
+  if (Array.isArray(data)) {
+    data.forEach((item) => {
+      if (item.group) {
+        item.group = item.group
+          .replace('Federal', 'Fed.')
+          .replace('Appellate', 'App.')
+          .replace('Trial', 'Tr.')
+          .replace('Special', 'Spec.')
+          .replace('Bankruptcy', 'Bank.')
+          .replace('Panel', 'Pnl')
+          .replace('District', 'Dist.')
+          .replace('Supreme', 'Sup.');
+      }
+    });
+  }
+}
+
 document.body.addEventListener('htmx:afterSettle', function (event) {
   var results = JSON.parse(event.detail.xhr.response);
-  TimelinesChart()(`#timeline-body`)
+  $('#json-data').data('json', JSON.stringify(results));
+  $('#fullScreenModal').modal('show');
+});
+
+$('#fullScreenModal').on('shown.bs.modal', function () {
+  updateChartOnResize();
+});
+
+$(document).ready(function () {
+  $('.btn-default').on('click', function () {
+    var circuitName = $(this).text();
+    $('#modalLabel').text(circuitName);
+  });
+});
+
+$(document).ready(function () {
+  // Check if the screen size is xs and automatically toggle the collapse accordingly
+  if ($(window).width() < 767) {
+    $('#federal_courts').collapse('hide');
+    $('#state_courts').collapse('hide');
+  }
+});
+
+function get_right_margins(results) {
+  let right_margin = 0;
+  var longest_label;
+  results.forEach((group) => {
+    group.data.forEach((item) => {
+      const labelLength = item.label.length;
+      if (labelLength > right_margin) {
+        right_margin = labelLength;
+        longest_label = item.label;
+      }
+    });
+  });
+  return calculateWordWidth(longest_label) + 150;
+}
+
+function initializeTimelinesChart() {
+  // Try to fit the chart to the size of the users screen
+  const container = document.getElementById('timeline-body');
+  const containerWidth = container.clientWidth;
+  var right_margin;
+  var left_margin;
+  var jsonData;
+  jsonData = $('#json-data').data('json');
+  results = JSON.parse(jsonData);
+  abbreviate_group_names(results);
+
+  if (containerWidth > 750) {
+    right_margin = get_right_margins(results);
+    left_margin = 150;
+  } else {
+    make_data_mobile_friendly(results);
+    right_margin = 150;
+    left_margin = 0; // drop the margin all together
+  }
+  this.chart = new TimelinesChart()(`#timeline-body`)
     .zQualitative(false)
     .enableOverview(true)
-    .leftMargin(150)
-    .rightMargin(400)
+    .leftMargin(left_margin)
+    .rightMargin(right_margin)
     .maxHeight(function (d) {
       return 8000;
     })
+    .maxLineHeight(25)
     .data([results[0]])
     .enableAnimations(false)
     .timeFormat('%Y-%m-%d')
@@ -40,24 +148,20 @@ document.body.addEventListener('htmx:afterSettle', function (event) {
     .onSegmentClick(function (d) {
       window.open(`/?court=${d.data.id}`);
     })
-    .refresh()
+    .width(containerWidth)
     .data(results)
     .refresh();
+}
 
-  $('#fullScreenModal').modal('show');
-});
+function updateChartOnResize() {
+  $('#timeline-body').empty();
+  initializeTimelinesChart();
+}
 
-$(document).ready(function () {
-  $('.btn-default').on('click', function () {
-    var circuitName = $(this).text();
-    $('#modalLabel').text(circuitName);
-  });
-});
-
-$(document).ready(function () {
-  // Check if the screen size is xs and automatically toggle the collapse accordingly
-  if ($(window).width() < 767) {
-    $('#federal_courts').collapse('hide');
-    $('#state_courts').collapse('hide');
-  }
+let resizeTimeout;
+window.addEventListener('resize', () => {
+  clearTimeout(resizeTimeout);
+  resizeTimeout = setTimeout(() => {
+    updateChartOnResize();
+  }, 100);
 });
