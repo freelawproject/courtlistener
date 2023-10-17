0c8dba07af1b1bb55bd6d8f7c07efefe45c25bdd
Eduardo Rosendo <eduardojra96@gmail.com>
2023-10-17 19:57:35 -0400

    feat(settings): Add setting to tweak the queue's name for ETL tasks

--- cl/search/tasks.py
+++ cl/search/tasks.py
@@ -336,7 +336,7 @@ def save_document_in_es(
     autoretry_for=(ConnectionError,),
     max_retries=3,
     interval_start=5,
-    queue="etl_tasks",
+    queue=settings.CELERY_ETL_TASKS_QUEUE,
 )
 def es_save_document(
     self: Task,
@@ -460,7 +460,7 @@ def update_document_in_es(
     autoretry_for=(ConnectionError,),
     max_retries=3,
     interval_start=5,
-    queue="etl_tasks",
+    queue=settings.CELERY_ETL_TASKS_QUEUE,
 )
 def es_document_update(
     self: Task,
@@ -591,7 +591,7 @@ def update_child_documents_by_query(
     autoretry_for=(ConnectionError, NotFoundError),
     max_retries=3,
     interval_start=5,
-    queue="etl_tasks",
+    queue=settings.CELERY_ETL_TASKS_QUEUE,
 )
 @throttle_task(settings.ES_THROTTLING_TASKS_RATE, key="throttling_id")
 def update_children_documents_by_query(
@@ -672,7 +672,7 @@ def update_children_documents_by_query(
     autoretry_for=(ConnectionError, NotFoundError),
     max_retries=3,
     interval_start=5,
-    queue="etl_tasks",
+    queue=settings.CELERY_ETL_TASKS_QUEUE,
 )
 @throttle_task(settings.ES_THROTTLING_TASKS_RATE, key="docket_id")
 def index_docket_parties_in_es(
@@ -796,7 +796,7 @@ def index_parent_and_child_docs(
     max_retries=3,
     interval_start=5,
     ignore_result=True,
-    queue="etl_tasks",
+    queue=settings.CELERY_ETL_TASKS_QUEUE,
 )
 def remove_document_from_es_index(
     self: Task, es_document_name: str, instance_id: int
