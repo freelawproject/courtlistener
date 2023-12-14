359af60982164bdc30d03cca823f993e7c4db9d1
Eduardo Rosendo <eduardojra96@gmail.com>
2023-12-14 11:25:59 -0400

    feat(donate): Add a factory for the NeonWebhookEvents model

--- cl/donate/factories.py
+++ cl/donate/factories.py
@@ -2,7 +2,7 @@ from factory import Faker, SubFactory
 from factory.django import DjangoModelFactory
 from factory.fuzzy import FuzzyChoice
 
-from cl.donate.models import PROVIDERS, Donation
+from cl.donate.models import PROVIDERS, Donation, NeonWebhookEvents
 from cl.users.factories import UserWithChildProfileFactory
 
 
@@ -22,3 +22,10 @@ class DonationFactory(DjangoModelFactory):
     payment_provider = FuzzyChoice(PROVIDERS.NAMES, getter=lambda c: c[0])
     status = FuzzyChoice(Donation.PAYMENT_STATUSES, getter=lambda c: c[0])
     send_annual_reminder = Faker("bool")
+
+
+class NeonWebhookEventFactory(DjangoModelFactory):
+    class Meta:
+        model = NeonWebhookEvents
+
+    trigger = FuzzyChoice(NeonWebhookEvents.TYPES, getter=lambda c: c[0])
