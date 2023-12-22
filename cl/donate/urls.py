da534adae95fdb424ae71437156d1b5ad5efa5ff
Eduardo Rosendo <eduardojra96@gmail.com>
2023-12-22 13:31:25 -0400

    feat(donate): Removes view to toggle monthly donations

--- cl/donate/urls.py
+++ cl/donate/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from cl.donate.paypal import donate_paypal_cancel, process_paypal_callback
 from cl.donate.stripe_helpers import process_stripe_callback
-from cl.donate.views import payment_complete, toggle_monthly_donation
+from cl.donate.views import payment_complete
 
 urlpatterns = [
     path(
@@ -23,11 +23,5 @@ urlpatterns = [
         "donate/callbacks/stripe/",
         process_stripe_callback,
         name="stripe_callback",
-    ),
-    # Monthly donations
-    path(
-        "monthly-donation/toggle/",
-        toggle_monthly_donation,
-        name="toggle_monthly_donation",
     ),
 ]
--- cl/donate/views.py
+++ cl/donate/views.py
@@ -1,10 +1,7 @@
 import logging
 
-from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-
-from cl.donate.models import MonthlyDonation
-from cl.lib.http import is_ajax
 
 logger = logging.getLogger(__name__)
 
@@ -33,23 +30,3 @@ def payment_complete(
         template_name,
         {"error": error, "private": True},
     )
-
-
-def toggle_monthly_donation(request: HttpRequest) -> HttpResponse:
-    """Use Ajax to enable/disable monthly contributions"""
-    if is_ajax(request) and request.method == "POST":
-        monthly_pk = request.POST.get("id")
-        m_donation = MonthlyDonation.objects.get(pk=monthly_pk)
-        state = m_donation.enabled
-        if state:
-            m_donation.enabled = False
-            msg = "Monthly contribution disabled successfully"
-        else:
-            m_donation.enabled = True
-            msg = "Monthly contribution enabled successfully"
-        m_donation.save()
-        return HttpResponse(msg)
-    else:
-        return HttpResponseNotAllowed(
-            permitted_methods={"POST"}, content="Not an Ajax POST request."
-        )
