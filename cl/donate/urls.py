5d7a9646124d4684a6dc190886d871cec2f1385b
Eduardo Rosendo <eduardojra96@gmail.com>
2023-12-22 13:37:04 -0400

    feat(donate): Removes paypal callback views

--- cl/donate/urls.py
+++ cl/donate/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-from cl.donate.paypal import donate_paypal_cancel, process_paypal_callback
 from cl.donate.stripe_helpers import process_stripe_callback
 from cl.donate.views import payment_complete
 
@@ -10,13 +9,6 @@ urlpatterns = [
         payment_complete,
         {"template_name": "donate_complete.html"},
         name="donate_complete",
-    ),
-    # Paypal
-    path("donate/paypal/cancel/", donate_paypal_cancel, name="paypal_cancel"),
-    path(
-        "donate/callbacks/paypal/",
-        process_paypal_callback,
-        name="paypal_callback",
     ),
     # Stripe
     path(
