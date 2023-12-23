9a189e64630efa1d36c7e025ac41e910438f2970
Eduardo Rosendo <eduardojra96@gmail.com>
2023-12-22 20:02:49 -0400

    feat(donate): Removes the payment_complete view

--- cl/donate/urls.py
+++ cl/donate/urls.py
@@ -2,18 +2,11 @@ from django.urls import path
 from django.views.generic.base import RedirectView
 
 from cl.donate.stripe_helpers import process_stripe_callback
-from cl.donate.views import payment_complete
 
 urlpatterns = [
     path(
         "donate/",
         RedirectView.as_view(url="https://free.law/donate"),
-    ),
-    path(
-        "donate/complete/",
-        payment_complete,
-        {"template_name": "donate_complete.html"},
-        name="donate_complete",
     ),
     # Stripe
     path(
