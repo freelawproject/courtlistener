b2b26efdc33271e0d881e1044e3e1c6521fd14bd
Eduardo Rosendo <eduardojra96@gmail.com>
2023-12-24 14:19:50 -0400

    feat(donate): Move the donate redirection to the cl/urls.py file

--- cl/donate/urls.py
+++ cl/donate/urls.py
@@ -4,10 +4,6 @@ from django.views.generic.base import RedirectView
 from cl.donate.stripe_helpers import process_stripe_callback
 
 urlpatterns = [
-    path(
-        "donate/",
-        RedirectView.as_view(url="https://free.law/donate"),
-    ),
     # Stripe
     path(
         "donate/callbacks/stripe/",
--- cl/urls.py
+++ cl/urls.py
@@ -74,6 +74,10 @@ urlpatterns = [
     ),
     # Redirects
     path(
+        "donate/",
+        RedirectView.as_view(url="https://free.law/donate"),
+    ),
+    path(
         "privacy/",
         RedirectView.as_view(url="/terms/#privacy", permanent=True),
     ),
