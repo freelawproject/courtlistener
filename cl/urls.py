90234d5c6085bbbba3ef3c680308420498d2fc05
Eduardo Rosendo <eduardojra96@gmail.com>
2023-12-26 21:22:27 -0400

    feat(donate): Adds redirection to free.law/donate/help/

--- cl/urls.py
+++ cl/urls.py
@@ -75,7 +75,12 @@ urlpatterns = [
     # Redirects
     path(
         "donate/",
-        RedirectView.as_view(url="https://free.law/donate"),
+        RedirectView.as_view(url="https://free.law/donate/"),
+    ),
+    path(
+        "help/donations/",
+        RedirectView.as_view(url="https://free.law/donate/help/"),
+        name="donation_help",
     ),
     path(
         "privacy/",
