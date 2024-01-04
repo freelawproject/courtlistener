dc825747a33d2729e71eba185eb92eb563e6592e
Eduardo Rosendo <eduardojra96@gmail.com>
2024-01-03 22:35:03 -0400

    feat(search): Add a hash lookup index on the docket_number

--- cl/search/migrations/0025_docket_unique_docket_per_court.sql
+++ cl/search/migrations/0025_docket_unique_docket_per_court.sql
@@ -3,4 +3,8 @@ BEGIN;
 -- Create constraint unique_docket_per_court on model docket
 --
 CREATE UNIQUE INDEX CONCURRENTLY "unique_docket_per_court" ON "search_docket" ((MD5("docket_number")), "pacer_case_id", "court_id");
+--
+-- Concurrently create index hash_docket_number_lookup_idx on field(s)  of model docket
+--
+CREATE INDEX CONCURRENTLY "hash_docket_number_lookup_idx" ON "search_docket" ((MD5("docket_number")));
 COMMIT;
