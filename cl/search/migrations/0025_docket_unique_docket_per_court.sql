0d53bb74fcc4edd63d9e56a42328503cb986cdf3
Eduardo Rosendo <eduardojra96@gmail.com>
2024-01-04 00:15:09 -0400

    fix(search): Removes the transaction block from the sql migration file

--- cl/search/migrations/0025_docket_unique_docket_per_court.sql
+++ cl/search/migrations/0025_docket_unique_docket_per_court.sql
@@ -1,4 +1,3 @@
-BEGIN;
 --
 -- Concurrently create index hash_docket_number_lookup_idx on field(s)  of model docket
 --
@@ -7,4 +6,3 @@ CREATE INDEX CONCURRENTLY "hash_docket_number_lookup_idx" ON "search_docket" ((M
 -- Create constraint unique_docket_per_court on model docket
 --
 CREATE UNIQUE INDEX CONCURRENTLY "unique_docket_per_court" ON "search_docket" ((MD5("docket_number")), "pacer_case_id", "court_id");
-COMMIT;
