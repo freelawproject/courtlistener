3f43fb7f289209801c13cb496eb3f9772ca065e2
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2024-01-10 14:37:02 -0500

    feat(sentry): Group sentry issues by scraper

--- cl/settings/third_party/sentry.py
+++ cl/settings/third_party/sentry.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 import environ
 import sentry_sdk
 from sentry_sdk.integrations.celery import CeleryIntegration
@@ -13,6 +15,22 @@ SENTRY_REPORT_URI = env("SENTRY_REPORT_URI", default="")
 ignore_logger("internetarchive.session")
 ignore_logger("internetarchive.item")
 
+
+def refine_fingerprint(event: Dict[str, Any], hint: Dict[str, Any]):
+    """If a fingerprint key is present in "extra", pass it to Sentry
+
+    This fingerprint will be a "group ID" on Sentry, forcing all events
+    with that key to be grouped on a single issue
+
+    This implementation expects the fingerprint to be a string
+    The event["fingerprint"] should be a list
+    """
+    if fingerprint := event.get("extra", {}).pop("fingerprint", ""):
+        event["fingerprint"] = [fingerprint]
+
+    return event
+
+
 if SENTRY_DSN:
     sentry_sdk.init(
         dsn=SENTRY_DSN,
@@ -22,4 +40,5 @@ if SENTRY_DSN:
             RedisIntegration(),
         ],
         ignore_errors=[KeyboardInterrupt],
+        before_send=refine_fingerprint,
     )
