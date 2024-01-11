4ecdbe2379b1c0879571445936ae10eed2c50300
Eduardo Rosendo <eduardojra96@gmail.com>
2024-01-11 15:43:34 -0400

    feat(base.js): Moves the getCookie helper out of the .ready method

--- cl/assets/static-global/js/base.js
+++ cl/assets/static-global/js/base.js
@@ -13,6 +13,22 @@ function recapIsInstalled(event) {
   );
 }
 
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie != '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) == (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
 $(document).ready(function () {
   // 'use strict'; // uncomment later on after full cleanup
   var citedGreaterThan = $('#id_cited_gt');
@@ -239,21 +255,6 @@ $(document).ready(function () {
   ///////////////////////
   // Make sure that a CSRF Header is sent with every ajax request.
   // https://docs.djangoproject.com/en/dev/ref/csrf/#ajax
-  function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-      var cookies = document.cookie.split(';');
-      for (var i = 0; i < cookies.length; i++) {
-        var cookie = jQuery.trim(cookies[i]);
-        // Does this cookie string begin with the name we want?
-        if (cookie.substring(0, name.length + 1) == (name + '=')) {
-          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-          break;
-        }
-      }
-    }
-    return cookieValue;
-  }
   var csrfToken = getCookie('csrftoken');
 
   function csrfSafeMethod(method) {
