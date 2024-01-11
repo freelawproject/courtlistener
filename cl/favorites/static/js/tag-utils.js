a56357205c8b77bba4547d4a262ad0ae84347d41
Eduardo Rosendo <eduardojra96@gmail.com>
2024-01-11 18:33:06 -0400

    feat(favorites): Avoids reloading the tag details page

--- cl/favorites/static/js/tag-utils.js
+++ cl/favorites/static/js/tag-utils.js
@@ -12,7 +12,8 @@ $(document).ready(function () {
       a.textContent.includes('Untag this item')
     );
     untagOptionButtons.map((button) => {
-      button.addEventListener('click', () => {
+      button.addEventListener('click', (e) => {
+        e.preventDefault()
         fetch('/api/rest/v3/docket-tags/' + button.dataset.id + '/', {
           method: 'DELETE',
           headers: { 'X-CSRFToken': token },
@@ -23,9 +24,22 @@ $(document).ready(function () {
             // button is contained in html as follows
             // li => div.dropdown.float-right => ul.dropdown-menu => li => button
             button.parentNode.parentNode.parentNode.parentNode.remove();
+
+            // count the number of rows after removing the docket to update title
+            let rows = document.querySelectorAll('#docket-list > ul >li');
+            let title = document.getElementById('tag-list-title');
+            let docket_icon = document.getElementById('docket-icon');
+            const pluralize = (count, noun, suffix = 's') => `${noun}${count !== 1 ? suffix : ''}`;
+            if (rows.length) {
+              let div = document.createElement('div');
+              div.appendChild(docket_icon);
+              div.innerHTML += ` ${rows.length} Tagged ${pluralize(rows.length, 'Docket')}`;
+              title.innerHTML = div.innerHTML;
+            } else {
+              title.innerHTML = 'Nothing tagged yet';
+            }
           })
           .catch((err) => console.log(err.message));
-        return false;
       });
     });
   }
