bd59984ced89690999475a32aab7ccf9300ed39f
Eduardo Rosendo <eduardojra96@gmail.com>
2024-01-12 12:30:40 -0400

    feat(citations): Cleans up imports in the types.py file

--- cl/citations/types.py
+++ cl/citations/types.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Union
+from typing import Union
 
 from eyecite.models import (
     FullCaseCitation,
@@ -14,5 +14,5 @@ SupportedCitationType = Union[
     FullCaseCitation, ShortCaseCitation, SupraCitation, IdCitation
 ]
 MatchedResourceType = Union[Opinion, Resource]
-ResolvedFullCite = Tuple[FullCaseCitation, MatchedResourceType]
-ResolvedFullCites = List[ResolvedFullCite]
+ResolvedFullCite = tuple[FullCaseCitation, MatchedResourceType]
+ResolvedFullCites = list[ResolvedFullCite]
