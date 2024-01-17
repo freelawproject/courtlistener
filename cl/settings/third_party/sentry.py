d621b514734149ec033fa98e084358244f390568
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2024-01-17 18:16:45 -0500

    feat(sentry): group sentry issues by scraper
    Code review changes

--- cl/settings/third_party/sentry.py
+++ cl/settings/third_party/sentry.py
@@ -1,5 +1,3 @@
-from typing import Any, Dict
-
 import environ
 import sentry_sdk
 from sentry_sdk.integrations.celery import CeleryIntegration
@@ -16,24 +14,26 @@ ignore_logger("internetarchive.session")
 ignore_logger("internetarchive.item")
 
 
-def fingerprint_event(
-    event: Dict[str, Any], hint: Dict[str, Any]
-) -> Dict[str, Any]:
-    """If a fingerprint key is present in "extra", pass it to Sentry
+def fingerprint_sentry_error(event: dict, hint: dict) -> dict:
+    """Captures fingerprint information from logger.error call, if present
 
-    This function expects error.log(extra={"fingerprint": []})
-    to be a List
+    logger.error calls allow to pass an `extra` dictionary with arbitrary keys
+    We use this to pass a `fingerprint` key. The value should be a list.
+    For example:
+    error.log(extra={"fingerprint": [court_id, "citation-not-found"]})
 
-    The fingerprint will be a "group ID" on Sentry, forcing all events
-    with that key to be grouped on a single issue
+    By default, Sentry events are grouped into issues using Sentry's
+    internal algorithms.
+    By passing an explicit `fingerprint` key in the `event` dictionary,
+    we can force the grouping of all events with the same fingerprint
+    making Sentry's issues more granular and useful.
 
     :param event: event dict to be sent to Sentry
     :param hint: dict with extra information about the event
 
-    :return: the event that will be sent to Sentry
+    :return: the event that will be sent to Sentry,
+                with explicit fingerprint values
     """
-    # logger.error calls can take an 'extra' keyword argument
-    # which is used here to pass the fingerprint key
     if fingerprint := event.get("extra", {}).pop("fingerprint", []):
         event["fingerprint"] = fingerprint
 
@@ -49,5 +49,5 @@ if SENTRY_DSN:
             RedisIntegration(),
         ],
         ignore_errors=[KeyboardInterrupt],
-        before_send=fingerprint_event,
+        before_send=fingerprint_sentry_error,
     )
