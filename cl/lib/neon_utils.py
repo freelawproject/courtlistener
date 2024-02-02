5ebf524931a01b7a999dd81a93e8f006da6844bf
Eduardo Rosendo <eduardojra96@gmail.com>
2024-02-01 22:37:26 -0400

    feat(lib): Adds helper methods to create accounts to the NeonClient

--- cl/lib/neon_utils.py
+++ cl/lib/neon_utils.py
@@ -1,6 +1,10 @@
+from typing import Any
+
 import requests
 from django.conf import settings
 from requests.auth import HTTPBasicAuth
+
+from cl.users.models import UserProfile
 
 NEON_API_URL = "https://api.neoncrm.com/v2"
 
@@ -26,3 +30,51 @@ class NeonClient:
             if json_data["individualAccount"]
             else json_data["companyAccount"]
         )
+
+    def search_account_by_email(self, email: str) -> list[dict[str, str]]:
+        search_payload = {
+            "searchFields": [
+                {"field": "Email", "operator": "EQUAL", "value": email}
+            ],
+            "outputFields": ["Account ID"],
+            "pagination": {"pageSize": 10},
+        }
+
+        response = requests.post(
+            f"{NEON_API_URL}/accounts/search",
+            auth=self._basic,
+            timeout=self._timeout,
+            json=search_payload,
+        )
+
+        response.raise_for_status()
+        json_data = response.json()
+
+        return json_data["searchResults"]
+
+    def get_individual_account_payload(
+        self, user: UserProfile.user
+    ) -> dict[str, Any]:
+        return {
+            "individualAccount": {
+                "primaryContact": {
+                    "firstName": user.first_name,
+                    "lastName": user.last_name,
+                    "email1": user.email,
+                }
+            }
+        }
+
+    def create_account(self, user: UserProfile.user) -> int:
+        payload = self.get_individual_account_payload(user)
+        response = requests.post(
+            f"{NEON_API_URL}/accounts/",
+            auth=self._basic,
+            timeout=self._timeout,
+            json=payload,
+        )
+
+        response.raise_for_status()
+        json_data = response.json()
+
+        return json_data["id"]
