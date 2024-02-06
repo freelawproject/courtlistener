fd8caf2bf20368ac825d325b3717cdcf40f55336
Alberto Islas <albertisfu@gmail.com>
2024-02-06 17:57:09 -0600

    fix(elasticsearch): Update people index name

--- cl/search/es_indices.py
+++ cl/search/es_indices.py
@@ -27,7 +27,7 @@ oral_arguments_percolator_index.settings(
 
 
 # Define people elasticsearch index
-people_db_index = Index("people_db_index_vectors")
+people_db_index = Index("people_vectors")
 people_db_index.settings(
     number_of_shards=settings.ELASTICSEARCH_PEOPLE_NUMBER_OF_SHARDS,
     number_of_replicas=settings.ELASTICSEARCH_PEOPLE_NUMBER_OF_REPLICAS,
