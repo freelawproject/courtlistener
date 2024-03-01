5fe55ce672e59feb408847262361a50957e0a20e
Eduardo Rosendo <eduardojra96@gmail.com>
2024-02-29 21:18:20 -0400

    feat(settings): Adds global object with redis clients

--- cl/settings/third_party/redis.py
+++ cl/settings/third_party/redis.py
@@ -1,4 +1,8 @@
 import environ
+from redis import Redis
+from redis.backoff import ExponentialBackoff
+from redis.exceptions import ConnectionError, TimeoutError
+from redis.retry import Retry
 
 env = environ.FileAwareEnv()
 
@@ -13,4 +17,20 @@ REDIS_DATABASES = {
     "CACHE": 1,
     "STATS": 2,
     "ALERTS": 3,
+}
+# Run 3 retries with exponential backoff strategy
+retry = Retry(ExponentialBackoff(cap=8, base=2), 3)
+# Redis clients with retries on custom errors
+REDIS_CLIENTS = {
+    f"{name}-{decode}": Redis(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        db=db,  # type: ignore
+        decode_responses=decode,
+        retry=retry,
+        retry_on_error=[ConnectionError, TimeoutError],
+        health_check_interval=15,
+    )
+    for decode in [True, False]
+    for name, db in REDIS_DATABASES.items()
 }
