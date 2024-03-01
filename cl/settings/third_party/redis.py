a3cd47dd862185c611f9b05c6b49333e48384294
Eduardo Rosendo <eduardojra96@gmail.com>
2024-03-01 11:53:44 -0400

    feat(settings): Adds environment variables to configure Redis retry

--- cl/settings/third_party/redis.py
+++ cl/settings/third_party/redis.py
@@ -9,6 +9,10 @@ env = environ.FileAwareEnv()
 
 REDIS_HOST = env("REDIS_HOST", default="cl-redis")
 REDIS_PORT = 6379
+REDIS_MAX_RETRIES = env("REDIS_MAX_RETRIES", default=3)
+REDIS_MAX_DELAY_IN_SECONDS = env("REDIS_MAX_DELAY_IN_SECONDS", default=0.5)
+REDIS_BACKOFF_BASE = env("REDIS_BACKOFF_BASE", default=0.5)
+REDIS_HEALTH_CHECK_INTERVAL = env("REDIS_HEALTH_CHECK_INTERVAL", default=15)
 
 # Redis is configured with 16 databases out of the box. This keeps them neatly
 # mapped.
@@ -19,7 +23,12 @@ REDIS_DATABASES = {
     "ALERTS": 3,
 }
 # Run 3 retries with exponential backoff strategy
-retry = Retry(ExponentialBackoff(cap=8, base=2), 3)
+retry = Retry(
+    ExponentialBackoff(
+        cap=REDIS_MAX_DELAY_IN_SECONDS, base=REDIS_BACKOFF_BASE
+    ),
+    REDIS_MAX_RETRIES,
+)
 # Redis clients with retries on custom errors
 REDIS_CLIENTS = {
     f"{name}-{decode}": Redis(
@@ -29,7 +38,7 @@ REDIS_CLIENTS = {
         decode_responses=decode,
         retry=retry,
         retry_on_error=[ConnectionError, TimeoutError],
-        health_check_interval=15,
+        health_check_interval=REDIS_HEALTH_CHECK_INTERVAL,
     )
     for decode in [True, False]
     for name, db in REDIS_DATABASES.items()
