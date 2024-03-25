2e3008916242e640d54b906b71ff5bc67d9b6408
Eduardo Rosendo <eduardojra96@gmail.com>
2024-03-25 12:09:53 -0400

    feat(citations): Tweak the citation lookup view to handle multi-lookups

--- cl/citations/api_views.py
+++ cl/citations/api_views.py
@@ -5,6 +5,7 @@ from asgiref.sync import async_to_sync
 from django.db.models import QuerySet
 from django.http import HttpResponse
 from django.template.defaultfilters import slugify
+from django.utils.safestring import SafeString
 from rest_framework.exceptions import NotFound, ValidationError
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.request import Request
@@ -16,7 +17,7 @@ from cl.citations.utils import SLUGIFIED_EDITIONS, get_canonicals_from_reporter
 from cl.search.api_serializers import OpinionClusterSerializer
 from cl.search.models import OpinionCluster
 from cl.search.selectors import get_clusters_from_citation_str
-from django.utils.safestring import SafeString
+
 
 class CitationLookupViewSet(CreateModelMixin, GenericViewSet):
     queryset = OpinionCluster.objects.all()
@@ -28,29 +29,59 @@ class CitationLookupViewSet(CreateModelMixin, GenericViewSet):
         citation_serializer.is_valid(raise_exception=True)
 
         # Get query parameters from the validated data
+        citations = []
         data = citation_serializer.validated_data
-        self.full_text_citation = data.get("text_citation", None)
-        self.reporter = data.get("reporter", None)
-        self.volume = data.get("volume", None)
-        self.page = data.get("page", None)
-
-        if self.full_text_citation:
-            citations = eyecite.get_citations(self.full_text_citation)
-            if not citations:
+        full_text_citation = data.get("text_citation", None)
+        if full_text_citation:
+            citation_objs = eyecite.get_citations(full_text_citation)
+            if not citation_objs:
                 raise NotFound(f"No citations found in 'text_citation'.")
 
-            if not citations[0].groups:
-                raise ValidationError({"text_citation": ["Invalid citation."]})
+            for citation in citation_objs:
+                start_index, end_index = citation.span()
+                citation_data = {
+                    "citation": citation.matched_text(),
+                    "start_index": start_index,
+                    "end_index": end_index,
+                }
+                if not citation.groups:
+                    error_data = {
+                        "status": HTTPStatus.BAD_REQUEST,
+                        "error_message": "Invalid citation.",
+                    }
+                    citations.append({**citation_data, **error_data})
+                    continue
 
-            c = citations[0]
-            self.reporter = c.groups["reporter"]
-            self.volume = c.groups["volume"]
-            self.page = c.groups["page"]
+                reporter = citation.groups["reporter"]
+                volume = citation.groups["volume"]
+                page = citation.groups["page"]
+                citations.append(
+                    {
+                        **citation_data,
+                        **self._citation_handler(
+                            request, reporter, volume, page
+                        ),
+                    }
+                )
+        else:
+            reporter = data.get("reporter")
+            volume = data.get("volume")
+            page = data.get("page")
 
-        return self._citation_handler(request, self.reporter)
+            citations.append(
+                {
+                    "citation": " ".join([str(volume), reporter, page]),
+                    **self._citation_handler(request, reporter, volume, page),
+                }
+            )
 
-    def _attempt_reporter_variation(self) -> list[SafeString]:
+        return Response(citations)
+
+    def _attempt_reporter_variation(self, reporter) -> list[SafeString]:
         """Try to disambiguate an unknown reporter using the variations dict.
+
+        Args:
+            reporter (str): The name of the reporter.
 
         Raises:
             NotFound: If the unknown reporter string doesn't match a canonical.
@@ -58,18 +89,18 @@ class CitationLookupViewSet(CreateModelMixin, GenericViewSet):
         Returns:
             str: canonical name for the reporter.
         """
-        potential_canonicals = get_canonicals_from_reporter(self.reporter_slug)
+        potential_canonicals = get_canonicals_from_reporter(slugify(reporter))
 
         if len(potential_canonicals) == 0:
             # Couldn't find it as a variation. Give up.
             raise NotFound(
-                f"Unable to find reporter with abbreviation of '{self.reporter}'"
+                f"Unable to find reporter with abbreviation of '{reporter}'"
             )
 
         return potential_canonicals
 
     def _citation_handler(
-        self, request: Request, reporter: str
+        self, request: Request, reporter: str, volume: int, page: str
     ) -> HttpResponse:
         """
         This method retrieves opinion clusters that match a citation string.
@@ -78,6 +109,8 @@ class CitationLookupViewSet(CreateModelMixin, GenericViewSet):
             request (Request): The HTTP object containing information
                 about the request.
             reporter (str): The name of the reporter.
+            volume (int): The volume number of citation.
+            page (str): The page number where the citation is located.
 
         Raises:
             NotFound: If the citation string doesn't match any opinion clusters
@@ -86,11 +119,11 @@ class CitationLookupViewSet(CreateModelMixin, GenericViewSet):
             HttpResponse: An HTTP response object containing a list of matching
                 opinion clusters.
         """
-        citation_str = " ".join([str(self.volume), reporter, self.page])
+        citation_str = " ".join([str(volume), reporter, page])
 
-        self.reporter_slug = slugify(self.reporter)
         # Look up the reporter to get its proper version (so-2d -> So. 2d)
-        proper_reporter = SLUGIFIED_EDITIONS.get(self.reporter_slug, None)
+        proper_reporter: None | str | list[SafeString]
+        proper_reporter = SLUGIFIED_EDITIONS.get(slugify(reporter), None)
         if not proper_reporter:
             proper_reporter = self._attempt_reporter_variation()
 
@@ -99,10 +132,10 @@ class CitationLookupViewSet(CreateModelMixin, GenericViewSet):
             # SLUGIFIED_EDITIONS dictionary.
             clusters, cluster_count = async_to_sync(
                 get_clusters_from_citation_str
-            )(proper_reporter, str(self.volume), self.page)
+            )(proper_reporter, str(volume), page)
         else:
             clusters, cluster_count = self._get_clusters_for_canonical_list(
-                proper_reporter
+                proper_reporter, volume, page
             )
 
         if cluster_count == 0:
@@ -111,7 +144,7 @@ class CitationLookupViewSet(CreateModelMixin, GenericViewSet):
         return self._show_response(request, clusters)
 
     def _get_clusters_for_canonical_list(
-        self, reporters: list[SafeString]
+        self, reporters: list[SafeString], volume: int, page: str
     ) -> tuple[QuerySet[OpinionCluster] | None, int]:
         """
         Retrieves opinion clusters associated with a list of reporter slugs.
@@ -120,8 +153,10 @@ class CitationLookupViewSet(CreateModelMixin, GenericViewSet):
         to find all associated opinion clusters.
 
         Args:
-            reporters (list[str]): A list of strings representing the reporter
-            slugs.
+            reporters (list[SafeString]): A list of strings representing the reporter
+                                  slugs.
+            volume (int): The volume number of citation.
+            page (str): The page number where the citation is located.
 
         Returns:
             A tuple containing two elements:
@@ -136,17 +171,7 @@ class CitationLookupViewSet(CreateModelMixin, GenericViewSet):
                 continue
 
             opinions, _count = async_to_sync(get_clusters_from_citation_str)(
-                reporter, str(self.volume), self.page
-            )
-
-            if not _count:
-                continue
-
-            clusters = clusters | opinions if clusters else opinions
-            cluster_count += _count
-
-            opinions, _count = async_to_sync(get_clusters_from_citation_str)(
-                reporter, str(self.volume), self.page
+                reporter, str(volume), page
             )
 
             if not _count:
