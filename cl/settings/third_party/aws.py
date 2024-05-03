f2c22ccb6468332c53e380d02e9d4382e4be34af
Ramiro Blanco <freelaw@red-skull>
2024-05-03 16:36:07 -0300

    fix(docker): AWS settings vars
    These variables are not used anywhere in the code. Boto picks up the credentials from the environment. TBD.
    
    Files: cl/settings/third_party/aws.py

--- cl/settings/third_party/aws.py
+++ cl/settings/third_party/aws.py
@@ -3,15 +3,10 @@ import environ
 env = environ.FileAwareEnv()
 DEVELOPMENT = env.bool("DEVELOPMENT", default=True)
 
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID", default="")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY", default="")
 
 # S3
-if DEVELOPMENT:
-    AWS_ACCESS_KEY_ID = env("AWS_DEV_ACCESS_KEY_ID", default="")
-    AWS_SECRET_ACCESS_KEY = env("AWS_DEV_SECRET_ACCESS_KEY", default="")
-else:
-    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID", default="")
-    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY", default="")
-
 AWS_STORAGE_BUCKET_NAME = env(
     "AWS_STORAGE_BUCKET_NAME", default="com-courtlistener-storage"
 )
