6e96caa23616bf610c6ecdf65d1d95bb4c460222
Eduardo Rosendo <eduardojra96@gmail.com>
2024-06-19 02:12:36 -0400

    feat(corpus_importer): Add flag to use replica database during opinion import

--- cl/corpus_importer/management/commands/recap_into_opinions.py
+++ cl/corpus_importer/management/commands/recap_into_opinions.py
@@ -1,4 +1,5 @@
 from asgiref.sync import async_to_sync
+from django.conf import settings
 from django.core.management import BaseCommand
 from eyecite.tokenizers import HyperscanTokenizer
 from httpx import (
@@ -47,9 +48,10 @@ def extract_recap_document(rd: RECAPDocument) -> Response:
 
 
 def import_opinions_from_recap(
-    court=None,
-    total_count=0,
-    queue="batch1",
+    court: str | None = None,
+    total_count: int = 0,
+    queue: str = "batch1",
+    db_connection: str = "default",
 ) -> None:
     """Import recap documents into opinion db
 
@@ -58,14 +60,13 @@ def import_opinions_from_recap(
     :param queue: The queue to use for celery
     :return: None
     """
+    courts = Court.objects.using(db_connection)
     if not court:
-        courts = Court.objects.filter(
-            jurisdiction=Court.FEDERAL_DISTRICT
-        ).exclude(
+        courts = courts.filter(jurisdiction=Court.FEDERAL_DISTRICT).exclude(
             id__in=["orld", "dcd"]
         )  # orld is historical and we gather dcd opinions from the court
     else:
-        courts = Court.objects.filter(pk=court)
+        courts = courts.filter(pk=court)
 
     count = 0
     for court in courts:
@@ -74,7 +75,7 @@ def import_opinions_from_recap(
         # Manually select the replica db which has an addt'l index added to
         # improve this query.
         latest_date_filed = (
-            OpinionCluster.objects.using("replica")
+            OpinionCluster.objects.using(db_connection)
             .filter(docket__court=court)
             .exclude(source=SOURCES.RECAP)
             .order_by("-date_filed")
@@ -83,7 +84,8 @@ def import_opinions_from_recap(
         )
 
         recap_document_ids = (
-            RECAPDocument.objects.filter(
+            RECAPDocument.objects.using(db_connection)
+            .filter(
                 docket_entry__docket__court=court,
                 docket_entry__date_filed__gt=latest_date_filed,
                 is_available=True,
@@ -97,7 +99,7 @@ def import_opinions_from_recap(
         for recap_document_id in recap_document_ids.iterator():
             throttle.maybe_wait()
             ingest_recap_document.apply_async(
-                args=[recap_document_id], queue=queue
+                args=[recap_document_id.id], queue=queue
             )
             if total_count > 0 and count >= total_count:
                 logger.info(
@@ -131,10 +133,21 @@ class Command(BaseCommand):
             default="batch1",
             help="The celery queue where the tasks should be processed",
         )
+        parser.add_argument(
+            "--use-replica",
+            action="store_true",
+            default=False,
+            help="Use this flag to run the queries in the replica db",
+        )
 
     def handle(self, *args, **options):
         court = options.get("court")
         total_count = options.get("total")
         queue = options.get("queue")
+        db_connection = (
+            "replica"
+            if options.get("use_replica") and "replica" in settings.DATABASES
+            else "default"
+        )
 
-        import_opinions_from_recap(court, total_count, queue)
+        import_opinions_from_recap(court, total_count, queue, db_connection)
