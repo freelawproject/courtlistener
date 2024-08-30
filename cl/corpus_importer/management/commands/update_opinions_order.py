0ae37b91d33210c8b76bbfc8ce04898867f51473
Kevin Ramirez <kvnzavalza@hotmail.com>
2024-08-30 11:09:18 -0600

    feat(columbia_ordering): update columbia ordering code

--- cl/corpus_importer/management/commands/update_opinions_order.py
+++ cl/corpus_importer/management/commands/update_opinions_order.py
@@ -1,20 +1,12 @@
 import argparse
-import os
 import re
 import time
-from typing import Optional
+from typing import Any, List
 
 from bs4 import BeautifulSoup
 from django.db import transaction
-from django.db.models import Count, Exists, OuterRef
+from django.db.models import Count
 
-from cl.corpus_importer.import_columbia.columbia_utils import (
-    extract_columbia_opinions,
-    map_opinion_types,
-    process_extracted_opinions,
-    read_xml_to_soup,
-)
-from cl.corpus_importer.utils import EmptyOpinionException, match_opinion_lists
 from cl.lib.command_utils import VerboseCommand, logger
 from cl.search.models import SOURCES, Opinion, OpinionCluster
 
@@ -84,157 +76,73 @@ def sort_harvard_opinions(options: dict) -> None:
             time.sleep(options["delay"])
 
 
-def fix_filepath(filepath: str) -> str:
-    """Fix filepath from file field
+def get_xml(filepath: str) -> str:
+    """Get cleaned columbia content to compare opinions against
 
-    :param filepath: path from file field
-    :return: new file path
+    :param filepath: the filepath
+    :return: the opinion text cleaned up
     """
-    if "/home/mlissner/columbia/opinions/" in filepath:
-        filepath = filepath.replace("/home/mlissner/columbia/opinions/", "")
-    return filepath
+    with open(filepath, "r") as f:
+        content = f.read()
+
+    columbia_xml = BeautifulSoup(content, "html.parser")
+    clean_columbia_text = re.sub(r"[^a-zA-Z0-9\s]+", " ", columbia_xml.text)
+    clean_columbia_text = re.sub(r"\s+", " ", clean_columbia_text)
+    return clean_columbia_text
 
 
-def clean_opinion_content(text: str) -> str:
-    """Clean opinion content
+def generate_ngrams(words: List[str]) -> List[List[str]]:
+    """Generate n-grams based on the length of the word list.
 
-    :param text: text to clean
-    :return: cleaned text
+    Pass in a list of words in an opinion and divide it up into n-grams
+    based on the length of it.  for small opinions look for bigrams or
+    single unique words
+
+    :param words: a list of words obtained splitting the opinion
+    :return: n-grams
     """
+    if len(words) <= 5:
+        return [[x] for x in words]
+    elif 5 < len(words) < 25:
+        return [words[i : i + 2] for i in range(len(words) - 1)]
+    else:
+        return [words[i : i + 5] for i in range(len(words) - 4)]
 
-    # Replace line breaks with spaces and get rid of double spaces
-    text = re.sub(" +", " ", " ".join(text.split("\n"))).strip()
 
-    # Remove non-alphanumeric and non-whitespace characters from lowercased text
-    return re.sub(r"[^a-zA-Z0-9 ]", "", text.lower())
+def match_text(opinions: List[Any], xml_dir: str) -> List[List[Any]]:
+    """Identify a unique set of text in opinions to identify order of opinions
 
+    In a small subset of opinions, duplicate text or bad data fails and assign
+    the end of the index.  These opinions are usually short dissents that we
+    add to be the back of the order.
 
-def get_opinions_cleaned_content(
-    cluster_id,
-) -> tuple[Optional[str], list[dict]]:
-    """Get cleaned opinions content for a cluster object
-
-    :param cluster_id: Cluster ID for a set of opinions
-    :return: (xml path, list of extracted opinions)
+    :param opinions: Opinions to sort
+    :param xml_dir: Path to directory of the xml files
+    :return: Ordered opinion list
     """
-    cl_cleaned_opinions = []
-    # by default the opinions are ordered by pk
-    opinions_from_cluster = (
-        Opinion.objects.filter(cluster_id=cluster_id)
-        .order_by("id")
-        .exclude(type="010combined")
-    )
+    local_path = opinions[0][-1]
+    filepath = local_path.replace("/home/mlissner/columbia/opinions", xml_dir)
 
-    xml_path = None
+    columbia_words = get_xml(filepath=filepath)
+    matches = []
 
-    for i, op in enumerate(opinions_from_cluster):
-        if op.local_path and not xml_path:
-            xml_path = str(op.local_path)
+    for opinion in opinions:
+        # assign back up index to the end of the opinion
+        match_index = len(columbia_words)
+        soup = BeautifulSoup(opinion[2], "html.parser")
+        words = re.findall(r"\b\w+\b", soup.text)
+        ngrams_to_check = generate_ngrams(words)
 
-        content = None
-
-        # We can only use columbia's content to infer the ordering
-        if len(op.html_columbia) > 1:
-            content = op.html_columbia
-
-        if not content:
-            raise EmptyOpinionException(
-                "There is no content in html_columbia field"
-            )
-
-        soup = BeautifulSoup(content, features="html.parser")
-        opinion_text = soup.getText(separator=" ", strip=True)
-        prep_text = clean_opinion_content(opinion_text)
-
-        cl_cleaned_opinions.append(
-            {
-                "id": op.id,
-                "byline": op.author_str,
-                "type": op.type,
-                "opinion": prep_text,
-                "order": i,
-            }
-        )
-
-    return (
-        xml_path,
-        cl_cleaned_opinions,
-    )
-
-
-def get_opinions_columbia_file(xml_filepath: str) -> list:
-    """Get opinions from columbia xml file and convert it into dict
-
-    :param xml_filepath: path of xml file
-    :return: dict with data
-    """
-    soup = read_xml_to_soup(xml_filepath)
-
-    # Find the outer <opinion> tag to have all elements inside
-    outer_opinion = soup.find("opinion")
-
-    extracted_opinions = extract_columbia_opinions(outer_opinion)
-    opinions = process_extracted_opinions(extracted_opinions)
-    map_opinion_types(opinions)
-
-    for op in opinions:
-        opinion_content = op.get("opinion")
-        soup = BeautifulSoup(opinion_content, "html.parser")
-        opinion_text = soup.getText(separator=" ", strip=True)
-        cleaned_opinion = clean_opinion_content(opinion_text)
-        op["opinion"] = cleaned_opinion
-
-    return opinions
-
-
-def update_opinions(
-    cluster_id: int,
-    cl_opinions: list,
-    columbia_opinions: list,
-    matches: dict,
-):
-    """Update opinions with correct order
-
-    :param cluster_id:
-    :param cl_opinions: a list with cleaned opinions from cl
-    :param columbia_opinions: an ordered list with cleaned opinions from xml file
-    :param matches: a dict with the matches of each opinion of both lists
-    :return: None
-    """
-    update_failed = False
-
-    with transaction.atomic():
-        for file_pos, cl_pos in matches.items():
-            # file_pos is the correct index to find the opinion id to update
-            file_opinion = columbia_opinions[file_pos]
-            # the order was calculated using the xml file
-            file_order = file_opinion.get("order")
-            cl_opinion = cl_opinions[cl_pos]
-            opinion_id_to_update = cl_opinion.get("id")
-
-            if opinion_id_to_update:
-                try:
-                    # Update opinion order
-                    op = Opinion.objects.get(id=opinion_id_to_update)
-                    op.ordering_key = file_order
-                    op.save()
-                except Opinion.DoesNotExist:
-                    # This should not happen, but it is better to be cautious
-                    logger.warning(
-                        f"We can't update opinion, opinion doesn't exist "
-                        f"with id: {opinion_id_to_update}"
-                    )
-                    update_failed = True
-                    break
-
-        if update_failed:
-            # There was an error updating an opinion, rollback all changes for
-            # cluster's opinions
-            logger.warning(
-                f"There was an error updating the order of opinions of the "
-                f"cluster id: {cluster_id}"
-            )
-            transaction.set_rollback(True)
+        # Check for unique matches in columbia_text
+        for word_group in ngrams_to_check:
+            phrase = " ".join(word_group)
+            if columbia_words.count(phrase) == 1:
+                # Found a match - break the check
+                match_index = columbia_words.find(phrase)
+                break
+        matches.append([opinion[0], opinion[1], match_index])
+    ordered_opinions = sorted(matches, key=lambda x: x[-1])
+    return ordered_opinions
 
 
 def sort_columbia_opinions(options: dict) -> None:
@@ -243,138 +151,54 @@ def sort_columbia_opinions(options: dict) -> None:
     :param options: dict of arguments passed to the command
     :return: None
     """
-
+    xml_dir = options["xml_dir"]
     skip_until = options.get("skip_until", None)
-    limit = options.get("limit", None)
-
-    # Get all columbia cluster ids with more than one opinion and where opinions has
-    # a local_path, we require the xml path and file to infer the ordering
     clusters = (
-        OpinionCluster.objects.annotate(
-            opinions_count=Count("sub_opinions"),
-            has_non_empty_file=Exists(
-                Opinion.objects.filter(
-                    cluster=OuterRef("pk"),
-                    local_path__isnull=False,
-                    local_path__gt="",
-                )
-            ),
-        )
-        .filter(
-            opinions_count__gt=1,
-            source__in=VALID_COLUMBIA_SOURCES,
-            has_non_empty_file=True,
+        OpinionCluster.objects.filter(
+            source__contains=SOURCES.COLUMBIA_ARCHIVE
         )
         .order_by("id")
         .values_list("id", flat=True)
     )
-
-    if skip_until:
-        clusters = clusters.filter(pk__gte=skip_until)
-
-    if limit:
-        clusters = clusters[:limit]
+    if skip_until is not None:
+        clusters = clusters.filter(id__gt=skip_until)
 
     for cluster_id in clusters:
-        logger.info(f"Processing cluster id: {cluster_id}")
-
-        try:
-            xml_path, cl_cleaned_opinions = get_opinions_cleaned_content(
-                cluster_id
-            )
-        except EmptyOpinionException:
-            logger.warning(
-                f"At least one of the html_columbia fields in the opinions from "
-                f"cluster id: {cluster_id} is empty"
-            )
+        logger.info(f"Starting opinion cluster: {cluster_id}")
+        opinions = (
+            Opinion.objects.filter(cluster=cluster_id)
+            .exclude(local_path="")
+            .values_list("id", "type", "html_columbia", "local_path")
+        )
+        op_types = [op[1] for op in opinions]
+        if len(opinions) < 2:
+            # less than two opinions ... easy peasy
+            logger.info(f"Skipping opinion cluster with only one opinion.")
             continue
+        elif (
+            len(op_types) == 2
+            and Opinion.LEAD in op_types
+            and len(set(op_types)) == 2
+        ):
+            # If only two opinions and one is the lead - assign it to the number 1
+            logger.info(f"Sorting opinions with 1 Lead Opinion.")
+            opinions = [op[:2] for op in opinions]
+            ordered_opinions = sorted(opinions, key=lambda fields: fields[1])
+        else:
+            logger.info(f"Sorting order by location.")
+            ordered_opinions = match_text(opinions, xml_dir)
 
-        if not xml_path:
-            logger.warning(
-                f"Unable to find an xml file assigned to any opinion for cluster id: "
-                f"{cluster_id}"
-            )
-            continue
+        order_key = 1
+        for op in ordered_opinions:
+            opinion_obj = Opinion.objects.get(id=op[0])
+            opinion_obj.ordering_key = order_key
+            opinion_obj.save()
+            order_key += 1
 
-        extracted_columbia_opinions = None
-        if xml_path:
-            fixed_xml_filepath = os.path.join(
-                options.get("xml_dir"), fix_filepath(xml_path)  # type: ignore
-            )
+        logger.info(f"Opinion Cluster Saved.")
 
-            if not os.path.exists(fixed_xml_filepath):
-                logger.warning(
-                    f"Xml file not found in {fixed_xml_filepath}, cluster id: {cluster_id}"
-                )
-                continue
-
-            try:
-                extracted_columbia_opinions = get_opinions_columbia_file(
-                    fixed_xml_filepath
-                )
-            except UnicodeDecodeError:
-                logger.warning(
-                    f"Cannot decode file: {fixed_xml_filepath}, cluster id: {cluster_id}"
-                )
-                continue
-
-        if cl_cleaned_opinions and extracted_columbia_opinions:
-            columbia_opinions_content = [
-                op.get("opinion")
-                for op in extracted_columbia_opinions
-                if op.get("opinion")
-            ]
-            cl_opinions_content = [
-                op.get("opinion")
-                for op in cl_cleaned_opinions
-                if op.get("opinion")
-            ]
-
-            if len(columbia_opinions_content) != len(cl_opinions_content):
-                logger.warning(
-                    f"The number of opinions in cl and the number of opinions in the "
-                    f"xml is different, cluster id: {cluster_id}"
-                )
-                continue
-
-            # Try to match content between cl and xml
-            matches = match_opinion_lists(
-                columbia_opinions_content,
-                cl_opinions_content,
-            )
-
-            if matches:
-                if len(matches.values()) != len(set(matches.values())):
-                    # We don't have a unique match for each opinion, they were
-                    # probably combined incorrectly
-                    logger.info(
-                        f"We can't infer opinions order for cluster id: {cluster_id}"
-                    )
-                    # Go to next cluster id
-                    continue
-
-                if len(cl_cleaned_opinions) > len(set(matches.values())):
-                    # We have more opinions than matches
-                    logger.info(
-                        f"We couldn't match all cl opinions to the file's "
-                        f"content, cluster id: {cluster_id}"
-                    )
-                    # Go to next cluster id
-                    continue
-
-                # All opinions matched, update all opinions order
-                update_opinions(
-                    cluster_id,
-                    cl_cleaned_opinions,
-                    extracted_columbia_opinions,
-                    matches,
-                )
-
-                logger.info(
-                    msg=f"Columbia opinions reordered for cluster id: {cluster_id}"
-                )
-                # Wait between each processed cluster to avoid issues with elastic
-                time.sleep(options["delay"])
+        # Wait between each processed cluster to avoid issues with elastic
+        time.sleep(options["delay"])
 
 
 class Command(VerboseCommand):
@@ -421,7 +245,7 @@ class Command(VerboseCommand):
         )
         parser.add_argument(
             "--xml-dir",
-            default="/opt/courtlistener/_columbia",
+            default="/opt/courtlistener/columbia/usb",
             required=False,
             help="The absolute path to the directory with columbia xml files",
         )
