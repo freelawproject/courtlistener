580585e374511fb13bf1a89a77e690b2a6b967a3
elisa-av <elisa@djavu.cl>
2024-09-17 19:51:16 -0300

    docs(static): Add docstring to responsive-table.js

--- cl/users/static/js/responsive-table.js
+++ cl/users/static/js/responsive-table.js
@@ -1,4 +1,9 @@
 /**
+ *  To make a table responsive, add the class `responsive-table-wrapper` to the parent of the <table> element.
+ *  To add the header labels to the stacked columns, include the attribute `data-label` in the <th> elements (optional)
+ *  */
+
+/**
  * Adds header labels as `::before` pseudo-elements to table cells for mobile views.
  */
 function ResponsiveCellHeaders() {
@@ -34,44 +39,8 @@ function ResponsiveCellHeaders() {
   }
 }
 
-// https://adrianroselli.com/2018/02/tables-css-display-properties-and-aria.html
-// https://adrianroselli.com/2018/05/functions-to-add-aria-to-tables-and-lists.html
-function AddTableARIA() {
-  try {
-    var allTables = document.querySelectorAll('table');
-    for (var i = 0; i < allTables.length; i++) {
-      allTables[i].setAttribute('role', 'table');
-    }
-    var allRowGroups = document.querySelectorAll('thead, tbody, tfoot');
-    for (var i = 0; i < allRowGroups.length; i++) {
-      allRowGroups[i].setAttribute('role', 'rowgroup');
-    }
-    var allRows = document.querySelectorAll('tr');
-    for (var i = 0; i < allRows.length; i++) {
-      allRows[i].setAttribute('role', 'row');
-    }
-    var allCells = document.querySelectorAll('td');
-    for (var i = 0; i < allCells.length; i++) {
-      allCells[i].setAttribute('role', 'cell');
-    }
-    var allHeaders = document.querySelectorAll('th');
-    for (var i = 0; i < allHeaders.length; i++) {
-      allHeaders[i].setAttribute('role', 'columnheader');
-    }
-    // this accounts for scoped row headers
-    var allRowHeaders = document.querySelectorAll('th[scope=row]');
-    for (var i = 0; i < allRowHeaders.length; i++) {
-      allRowHeaders[i].setAttribute('role', 'rowheader');
-    }
-    // caption role not needed as it is not a real role and
-    // browsers do not dump their own role with display block
-  } catch (e) {
-    console.log('AddTableARIA(): ' + e);
-  }
-}
 document.addEventListener('DOMContentLoaded', function () {
   ResponsiveCellHeaders();
-  AddTableARIA();
 });
 
 function toggle() {
