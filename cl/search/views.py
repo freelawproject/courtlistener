a47fb9e90a05ab82beb691bb7d13e4fc4ab2cd0e
legaltextai <144342123+legaltextai@users.noreply.github.com>
2024-09-17 17:12:56 +0000

    fix(search): implements changes to search to capture user queries
    Makes changes to models, views, adds migrations
    
     Fixes: #4230

--- cl/search/views.py
+++ cl/search/views.py
@@ -79,13 +79,12 @@ from cl.search.exception import (
     UnbalancedQuotesQuery,
 )
 from cl.search.forms import SearchForm, _clean_form
-from cl.search.models import SEARCH_TYPES, Court, Opinion, OpinionCluster
+from cl.search.models import SEARCH_TYPES, Court, Opinion, OpinionCluster, SearchQuery
 from cl.stats.models import Stat
 from cl.stats.utils import tally_stat
 from cl.visualizations.models import SCOTUSMap
 
 logger = logging.getLogger(__name__)
-
 
 def check_pagination_depth(page_number):
     """Check if the pagination is too deep (indicating a crawler)"""
@@ -347,7 +346,6 @@ def show_results(request: HttpRequest) -> HttpResponse:
     All of these paths have tests.
     """
     # Create a search string that does not contain the page numbers
-
     get_string = make_get_string(request)
     get_string_sans_alert = make_get_string(
         request, ["page", "edit_alert", "show_alert_modal"]
@@ -513,42 +511,51 @@ def show_results(request: HttpRequest) -> HttpResponse:
                 # Just a regular search
                 if not is_bot(request):
                     async_to_sync(tally_stat)("search.results")
+                    
+                    # Perform the search
+                    search_type = request.GET.get("type", SEARCH_TYPES.OPINION)
+
+                    if search_type == SEARCH_TYPES.PARENTHETICAL:
+                        render_dict.update(do_es_search(request.GET.copy()))
+                    elif search_type == SEARCH_TYPES.ORAL_ARGUMENT:
+                        # Check if waffle flag is active.
+                        if waffle.flag_is_active(request, "oa-es-active"):
+                            render_dict.update(do_es_search(request.GET.copy()))
+                        else:
+                            render_dict.update(do_search(request.GET.copy()))
+                    elif search_type == SEARCH_TYPES.PEOPLE:
+                        if waffle.flag_is_active(request, "p-es-active"):
+                            render_dict.update(do_es_search(request.GET.copy()))
+                        else:
+                            render_dict.update(do_search(request.GET.copy()))
+                    elif search_type in [SEARCH_TYPES.RECAP, SEARCH_TYPES.DOCKETS]:
+                        if waffle.flag_is_active(request, "r-es-active"):
+                            search_results = do_es_search(request.GET.copy())
+                        else:
+                            search_results = do_search(request.GET.copy())
+                        render_dict.update(search_results)
+                    elif search_type == SEARCH_TYPES.OPINION:
+                        if waffle.flag_is_active(request, "o-es-active"):
+                            render_dict.update(do_es_search(request.GET.copy()))
+                        else:
+                            render_dict.update(do_search(request.GET.copy()))
+                    elif search_type == SEARCH_TYPES.RECAP_DOCUMENT:
+                        render_dict.update(do_es_search(request.GET.copy()))
+                    else:
+                        render_dict.update(do_search(request.GET.copy()))
+
+                    # Create and save the SearchQuery object
+                    SearchQuery.objects.create(
+                        get_params=request.GET.urlencode(),
+                        query_time_ms=render_dict.get('query_time', 0),
+                        hit_cache=render_dict.get('hit_cache', False)
+                    )
 
                 # Create bare-bones alert form.
                 alert_form = CreateAlertForm(
                     initial={"query": get_string, "rate": "dly"},
                     user=request.user,
                 )
-            search_type = request.GET.get("type", SEARCH_TYPES.OPINION)
-            match search_type:
-                case SEARCH_TYPES.PARENTHETICAL:
-                    render_dict.update(do_es_search(request.GET.copy()))
-                case SEARCH_TYPES.ORAL_ARGUMENT:
-                    # Check if waffle flag is active.
-                    if waffle.flag_is_active(request, "oa-es-active"):
-                        render_dict.update(do_es_search(request.GET.copy()))
-                    else:
-                        render_dict.update(do_search(request.GET.copy()))
-                case SEARCH_TYPES.PEOPLE:
-                    if waffle.flag_is_active(request, "p-es-active"):
-                        render_dict.update(do_es_search(request.GET.copy()))
-                    else:
-                        render_dict.update(do_search(request.GET.copy()))
-                case SEARCH_TYPES.RECAP | SEARCH_TYPES.DOCKETS:
-                    if waffle.flag_is_active(request, "r-es-active"):
-                        search_results = do_es_search(request.GET.copy())
-                    else:
-                        search_results = do_search(request.GET.copy())
-                    render_dict.update(search_results)
-                case SEARCH_TYPES.OPINION:
-                    if waffle.flag_is_active(request, "o-es-active"):
-                        render_dict.update(do_es_search(request.GET.copy()))
-                    else:
-                        render_dict.update(do_search(request.GET.copy()))
-                case SEARCH_TYPES.RECAP_DOCUMENT:
-                    render_dict.update(do_es_search(request.GET.copy()))
-                case _:
-                    render_dict.update(do_search(request.GET.copy()))
 
             # Set the value to the query as a convenience
             alert_form.fields["name"].widget.attrs["value"] = render_dict[
@@ -557,7 +564,6 @@ def show_results(request: HttpRequest) -> HttpResponse:
             render_dict.update({"alert_form": alert_form})
 
             return TemplateResponse(request, "search.html", render_dict)
-
 
 def advanced(request: HttpRequest) -> HttpResponse:
     render_dict = {"private": False}
