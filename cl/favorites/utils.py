22c7a7d4ba745d352d62dd0aef662b4dc2eafa77
v_anne <69829523+v-anne@users.noreply.github.com>
2024-09-23 18:39:27 -0400

    starting email alerts system

--- cl/favorites/utils.py
+++ cl/favorites/utils.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
 from django.db.models import (
     Avg,
     Count,
