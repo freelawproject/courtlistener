bc365d6daf10525b49f10710d50cc8ed486c2c4a
Alberto Islas <albertisfu@gmail.com>
2024-10-10 18:56:12 -0600

    feat(recap): Introduced clean_up_docket_judges
    Fixes:  #4541

---
+++ cl/recap/management/commands/clean_up_docket_judges.py
@@ -0,0 +1,77 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+import time
+
+from asgiref.sync import async_to_sync
+from django.db.models import Q
+
+from cl.lib.command_utils import VerboseCommand, logger
+from cl.people_db.lookup_utils import lookup_judge_by_full_name
+from cl.search.models import Docket
+
+
+def find_and_fix_docket_judges(
+    iteration_wait: float, testing_mode: bool
+) -> None:
+    """Find and fix Dockets referred_to and assigned_to judges.
+
+    :param iteration_wait: A float that indicates the time to wait between iterations.
+    :param testing_mode: True if testing mode is enabled to avoid wait between
+    iterations.
+    :return: None
+    """
+
+    logger.info("Finding dockets with judges.")
+    dockets_with_judges = Docket.objects.filter(
+        Q(referred_to__isnull=False) | Q(assigned_to__isnull=False)
+    )
+    fixed_dockets = 0
+    for d in dockets_with_judges.iterator():
+        new_referred = (
+            async_to_sync(lookup_judge_by_full_name)(
+                d.referred_to_str, d.court_id, d.date_filed
+            )
+            if d.referred_to_str
+            else None
+        )
+        new_assigned = (
+            async_to_sync(lookup_judge_by_full_name)(
+                d.assigned_to_str, d.court_id, d.date_filed
+            )
+            if d.assigned_to_str
+            else None
+        )
+
+        if d.referred_to != new_referred or d.assigned_to != new_assigned:
+            logger.info("Fixing Docket with ID %s", d.pk)
+            d.referred_to = new_referred
+            d.assigned_to = new_assigned
+            d.save()
+            fixed_dockets += 1
+            if not testing_mode:
+                # This will hit ES. So better to do it slowly.
+                time.sleep(iteration_wait)
+
+    logger.info("Fixed: %s Dockets", fixed_dockets)
+
+
+class Command(VerboseCommand):
+    help = "Find and fix Dockets referred and assigned judges."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--iteration-wait",
+            type=float,
+            default="0.1",
+            help="The time to wait between each iteration.",
+        )
+        parser.add_argument(
+            "--testing-mode",
+            action="store_true",
+            help="Use this flag only when running the command in tests based on TestCase",
+        )
+
+    def handle(self, *args, **options):
+        testing_mode = options.get("testing_mode", False)
+        iteration_wait = options.get("iteration_wait", 0.1)
+        find_and_fix_docket_judges(iteration_wait, testing_mode)
