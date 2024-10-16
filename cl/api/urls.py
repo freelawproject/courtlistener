84e9f91276747f6deafe201b7eb09dfe437a4a51
Mike Lissner <mike@free.law>
2024-10-15 21:09:01 -0700

    Revert "4507 pray and pay api"

--- cl/api/urls.py
+++ cl/api/urls.py
@@ -96,9 +96,6 @@ router.register(
     r"docket-tags", favorite_views.DocketTagViewSet, basename="DocketTag"
 )
 
-# Prayers
-router.register(r"prayers", favorite_views.PrayerViewSet, basename="prayer")
-
 # Visualizations
 router.register(
     r"visualizations/json", viz_views.JSONViewSet, basename="jsonversion"
