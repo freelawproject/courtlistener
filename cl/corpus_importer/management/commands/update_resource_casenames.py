23e3ef7a6dc4a30dae7a453b9c574f2b46ded34f
Kevin Ramirez <kvnzavalza@hotmail.com>
2024-11-12 20:43:33 -0600

    feat(resource): command to update case names using wl dataset

--- cl/corpus_importer/management/commands/update_resource_casenames.py
+++ cl/corpus_importer/management/commands/update_resource_casenames.py
@@ -2,7 +2,6 @@ import logging
 import re
 import time
 from datetime import date, datetime
-from typing import Set
 
 import pandas as pd
 from django.core.management.base import BaseCommand, CommandError
@@ -10,12 +9,240 @@ from eyecite import get_citations
 from eyecite.models import FullCaseCitation
 from eyecite.tokenizers import HyperscanTokenizer
 
-from cl.corpus_importer.utils import winnow_case_name
-from cl.lib.model_helpers import clean_docket_number
 from cl.search.models import Citation
 
 logger = logging.getLogger(__name__)
 HYPERSCAN_TOKENIZER = HyperscanTokenizer(cache_dir=".hyperscan")
+
+
+def find_matches(csv_case_name: str, cl_case_name: str):
+    """Compare two case name and decide whether they are the same or not
+
+    :param csv_case_name: case name from csv
+    :param cl_case_name: case name from cluster
+    :return: True if they match else False
+    """
+    # Tokenize each string, capturing both words and abbreviations with periods
+    csv_case_name_words = re.findall(r"\b\w+\b|\b\w+\.\b", csv_case_name)
+    cluster_case_name_words = re.findall(r"\b\w+\b|\b\w+\.\b", cl_case_name)
+
+    # Helper function to check if word1 is an abbreviation of word2 or vice versa
+    # Convert all words to lowercase for case-insensitive matching
+    csv_case_name_words_lower = [
+        word.lower() for word in csv_case_name_words if len(word) > 1
+    ]
+    cluster_case_name_words_lower = [
+        word.lower() for word in cluster_case_name_words if len(word) > 1
+    ]
+
+    overlap = set(csv_case_name_words_lower) & set(
+        cluster_case_name_words_lower
+    )
+
+    # print("overlap", overlap)
+
+    false_positive_set = {
+        "and",
+        "personal",
+        "restraint",
+        "matter",
+        "county",
+        "city",
+        "of",
+        "the",
+        "estate",
+        "in",
+        "inc",
+        "re",
+        "st",
+        "ex",
+        "rel",
+        "v",
+        "vs",
+        "for",
+        "a",
+    }
+
+    # these are valid overlaps, excluding anything in false_positive_set
+    hits = [item for item in overlap if item not in false_positive_set]
+
+    if not hits:
+        # if no hits no match on name - move along
+        return False
+
+    # Check for "v." in title
+    if "v." not in csv_case_name.lower():
+        # in the matter of Smith
+        # if no V. - likely a in re. case and only match on atleast 1 name
+        return True
+
+    # otherwise check if a match occurs on both sides of the V
+    v_index = csv_case_name.lower().index("v.")
+    hit_index = [csv_case_name.lower().index(hit) for hit in hits]
+
+    if min(hit_index) < v_index < max(hit_index):
+        return True
+
+    # logger.info(f"Row index: {row_index} - No match found with: {match.cluster}")
+    return False
+
+
+def parse_date(date_str: str) -> date | None:
+    """Attempts to parse the filed date into a datetime object.
+
+    # January 10, 1999
+    # 24-Jul-97
+    # 21-Jan-94
+    # 1/17/1961
+    # 12/1/1960
+    # 26-Sep-00
+    # Feb. 28, 2001
+
+    :param date_str: date string
+    :return: date object or none
+    """
+    for fmt in ("%B %d, %Y", "%d-%b-%y", "%m/%d/%Y", "%m/%d/%y", "%b. %d, %Y"):
+        try:
+            return datetime.strptime(date_str, fmt).date()
+        except (ValueError, TypeError):
+            continue
+    logger.warning(f"Invalid date format: {date_str}")
+    return None
+
+
+def make_citations(cite1, cite2) -> list[FullCaseCitation]:
+    """Validate citations with eyecite
+
+    :param cite1: first string citation
+    :param cite2: second string citation
+    :return: list of valid FullCaseCitation objects
+    """
+    cite_one = get_citations(cite1, tokenizer=HYPERSCAN_TOKENIZER)
+    cite_two = get_citations(cite2, tokenizer=HYPERSCAN_TOKENIZER)
+
+    citations = cite_one + cite_two
+    cites = [cite for cite in citations if isinstance(cite, FullCaseCitation)]
+    return cites
+
+
+def process_csv(
+    filepath: str, delay: float, dry_run: bool, chunk_size: int
+) -> None:
+    """Process rows from csv file
+
+    :param filepath: path to csv file
+    :param delay: delay between saves in seconds
+    :param dry_run: flag to simulate update process
+    :param chunk_size: number of rows to read at a time
+    """
+
+    logger.info(f"Processing {filepath}")
+    for chunk in pd.read_csv(filepath, chunksize=chunk_size):
+        for row in chunk.dropna().itertuples():
+            index, title, court, date_str, cite1, cite2, docket, volume = row
+            valid_citations = make_citations(cite1, cite2)
+
+            if len(valid_citations) < 2:
+                # Skipping row without two citations
+                continue
+
+            # Check if already have both citations from row
+            try:
+                c = Citation.objects.filter(
+                    **valid_citations[0].groups
+                ).values_list("cluster", flat=True)
+                d = Citation.objects.filter(
+                    **valid_citations[1].groups
+                ).values_list("cluster", flat=True)
+            except ValueError:
+                logger.warning(
+                    f"Row index: {index} - Citation parsing failed."
+                )
+                continue
+
+            overlapping_clusters = c.intersection(d)
+            if overlapping_clusters:
+                logger.info(
+                    f"Row index: {index} - Both citations exist for this cluster: {list(overlapping_clusters)}"
+                )
+                continue
+
+            if not valid_citations:
+                logger.info(f"Row index: {index} - No valid citations found.")
+                continue
+
+            clean_docket_num = docket.strip('="').strip('"')
+
+            date_filed = parse_date(date_str)
+            if not date_filed:
+                logger.info(f"Row index: {index} - No valid date found.")
+                continue
+
+            # Keep non westlaw citations, use them to try to find a match
+            remaining_citations = [
+                cite
+                for cite in valid_citations
+                if cite.corrected_reporter() != "WL"
+            ]
+
+            for citation in remaining_citations:
+
+                possible_matches = Citation.objects.filter(
+                    **citation.groups,
+                    cluster__docket__docket_number__contains=clean_docket_num,
+                    cluster__date_filed=date_filed,
+                )
+                if not possible_matches:
+                    # Match not found with citation, docket number and date filed
+                    continue
+
+                single_match = None
+                for match in possible_matches:
+                    case_name = (
+                        match.cluster.case_name_full
+                        if match.cluster.case_name_full
+                        else match.cluster.case_name
+                    )
+                    match_on_caption = find_matches(title, case_name)
+                    if match_on_caption:
+                        if not single_match:
+                            single_match = match.cluster
+                            continue
+                        else:
+                            logger.warning(
+                                f"Row index: {index} - Failed: too many matches"
+                            )
+                            single_match = None
+                            # Exit possible_matches loop
+                            break
+
+                if single_match:
+                    m = (
+                        single_match.case_name_full
+                        if single_match.case_name_full
+                        else single_match.case_name
+                    )
+                    citation_to_add = [
+                        cite
+                        for cite in valid_citations
+                        if cite.corrected_citation()
+                        != citation.corrected_citation()
+                    ]
+                    logger.info(
+                        f"Row index: {index} - Match found: {single_match} - New casename: {title} - Used citation: {citation.corrected_citation()} - To add: {citation_to_add[0].corrected_citation() if citation_to_add else 'Invalid'}"
+                    )
+                    # We already have matched the case using one of the citations
+
+                    if not dry_run:
+                        # Logic to save new case name and add citation
+
+                        # Wait between each processed cluster to avoid issues with redis memory
+                        time.sleep(delay)
+                    else:
+                        pass
+
+                    # Exit remaining_citations loop
+                    break
 
 
 class Command(BaseCommand):
@@ -58,284 +285,3 @@ class Command(BaseCommand):
             )
 
         process_csv(filepath, delay, dry_run, chunk_size)
-
-
-def process_csv(
-    filepath: str, delay: float, dry_run: bool, chunk_size: int
-) -> None:
-    """Process rows from csv file
-
-    :param filepath: path to csv file
-    :param delay: delay between saves in seconds
-    :param dry_run: flag to simulate update process
-    :param chunk_size: number of rows to read at a time
-    """
-    match_count = 0
-    total = 0
-    rcount = 0
-    row_count = pd.read_csv(filepath).shape[0]
-    logger.info(f"Total rows in CSV: {row_count}")
-    start_time = datetime.now()
-    logger.info(f"Start time: {start_time}")
-
-    for chunk in pd.read_csv(filepath, chunksize=chunk_size):
-        for _, row in chunk.iterrows():
-            rcount += 1
-            match_found = False
-            try:
-                # Retrieve fields and parse date
-                citation, docket_num, case_title, parallel_cite, filed_date = (
-                    row.get("Citation"),
-                    row.get("Docket Num"),
-                    row.get("Title"),
-                    row.get("Parallel Cite"),
-                    row.get("Filed Date"),
-                )
-
-                citation_str = citation
-                parallel_cite_str = parallel_cite
-
-                clean_cite = re.sub(r"\s+", " ", citation)
-                cites = get_citations(
-                    clean_cite, tokenizer=HYPERSCAN_TOKENIZER
-                )
-                cites = [
-                    cite
-                    for cite in cites
-                    if isinstance(cite, FullCaseCitation)
-                ]
-                if not cites:
-                    logger.warning(f"Invalid citation: {clean_cite}")
-                    continue
-
-                clean_cite = re.sub(r"\s+", " ", parallel_cite)
-                parallel_cites = get_citations(
-                    clean_cite, tokenizer=HYPERSCAN_TOKENIZER
-                )
-                parallel_cites = [
-                    cite
-                    for cite in parallel_cites
-                    if isinstance(cite, FullCaseCitation)
-                ]
-                if not parallel_cites:
-                    logger.warning(f"Invalid parallel citation: {clean_cite}")
-                    continue
-
-                main_citation = cites[0]
-                parallel_cite = parallel_cites[0]
-
-                if Citation.objects.filter(
-                    volume=parallel_cite.groups["volume"],
-                    reporter=parallel_cite.corrected_reporter(),
-                    page=parallel_cite.groups["page"],
-                ).exists():
-                    continue
-
-                if not all([main_citation, docket_num, case_title]):
-                    logger.warning(
-                        "Missing essential fields in row; skipping."
-                    )
-                    continue
-
-                formatted_date = parse_date(filed_date)
-
-                # Query citations in the database
-                citations = Citation.objects.filter(
-                    volume=main_citation.groups["volume"],
-                    reporter=main_citation.corrected_reporter(),
-                    page=main_citation.groups["page"],
-                )
-                if not citations:
-                    continue
-                total += 1
-
-                for citation_obj in citations:
-                    if is_match(
-                        citation_obj, docket_num, formatted_date, case_title
-                    ):
-                        match_found = True
-                        match_count += 1
-
-                        display_match_info(
-                            citation_obj,
-                            case_title,
-                            parallel_cite_str,
-                        )
-
-                        if not dry_run:
-                            cluster_casename = (
-                                citation_obj.cluster.case_name
-                                if citation_obj.cluster.case_name
-                                else citation_obj.cluster.case_name_full
-                            )
-                            docket_casename = (
-                                citation_obj.cluster.docket.case_name
-                                if citation_obj.cluster.docket.case_name
-                                else citation_obj.cluster.docket.case_name_full
-                            )
-                            if len(case_title) < len(cluster_casename):
-                                # Save new case name in cluster
-                                logger.info(
-                                    f"Case name updated for cluster id: {citation_obj.cluster_id}"
-                                )
-                                citation_obj.cluster.case_name = case_title
-                                citation_obj.cluster.save()
-                            else:
-                                logger.info(
-                                    f"Cluster: {citation_obj.cluster_id} already have the best name."
-                                )
-
-                            if len(case_title) < len(docket_casename):
-                                # Save new case name in docket
-                                logger.info(
-                                    f"Case name updated for docket id: {citation_obj.cluster.docket_id}"
-                                )
-                                citation_obj.cluster.docket.case_name = (
-                                    case_title
-                                )
-                                citation_obj.cluster.docket.save()
-
-                            else:
-                                logger.info(
-                                    f"Docket: {citation_obj.cluster.docket_id} already have the best name."
-                                )
-
-                            # Wait between updates to avoid issues with redis memory
-                            time.sleep(delay)
-
-                        break
-
-                if not match_found:
-                    logger.info(
-                        f"Failed: {citation_str} - {docket_num} - {case_title} - {parallel_cite_str} - {filed_date}"
-                    )
-
-            except Exception as e:
-                logger.error(f"Unexpected error processing row {row}: {e}")
-
-    logger.info(f"Total matches found: {match_count}")
-    end_time = datetime.now()
-    logger.info(f"End time: {end_time - start_time}")
-
-
-def parse_date(date_str: str) -> date | None:
-    """Attempts to parse the filed date into a datetime object.
-
-    # January 10, 1999
-    # 24-Jul-97
-    # 21-Jan-94
-    # 1/17/1961
-    # 12/1/1960
-    # 26-Sep-00
-    # Feb. 28, 2001
-
-    :param date_str: date string
-    :return: date object or none
-    """
-    for fmt in ("%B %d, %Y", "%d-%b-%y", "%m/%d/%Y", "%m/%d/%y", "%b. %d, %Y"):
-        try:
-            return datetime.strptime(date_str, fmt).date()
-        except (ValueError, TypeError):
-            continue
-    logger.warning(f"Invalid date format: {date_str}")
-    return None
-
-
-def is_match(citation, docket_num, formatted_date, case_title) -> bool:
-    """Checks if the database citation matches docket number, filing date, and case
-    title.
-
-    :param citation: Citation object that matched csv citation
-    :param docket_num: Docket number from csv
-    :param formatted_date: Formated date from csv
-    :param case_title: Case name from csv
-    :return: True if match found else False
-    """
-
-    if not citation.cluster.docket.docket_number:
-        # There is no docket number, abort
-        return False
-
-    # Prepare docket numbers
-    cleaned_cluster_docket_number = clean_docket_number(
-        citation.cluster.docket.docket_number
-    )
-    cleaned_docket_num = clean_docket_number(docket_num)
-
-    # In some cases clean_docket_number returns an empty string, try with original
-    # docket numbers
-    if not cleaned_cluster_docket_number:
-        cleaned_cluster_docket_number = citation.cluster.docket.docket_number
-
-    if not cleaned_docket_num:
-        cleaned_docket_num = docket_num
-
-    # Check docket number and date
-    failed = 0
-    if (
-        cleaned_cluster_docket_number.lower() not in cleaned_docket_num.lower()
-        or citation.cluster.date_filed != formatted_date
-    ):
-        if (
-            cleaned_cluster_docket_number.lower()
-            not in cleaned_docket_num.lower()
-        ):
-            failed += 1
-        if citation.cluster.date_filed != formatted_date:
-            failed += 10
-        return False
-
-    if (
-        not citation.cluster.case_name_full
-        or citation.cluster.case_name_full == ""
-    ):
-        cn = citation.cluster.case_name
-    else:
-        cn = citation.cluster.case_name_full
-
-    c1 = winnow_case_name(cn)
-    c2 = winnow_case_name(case_title)
-
-    overlap = c1 & c2
-    if overlap:
-        cf = (
-            citation.cluster.case_name_full
-            if citation.cluster.case_name_full
-            else citation.cluster.case_name
-        )
-        order1 = get_term_indices(overlap, cf)
-        order2 = get_term_indices(overlap, case_title)
-
-        return list(order1.keys()) == list(order2.keys()) and len(overlap) > 1
-
-    return False
-
-
-def get_term_indices(terms: Set, text: str) -> dict:
-    """Returns a dictionary of each term's index in the text, sorted by appearance order.
-
-    :param terms: set of terms to search for
-    :param text: text to search for terms
-    :return: dict of each term's index in the text
-    """
-    term_indices = {
-        term: match.start()
-        for term in terms
-        if (match := re.search(r"\b" + re.escape(term) + r"\b", text.lower()))
-    }
-    return dict(sorted(term_indices.items(), key=lambda item: item[1]))
-
-
-def display_match_info(citation, case_title, parallel_cite):
-    """Displays information about a match in a structured format.
-
-    :param citation: Citation object
-    :param case_title: case name from csv
-    :param parallel_cite: cite from csv
-    """
-    logger.info(
-        f"Matched Case in DB: {citation.cluster.id} - {citation.cluster.case_name_full if citation.cluster.case_name_full else citation.cluster.case_name}"
-    )
-    logger.info(f"CSV Case Title: {case_title}")
-    logger.info(f"Matching Citation: {citation}")
-    logger.info(f"Parallel Cite: {parallel_cite}")
