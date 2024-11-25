3337f5bfb273cc2ba3846310f828e3962eeae602
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2024-11-25 12:21:51 -0500

    feat(scrapers.admin): create admin page for a scraper status page
    - Includes a migration file for the materialized view
    - MV will have to be refreshed manually or via a cronjob
    - MV considers only courts that have an active scraper, and that have no updates in a week

--- cl/scrapers/admin.py
+++ cl/scrapers/admin.py
@@ -32,68 +32,37 @@ class PACERFreeDocumentRowAdmin(admin.ModelAdmin):
 admin.site.register(UrlHash)
 
 
-class MVLatestOpinions(models.Model):
+class MVLatestOpinion(models.Model):
     """
     Model linked to materialized view for monitoring scrapers
+
+    The SQL for creating the view is on it's migration file.
 
     Must use `REFRESH MATERIALIZED VIEW scrapers_mv_latest_opinion`
     periodically
     """
 
-    query = """
-    CREATE MATERIALIZED VIEW
-        scrapers_mv_latest_opinion
-    AS
-    (
-    SELECT
-        court_id,
-        max(so.date_created) as latest_creation_date,
-        (now() - max(so.date_created))::text as time_since
-    FROM
-        (
-            SELECT id, court_id
-            FROM search_docket
-            WHERE court_id IN (
-                SELECT id
-                FROM search_court
-                /*
-                    Only check courts with scrapers in use
-                */
-                WHERE
-                    has_opinion_scraper
-                    AND in_use
-            )
-        ) sd
-    INNER JOIN
-        (SELECT id, docket_id FROM search_opinioncluster) soc ON soc.docket_id = sd.id
-    INNER JOIN
-        search_opinion so ON so.cluster_id = soc.id
-    GROUP BY
-        sd.court_id
-    HAVING
-        /*
-            Only return results for courts with no updates in a week
-        */
-        now() - max(so.date_created) > interval '7 days'
-    ORDER BY
-        2 DESC
-    )
-    """
     # a django model must have a primary key
     court_id = models.TextField(primary_key=True)
-    latest_creation_date = models.DateField()
+    latest_creation_date = models.DateTimeField()
     time_since = models.TextField()
+    view_last_updated = models.DateTimeField()
 
     class Meta:
-        managed = False  # ignore this model in migrations
+        managed = False
         db_table = "scrapers_mv_latest_opinion"
 
 
-@admin.register(MVLatestOpinions)
-class MVLatestOpinionsAdmin(admin.ModelAdmin):
+@admin.register(MVLatestOpinion)
+class MVLatestOpinionAdmin(admin.ModelAdmin):
     """Admin page to look at the latest opinion for each court
 
     Use this to monitor silently failing scrapers
     """
 
-    list_display = ["court_id", "latest_creation_date", "time_since"]
+    list_display = [
+        "court_id",
+        "latest_creation_date",
+        "time_since",
+        "view_last_updated",
+    ]
