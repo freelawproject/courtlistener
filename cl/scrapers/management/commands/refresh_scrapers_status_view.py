610f9ff198d56319c8b8e9d5d0af72417aa05211
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2024-12-02 11:14:38 -0500

    feat(scrapers.admin): add refresh_scrapers_status_view command
    Also, delete customer's SQL

---
+++ cl/scrapers/management/commands/refresh_scrapers_status_view.py
@@ -0,0 +1,16 @@
+from cl.lib.command_utils import VerboseCommand,logger
+from django.db import connection
+
+class Command(VerboseCommand):
+    help = """Refreshes the `scrapers_mv_latest_opinion` materialized view.
+    
+    Check the cl.scrapers.admin.py file for more info about the view
+    """
+
+    def handle(self, *args, **options):
+        query = "REFRESH MATERIALIZED VIEW scrapers_mv_latest_opinion;"
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            
+        logger.info("View refresh completed successfully")
+        
