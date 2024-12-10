8462423e12890a526020f329bfa13ebf513ec20b
Eduardo Rosendo <eduardojra96@gmail.com>
2024-12-10 00:43:03 -0400

    docs(api): Updates helper method docstring

--- cl/api/tasks.py
+++ cl/api/tasks.py
@@ -93,7 +93,7 @@ def send_es_search_alert_webhook(
     """Send a search alert webhook event containing search results from a
     search alert object.
 
-    :param results: The search results returned by SOLR for this alert.
+    :param results: The search results returned for this alert.
     :param webhook_pk: The webhook endpoint ID object to send the event to.
     :param alert: The search alert object.
     """
@@ -134,7 +134,7 @@ def send_search_alert_webhook_es(
     """Send a search alert webhook event containing search results from a
     search alert object.
 
-    :param results: The search results returned by SOLR for this alert.
+    :param results: The search results returned for this alert.
     :param webhook_pk: The webhook endpoint ID object to send the event to.
     :param alert_pk: The search alert ID.
     """
