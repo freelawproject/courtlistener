8feb6c37b3f5c09ed2aec9f8bbce2ddeacb3d9f4
Eduardo Rosendo <eduardojra96@gmail.com>
2024-12-09 20:13:06 -0400

    feat(lib): Remove unused normalize_search_dicts helper

--- cl/lib/search_index_utils.py
+++ cl/lib/search_index_utils.py
@@ -28,32 +28,6 @@ null_map = dict.fromkeys(
 )
 
 
-def normalize_search_dicts(d):
-    """Prepare search dicts for indexing by solr.
-
-    1. Remove any kv from a dictionary if v is None
-
-       This is needed to send dictionaries to Scorched, instead of
-       sending objects, and should provide a performance improvement. If you try
-       to send None values to integer fields (for example), things break, b/c
-       integer fields shouldn't be getting None values. Fair 'nuf.
-
-    2. Convert any sets to lists.
-
-       This is needed because sets aren't JSON serializable, but they're
-       convenient to use when building up a search object.
-    """
-    new_dict = {}
-    for k, v in d.items():
-        if v is None:
-            continue
-        if isinstance(v, set):
-            new_dict[k] = list(v)
-        else:
-            new_dict[k] = v
-    return new_dict
-
-
 def get_parties_from_case_name(case_name: str) -> list[str]:
     """Extracts the parties from case_name by splitting on common case_name
     separators.
