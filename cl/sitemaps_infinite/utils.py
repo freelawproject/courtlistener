9d2c63b3fbcf98ae02590b75a2d6c98d3725d7dc
Alex Topal <alextopal@gmail.com>
2024-12-24 01:16:35 +0100

    use sitemap section as a string, do not import

--- cl/sitemaps_infinite/utils.py
+++ cl/sitemaps_infinite/utils.py
@@ -5,13 +5,11 @@ from django.utils.module_loading import import_string
 
 
 def make_sitemaps_list(
-    sitemaps: dict[str:str],
-) -> OrderedDict[str : sitemaps.Sitemap]:
+    sitemaps: dict[str, str],
+) -> OrderedDict[str, sitemaps.Sitemap]:
     """Constructs an OrderedDict of Django Sitemap classes from a dictionary of string keys and string values representing the Sitemap class paths.
 
     The keys in the input dictionary are the names of the Sitemaps, and the values are the dotted paths to the Sitemap classes.
     This function uses `import_string` to dynamically import the Sitemap classes and return an OrderedDict mapping the names to the Sitemap instances.
     """
-    return OrderedDict(
-        {import_string(k): import_string(v) for k, v in sitemaps.items()}
-    )
+    return OrderedDict({k: import_string(v) for k, v in sitemaps.items()})
