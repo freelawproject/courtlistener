b39f5e32e9acc38928b359832a14596f7f987759
Alex Topal <alextopal@gmail.com>
2024-12-31 03:47:51 +0100

    main command with 'reset' option

--- cl/sitemaps_infinite/management/commands/generate_sitemaps.py
+++ cl/sitemaps_infinite/management/commands/generate_sitemaps.py
@@ -1,11 +1,28 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import CommandParser
 
-from cl.sitemaps_infinite.sitemap_generator import generate_urls_chunk
+from cl.lib.command_utils import VerboseCommand, logger
+from cl.sitemaps_infinite.sitemap_generator import (
+    generate_urls_chunk,
+    reset_sitemaps_cursor,
+)
 
 
-class Command(BaseCommand):
+class Command(VerboseCommand):
     help = """The command starts or continues the sitemap urls generation. 
     The place where the generation was stopped last time is saved into the redis cache and then loaded when the command starts"""
 
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            "--force-regenerate",
+            action="store_true",
+            required=False,
+            help="Force all cached pages to be regenerated",
+        )
+
     def handle(self, *args, **options):
-        generate_urls_chunk()
+
+        if options["force_regenerate"]:
+            reset_sitemaps_cursor()
+            logger.info("Sitemaps cursor was reset, run `generate_sitemaps` again to start the generation")
+
+        generate_urls_chunk(options["force_regenerate"])
