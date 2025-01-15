93d31a2aa1040710a996f33ecbdb48d7647ea4d7
Eduardo Rosendo <eduardojra96@gmail.com>
2025-01-15 19:13:27 -0400

    refactor(search): Moves helper functions to search_utils.py
    This commit refactors the search module by moving helper functions from `view.py` to `search_utils.py`. This improves code organization and makes these helper functions reusable across different modules.

--- cl/search/tests/tests_es_recap.py
+++ cl/search/tests/tests_es_recap.py
@@ -2584,7 +2584,7 @@ class RECAPSearchTest(RECAPSearchTestCase, ESIndexTestCase, TestCase):
         for docket in dockets_to_remove:
             docket.delete()
 
-    @mock.patch("cl.search.views.fetch_es_results")
+    @mock.patch("cl.lib.search_utils.fetch_es_results")
     @override_settings(
         RECAP_SEARCH_PAGE_SIZE=2, ELASTICSEARCH_MICRO_CACHE_ENABLED=True
     )
@@ -7162,7 +7162,7 @@ class RECAPIndexingTest(
         # 100 results, 10 pages.
         total_results = 100
         with mock.patch(
-            "cl.search.views.fetch_es_results",
+            "cl.lib.search_utils.fetch_es_results",
             side_effect=lambda *x: (
                 [],
                 1,
@@ -7182,7 +7182,7 @@ class RECAPIndexingTest(
         # 101 results, 11 pages.
         total_results = 101
         with mock.patch(
-            "cl.search.views.fetch_es_results",
+            "cl.lib.search_utils.fetch_es_results",
             side_effect=lambda *x: (
                 [],
                 1,
@@ -7202,7 +7202,7 @@ class RECAPIndexingTest(
         # 20,000 results, 2,000 pages.
         total_results = 20_000
         with mock.patch(
-            "cl.search.views.fetch_es_results",
+            "cl.lib.search_utils.fetch_es_results",
             side_effect=lambda *x: (
                 [],
                 1,
