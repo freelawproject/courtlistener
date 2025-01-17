bc87e570aed40de4171162e8cdb176cb65724ee7
v_anne <69829523+v-anne@users.noreply.github.com>
2025-01-16 22:05:29 -0500

    str, not float (hopefully last issue!)

--- cl/favorites/tests.py
+++ cl/favorites/tests.py
@@ -902,7 +902,7 @@ class RECAPPrayAndPay(TestCase):
         # Verify that the initial prayer count and total cost are 0.
         user_history = await get_user_prayer_history(self.user)
         self.assertEqual(user_history.prayer_count, 0)
-        self.assertEqual(user_history.total_cost, 0.00)
+        self.assertEqual(user_history.total_cost, "0.00")
 
         # Update `rd_3`'s page count and set `prayer_rd3`'s status to `GRANTED`
         self.rd_3.page_count = 2
@@ -914,7 +914,7 @@ class RECAPPrayAndPay(TestCase):
         # Verify that the count is 1 and total cost is 0.20.
         user_history = await get_user_prayer_history(self.user)
         self.assertEqual(user_history.prayer_count, 1)
-        self.assertEqual(user_history.total_cost, 0.20)
+        self.assertEqual(user_history.total_cost, "0.20")
 
         # Update `rd_5`'s page count and set `prayer_rd5`'s status to `GRANTED`
         self.rd_5.page_count = 40
@@ -926,7 +926,7 @@ class RECAPPrayAndPay(TestCase):
         # Verify that the count is 2 and the total cost is now 3.20.
         user_history = await get_user_prayer_history(self.user)
         self.assertEqual(user_history.prayer_count, 2)
-        self.assertEqual(user_history.total_cost, 3.20)
+        self.assertEqual(user_history.total_cost, "3.20")
 
     @patch("cl.favorites.utils.cache.aget")
     async def test_get_lifetime_prayer_stats(self, mock_cache_aget) -> None:
