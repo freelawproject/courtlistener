b5ae76d065dbc224af9426c6d08de79873a284c2
Alex Topal <alextopal@gmail.com>
2025-02-04 02:43:59 +0100

    SITEMAPS_URL_SCHEME is added

--- cl/settings/project/sitemaps.py
+++ cl/settings/project/sitemaps.py
@@ -1,11 +1,18 @@
+import environ
+
+env = environ.FileAwareEnv()
+
 # The number of sitemap 'files' (pages) to cache per sitemap generation call
-SITEMAPS_FILES_PER_CALL = 100
+SITEMAPS_FILES_PER_CALL = 10
 
 # list of the sitemaps that should be generated using the infinite pagination
 # dict format:   {'section_name': 'sitemap_class', ...}
 SITEMAPS_GENERATED_OFFLINE = {
-    "r": "cl.opinion_page.sitemap.DocketSitemap" # cl.search.models.SEARCH_TYPES.RECAP
+    "r": "cl.opinion_page.sitemap.DocketSitemap"  # cl.search.models.SEARCH_TYPES.RECAP
 }
+
+# generated urls schema
+SITEMAPS_URL_SCHEME = env.str("SITEMAPS_URL_SCHEME", "http")
 
 # @deprecated call sitemap file generation every 10 minutes via celery, set 0 to disable task (default)
 SITEMAPS_TASK_REPEAT_SEC = 0
