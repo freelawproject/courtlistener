3a0f58c7adfc9f57e420d6a5385666db30ad6874
v_anne <69829523+v-anne@users.noreply.github.com>
2025-02-18 12:57:29 -0500

    feat(favorites): Removes cache decorator from top prayers view

--- cl/favorites/views.py
+++ cl/favorites/views.py
@@ -187,7 +187,6 @@ async def view_tags(request, username):
     )
 
 
-@cache_page_ignore_params(30)  # Cache for 30 seconds
 async def open_prayers(request: HttpRequest) -> HttpResponse:
     """Show the user top open prayer requests."""
 
