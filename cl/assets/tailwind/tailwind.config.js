b2d188f0b9fe92fbdbeb7d2afba1f543ee66ef8f
Elisa Anguita <elisa@free.law>
2025-03-26 18:19:12 -0300

    refactor(tailwind): clean up content section of tailwind config
    - Enforce relative paths to avoid unexpected behavior.
    - Remove non-existent path.

--- cl/assets/tailwind/tailwind.config.js
+++ cl/assets/tailwind/tailwind.config.js
@@ -1,25 +1,21 @@
 module.exports = {
-  content: [
-    /**
-     * HTML. Paths to Django template files that will contain Tailwind CSS classes.
-     */
+  content: {
+    relative: true,
+    files: [
+      /**
+       * HTML. Paths to Django template files that will contain Tailwind CSS classes.
+       */
 
-    /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
-    '../templates/**/*.html',
+      /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
+      '../templates/**/*.html',
 
-    /*
-     * Main templates directory of the project (BASE_DIR/templates).
-     * Adjust the following line to match your project structure.
-     */
-    '../../templates/**/*.html',
-
-    /*
-     * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
-     * Adjust the following line to match your project structure.
-     */
-    '../../**/templates/**/*.html',
-    '../../**/templates/**/*.svg',
-  ],
+      /*
+       * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
+       * Adjust the following line to match your project structure.
+       */
+      '../../**/templates/**/*.html',
+    ],
+  },
   theme: {
     extend: {
       fontFamily: {
