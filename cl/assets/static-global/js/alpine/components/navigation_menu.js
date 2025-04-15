009dbabb32fe5221dfb3887ed7ce4759c11b5787
Elisa Anguita <elisa@free.law>
2025-04-14 21:32:11 -0400

    chore(assets): include Alpine component to handle nav menu

---
+++ cl/assets/static-global/js/alpine/components/navigation_menu.js
@@ -0,0 +1,58 @@
+document.addEventListener('alpine:init', () => {
+  Alpine.data('navMenu', () => ({
+    menuOpen: false,
+    expandedSections: [],
+    ids: ['listbox-option'],
+    options: [],
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen;
+    },
+    closeMenu() {
+      this.menuOpen = false;
+    },
+    toggleExpansion() {
+      const index = this.expandedSections.findIndex((el) => el === this.target);
+      if (index >= 0) {
+        this.expandedSections.splice(index, 1);
+      } else {
+        this.expandedSections.push(this.target);
+      }
+    },
+    get target() {
+      return this.$el.dataset.intersectTarget;
+    },
+    get isExpanded() {
+      return this.expandedSections.includes(this.target);
+    },
+    get visibleSectionText() {
+      const index = this.options.findIndex((opt) => opt.href === `#${this.$data.visibleSection}`);
+      return index >= 0 ? this.options[index].text : '';
+    },
+    get iconClass() {
+      return this.isExpanded ? 'transform rotate-180' : '';
+    },
+    get itemClass() {
+      return this.isVisible ? 'text-primary-600' : '';
+    },
+    get hasVisibleChild() {
+      const index = this.options.findIndex((el) => el.href === this.target);
+      if (index === -1 || !this.options[index].children) return false;
+      return this.options[index].children.some((child) => `#${this.visibleSection}` === child.href);
+    },
+    get markerClass() {
+      return this.isVisible || this.hasVisibleChild ? 'marker:text-primary-600' : 'marker:text-greyscale-200';
+    },
+    get childClass() {
+      return this.isVisible ? this.activeItemClasses : this.inactiveItemClasses;
+    },
+    get hasChildren() {
+      const index = this.options.findIndex((el) => el.href === this.target);
+      return index > -1 ? this.options[index].children : false;
+    },
+    init() {
+      this.$nextTick(() => {
+        this.options = JSON.parse(document.getElementById('nav-items').textContent);
+      });
+    },
+  }));
+});
