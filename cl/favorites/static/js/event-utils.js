7b1456cc9d482acbac6e27502b93074ee5921893
Eduardo Rosendo <eduardojra96@gmail.com>
2025-07-04 19:55:47 -0400

    feat(templates): Adds logic to trigger count increment via JS

---
+++ cl/favorites/static/js/event-utils.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  // get the csrf token
+  const token = getCookie('csrftoken');
+  const url = '/api/rest/v4/increment-event/';
+
+  // Get the event label element
+  const labelElement = document.getElementById('event_label');
+  if (!labelElement || !labelElement.value) {
+    console.warn('Event label is missing or empty.');
+    return;
+  }
+
+  const payload = { label: labelElement.value };
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRFToken': token,
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
+    console.error(' Error when trying to increment count for event ' + event_label.value);
+    return;
+  }
+
+  // Update count in the DOM if the element exists
+  const countElement = document.getElementById('event_count');
+  if (countElement) {
+    const data = await response.json();
+    countElement.textContent = data.value;
+  }
+});
