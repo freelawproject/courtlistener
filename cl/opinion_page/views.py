1d4ee347bc2516c677fe7e075325b541348357f4
Nathan Florea <florean@gmail.com>
2025-07-08 14:43:00 -0700

    refactor(opinion_page): remove redundant logic
    The logic used here is the exact same as Paginator.get_page().
    https://docs.djangoproject.com/en/5.2/ref/paginator/#django.core.paginator.Paginator.get_page

--- cl/opinion_page/views.py
+++ cl/opinion_page/views.py
@@ -366,13 +366,9 @@ async def view_docket(
 
     @sync_to_async
     def paginate_docket_entries(docket_entries, docket_page):
-        paginator = Paginator(docket_entries, 200, orphans=10)
-        try:
-            return paginator.page(docket_page)
-        except PageNotAnInteger:
-            return paginator.page(1)
-        except EmptyPage:
-            return paginator.page(paginator.num_pages)
+        return Paginator(
+            docket_entries, 200, orphans=10
+        ).get_page(docket_page)
 
     paginated_entries = await paginate_docket_entries(de_list, page)
 
