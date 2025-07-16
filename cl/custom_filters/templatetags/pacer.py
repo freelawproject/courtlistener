63af9b1c928f0f284b9c2f9b2c35d0df9393fbb3
Mike Weinberg <5876158+mikeweinberg@users.noreply.github.com>
2025-07-16 16:44:31 -0400

    Update pacer.py to fix price/cost of transcripts, which are not capped

--- cl/custom_filters/templatetags/pacer.py
+++ cl/custom_filters/templatetags/pacer.py
@@ -18,5 +18,8 @@ def price(rd: RECAPDocument) -> str:
     if rd.page_count:
         page_count = rd.page_count  # Create a variable for Sentry debugging
         cost = rd.page_count * 0.10
+        # cost is uncapped for transcripts
+        if (rd.description or "").lower() == "transcript":
+            return f"{cost:.2f}"
         return f"{min(3, cost):.2f}"
     return ""
