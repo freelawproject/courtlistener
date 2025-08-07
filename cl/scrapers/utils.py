freelawproject/courtlistener

commit e8289ac900f50ae003376f1f2f35443049c119dc

feat(scrapers): create OriginatingCourtInformation objects from scrapers (#6136)
* feat(scrapers): create OriginatingCourtInformation objects from scrapers

Fixes #5453

Use juriscraper values to populate OriginatingCourtInformation objects
- "lower_court_id" goes to Docket.appeal_from_id
- "lower_court_numbers" goes to OriginatingCourtInformation.docket_number
- "lower_court_judges" goes to OriginatingCourtInformation.assigned_to_str
- Update tests to check creation of OriginatingCourtInformation and confirm updates if they exist

cl/scrapers/utils.py
@@ -31,7 +31,13 @@ from cl.scrapers.exceptions import (
     NoDownloadUrlError,
     UnexpectedContentTypeError,
 )
-from cl.search.models import Citation, Court, Docket, OpinionCluster
+from cl.search.models import (
+    Citation,
+    Court,
+    Docket,
+    OpinionCluster,
+    OriginatingCourtInformation,
+)
 
 HYPERSCAN_TOKENIZER = HyperscanTokenizer(cache_dir=".hyperscan")
 
@@ -406,6 +412,7 @@ def update_or_create_docket(
     date_argued: date | None = None,
     ia_needs_upload: bool | None = None,
     appeal_from_str: str = "",
+    appeal_from_id: str = "",
 ) -> Docket:
     """Look for an existing Docket and update it or create a new one if it's
     not found.
@@ -425,6 +432,7 @@ def update_or_create_docket(
     :param date_argued: The docket date_argued if it's an oral argument.
     :param ia_needs_upload: If the docket needs upload to IA, default None.
     :param appeal_from_str: Name (not standardized id) of the lower level court.
+    :param appeal_from_id: Lower court id.
     :return: The docket.
     """
     docket_fields = {
@@ -434,9 +442,19 @@ def update_or_create_docket(
         "blocked": blocked,
         "ia_needs_upload": ia_needs_upload,
         "appeal_from_str": appeal_from_str,
+        "appeal_from_id": appeal_from_id,
         "date_blocked": date_blocked,
         "date_argued": date_argued,
     }
+    if not appeal_from_id:
+        docket_fields.pop("appeal_from_id", "")
+
+    if not Court.objects.filter(id=appeal_from_id).exists():
+        docket_fields.pop("appeal_from_id", "")
+        logger.error(
+            "Docket.appeal_from_id has non existing Court.id '%s' as value",
+            appeal_from_id,
+        )
 
     court_id = court.pk
     uses_docket_number_core = court.jurisdiction in Court.FEDERAL_JURISDICTIONS
@@ -597,3 +615,36 @@ def check_duplicate_ingestion(local_path_name: str) -> None:
             repeated_count,
             base_file_name,
         )
+
+
+def update_or_create_originating_court_information(
+    docket: Docket, lower_court_number: str, lower_court_judge: str
+) -> OriginatingCourtInformation | None:
+    """Update or create an OriginatingCourtInformation given the scraped values
+
+    :param docket: the docket to which the OCI will be linked
+    :param lower_court_number: will go into OCI.docket_number
+    :param lower_court_judge: will go into OCI.assigned_to_str
+    """
+    if not (lower_court_judge or lower_court_number):
+        return
+
+    if existing_oci := docket.originating_court_information:
+        update = False
+        if not existing_oci.docket_number and lower_court_number:
+            existing_oci.docket_number = lower_court_number
+            update = True
+        if not existing_oci.assigned_to_str and lower_court_judge:
+            existing_oci.assigned_to_str = lower_court_judge
+            update = True
+
+        if update:
+            existing_oci.save()
+
+        # If the docket already had a OriginatingCourtInformation, just return
+        return
+
+    return OriginatingCourtInformation(
+        docket_number=lower_court_number or "",
+        assigned_to_str=lower_court_judge or "",
+    )

cl/corpus_importer/management/commands/import_tn.py
@@ -120,7 +120,8 @@ def import_tn_corpus(
                 op.cluster.case_name,
             )
 
-        docket, opinion, cluster, citations = make_objects(
+        # oci - originating_court_information is always `None`` for this import
+        docket, opinion, cluster, citations, oci = make_objects(
             make_item(case),
             courts[case["court"]],
             sha1_hash,

cl/opinion_page/forms.py
@@ -487,7 +487,7 @@ class BaseCourtUploadForm(forms.Form):
         sha1_hash = sha1(force_bytes(self.cleaned_data.get("pdf_upload")))
         court = Court.objects.get(pk=self.cleaned_data.get("court_str"))
 
-        docket, opinion, cluster, citations = make_objects(
+        docket, opinion, cluster, citations, _ = make_objects(
             self.cleaned_data.get("item"),
             court,
             sha1_hash,

cl/scrapers/management/commands/cl_scrape_opinions.py
@@ -36,6 +36,7 @@ from cl.scrapers.utils import (
     save_response,
     signal_handler,
     update_or_create_docket,
+    update_or_create_originating_court_information,
 )
 from cl.search.models import (
     SEARCH_TYPES,
@@ -45,6 +46,7 @@ from cl.search.models import (
     Docket,
     Opinion,
     OpinionCluster,
+    OriginatingCourtInformation,
 )
 
 # for use in catching the SIGINT (Ctrl+4)
@@ -58,7 +60,13 @@ def make_objects(
     court: Court,
     sha1_hash: str,
     content: bytes,
-) -> tuple[Docket, Opinion, OpinionCluster, list[Citation]]:
+) -> tuple[
+    Docket,
+    Opinion,
+    OpinionCluster,
+    list[Citation],
+    OriginatingCourtInformation,
+]:
     """Takes the meta data from the scraper and associates it with objects.
 
     The keys returned by juriscraper scrapers are defined by `self._all_attrs`
@@ -92,6 +100,12 @@ def make_objects(
         blocked=blocked,
         date_blocked=date_blocked,
         appeal_from_str=item.get("lower_courts", ""),
+        appeal_from_id=item.get("lower_court_ids", ""),
+    )
+    originating_court_info = update_or_create_originating_court_information(
+        docket,
+        item.get("lower_court_numbers", ""),
+        item.get("lower_court_judges", ""),
     )
 
     # Note that if opinion.author_str has no value, and cluster.judges find
@@ -144,7 +158,7 @@ def make_objects(
     opinion.local_path.save(file_name, cf, save=False)
     check_duplicate_ingestion(opinion.local_path.name)
 
-    return docket, opinion, cluster, citations
+    return docket, opinion, cluster, citations, originating_court_info
 
 
 @transaction.atomic
@@ -155,6 +169,14 @@ def save_everything(
     """Saves all the sub items and associates them as appropriate."""
     docket, cluster = items["docket"], items["cluster"]
     opinion, citations = items["opinion"], items["citations"]
+    originating_court_info = items.get("originating_court_information")
+
+    # if the docket already had a related `originating_court_information`
+    # the update was saved in the `make_objects` call
+    if originating_court_info and not docket.originating_court_information:
+        originating_court_info.save()
+        docket.originating_court_information = originating_court_info
+
     docket.save()
     cluster.docket = docket
     cluster.save()
@@ -334,8 +356,8 @@ class Command(ScraperCommand):
 
         child_court = get_child_court(item.get("child_courts", ""), court.id)
 
-        docket, opinion, cluster, citations = make_objects(
-            item, child_court or court, sha1_hash, content
+        docket, opinion, cluster, citations, originating_court_info = (
+            make_objects(item, child_court or court, sha1_hash, content)
         )
 
         save_everything(
@@ -344,6 +366,7 @@ class Command(ScraperCommand):
                 "opinion": opinion,
                 "cluster": cluster,
                 "citations": citations,
+                "originating_court_information": originating_court_info,
             }
         )
         extract_doc_content.delay(

cl/scrapers/test_assets/test_opinion_scraper.py
@@ -55,6 +55,14 @@ class Site(OpinionSite):
         path = "//judge/text()"
         return list(self.html.xpath(path))
 
+    def _get_lower_court_numbers(self):
+        path = "//lower_court_number"
+        return [i.text for i in self.html.xpath(path)]
+
+    def _get_lower_court_judges(self):
+        path = "//lower_court_judge"
+        return [i.text for i in self.html.xpath(path)]
+
     def extract_from_text(self, scraped_text):
         metadata = {}
         docket_regex = r"Docket Number: (?P<docket>\d+-\d+)"

cl/scrapers/test_assets/test_opinion_site.xml
@@ -13,6 +13,8 @@
         <federal_cite></federal_cite>
         <west_state_cite></west_state_cite>
         <nature_of_suit></nature_of_suit>
+        <lower_court_number></lower_court_number>
+        <lower_court_judge></lower_court_judge>
     </opinion>
     -->
 
@@ -28,6 +30,8 @@
         <west_state_cite>4 Neb. 33</west_state_cite>
         <nature_of_suit>copyright</nature_of_suit>
         <judge>david</judge>
+        <lower_court_number>09-1111</lower_court_number>
+        <lower_court_judge>another david</lower_court_judge>
     </opinion>
     <opinion>
         <!-- A PDF requiring OCR -->
@@ -40,6 +44,8 @@
         <federal_cite>2 F.2d 4</federal_cite>
         <nature_of_suit>social welfare</nature_of_suit>
         <judge>Jalal</judge>
+        <lower_court_number>09-2222</lower_court_number>
+        <lower_court_judge>another jalal</lower_court_judge>
     </opinion>
     <opinion>
         <!-- A MS Word doc -->
@@ -52,6 +58,8 @@
         <federal_cite>23 F.2d 449</federal_cite>
         <nature_of_suit>family relations</nature_of_suit>
         <judge>gaby</judge>
+        <lower_court_number></lower_court_number>
+        <lower_court_judge></lower_court_judge>
     </opinion>
     <opinion>
         <!-- An HTML doc from Resource.org, with a real West Cite-->
@@ -64,6 +72,8 @@
         <federal_cite>138 U.S. 109</federal_cite>
         <nature_of_suit>applesauce</nature_of_suit>
         <judge>namir</judge>
+        <lower_court_number></lower_court_number>
+        <lower_court_judge></lower_court_judge>
     </opinion>
     <opinion>
         <!-- A WPD doc -->
@@ -76,6 +86,8 @@
         <federal_cite>445 U.S. 332</federal_cite>
         <nature_of_suit>grape jelly</nature_of_suit>
         <judge>Khaled</judge>
+        <lower_court_number></lower_court_number>
+        <lower_court_judge></lower_court_judge>
     </opinion>
     <opinion>
         <!-- A txt doc -->
@@ -88,5 +100,7 @@
         <federal_cite>23 F.2d 398</federal_cite>
         <nature_of_suit>gay marriage</nature_of_suit>
         <judge>Steph</judge>
+        <lower_court_number></lower_court_number>
+        <lower_court_judge></lower_court_judge>
     </opinion>
 </court>

cl/scrapers/tests.py
@@ -79,6 +79,7 @@ from cl.search.models import (
     Opinion,
     OpinionCluster,
     OpinionsCited,
+    OriginatingCourtInformation,
     Parenthetical,
 )
 from cl.settings import MEDIA_ROOT
@@ -110,6 +111,10 @@ class ScraperIngestionTest(ESIndexTestCase, TestCase):
                 query="type=oa",
                 alert_type=SEARCH_TYPES.ORAL_ARGUMENT,
             )
+        cls.existing_oci_id = 111111
+        cls.oci = OriginatingCourtInformation.objects.create(
+            docket_number="09-1111", id=cls.existing_oci_id
+        )
 
     def test_extension(self):
         r = async_to_sync(microservice)(
@@ -120,13 +125,13 @@ class ScraperIngestionTest(ESIndexTestCase, TestCase):
 
     def test_ingest_opinions_from_scraper(self) -> None:
         """Can we successfully ingest opinions at a high level?"""
-
         d_1 = DocketFactory(
             case_name="Tarrant Regional Water District v. Herrmann",
             docket_number="11-889",
             court=self.court,
             source=Docket.RECAP,
             pacer_case_id=None,
+            originating_court_information=self.oci,
         )
 
         d_2 = DocketFactory(
@@ -178,6 +183,34 @@ class ScraperIngestionTest(ESIndexTestCase, TestCase):
         self.assertEqual(d_2.case_name, "State of Indiana v. Charles Barker")
         self.assertEqual(
             d_3.case_name, "Intl Fidlty Ins Co v. Ideal Elec Sec Co"
+        )
+
+        oci = Docket.objects.get(
+            case_name="In Re Motion for Consent to Disclosure of Court Records"
+        ).originating_court_information
+        self.assertEqual(
+            oci.docket_number, "09-2222", "New OCI.docket_number was not saved"
+        )
+        self.assertEqual(
+            oci.assigned_to_str,
+            "another jalal",
+            "New OCI.assigned_to_str was not saved",
+        )
+
+        self.assertEqual(
+            d_1.originating_court_information.docket_number,
+            "09-1111",
+            "Existing OCI.docket_number number changed",
+        )
+        self.assertEqual(
+            d_1.originating_court_information.assigned_to_str,
+            "another david",
+            "Existing OCI.assigned_to_str was not updated",
+        )
+        self.assertEqual(
+            d_1.originating_court_information.id,
+            self.existing_oci_id,
+            "Existing OCI id should not change",
         )
 
     def test_opinion_dockets_source_assigment(self) -> None:

commit 65f707cc61e092a81fe648aa55bd6cdae9acc66b

fix(scrapers.utils): fix `appeal_from_id` logger.error for empty strings
Solves a bug introduced in #6136

The logger.error is triggered if the Court.id does not exist; but it's not checking if the "appeal_from_id" value is a non empty string; so, it is triggering for empty strings creating many false positives

cl/scrapers/utils.py
@@ -448,8 +448,7 @@ def update_or_create_docket(
     }
     if not appeal_from_id:
         docket_fields.pop("appeal_from_id", "")
-
-    if not Court.objects.filter(id=appeal_from_id).exists():
+    elif not Court.objects.filter(id=appeal_from_id).exists():
         docket_fields.pop("appeal_from_id", "")
         logger.error(
             "Docket.appeal_from_id has non existing Court.id '%s' as value",