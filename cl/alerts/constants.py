6d141549120b6d6277abd698c539f281015eb222
Eduardo Rosendo <eduardojra96@gmail.com>
2025-09-15 20:13:36 -0400

    refactor(memberships): Replaces NeonMembership constants with NeonMembershipLevel
    - Updates references in utils, views and tests.
    - Replaces usage of NeonMembership.* constants with NeonMembershipLevel.*
    - Ensures consistent use of centralized membership constants
    - Prepares codebase for easier future maintenance and expansion

--- cl/alerts/constants.py
+++ cl/alerts/constants.py
@@ -1,23 +1,23 @@
 from cl.alerts.models import Alert
-from cl.donate.models import NeonMembership
+from cl.donate.models import NeonMembershipLevel
 
 RECAP_ALERT_QUOTAS = {
     Alert.REAL_TIME: {
         "free": 0,
-        NeonMembership.LEGACY: 0,
-        NeonMembership.TIER_1: 5,
-        NeonMembership.EDU: 5,
-        NeonMembership.TIER_2: 10,
-        NeonMembership.TIER_3: 25,
-        NeonMembership.TIER_4: 50,
+        NeonMembershipLevel.LEGACY: 0,
+        NeonMembershipLevel.TIER_1: 5,
+        NeonMembershipLevel.EDU: 5,
+        NeonMembershipLevel.TIER_2: 10,
+        NeonMembershipLevel.TIER_3: 25,
+        NeonMembershipLevel.TIER_4: 50,
     },
     "other_rates": {
         "free": 5,
-        NeonMembership.LEGACY: 5,
-        NeonMembership.TIER_1: 10,
-        NeonMembership.EDU: 10,
-        NeonMembership.TIER_2: 25,
-        NeonMembership.TIER_3: 50,
-        NeonMembership.TIER_4: 100,
+        NeonMembershipLevel.LEGACY: 5,
+        NeonMembershipLevel.TIER_1: 10,
+        NeonMembershipLevel.EDU: 10,
+        NeonMembershipLevel.TIER_2: 25,
+        NeonMembershipLevel.TIER_3: 50,
+        NeonMembershipLevel.TIER_4: 100,
     },
 }
