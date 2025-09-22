af6c92bc7895b1f0074fd41c3b67654aa8c23909
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2025-09-22 15:21:31 -0500

    feat(delete_duplicates): add "cleanup_content" method
    Should help solve #6315, and part of #5315
    
    Add a "cleanup_content" method to the `delete_duplicates` command. Together with optional arguments for "court_id" and "juriscraper_module", this will group opinions wth different hash and the same URL and recompute the hash over the cleaned up content (using Site.cleanup_content), and check if they are duplicates
    
    Add tests
    
    Add a cleanup_content method to the `test_opinion_scraper.Site`

--- cl/scrapers/test_assets/test_opinion_scraper.py
+++ cl/scrapers/test_assets/test_opinion_scraper.py
@@ -93,3 +93,7 @@ class Site(OpinionSite):
             }
 
         return metadata
+
+    def cleanup_content(content):
+        """Implemented just to override OpinionSite.cleanup_content for tests"""
+        return content
