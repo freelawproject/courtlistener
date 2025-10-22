6ef4e3eb1ca66ecae2d85ac1a1170a825276500c
“Kevin <kvnzavalza@hotmail.com>
2025-10-22 09:42:54 -0600

    feat(citations): remove 0002(citations), 0044 and 0046(search) customers sql migrations, same as replica sql

--- cl/search/migrations/0044_remove_citation_indexes_customers.sql
+++
@@ -1,6 +0,0 @@
---
--- Custom state/database change combination
---
-DROP INDEX CONCURRENTLY IF EXISTS "search_citation_volume_ae340b5b02e8912_idx";
-DROP INDEX CONCURRENTLY IF EXISTS "search_citation_volume_251bc1d270a8abee_idx";
-ALTER TABLE search_citation DROP CONSTRAINT IF EXISTS search_citation_cluster_id_7a668830aad411f5_uniq;
