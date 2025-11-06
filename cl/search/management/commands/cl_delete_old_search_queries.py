51dd107e665deff338fd9273e818de48b7f3f1bf
Eduardo Rosendo <eduardojra96@gmail.com>
2025-11-06 19:22:42 -0400

    feat(search): Update cutoff date calculation

--- cl/search/management/commands/cl_delete_old_search_queries.py
+++ cl/search/management/commands/cl_delete_old_search_queries.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
@@ -10,7 +11,11 @@ class Command(BaseCommand):
     help = "Deletes SearchQuery records older than 12 weeks."
 
     def handle(self, *args, **options):
-        cutoff_date = timezone.now() - timedelta(weeks=12)
+        cutoff_date = (
+            timezone.now()
+            - timedelta(days=settings.PRIVACY_POLICY_CUTOFF_DAYS)
+            + timedelta(days=1)
+        )
         old_queries = SearchQuery.objects.filter(date_created__lt=cutoff_date)
         record_count, details = old_queries.delete()
         self.stdout.write(
