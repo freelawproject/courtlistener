6b0e7acff2c5997e48736b6750eb8af00d4094c4
“Kevin <kvnzavalza@hotmail.com>
2026-01-05 16:02:09 -0600

    feat(search): update migrations

--- cl/search/migrations/0050_opinioncontent_remove_opinioncluster_update_update_and_more.py
+++ cl/search/migrations/0050_opinioncontent_remove_opinioncluster_update_update_and_more.py
@@ -11,7 +11,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('search', '0048_create_scotus_docket_metadata'),
+        ('search', '0049_add_fields_to_originating_court_information'),
     ]
 
     operations = [
