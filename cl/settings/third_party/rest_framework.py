1f4cca1db90f070553d1ab20f6d363c722b0322e
Mike Lissner <mike@free.law>
2026-01-10 15:58:29 -0800

    Revert "Add rate limit for user"

--- cl/settings/third_party/rest_framework.py
+++ cl/settings/third_party/rest_framework.py
@@ -149,7 +149,6 @@ REST_FRAMEWORK = {
         "NicolasMadan": "10/hour",
         # Running bad queries
         # Mega queries on the docket endpoint causing issues
-        "elisadamico95": "1/hour",
         "ashabana85": "1/hour",
         "tannero20": "1/hour",
         # Too difficult
