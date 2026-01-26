699a4f3159e47c9490d360adf3a314da148381d7
EstiShay <esti@free.law>
2026-01-25 20:22:32 -0800

    fix(bug): Fix path bug by altering which file name is used

--- cl/lib/middleware.py
+++ cl/lib/middleware.py
@@ -95,7 +95,7 @@ class IncrementalNewTemplateMiddleware:
         # {response.template_name} could return a list if TemplateView is used directly
         old_template = response.template_name
         if isinstance(old_template, list | tuple):
-            old_template = old_template[0]
+            old_template = old_template[1]
 
         if not isinstance(old_template, str):
             return response
