533da18de47dd9dd896bf478a61688df1b417ced
“Kevin <kvnzavalza@hotmail.com>
2026-02-18 16:27:12 -0600

    feat(help): add new help page for cluster redirections

--- cl/simple_pages/views.py
+++ cl/simple_pages/views.py
@@ -201,6 +201,14 @@ async def mcp_help(request: HttpRequest) -> HttpResponse:
     return TemplateResponse(request, "help/mcp_help.html", {"private": False})
 
 
+async def cluster_redirections_help(request: HttpRequest) -> HttpResponse:
+    return TemplateResponse(
+        request,
+        "help/cluster_redirections_help.html",
+        {"private": False},
+    )
+
+
 async def build_court_dicts(courts: QuerySet) -> list[dict[str, str]]:
     """Takes the court objects, and manipulates them into a list of more useful
     dictionaries"""
