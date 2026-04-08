aaed551a8066905904a6c8e59aaa3624cc044281
Brennan Chesley <brennan@free.law>
2026-04-08 09:28:21 -0500

    fix(migration) renumber migration

--- cl/search/migrations/0058_texasdocument_processing_error.py
+++ cl/search/migrations/0058_texasdocument_processing_error.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('search', '0056_trialcourtdata'),
+        ('search', '0057_update_source_field_noop'),
     ]
 
     operations = [
