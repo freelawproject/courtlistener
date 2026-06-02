1683ccce7518a65f6923bc7e233fe13b22b9229f
Morgan Bennet <98787614+MorganBennetDev@users.noreply.github.com>
2026-06-02 17:39:53 -0600

    docs(corpusimporter): Abstract merger documentation

--- cl/corpus_importer/state/merger.py
+++ cl/corpus_importer/state/merger.py
@@ -24,6 +24,11 @@ logger = logging.getLogger(__name__)
 
 
 class InputMap[ScrapedData, Output]:
+    """Apply a transformation to input data before passing it to a merger.
+
+    :ivar map: Function to apply to input data. Should be a lambda in most cases.
+    """
+
     def __init__(
         self, map: Callable[[ScrapedData], Output] | None = None
     ) -> None:
@@ -35,6 +40,10 @@ class InputMap[ScrapedData, Output]:
 
 
 class InputField[ScrapedData, Output](InputMap[ScrapedData, Output]):
+    """Utility transformation to return a field from the input data unchanged.
+
+    :ivar path: The path to the relevant field. Will handle dictionaries, objects, and combinations thereof."""
+
     def __init__(self, *path: str, default: Output | None = None) -> None:
         if not path:
             raise ValueError("Path must not be empty")
@@ -57,27 +66,42 @@ class InputField[ScrapedData, Output](InputMap[ScrapedData, Output]):
 
 
 class NeverMapping[ScrapedData, T](InputMap[ScrapedData, T]):
+    """Transform that always returns None. Useful for testing or overriding base class mappings."""
+
     @override
     def map(self, i: ScrapedData) -> None:
         return None
 
 
 class ParameterMapping[T](InputMap[T, T]):
+    """Mapping indicating that a value should always be passed as a parameter to the merge method. Useful for passing
+    information which won't change over the course of many merges (i.e., a parent `Docket` for `DocketEntry`s)."""
+
     @override
     def map(self, i: T) -> T:
         return i
 
 
 Parameter = ParameterMapping[Any]()
+"""Shorthand for an attribute which must be passed as a parameter to the merge method."""
 
 
 class MergeStrategy[T](ABC):
+    """Indicates the approach to take when data differs in the scrape and DB."""
+
     @abstractmethod
     def merge_values(self, scrape: T, db: T) -> T:
+        """Computes what value should be written when data is present in the scrape and DB.
+
+        :param scrape: The value from the scrape
+        :param db: The value from the DB
+        :return: The value to write. If this is equal to `db` nothing will be written."""
         raise NotImplementedError
 
 
 class OverwriteExisting[T](MergeStrategy[T]):
+    """Merge strategy that always overwrites the existing value with the scraped value."""
+
     @override
     def merge_values(self, scrape: T, db: T) -> T:
         return scrape
@@ -85,6 +109,11 @@ class OverwriteExisting[T](MergeStrategy[T]):
 
 @dataclass
 class OverwriteConditionally[T](MergeStrategy[T]):
+    """Merge strategy that only overwrites the existing value if the condition is met.
+
+    :param condition: Function that takes the scraped and DB values and returns True if the DB value should be
+        overwritten."""
+
     condition: Callable[[T, T], bool]
 
     @override
@@ -95,34 +124,45 @@ class OverwriteConditionally[T](MergeStrategy[T]):
 
 
 class AttributeMerger[ScrapedData, T]:
+    """Class encapsulating logic for merging a single attribute from a scrape into a DB object.
+
+    :param transform: Defines how to get the DB value from the scrape data using a subclass of `InputMap`
+    :param strategy: How to behave when data is present in the scrape and DB. Defaults to overwriting the DB value."""
+
     def __init__(
         self,
         transform: InputMap[ScrapedData, T],
         *,
         strategy: MergeStrategy[T] = OverwriteExisting(),
     ) -> None:
-        self.transform = transform
+        self.transform: InputMap[ScrapedData, T] = transform
         self.strategy: MergeStrategy[T] = strategy
         self.name: str = ""
 
     def __attach_to_merger__(
         self, merger: "type[Merger[Any, Any]]", attr_name: str
     ) -> None:
-        """Run any special logic needed to attach this to a Merger object. Should be run after all class setup code for
-        the parent merger. For one-to-one relationships, register the name of the attribute this is attached to on the
-        parent."""
+        """Run any special logic needed to attach this to a Merger object."""
         self.name = attr_name
 
     def get_value(
         self, i: ScrapedData, param_value: T | None = None
     ) -> T | None:
-        """Compute the value of an attribute from scraped data."""
+        """Compute the value of an attribute from scraped data.
+
+        :param i: The full scrape data for the object being merged
+        :param param_value: Value passed as a kwarg to the merge method. Only used if `transform` is `Parameter`."""
         if isinstance(self.transform, ParameterMapping):
             return param_value
         return self.transform.map(i)
 
     def merge_values(self, scrape: T, db: T) -> T:
-        """Merge the values of two attributes (scrape and DB) and return the merged value."""
+        """Merge the values of two attributes (scrape and DB) and return the merged value.
+
+        Shorthand for `self.strategy.merge_values(scrape, db)`.
+
+        :param scrape: The value from the scrape
+        :param db: The value from the DB"""
         return self.strategy.merge_values(scrape, db)
 
 
@@ -137,13 +177,22 @@ class ChildRelationship:
 # Pretend Python has sum types
 class Relationship:
     OneToOne: OneToOneRelationship = OneToOneRelationship()
+    """One-to-one relationship. Parent objects have a foreign key to relatives."""
     Child: type[ChildRelationship] = ChildRelationship
+    """Parent-child relationship. Child objects have a foreign key to the parent."""
 
 
 RelationshipType = OneToOneRelationship | ChildRelationship
 
 
 class RelatedMerger:
+    """Class encapsulating logic for merging one or more related objects. Can be used to merge one-to-one relationships
+    or parent-child relationships.
+
+    :ivar merger: The `Merger` to use for the related object
+    :ivar transform: Defines how to get the input value for `merger.merge` using a subclass of `InputMap`
+    :ivar relationship: The type of relationship to merge"""
+
     def __init__(
         self,
         merger: "type[Merger[Any, Any]]",
@@ -151,12 +200,6 @@ class RelatedMerger:
         *,
         relationship: RelationshipType,
     ) -> None:
-        """
-
-        :param merger:
-        :param transform:
-        :param relationship:
-        """
         self.merger: type[Merger[Any, Any]] = merger
         self.transform: InputMap[Any, Any] = transform
         self.relationship: RelationshipType = relationship
@@ -166,8 +209,7 @@ class RelatedMerger:
         self, merger: "type[Merger[Any, Any]]", attr_name: str
     ) -> None:
         """Run any special logic needed to attach this to a Merger object. Should be run after all class setup code for
-        the parent merger. For one-to-one relationships, register the name of the attribute this is attached to on the
-        parent."""
+        the parent merger."""
         self.name = attr_name
 
     def _merge_one_to_one(
@@ -196,6 +238,14 @@ class RelatedMerger:
         return result
 
     def merge(self, parent: Model, i: Any) -> MergeResult[Any]:
+        """Run the merge method on the appropriate inputs for the given relationship.
+
+        For one-to-one relationships, the input is transformed then passed directly to the related merger. For
+        parent-child relationships, we first check if the input is iterable and then run the related merger for each
+        item, passing the parent object as a parameter.
+
+        :param parent: The parent of the object being merged. Must already exist in the DB when this method is called.
+        :param i: The input data for the object being merged."""
         merger_input = self.transform.map(i)
 
         match self.relationship:
@@ -210,7 +260,9 @@ class RelatedMerger:
 
 
 def get_ancestor_classes(cls: type) -> Generator[type]:
-    """Get all ancestor classes of a class, including the class itself"""
+    """Get all ancestor classes of a class, including the class itself
+
+    :param cls: The class to get ancestors for"""
     for base in types.get_original_bases(cls):
         if isinstance(base, type):
             yield from get_ancestor_classes(base)
@@ -218,6 +270,16 @@ def get_ancestor_classes(cls: type) -> Generator[type]:
 
 
 class Merger[ScrapedData, DBModel: Model](ABC):
+    """Base class for a merger which takes in `ScrapedData` and merges it into `DBModel`. Subclasses should generally
+    not be instantiated; methods and attributes should be accessed directly from the class.
+
+    :ivar atomic: Whether to wrap the merge operation in a `transaction.atomic()` context manager. Note that this only
+        applies to attributes which do not reference other objects. Related object mergers will be run outside the block
+        and can set their own `atomic` value.
+    :ivar existing: A natural key or a function to find DB objects to merge into. Setting this to an iterable value
+        and using natural key lookups is recommended, but if more complex logic is necessary, this can be a method
+        that takes in a `DBModel` object and returns the matching object from the DB or `None` if none was found."""
+
     __attr_mergers__: dict[str, AttributeMerger[ScrapedData, Any]]
     __related_mergers__: ClassVar[dict[str, RelatedMerger]]
     __model__: type[Model]
@@ -325,6 +387,10 @@ class Merger[ScrapedData, DBModel: Model](ABC):
 
     @classmethod
     def get_existing(cls, i: DBModel) -> DBModel | None:
+        """Attempts to find an existing object in the DB to merge into based on either the natural key or a custom
+        lookup function.
+
+        :param i: The object to attempt to find a match for"""
         if callable(cls.existing):
             return cls.existing(i)
         try:
@@ -345,6 +411,11 @@ class Merger[ScrapedData, DBModel: Model](ABC):
     def merge(
         cls, i: ScrapedData, *, existing: DBModel | None = None, **kwargs: Any
     ) -> MergeResult[Any]:
+        """Merge scraped data into the DB.
+
+        :param i: The input data to merge
+        :param existing: An existing object to merge into, skipping lookup. Primarily useful for merging one-to-one
+            relationships and should generally not be passed."""
         if not cls.validate(i):
             logger.error(f"Merger {cls.__name__} received invalid input.")
             return MergeResult.failed(cls.__name__)
