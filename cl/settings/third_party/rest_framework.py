d2ed71a7f2d02258d1a881cf74094ff921de6fb8
Eduardo Rosendo <eduardojra96@gmail.com>
2026-07-29 18:17:43 -0400

    feat(api): Throttles api-usage endpoint under a dedicated scope

--- cl/settings/third_party/rest_framework.py
+++ cl/settings/third_party/rest_framework.py
@@ -28,6 +28,7 @@ REST_FRAMEWORK = {
         "user": ["5/min", "50/hour", "125/day"],
         "citations": "60/min",
         "tags": "1000/hour",
+        "api_usage": ["10/min", "120/hour"],
     },
     # Auth
     "DEFAULT_AUTHENTICATION_CLASSES": (
