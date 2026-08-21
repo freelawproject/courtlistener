f7e731bf2d178768fbf46af5a3987e15f92abb36
Brennan Chesley <brennan@free.law>
2026-08-20 21:30:05 -0500

    refactor(nycoa) propagate field and factory changes

--- cl/corpus_importer/state/new_york/nycourts_gov.py
+++ cl/corpus_importer/state/new_york/nycourts_gov.py
@@ -205,7 +205,6 @@ class NYCoDocketEntry(DocketEntry[NYCoAFile]):
             self.entry_filing_type = Unclassified.UNASSIGNED
         return self
 
-
 class NYCoAIssue(BaseModel):
     """An issue the Court of Appeals assigned to a case, classified.
 
